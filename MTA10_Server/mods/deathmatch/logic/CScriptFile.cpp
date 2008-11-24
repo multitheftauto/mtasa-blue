e2a2f24d91072d2d849c7e3101eddf07d9cd87b9
matthewm.hinson <matthewm.hinson@gmail.com>
2008-11-24 20:58:25 +0000

    Added checks to file functions to prevent using closed files (#3905)

--- MTA10_Server/mods/deathmatch/logic/CScriptFile.cpp
+++ MTA10_Server/mods/deathmatch/logic/CScriptFile.cpp
@@ -77,20 +77,28 @@ void CScriptFile::Unload ( void )
 
 bool CScriptFile::IsEOF ( void )
 {
-    // Reached end of file? We're at the end of the file if
-    // our pointer equals to the filesize - 1
-    return GetPointer () >= GetSize () - 1;
+    if ( !m_pFile )
+        return true;
+
+    // Reached end of file?
+    return feof ( m_pFile ) != 0;
 }
 
 
 long CScriptFile::GetPointer ( void )
 {
+    if ( !m_pFile )
+        return -1;
+
     return ftell ( m_pFile );
 }
 
 
 long CScriptFile::GetSize ( void )
 {
+    if ( !m_pFile )
+        return -1;
+
     // Remember current position and seek to the end
     long lCurrentPos = ftell ( m_pFile );
     fseek ( m_pFile, 0, SEEK_END );
@@ -106,8 +114,11 @@ long CScriptFile::GetSize ( void )
 }
 
 
-unsigned long CScriptFile::SetPointer ( unsigned long ulPosition )
+long CScriptFile::SetPointer ( unsigned long ulPosition )
 {
+    if ( !m_pFile )
+        return -1;
+
     // Is the new position bigger than the file?
     if ( GetSize () < static_cast < long > ( ulPosition ) )
     {
@@ -122,10 +133,10 @@ unsigned long CScriptFile::SetPointer ( unsigned long ulPosition )
     fseek ( m_pFile, ulPosition, SEEK_SET );
 
     // Bigger than file size? Tell the script how far we were able to move it
-    unsigned long lSize = static_cast < unsigned long > ( GetSize () );
-    if ( ulPosition > lSize  )
+    long lSize = GetSize ();
+    if ( ulPosition > static_cast < unsigned long > ( lSize ) )
     {
-        ulPosition = lSize;
+        ulPosition = static_cast < unsigned long > ( lSize );
     }
 
     // Return the new position
@@ -136,24 +147,36 @@ unsigned long CScriptFile::SetPointer ( unsigned long ulPosition )
 void CScriptFile::SetSize ( unsigned long ulNewSize )
 {
     // TODO: A way to truncate a file
+    if ( !m_pFile )
+        return;
+
 }
 
 
 void CScriptFile::Flush ( void )
 {
+    if ( !m_pFile )
+        return;
+
     fflush ( m_pFile );
 }
 
 
-unsigned long CScriptFile::Read ( unsigned long ulSize, char* pData )
+long CScriptFile::Read ( unsigned long ulSize, char* pData )
 {
+    if ( !m_pFile )
+        return -1;
+
     // Try to read data into the given block. Return number of bytes we read.
     return fread ( pData, 1, ulSize, m_pFile );
 }
 
 
-unsigned long CScriptFile::Write ( unsigned long ulSize, const char* pData )
+long CScriptFile::Write ( unsigned long ulSize, const char* pData )
 {
+    if ( !m_pFile )
+        return -1;
+
     // Write the data into the given block. Return number of bytes we wrote.
     return fwrite ( pData, 1, ulSize, m_pFile );
 }
