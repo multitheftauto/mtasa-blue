b0f186d5cda680f06d051c7945888db220cf0f20
lil.Toady@gmail.com <lil.Toady@gmail.com@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2009-02-19 16:12:21 +0000

    Replaced the default Lua by LuaJIT on client and server, should make scripts run faster. Also enhances coroutine functionality. ***The dependencies have been updated. Get the new archive from Google Code, don't forget to place the new lua5.1.dll and lua5.1d.dll in your server folder***

--- trunk/Shared/publicsdk/include/luaconf.h
+++ trunk/Shared/publicsdk/include/luaconf.h
@@ -1,5 +1,5 @@
 /*
-** $Id: luaconf.h,v 1.82a 2006/04/10 18:27:23 roberto Exp $
+** $Id: luaconf.h,v 1.82.1.7 2008/02/11 16:25:08 roberto Exp $
 ** Configuration file for Lua
 ** See Copyright Notice in lua.h
 */
@@ -251,7 +251,7 @@
 ** CHANGE it if your stand-alone interpreter has a different name and
 ** your system is not able to detect that name automatically.
 */
-#define LUA_PROGNAME		"lua"
+#define LUA_PROGNAME		"luajit"
 
 
 /*
@@ -318,6 +318,8 @@
 @@ LUA_COMPAT_GETN controls compatibility with old getn behavior.
 ** CHANGE it (define it) if you want exact compatibility with the
 ** behavior of setn/getn in Lua 5.0.
+**
+** Note: this is not supported by LuaJIT. Leave it undefined.
 */
 #undef LUA_COMPAT_GETN
 
@@ -332,6 +334,10 @@
 @@ LUA_COMPAT_VARARG controls compatibility with old vararg feature.
 ** CHANGE it to undefined as soon as your programs use only '...' to
 ** access vararg parameters (instead of the old 'arg' table).
+**
+** Note: this has a slightly negative performance impact with LuaJIT
+** for all vararg functions. Leave it off if possible and upgrade your
+** code (replace unpack(arg) with ... and/or add local arg = {...}).
 */
 #define LUA_COMPAT_VARARG
 
@@ -440,10 +446,10 @@
 @* can use.
 ** CHANGE it if you need lots of (Lua) stack space for your C
 ** functions. This limit is arbitrary; its only purpose is to stop C
-** functions to consume unlimited stack space.
+** functions to consume unlimited stack space. (must be smaller than
+** -LUA_REGISTRYINDEX)
 */
-#define LUAI_MAXCSTACK	2048
-
+#define LUAI_MAXCSTACK	8000
 
 
 /*
@@ -581,6 +587,24 @@ union luai_Cast { double l_d; long l_l; };
 
 #endif
 
+
+/*
+@@ LUA_TVALUE_ALIGN specifies extra alignment constraints for the
+@@ tagged value structure to get better lua_Number alignment.
+** CHANGE it to an empty define if you want to save some space
+** at the cost of execution time. Note that this is only needed
+** for the x86 ABI on most POSIX systems, but not on Windows and
+** not for most other CPUs. If you change it then you need to follow
+** the instructions in ljit_x86.dash, too (look for TVALUE_SIZE).
+*/
+
+#if defined(LUA_NUMBER_DOUBLE) && defined(__GNUC__) && \
+    (defined(__i386) || defined(__i386__)) && !defined(_WIN32)
+#define LUA_TVALUE_ALIGN	__attribute__ ((aligned(8)))
+#else
+#define LUA_TVALUE_ALIGN
+#endif
+
 /* }================================================================== */
 
 
@@ -666,7 +690,7 @@ union luai_Cast { double l_d; long l_l; };
 */
 #if defined(LUA_USE_POPEN)
 
-#define lua_popen(L,c,m)	((void)L, popen(c,m))
+#define lua_popen(L,c,m)	((void)L, fflush(NULL), popen(c,m))
 #define lua_pclose(L,file)	((void)L, (pclose(file) != -1))
 
 #elif defined(LUA_WIN)
