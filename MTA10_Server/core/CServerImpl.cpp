4986d2de8ee1fe521dd613bf1e817682adc436bd
Alberto Alonso <rydencillo@gmail.com>
2009-03-23 16:42:27 +0000

    Added CNetHTTPDownloadManager::GetErrorNum and nicened the way to retreive the network module version.

--- MTA10_Server/core/CServerImpl.cpp
+++ MTA10_Server/core/CServerImpl.cpp
@@ -251,7 +251,7 @@ int CServerImpl::Run ( int iArgumentCount, char* szArguments [] )
         // Network module compatibility check
         typedef unsigned long (*PFNCHECKCOMPATIBILITY) ( unsigned long );
         PFNCHECKCOMPATIBILITY pfnCheckCompatibility = static_cast< PFNCHECKCOMPATIBILITY > ( m_NetworkLibrary.GetProcedureAddress ( "CheckCompatibility" ) );
-        if ( !pfnCheckCompatibility || !pfnCheckCompatibility ( 0x0002 ) )
+        if ( !pfnCheckCompatibility || !pfnCheckCompatibility ( MTA_DM_NET_MODULE_VERSION ) )
         {
             // net.dll doesn't like our version number
             Print ( "Network module not compatible!\n" );
