6d6f20f856d98c9c6086e42ebbb026e66e0c2e64
Talidan <Talidan@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2009-05-04 23:08:47 +0000

    Added a script to set the revision version numbers when building nightlies.

---
+++ trunk/Shared/installer/nightly.py
@@ -0,0 +1,27 @@
+import optparse
+import os.path
+prefix = "#define MTA_DM_BUILDTYPE "
+
+parser = optparse.OptionParser()
+parser.add_option("-r", "--revision", dest="revision" )
+(options,args) = parser.parse_args()
+
+def writeVersion (filePath):
+	if (not options.revision) or (options.revision == ""):
+		return
+		
+	if os.path.exists(filePath):
+		versionFile = open(filePath,"r+")
+		newLines = []
+		for line in versionFile:
+			#The prefix was found at the beginning of this line
+			if line.find (prefix) == 0:
+				newLines.append ( '%s "Nightly r%s"\n' % (prefix,options.revision) )
+			else:
+				newLines.append(line)
+		versionFile.seek(0)
+		versionFile.writelines(newLines)
+		versionFile.close()
+
+writeVersion("../../MTA10/version.h")
+writeVersion("../../MTA10_Server/version.h")
