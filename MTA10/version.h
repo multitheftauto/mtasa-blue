21ca31b462bd2135ad60d0a01d0ec3fa336b4196
Talidan <danthetaliban@gmail.com>
2009-05-04 19:34:02 +0000

    Changed the default version label.

--- MTA10/version.h
+++ MTA10/version.h
@@ -1,4 +1,4 @@
-#define MTA_VERSION_TAG "MTA:SA 1.0 Nightly build" 
+#define MTA_VERSION_TAG "MTA:SA 1.0 Compiled build" 
 #define MTA_BUILD "1.0"
-#define MTA_FORMAL_VERSION_TAG "MTA:SA 1.0 Nightly build"
+#define MTA_FORMAL_VERSION_TAG "MTA:SA 1.0 Compiled build"
 
