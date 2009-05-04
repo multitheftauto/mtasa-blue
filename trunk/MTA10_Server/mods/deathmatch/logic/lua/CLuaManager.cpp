50ef4ebc6bf208a2d6467c5c2b0383eeb4bc32dc
rydencillo <rydencillo@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2009-05-04 15:45:52 +0000

    More dense hash maps.

--- trunk/MTA10_Server/mods/deathmatch/logic/lua/CLuaManager.cpp
+++ trunk/MTA10_Server/mods/deathmatch/logic/lua/CLuaManager.cpp
@@ -44,6 +44,7 @@ CLuaManager::CLuaManager ( CObjectManager* pObjectManager,
 	m_pLuaModuleManager->_SetScriptDebugging ( g_pGame->GetScriptDebugging() );
 
     // Load our C Functions into LUA and hook callback
+    CLuaCFunctions::InitializeHashMaps ();
     LoadCFunctions ();
     lua_registerPreCallHook ( CLuaDefs::CanUseFunction );
 }
