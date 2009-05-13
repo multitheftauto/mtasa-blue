042b2f19e9da0bb58fb6f4227e73ff05f02f4c87
lil.Toady@gmail.com <lil.Toady@gmail.com>
2009-05-13 14:51:21 +0000

    Fixed getUnbanTime

--- MTA10_Server/mods/deathmatch/logic/lua/CLuaManager.cpp
+++ MTA10_Server/mods/deathmatch/logic/lua/CLuaManager.cpp
@@ -649,7 +649,7 @@ void CLuaManager::LoadCFunctions ( void )
     CLuaCFunctions::AddFunction ( "getBanUsername", CLuaFunctionDefinitions::GetBanUsername );
     CLuaCFunctions::AddFunction ( "getBanNick", CLuaFunctionDefinitions::GetBanNick );
     CLuaCFunctions::AddFunction ( "getBanTime", CLuaFunctionDefinitions::GetBanTime );
-    CLuaCFunctions::AddFunction ( "getUnbanTime", CLuaFunctionDefinitions::GetBanTime );
+    CLuaCFunctions::AddFunction ( "getUnbanTime", CLuaFunctionDefinitions::GetUnbanTime );
     CLuaCFunctions::AddFunction ( "getBanReason", CLuaFunctionDefinitions::GetBanReason );
     CLuaCFunctions::AddFunction ( "getBanAdmin", CLuaFunctionDefinitions::GetBanAdmin );
 
