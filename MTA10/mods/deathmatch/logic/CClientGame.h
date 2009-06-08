da463b09a921cb3755580705235f61bef10ba285
zzzjax@hotmail.com <zzzjax@hotmail.com>
2009-06-08 16:49:21 +0000

    onClientPlayerChoke source is now the choking ped (also added responsiblePed argument)

--- MTA10/mods/deathmatch/logic/CClientGame.h
+++ MTA10/mods/deathmatch/logic/CClientGame.h
@@ -406,7 +406,7 @@ private:
     static void                         StaticProjectileInitiateHandler ( CClientProjectile * pProjectile );
     static void                         StaticRender3DStuffHandler      ( void );
     static void                         StaticGameProcessHandler        ( void );
-    static bool                         StaticChokingHandler            ( unsigned char ucWeaponType );
+    static bool                         StaticChokingHandler            ( CPed* pChokingPed, CPed* pResponsiblePed, unsigned char ucWeaponType );
 
     bool                                DamageHandler                   ( CPed* pDamagePed, CEventDamage * pEvent );
     void                                FireHandler                     ( CFire* pFire );
@@ -415,7 +415,7 @@ private:
     void                                ProjectileInitiateHandler       ( CClientProjectile * pProjectile );
     void                                Render3DStuffHandler            ( void );
     void                                GameProcessHandler              ( void );
-    bool                                ChokingHandler                  ( unsigned char ucWeaponType );
+    bool                                ChokingHandler                  ( CPed* pChokingPed, CPed* pResponsiblePed, unsigned char ucWeaponType );
 
     static bool                         StaticProcessMessage            ( HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam );
     bool                                ProcessMessage                  ( HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam );
