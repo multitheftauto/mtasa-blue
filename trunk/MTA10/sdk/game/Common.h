fed301aa98a7937e2bbcc8f9f1de85ad6895d9a0
zzzjax@hotmail.com <zzzjax@hotmail.com@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2009-06-21 20:29:07 +0000

    Text displays now use the correct color order

--- trunk/MTA10/sdk/game/Common.h
+++ trunk/MTA10/sdk/game/Common.h
@@ -17,14 +17,20 @@
 typedef DWORD RGBA;
 typedef DWORD ARGB;
 
+// Jax: COLOR DEFS ARE OFF LIMITS !
 #define COLOR_ARGB(a,r,g,b) (((((a)&0xff)<<24)|(((r)&0xff)<<16)|(((g)&0xff)<<8)|((b)&0xff)))
-#define COLOR_RGBA(r,g,b,a) COLOR_ARGB(r,g,b,a)
+#define COLOR_ARGB_A(color) unsigned char ( color >> 24 )
+#define COLOR_ARGB_R(color) unsigned char ( color >> 16 )
+#define COLOR_ARGB_G(color) unsigned char ( color >> 8 )
+#define COLOR_ARGB_B(color) unsigned char ( color )
 
-#define COLOR_RGBA_A(color) unsigned char ( color >> 24 )
-#define COLOR_RGBA_R(color) unsigned char ( color >> 16 )
-#define COLOR_RGBA_G(color) unsigned char ( color >> 8 )
-#define COLOR_RGBA_B(color) unsigned char ( color )
-#define COLOR_ARGB_A(color) COLOR_RGBA_A(color)
+#define COLOR_RGBA(r,g,b,a) COLOR_ARGB(r,g,b,a)
+#define COLOR_ABGR(a,b,g,r) COLOR_ARGB(a,b,g,r)
+
+#define COLOR_RGBA_R(color) COLOR_ARGB_A(color)
+#define COLOR_RGBA_G(color) COLOR_ARGB_R(color)
+#define COLOR_RGBA_B(color) COLOR_ARGB_G(color)
+#define COLOR_RGBA_A(color) COLOR_ARGB_B(color)
 
 #define MAX_VEHICLES                        110
 #define MAX_PEDS                            140
