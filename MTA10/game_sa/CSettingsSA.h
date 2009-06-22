c6ad32d31fae137cd4dcd44739eba84aedf2218b
matthewm.hinson <matthewm.hinson@gmail.com>
2009-06-22 22:46:08 +0000

    Fixed #4079 (Changing resolution in Video tab doesn't save after quitting) Fixed #4062 (Widescreen setting in Video tab of settings doesn't get saved after restarting MTA)

--- MTA10/game_sa/CSettingsSA.h
+++ MTA10/game_sa/CSettingsSA.h
@@ -20,50 +20,74 @@
 
 #define CLASS_CMenuManager      0xBA6748
 
+#define FUNC_SAVE               0x57C660
+
 #define VAR_bMouseSteering      0xC1CC02
 #define VAR_bMouseFlying        0xC1CC03
 
-#define VAR_RadioVolume         0xBA6798
-#define VAR_SfxVolume           0xBA6797
-#define VAR_DrawDistance        0xBA6788
-
-
-struct Settings // see code around 0x57CE9A for where these are
+struct CSettingsSAInterface // see code around 0x57CE9A for where these are
 {
-    bool bPadding[75];
-    bool bUseWideScreen;
-    bool bFrameLimiter;
-    bool bAutoRetune;
+    BYTE pad1[4];
+    float fStatsScrollSpeed;    // 0x4
+    BYTE pad2[0x34];
+    float fBrightness;          // 0x3C
+    float fDrawDistance;        // 0x40
+    bool bSubtitles;            // 0x44
+    bool pad3[5];
+    bool bLegend;               // 0x4A
+    bool bUseWideScreen;        // 0x4B
+    bool bFrameLimiter;         // 0x4C
+    bool bAutoRetune;           // 0x4D
+    bool pad4;
+    BYTE ucSfxVolume;           // 0x4F
+    BYTE ucRadioVolume;         // 0x50
+    BYTE ucRadioEq;             // 0x51
+    BYTE ucRadioStation;        // 0x52
+    BYTE pad5[0x5E];
+    bool bInvertPadX1;          // 0xB1
+    bool bInvertPadY1;          // 0xB2
+    bool bInvertPadX2;          // 0xB3
+    bool bInvertPadY2;          // 0xB4
+    bool bSwapPadAxis1;         // 0xB5
+    bool bSwapPadAxis2;         // 0xB6
+    BYTE pad6[0x19];
+    bool bUseKeyboardAndMouse;  // 0xD0
+    BYTE pad7[3];
+    DWORD dwVideoMode;          // 0xD4
+    DWORD dwPrevVideoMode;      // 0xD8
 };
 
 class CSettingsSA : public CGameSettings
 {
-    friend class    COffsets;
+    friend class            COffsets;
 
 private:
-    Settings    * settings;
+    CSettingsSAInterface*   m_pInterface;
+
 public:
-                    CSettingsSA ( void );
+                            CSettingsSA                 ( void );
 
-    bool            IsFrameLimiterEnabled ( void );
-    void            SetFrameLimiterEnabled ( bool bEnabled );
-    bool            IsWideScreenEnabled ( void );
-    void            SetWideScreenEnabled ( bool bEnabled );
-    unsigned int    GetNumVideoModes ( void );
-    VideoMode *     GetVideoModeInfo ( VideoMode * modeInfo, unsigned int modeIndex );
-    unsigned int    GetCurrentVideoMode ( void );
-    void            SetCurrentVideoMode ( unsigned int modeIndex );
-    unsigned char   GetRadioVolume ( void );
-    void            SetRadioVolume ( unsigned char ucVolume );
-    unsigned char   GetSFXVolume ( void );
-    void            SetSFXVolume ( unsigned char ucVolume );
-    float           GetDrawDistance ( void );
+    bool                    IsFrameLimiterEnabled       ( void );
+    void                    SetFrameLimiterEnabled      ( bool bEnabled );
+    bool                    IsWideScreenEnabled         ( void );
+    void                    SetWideScreenEnabled        ( bool bEnabled );
+    unsigned int            GetNumVideoModes            ( void );
+    VideoMode *             GetVideoModeInfo            ( VideoMode * modeInfo, unsigned int modeIndex );
+    unsigned int            GetCurrentVideoMode         ( void );
+    void                    SetCurrentVideoMode         ( unsigned int modeIndex );
+    unsigned char           GetRadioVolume              ( void );
+    void                    SetRadioVolume              ( unsigned char ucVolume );
+    unsigned char           GetSFXVolume                ( void );
+    void                    SetSFXVolume                ( unsigned char ucVolume );
+    float                   GetDrawDistance             ( void );
+
+    void                    Save                        ( void );
 
 private:
-    static unsigned long        FUNC_GetNumVideoModes;
-    static unsigned long        FUNC_GetVideoModeInfo;
-    static unsigned long        FUNC_GetCurrentVideoMode;
-    static unsigned long        FUNC_SetCurrentVideoMode;
+    static unsigned long    FUNC_GetNumVideoModes;
+    static unsigned long    FUNC_GetVideoModeInfo;
+    static unsigned long    FUNC_GetCurrentVideoMode;
+    static unsigned long    FUNC_SetCurrentVideoMode;
 };
 
 #endif
--- MTA10/sdk/game/CSettings.h
+++ MTA10/sdk/game/CSettings.h
@@ -76,19 +76,20 @@ struct VideoMode //RwVideoMode
 class CGameSettings
 {
 public:
-    virtual bool            IsFrameLimiterEnabled ( void )=0;
-    virtual void            SetFrameLimiterEnabled ( bool bEnabled )=0;
-    virtual bool            IsWideScreenEnabled ( void )=0;
-    virtual void            SetWideScreenEnabled ( bool bEnabled )=0;
-    virtual unsigned int    GetNumVideoModes ( void )=0;
-    virtual VideoMode *     GetVideoModeInfo ( VideoMode * modeInfo, unsigned int modeIndex )=0;
-    virtual unsigned int    GetCurrentVideoMode ( void )=0;
-    virtual void            SetCurrentVideoMode ( unsigned int modeIndex )=0;
-    virtual unsigned char   GetRadioVolume ( void )=0;
-    virtual void            SetRadioVolume ( unsigned char ucVolume )=0;
-    virtual unsigned char   GetSFXVolume ( void )=0;
-    virtual void            SetSFXVolume ( unsigned char ucVolume )=0;
-    virtual float           GetDrawDistance ( void )=0;
+    virtual bool            IsFrameLimiterEnabled   ( void ) = 0;
+    virtual void            SetFrameLimiterEnabled  ( bool bEnabled ) = 0;
+    virtual bool            IsWideScreenEnabled     ( void ) = 0;
+    virtual void            SetWideScreenEnabled    ( bool bEnabled ) = 0;
+    virtual unsigned int    GetNumVideoModes        ( void ) = 0;
+    virtual VideoMode *     GetVideoModeInfo        ( VideoMode * modeInfo, unsigned int modeIndex ) = 0;
+    virtual unsigned int    GetCurrentVideoMode     ( void ) = 0;
+    virtual void            SetCurrentVideoMode     ( unsigned int modeIndex ) = 0;
+    virtual unsigned char   GetRadioVolume          ( void ) = 0;
+    virtual void            SetRadioVolume          ( unsigned char ucVolume ) = 0;
+    virtual unsigned char   GetSFXVolume            ( void ) = 0;
+    virtual void            SetSFXVolume            ( unsigned char ucVolume ) = 0;
+    virtual float           GetDrawDistance         ( void ) = 0;
+    virtual void            Save                    ( void ) = 0;
 };
 
 #endif
