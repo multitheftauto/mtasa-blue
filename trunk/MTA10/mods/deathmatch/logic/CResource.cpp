19a0ab9e6d041ea376bb4edb314b3c1e16692c77
zzzjax@hotmail.com <zzzjax@hotmail.com@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2009-06-22 12:06:08 +0000

    Fixed #4404: moved onClientResourceStop to CResource destructor

--- trunk/MTA10/mods/deathmatch/logic/CResource.cpp
+++ trunk/MTA10/mods/deathmatch/logic/CResource.cpp
@@ -76,6 +76,11 @@ CResource::CResource ( unsigned short usID, char* szResourceName, CClientEntity*
 
 CResource::~CResource ( void )
 {
+    // Call our lua event
+    CLuaArguments Arguments;
+    Arguments.PushUserData ( this );
+    m_pRootEntity->CallEvent ( "onClientResourceStop", Arguments, true );
+
     // Make sure we don't force the cursor on
     ShowCursor ( false );
 
