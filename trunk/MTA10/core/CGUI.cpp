0899b82056fc56eae456db824cc1302739ed1495
zzzjax@hotmail.com <zzzjax@hotmail.com@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2009-06-23 17:24:09 +0000

    Fixed cursor always showing in debug

--- trunk/MTA10/core/CGUI.cpp
+++ trunk/MTA10/core/CGUI.cpp
@@ -665,7 +665,10 @@ bool CLocalGUI::InputGoesToGUI ( void )
 
     // Here we're supposed to check if things like menues are up, console is up or the chatbox is expecting input
     // If the console is visible OR the chat is expecting input OR the mainmenu is visible
-    return ( IsConsoleVisible () || IsMainMenuVisible () || IsChatBoxInputEnabled () || m_bForceCursorVisible || pGUI->GetGUIInputEnabled () || !CCore::GetSingleton ().IsFocused () );
+#ifndef MTA_DEBUG
+    if ( !CCore::GetSingleton ().IsFocused () ) return true;
+#endif
+    return ( IsConsoleVisible () || IsMainMenuVisible () || IsChatBoxInputEnabled () || m_bForceCursorVisible || pGUI->GetGUIInputEnabled () );
 }
 
 
