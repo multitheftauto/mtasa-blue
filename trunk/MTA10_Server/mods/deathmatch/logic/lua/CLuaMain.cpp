33362bccdcc949b49ad0b265de6380ca2ce23b4a
sebasdevelopment@gmx.com <sebasdevelopment@gmx.com@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2010-05-05 11:47:05 +0000

    Applied #5316: (fix for 'exports' sometimes pointing to a bad resource) by: mabako

--- trunk/MTA10_Server/mods/deathmatch/logic/lua/CLuaMain.cpp
+++ trunk/MTA10_Server/mods/deathmatch/logic/lua/CLuaMain.cpp
@@ -39,6 +39,8 @@ const char szPreloadedScript [] = ""\
     "        local res = getResourceFromName(k)\n" \
     "        if res then\n" \
     "                self[k] = setmetatable({ res = res }, rescallMT)\n" \
+    "                addEventHandler(\"onResourceStop\", getResourceRootElement(res), function() self[k] = nil end)\n" \
+    "                addEventHandler(\"onResourceStop\", getResourceRootElement(res), function() self[k] = nil end)\n" \
     "                return self[k]\n" \
     "        else\n" \
     "                outputDebugString(\"exports: Call to non-existing resource (\" .. k .. \")\", 1)\n" \
