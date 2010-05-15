b0d1075158c15c3b0586ff3e9a28cc8c69b22d09
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2010-05-15 15:20:48 +0000

    Simplified building procedure. Incremented net module version. Renamed client net module to netc.

--- trunk/MTA10/version.h
+++ trunk/MTA10/version.h
@@ -1,70 +1,80 @@
-#define MTA_DM_VERSION              0x0104
-#define MTA_DM_VERSIONSTRING        "1.0.4"
-#define MTA_DM_FULL_STRING          "MTA:SA Client"
-#define MTA_DM_BITSTREAM_VERSION    0x08
+//
+// MTA10/version.h
+//
+// Client version file
+//
 
-// These defines are dependent on the build configuration:
-// MTA_DM_ASE_VERSION
-// MTA_DM_BUILDTYPE
-// MTA_DM_BUILDTAG_SHORT
-// MTA_DM_BUILDTAG_LONG
-// MTA_DM_NETCODE_VERSION
+//
+// To compile a client: 
+//      1. set MTASA_VERSION_TYPE to VERSION_TYPE_CUSTOM
+//      2. Use netc.dll from the latest unstable build (nightly.mtasa.com)
+//
+
+// New version info
+#define MTASA_VERSION_MAJOR         1
+#define MTASA_VERSION_MINOR         0
+#define MTASA_VERSION_MAINTENANCE   4
+#define MTASA_VERSION_TYPE          VERSION_TYPE_CUSTOM
+#define MTASA_VERSION_BUILD         0
+
+// Old version info
+#define MTA_DM_VERSION              ( ( ( MTASA_VERSION_MAJOR ) << 8 ) | ( ( MTASA_VERSION_MINOR ) << 4 ) | ( ( MTASA_VERSION_MAINTENANCE ) << 0 ) )
+#define MTA_DM_VERSIONSTRING        QUOTE_DEFINE ( MTASA_VERSION_MAJOR ) "." QUOTE_DEFINE ( MTASA_VERSION_MINOR ) "." QUOTE_DEFINE ( MTASA_VERSION_MAINTENANCE )
+#define MTA_DM_FULL_STRING          "MTA:SA Client"
+#define MTA_DM_BITSTREAM_VERSION    0x09
+
+// Compile types
+#define VERSION_TYPE_CUSTOM         0x01
+#define VERSION_TYPE_EXPERIMENTAL   0x03
+#define VERSION_TYPE_UNSTABLE       0x05
+#define VERSION_TYPE_UNTESTED       0x07
+#define VERSION_TYPE_RELEASE        0x09
+
 
 #define QUOTE_DEFINE2(c) #c
 #define QUOTE_DEFINE(x) QUOTE_DEFINE2(x)
 
-#if defined(BUILD_CONFIG_)
-    #pragma message("NOTE: It's safe to ignore 'environment variables were not found' warning.")
+// Implement compile types
+#if MTASA_VERSION_TYPE == VERSION_TYPE_CUSTOM
 
     #define MTA_DM_BUILDTYPE        "Custom"
     #define MTA_DM_BUILDTAG_SHORT   MTA_DM_VERSIONSTRING "-" MTA_DM_BUILDTYPE
     #define MTA_DM_BUILDTAG_LONG    MTA_DM_VERSIONSTRING "-" MTA_DM_BUILDTYPE
 
-#elif defined(BUILD_CONFIG_EXPERIMENTAL)
-
-    #define MTA_DM_BUILDTYPE        "Experimental"
-    #define MTA_DM_BUILDTAG_SHORT   QUOTE_DEFINE ( BUILD_NUMBER )
-    #define MTA_DM_BUILDTAG_LONG    QUOTE_DEFINE ( BUILD_NUMBER )
-
-#elif defined(BUILD_CONFIG_UNSTABLE)
+#elif MTASA_VERSION_TYPE == VERSION_TYPE_UNSTABLE
 
     #define MTA_DM_BUILDTYPE        "Unstable"
-    #define MTA_DM_BUILDTAG_SHORT   QUOTE_DEFINE ( BUILD_NUMBER )
-    #define MTA_DM_BUILDTAG_LONG    QUOTE_DEFINE ( BUILD_NUMBER )
+    #define MTA_DM_BUILDTAG_SHORT   MTA_DM_VERSIONSTRING "-" MTA_DM_BUILDTYPE "-" QUOTE_DEFINE ( MTASA_VERSION_BUILD )
+    #define MTA_DM_BUILDTAG_LONG    MTA_DM_VERSIONSTRING "-" MTA_DM_BUILDTYPE "-" QUOTE_DEFINE ( MTASA_VERSION_BUILD )
 
-#elif defined(BUILD_CONFIG_UNTESTED)
+#elif MTASA_VERSION_TYPE == VERSION_TYPE_UNTESTED
 
     #define MTA_DM_BUILDTYPE        "Untested"
-    #define MTA_DM_BUILDTAG_SHORT   QUOTE_DEFINE ( BUILD_NUMBER )
-    #define MTA_DM_BUILDTAG_LONG    QUOTE_DEFINE ( BUILD_NUMBER )
+    #define MTA_DM_BUILDTAG_SHORT   MTA_DM_VERSIONSTRING "-" MTA_DM_BUILDTYPE "-" QUOTE_DEFINE ( MTASA_VERSION_BUILD )
+    #define MTA_DM_BUILDTAG_LONG    MTA_DM_VERSIONSTRING "-" MTA_DM_BUILDTYPE "-" QUOTE_DEFINE ( MTASA_VERSION_BUILD )
     #define MTA_DM_CONNECT_TO_PUBLIC
-    #define MTA_DM_EXPIRE_DAYS      30
+    #define MTA_DM_EXPIRE_DAYS      60
 
-#elif defined(BUILD_CONFIG_RELEASE_CANDIDATE)
+#elif MTASA_VERSION_TYPE == VERSION_TYPE_RELEASE
 
     #define MTA_DM_BUILDTYPE        "Release"
     #define MTA_DM_BUILDTAG_SHORT   MTA_DM_VERSIONSTRING
-    #define MTA_DM_BUILDTAG_LONG    QUOTE_DEFINE ( BUILD_NUMBER )
+    #define MTA_DM_BUILDTAG_LONG    MTA_DM_VERSIONSTRING "-" MTA_DM_BUILDTYPE "-" QUOTE_DEFINE ( MTASA_VERSION_BUILD )
     #define MTA_DM_CONNECT_TO_PUBLIC
 
 #else
-    #error "'mergancy"
+    #error "Incorrect MTASA_VERSION_TYPE"
 #endif
+
 
 // To avoid user confusion, make sure the ASE version matches only if communication is possible
 #if defined(MTA_DM_CONNECT_TO_PUBLIC)
     #define MTA_DM_ASE_VERSION      "1.0"
     #define MTA_DM_NETCODE_VERSION  0x164
-    #define MTA_DM_CLIENT_NET_MODULE_VERSION   0x1010
+    #define MTA_DM_CLIENT_NET_MODULE_VERSION   0x1011
 #else
     #define MTA_DM_ASE_VERSION      "1.0n"
     #define MTA_DM_NETCODE_VERSION  0x165
-    #define MTA_DM_CLIENT_NET_MODULE_VERSION   0x1410
+    #define MTA_DM_CLIENT_NET_MODULE_VERSION   0x1411
 #endif
 
-// Make MTA_DM_BUILDTAG_LONG_SVN
-#ifdef SVN_REVISION
-    #define MTA_DM_BUILDTAG_LONG_SVN    MTA_DM_BUILDTAG_LONG "-" SVN_REVISION
-#else
-    #define MTA_DM_BUILDTAG_LONG_SVN    MTA_DM_BUILDTAG_LONG
-#endif
