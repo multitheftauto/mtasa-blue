8555422a5cfee83dfc2a4bc1d4f039bfdc3ff137
ccw808 <ccw808@googlemail.com>
2010-05-21 00:21:32 +0000

    Removed XML node limit

--- Shared/XML/CXMLArray.cpp
+++ Shared/XML/CXMLArray.cpp
@@ -14,42 +14,59 @@
 
 using namespace std;
 
-CStack < unsigned long, MAX_XML, INVALID_XML_ID > CXMLArray::m_IDStack;
-CXMLCommon* CXMLArray::m_Elements [MAX_XML];
+CStack < unsigned long, 1, INVALID_XML_ID > CXMLArray::m_IDStack;
+std::vector < CXMLCommon* >                 CXMLArray::m_Elements;
+unsigned long                               CXMLArray::m_ulCapacity = 0;
 
 void CXMLArray::Initialize ( void )
 {
-    // Initialize entry array
-    memset ( m_Elements, 0, sizeof ( m_Elements ) );
+    m_ulCapacity = 0;
+#ifndef MTA_DEBUG
+    ExpandBy ( 10000 );
+#endif
 }
-
 
 unsigned long CXMLArray::PopUniqueID ( CXMLCommon* pEntry )
 {
-    // Grab the ID and check that we had more left
-    unsigned long ulID = m_IDStack.Pop ();
-    if ( ulID != INVALID_XML_ID &&
-         ulID < MAX_XML )
+    // Add more ID's if required
+    if ( m_IDStack.GetUnusedAmount () < 1 )
     {
-        m_Elements [ulID] = pEntry;
-        return MAX_SERVER_ELEMENTS + MAX_CLIENT_ELEMENTS + ulID;
+#ifdef MTA_DEBUG
+        ExpandBy ( 5 );
+#else
+        ExpandBy ( 10000 );
+#endif
     }
-    
-     return INVALID_XML_ID;
+
+    // Grab the next unused ID
+    unsigned long ulPhysicalIndex = m_IDStack.Pop ();
+
+    // Checks
+    assert ( ulPhysicalIndex != INVALID_XML_ID );
+    assert ( ulPhysicalIndex <= m_ulCapacity );
+    assert ( m_Elements.size () > ulPhysicalIndex );
+    assert ( m_Elements [ ulPhysicalIndex ] == NULL );
+
+    m_Elements [ ulPhysicalIndex ] = pEntry;
+
+    // Map to ID
+    unsigned long ulLogicalID = ( ulPhysicalIndex + MAX_SERVER_ELEMENTS + MAX_CLIENT_ELEMENTS ) ;
+    return ulLogicalID;
 }
 
 
-void CXMLArray::PushUniqueID ( unsigned long ulID )
+void CXMLArray::PushUniqueID ( unsigned long ulLogicalID )
 {
-    // Push the ID back and NULL the entity there
-    if ( ulID != INVALID_XML_ID &&
-         ulID >= MAX_SERVER_ELEMENTS + MAX_CLIENT_ELEMENTS &&
-         ulID < MAX_SERVER_ELEMENTS + MAX_CLIENT_ELEMENTS + MAX_XML )
-    {
-        ulID -= MAX_SERVER_ELEMENTS + MAX_CLIENT_ELEMENTS;
-        m_IDStack.Push ( ulID );
-        m_Elements [ulID] = NULL;
-    }
+    // Map to index
+    unsigned long ulPhysicalIndex = ulLogicalID - (MAX_SERVER_ELEMENTS + MAX_CLIENT_ELEMENTS);
+
+    // Checks
+    assert ( ulLogicalID != INVALID_XML_ID );
+    assert ( ulPhysicalIndex <= m_ulCapacity );
+    assert ( m_Elements [ ulPhysicalIndex ] );
+
+    m_IDStack.Push ( ulPhysicalIndex );
+    m_Elements [ ulPhysicalIndex ] = NULL;
 }
 
 
@@ -59,14 +76,37 @@ void CXMLArray::PushUniqueID ( CXMLCommon* pEntry )
 }
 
 
-CXMLCommon* CXMLArray::GetEntry ( unsigned long ulID )
+CXMLCommon* CXMLArray::GetEntry ( unsigned long ulLogicalID )
 {
     // Return the element with the given ID
-    if ( ulID != INVALID_XML_ID &&
-         ulID >= MAX_SERVER_ELEMENTS + MAX_CLIENT_ELEMENTS &&
-         ulID < MAX_SERVER_ELEMENTS + MAX_CLIENT_ELEMENTS + MAX_XML )
-        return m_Elements [ulID - (MAX_SERVER_ELEMENTS + MAX_CLIENT_ELEMENTS)];
 
+    // Map to index
+    unsigned long ulPhysicalIndex = ulLogicalID - (MAX_SERVER_ELEMENTS + MAX_CLIENT_ELEMENTS);
+
+    if ( ulLogicalID != INVALID_XML_ID && ulPhysicalIndex <= m_ulCapacity )
+    {
+        return m_Elements [ ulPhysicalIndex ];
+    }
     return NULL;
 }
 
+
+void CXMLArray::ExpandBy ( unsigned long ulAmount )
+{
+    m_IDStack.ExpandBy ( ulAmount );
+    m_Elements.resize ( m_ulCapacity + ulAmount + 1, NULL );
+    m_ulCapacity += ulAmount;
+    assert ( m_IDStack.GetCapacity () == m_ulCapacity );
+}
+
+
+unsigned long CXMLArray::GetCapacity ( void )
+{
+    return m_ulCapacity;
+}
+
+
+unsigned long CXMLArray::GetUnusedAmount ( void )
+{
+    return m_IDStack.GetUnusedAmount ();
+}
