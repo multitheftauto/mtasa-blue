2eceed92cf10ef333a61e9525dd051be7b9c600d
flobu@gmx.net <flobu@gmx.net@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2010-06-02 18:38:12 +0000

    Fixed #5370: (Script error output does not always return the valid filename.)

--- trunk/MTA10/mods/shared_logic/lua/CLuaMain.h
+++ trunk/MTA10/mods/shared_logic/lua/CLuaMain.h
@@ -57,7 +57,7 @@ public:
     inline void                     SetOwner                ( int iOwner )                  { m_iOwner = iOwner; };
 
     bool                            LoadScriptFromFile      ( const char* szLUAScript );
-    bool                            LoadScriptFromBuffer    ( const char* cpBuffer, unsigned int uiSize );
+    bool                            LoadScriptFromBuffer    ( const char* cpBuffer, unsigned int uiSize, const char* szFileName );
     bool                            LoadScript              ( const char* szLUAScript );
     void                            UnloadScript            ( void );
 
