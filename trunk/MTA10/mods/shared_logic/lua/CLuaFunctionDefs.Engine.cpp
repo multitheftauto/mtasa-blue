846b07a4f558a25fb77be8fe7a529126dc616434
flobu@gmx.net <flobu@gmx.net@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2010-06-03 15:34:04 +0000

    Reapplied #5361: (engineLoadTXD - Filtering Is Now Togglable, Perfect For Mario Kart Maps - Patch) from r1714

--- trunk/MTA10/mods/shared_logic/lua/CLuaFunctionDefs.Engine.cpp
+++ trunk/MTA10/mods/shared_logic/lua/CLuaFunctionDefs.Engine.cpp
@@ -136,6 +136,11 @@ int CLuaFunctionDefs::EngineLoadTXD ( lua_State* luaVM )
         CResource* pResource = pLuaMain->GetResource ();
         if ( pResource )
         {
+            bool bFilteringEnabled = true;
+
+            if ( lua_type ( luaVM, 2 ) == LUA_TBOOLEAN )
+                bFilteringEnabled = ( lua_toboolean ( luaVM, 2 ) ) ? true:false;
+
             // Grab the filename
             SString strFile = ( lua_istype ( luaVM, 1, LUA_TSTRING ) ? lua_tostring ( luaVM, 1 ) : "" );
             
@@ -150,7 +155,7 @@ int CLuaFunctionDefs::EngineLoadTXD ( lua_State* luaVM )
                 CClientTXD* pTXD = new CClientTXD ( m_pManager, INVALID_ELEMENT_ID );
 
                 // Try to load the TXD file
-                if ( pTXD->LoadTXD ( strPath ) )
+                if ( pTXD->LoadTXD ( strPath, bFilteringEnabled ) )
                 {
                     // Success loading the file. Set parent to TXD root
                     pTXD->SetParent ( pRoot );
