4fd1643bf9f72e3f6b1591f892e3a9527df8bbeb
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2010-06-28 13:45:03 +0000

    Additional fix #2 for #5316: ('exports' sometimes pointing to a bad resource). Fixed compile warning.

--- trunk/MTA10_Server/mods/deathmatch/logic/CRegistryManager.cpp
+++ trunk/MTA10_Server/mods/deathmatch/logic/CRegistryManager.cpp
@@ -24,7 +24,7 @@ CRegistryManager::~CRegistryManager ( void )
 void CRegistryManager::DoPulse ( void )
 {
     // End automatic transactions started in the previous pulse
-    for ( int i = 0 ; i < m_RegistryList.size () ; i++ )
+    for ( unsigned int i = 0 ; i < m_RegistryList.size () ; i++ )
         m_RegistryList[i]->EndAutomaticTransaction ();
 }
 
