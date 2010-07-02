15248e5486344ce86df25c17caa2ef5b2cb82c32
Cazomino05 <Cazomino05@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2010-07-02 16:50:12 +0000

    Fixed #4981 - Inconsistent isElementInWater returns (Unoccupied Vehicle sync now syncs the in water state)

--- trunk/MTA10_Server/mods/deathmatch/logic/CUnoccupiedVehicleSync.cpp
+++ trunk/MTA10_Server/mods/deathmatch/logic/CUnoccupiedVehicleSync.cpp
@@ -383,6 +383,9 @@ void CUnoccupiedVehicleSync::Packet_UnoccupiedVehicleSync ( CUnoccupiedVehicleSy
                         // Derailed state
                         pVehicle->SetDerailed ( vehicle.data.bDerailed );
 
+                        // Set our In Water State
+                        pVehicle->SetInWater ( vehicle.data.bIsInWater );
+
                         // Run colpoint checks on vehicle
                         g_pGame->GetColManager()->DoHitDetection ( pVehicle->GetLastPosition (), pVehicle->GetPosition (), 0.0f, pVehicle );
 
