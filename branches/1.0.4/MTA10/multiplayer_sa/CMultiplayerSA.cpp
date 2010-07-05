cba99d8f6bd7dfadcdab31dc28eb679d242cc393
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2010-07-05 22:16:23 +0000

    [1.0.4] Backported r1817 (Fixed #5445: (MTA is crashing every 30-60 minutes))

--- branches/1.0.4/MTA10/multiplayer_sa/CMultiplayerSA.cpp
+++ branches/1.0.4/MTA10/multiplayer_sa/CMultiplayerSA.cpp
@@ -169,6 +169,10 @@ DWORD RETURN_CPhysical_ProcessCollisionSectorList =         0x54BB9A;
 #define HOOKPOS_CrashFix_Misc1                              0x5D9A6E
 DWORD RETURN_CrashFix_Misc1 =                               0x5D9A74;
 
+#define HOOKPOS_CrashFix_Misc2                              0x6B18B0
+DWORD RETURN_CrashFix_Misc2a =                              0x6B18B9;
+DWORD RETURN_CrashFix_Misc2b =                              0x6B1F6C;
+
 CPed* pContextSwitchedPed = 0;
 CVector vecCenterOfWorld;
 FLOAT fFalseHeading;
@@ -284,6 +288,7 @@ void HOOK_CPed_GetWeaponSkill ();
 void HOOK_CPed_AddGogglesModel ();
 void HOOK_CPhysical_ProcessCollisionSectorList ();
 void HOOK_CrashFix_Misc1 ();
+void HOOK_CrashFix_Misc2 ();
 
 void vehicle_lights_init ();
 
@@ -398,6 +403,7 @@ void CMultiplayerSA::InitHooks()
     HookInstall(HOOKPOS_CPed_AddGogglesModel, (DWORD)HOOK_CPed_AddGogglesModel, 6);
     HookInstall(HOOKPOS_CPhysical_ProcessCollisionSectorList, (DWORD)HOOK_CPhysical_ProcessCollisionSectorList, 7 );
     HookInstall(HOOKPOS_CrashFix_Misc1, (DWORD)HOOK_CrashFix_Misc1, 6 );
+    HookInstall(HOOKPOS_CrashFix_Misc2, (DWORD)HOOK_CrashFix_Misc2, 9 );
 
     HookInstallCall ( CALL_CBike_ProcessRiderAnims, (DWORD)HOOK_CBike_ProcessRiderAnims );
     HookInstallCall ( CALL_Render3DStuff, (DWORD)HOOK_Render3DStuff );
@@ -4344,5 +4350,22 @@ void _declspec(naked) HOOK_CrashFix_Misc1 ()
         test    ecx,ecx
     cont:
         jmp     RETURN_CrashFix_Misc1
+    }
+}
+
+
+void _declspec(naked) HOOK_CrashFix_Misc2 ()
+{
+    _asm
+    {
+        // Hooked from 006B18B0
+        test    eax,eax 
+        je      cont        // Skip much code if eax is zero (vehicle has no colmodel)
+
+        mov     eax,dword ptr [eax+2Ch] 
+        mov     cl,byte ptr [esi+429h]
+        jmp     RETURN_CrashFix_Misc2a
+    cont:
+        jmp     RETURN_CrashFix_Misc2b
     }
 }
