dab0cb9dbc0b53828ab2a1df36e7cc3d2a334840
Talidan <danthetaliban@gmail.com>
2010-07-07 18:42:26 +0000

    Applied Fenix1053's patch for server queueing #5454

--- MTA10_Server/version.h
+++ MTA10_Server/version.h
@@ -23,7 +23,7 @@
 #define MTASA_VERSION_MAJOR         1
 #define MTASA_VERSION_MINOR         1
 #define MTASA_VERSION_MAINTENANCE   0
-#define MTASA_VERSION_TYPE          VERSION_TYPE_RELEASE
+#define MTASA_VERSION_TYPE          VERSION_TYPE_CUSTOM
 #define MTASA_VERSION_BUILD         0
 
 // Old version info
