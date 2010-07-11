ac3e52da4a034be7216e99cd20b88f9f70528f0d
ccw808 <ccw808@googlemail.com>
2010-07-11 15:20:29 +0000

    Sped up screenshot saving

--- MTA10/core/CScreenShot.cpp
+++ MTA10/core/CScreenShot.cpp
@@ -11,12 +11,21 @@
 *****************************************************************************/
 
 #include "StdInc.h"
+#include <libpng/png.h>
 
 extern CCore* g_pCore;
 
 static char szScreenShotPath[MAX_PATH] = {0};
 static bool bIsChatVisible = false;
 static bool bIsDebugVisible = false;
+
+// Variables used for saving the screen shot file on a separate thread
+static bool     ms_bIsSaving        = false;
+static RECT     ms_ScreenSize       = { 0, 0, 0, 0 };
+static void*    ms_pBits            = NULL;
+static DWORD    ms_ulPitch          = 0;
+static SString  ms_strFileName;
+
 
 SString CScreenShot::PreScreenShot ()
 {
@@ -79,7 +88,7 @@ SString CScreenShot::GetValidScreenshotFilename ( void )
     //Get the system time
     SYSTEMTIME sysTime;
     GetLocalTime( &sysTime );
-    return SString("%s\\mta-screen %d-%02d-%02d %02d-%02d-%02d.png" ,&szScreenShotPath[0],sysTime.wYear, sysTime.wMonth, sysTime.wDay, sysTime.wHour, sysTime.wMinute, sysTime.wSecond);
+    return SString("%s\\mta-screen_%d-%02d-%02d_%02d-%02d-%02d.png" ,&szScreenShotPath[0],sysTime.wYear, sysTime.wMonth, sysTime.wDay, sysTime.wHour, sysTime.wMinute, sysTime.wSecond);
 }
 
 SString CScreenShot::GetScreenShotPath ( int iNumber )
@@ -114,4 +123,84 @@ SString CScreenShot::GetScreenShotPath ( int iNumber )
     FindClose(hFind); //Close the file handle
     // Get Return the file directory
     return strReturn;
+}
+
+
+// Callback for threaded save
+// Static function
+DWORD CScreenShot::ThreadProc ( LPVOID lpdwThreadParam )
+{
+    unsigned long ulScreenHeight = ms_ScreenSize.bottom - ms_ScreenSize.top;
+    unsigned long ulScreenWidth = ms_ScreenSize.right - ms_ScreenSize.left;
+
+    // Create the screen data buffer
+    BYTE** ppScreenData = NULL;
+    ppScreenData = new BYTE* [ ulScreenHeight ];
+    for ( unsigned short y = 0; y < ulScreenHeight; y++ ) {
+        ppScreenData[y] = new BYTE [ ulScreenWidth * 4 ];
+    }
+
+    // Copy the surface data into a row-based buffer for libpng
+    #define BYTESPERPIXEL 4
+    unsigned long ulLineWidth = ulScreenWidth * 4;
+    for ( unsigned int i = 0; i < ulScreenHeight; i++ ) {
+        memcpy ( ppScreenData[i], (BYTE*) ms_pBits + i* ms_ulPitch, ulLineWidth );
+        for ( unsigned int j = 3; j < ulLineWidth; j += BYTESPERPIXEL ) {
+            ppScreenData[i][j] = 0xFF;
+        }
+    }
+
+    FILE *file = fopen (ms_strFileName, "wb");
+        png_struct* png_ptr = png_create_write_struct ( PNG_LIBPNG_VER_STRING, NULL, NULL, NULL );
+        png_info* info_ptr = png_create_info_struct ( png_ptr );
+        png_init_io ( png_ptr, file );
+        png_set_filter ( png_ptr, 0, PNG_FILTER_NONE );
+        png_set_compression_level ( png_ptr, 1 );
+        png_set_IHDR ( png_ptr, info_ptr, ulScreenWidth, ulScreenHeight, 8, PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT );
+        png_set_rows ( png_ptr, info_ptr, ppScreenData );
+        png_write_png ( png_ptr, info_ptr, PNG_TRANSFORM_BGR | PNG_TRANSFORM_STRIP_ALPHA, NULL );
+        png_write_end ( png_ptr, info_ptr );
+        png_destroy_write_struct ( &png_ptr, &info_ptr );
+    fclose(file);
+
+    // Clean up the screen data buffer
+    if ( ppScreenData ) {
+        for ( unsigned short y = 0; y < ulScreenHeight; y++ ) {
+            delete [] ppScreenData[y];
+        }
+        delete [] ppScreenData;
+    }
+
+    ms_bIsSaving = false;
+    return 0;
+}
+
+// Static function
+void CScreenShot::BeginSave ( const char *szFileName, void* pBits, unsigned long ulPitch, RECT ScreenSize )
+{
+    if ( ms_bIsSaving )
+        return;
+
+    ms_strFileName = szFileName;
+    ms_pBits = pBits;
+    ms_ulPitch = ulPitch;
+    ms_ScreenSize = ScreenSize;
+
+    HANDLE hThread = CreateThread ( NULL, 0, (LPTHREAD_START_ROUTINE)CScreenShot::ThreadProc, NULL, CREATE_SUSPENDED, NULL );
+    if ( !hThread )
+    {
+        CCore::GetSingleton ().GetConsole ()->Printf ( "Could not create screenshot thread." );
+    }
+    else
+    {
+        ms_bIsSaving = true;
+        SetThreadPriority ( hThread, THREAD_PRIORITY_LOWEST );
+        ResumeThread ( hThread );
+    }
+}
+
+// Static function
+bool CScreenShot::IsSaving ( void )
+{
+    return ms_bIsSaving;
 }
