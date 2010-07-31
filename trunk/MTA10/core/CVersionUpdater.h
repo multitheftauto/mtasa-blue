b73b6563a311e56674e579c0a0fbfc65b8d4090c
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2010-07-31 15:31:49 +0000

    Added autofix for modified data files

--- trunk/MTA10/core/CVersionUpdater.h
+++ trunk/MTA10/core/CVersionUpdater.h
@@ -14,68 +14,17 @@
 #define __CVERSIONUPDATER_H
 
 
-class CVersionUpdater
+class CVersionUpdaterInterface
 {
 public:
-                            CVersionUpdater                 ( void );
-    void                    DoPulse                         ( void );
-    void                    InitiateUpdate                  ( const SString& strType, const SString& strHost );
-    bool                    IsOptionalUpdateInfoRequired    ( const SString& strHost );
-
-protected:
-    void                    ResetCheckTimer         ( void );
-    bool                    IsDownloadMandatory     ( void );
-    bool                    IsPeriodicCheck         ( void );
-    CQuestionBox&           GetQuestionBox          ( void );
-
-    void                    StartVersionCheck       ( void );
-    void                    PollVersionCheck        ( void );
-    void                    StartNoUpdateQuestion   ( void );
-    void                    PollNoUpdateQuestion    ( void );
-    void                    StartUpdateQuestion     ( void );
-    void                    PollUpdateQuestion      ( void );
-    void                    StartDownload           ( void );
-    void                    PollDownload            ( void );
-    void                    PollPostDownloadError   ( void );
-    void                    PollPostDownloadOk      ( void );
-    void                    ServerSaysOptionalUpdate        ( void );
-    void                    ServerSaysMandatoryUpdate       ( void );
-    void                    PollServerUpdateQuestion        ( void );
-
-    SString                     m_strStage;
-    SString                     m_strContext;
-    CHTTPClient                 m_HTTP;
-    long long                   m_llTimeStart;
-    std::map < SString, int >   m_DoneOptionalMap;
-
-    struct CCheckInfo
-    {
-                                CCheckInfo() : iCurrent ( -1 ), iRetryCount ( 0 ) {}
-        int                     iCurrent;
-        int                     iRetryCount;
-        std::vector < SString > serverList;
-    } m_CheckInfo;
-
-    struct CDownloadInfo
-    {
-                                CDownloadInfo() : iCurrent ( -1 ), iRetryCount ( 0 ), uiFilesize ( 0 ), uiBytesDownloaded ( 0 ) {}
-        int                     iCurrent;
-        int                     iRetryCount;
-        SString                 strStatus;
-        SString                 strTitle;
-        SString                 strMsg;
-        SString                 strMsg2;
-        SString                 strYes;
-        SString                 strNo;
-        SString                 strPriority;
-        SString                 strFilename;
-        unsigned int            uiFilesize;
-        std::vector < SString > serverList;
-        SString                 strMD5;
-        SString                 strEnd;
-        unsigned int            uiBytesDownloaded;
-    } m_DownloadInfo;
-
+    virtual                ~CVersionUpdaterInterface        ( void ) {}
+    virtual void            DoPulse                         ( void ) = 0;
+    virtual void            InitiateUpdate                  ( const SString& strType, const SString& strHost ) = 0;
+    virtual bool            IsOptionalUpdateInfoRequired    ( const SString& strHost ) = 0;
+    virtual void            InitiateDataFilesFix            ( void ) = 0;
 };
+
+CVersionUpdaterInterface* GetVersionUpdater ();
+
 
 #endif
