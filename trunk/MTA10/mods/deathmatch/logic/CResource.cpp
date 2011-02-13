a73234851589dbeec9188c5e0154f27e8575e975
Talidan <Talidan@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2011-02-13 15:00:49 +0000

    Corrected flawed BOM logic

--- trunk/MTA10/mods/deathmatch/logic/CResource.cpp
+++ trunk/MTA10/mods/deathmatch/logic/CResource.cpp
@@ -314,7 +314,7 @@ void CResource::Load ( CClientEntity *pRootEntity )
             if ( iSize > 0 && CChecksum::GenerateChecksumFromBuffer ( &buffer.at ( 0 ), iSize ).CompareWithLegacy ( pResourceFile->GetServerChecksum () ) )
             {
                 //UTF-8 BOM?  Compare by checking the standard UTF-8 BOM of 3 characters (in signed format, hence negative)
-                if ( buffer[0] != -0x11 || iSize < 1 || buffer[1] != -0x45 || iSize < 2 ||  buffer[2] != -0x41 ) //Not UTF-8
+                if ( iSize < 3 || buffer[0] != -0x11 || buffer[1] != -0x45 || buffer[2] != -0x41 ) //Not UTF-8
                     // Load the resource text
                     m_pLuaVM->LoadScriptFromBuffer ( &buffer.at ( 0 ), iSize, pResourceFile->GetName (), false );
                 else // Load ignoring the first 3 bytes
