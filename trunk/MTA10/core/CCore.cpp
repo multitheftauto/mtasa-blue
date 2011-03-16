c4d2d0cc9f965d9d9573a741b08aa468e94febb0
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2011-03-16 20:14:56 +0000

    Forward ported r2459 ([1.0.4] Fixed crash on key press.]

--- trunk/MTA10/core/CCore.cpp
+++ trunk/MTA10/core/CCore.cpp
@@ -1190,6 +1190,10 @@ void CCore::OnModUnload ( )
     m_pGUI->SelectInputHandlers( INPUT_CORE );
     // remove unused events
     m_pGUI->ClearInputHandlers( INPUT_MOD );
+
+    // Ensure all these have been removed
+    m_pKeyBinds->RemoveAllFunctions ();
+    m_pKeyBinds->RemoveAllControlFunctions ();
 }
 
 
