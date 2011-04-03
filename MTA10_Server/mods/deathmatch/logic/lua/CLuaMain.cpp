997a4a40c4eac46cc2d5e59879017c2874064b01
ccw808 <ccw808@googlemail.com>
2011-04-03 17:49:22 +0000

    Re-blocked some unwanted Lua functions. Added more fix for low FPS.

--- MTA10_Server/mods/deathmatch/logic/lua/CLuaMain.cpp
+++ MTA10_Server/mods/deathmatch/logic/lua/CLuaMain.cpp
@@ -151,9 +151,6 @@ void CLuaMain::InitVM ( void )
     // Create a new VM
     m_luaVM = lua_open ();
 
-    // Initialize security restrictions. Very important to prevent lua trojans and viruses!
-    InitSecurity ();
-
     // Set the instruction count hook
     lua_sethook ( m_luaVM, InstructionCountHook, LUA_MASKCOUNT, HOOK_INSTRUCTION_COUNT );
 
@@ -163,6 +160,9 @@ void CLuaMain::InitVM ( void )
     luaopen_string ( m_luaVM );
     luaopen_table ( m_luaVM );
     luaopen_debug ( m_luaVM );
+
+    // Initialize security restrictions. Very important to prevent lua trojans and viruses!
+    InitSecurity ();
 
     // Registering C functions
     CLuaCFunctions::RegisterFunctionsWithVM ( m_luaVM );
