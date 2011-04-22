d2df6d56fa554f6e949814d439b154ca80c8511e
Talidan <danthetaliban@gmail.com>
2011-04-22 21:55:57 +0000

    Tweaks to nickname stuff

--- Shared/sdk/CNickGen.h
+++ Shared/sdk/CNickGen.h
@@ -3,7 +3,7 @@
 *  PROJECT:     Multi Theft Auto v1.1
 *  LICENSE:     See LICENSE in the top level directory
 *  FILE:        CNickGen.h
-*  PURPOSE:     Generate hillariously poor nickames for players
+*  PURPOSE:     Generate hillariously poor nicknames for players
 *  DEVELOPERS:  Talidan
 *
 *****************************************************************************/
@@ -12,7 +12,7 @@
 #define __CNICKGEN_H
 
 #define NICKGEN_NUM_ADJECTIVES      1048
-#define NICKGEN_NUM_NOUNS           818
+#define NICKGEN_NUM_NOUNS           934
 
 class CNickGen
 {
