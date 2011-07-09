615807912024d525c19e73823acd649ff93f462f
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2011-07-09 10:01:02 +0000

    Fixed #6236 (Restarting a resource containing shaders can fail with "No valid technique")

--- trunk/MTA10/core/CDirect3DEvents9.cpp
+++ trunk/MTA10/core/CDirect3DEvents9.cpp
@@ -101,6 +101,19 @@ void CDirect3DEvents9::OnPresent ( IDirect3DDevice9 *pDevice )
     // before present, but that caused graphical issues randomly with the sky.
     pDevice->BeginScene ();
 
+    // Reset samplers on first call
+    static bool bDoneReset = false;
+    if ( !bDoneReset )
+    {
+        bDoneReset = true;
+        for ( uint i = 0 ; i < 16 ; i++ )
+        {
+            pDevice->SetSamplerState ( i, D3DSAMP_MAGFILTER, D3DTEXF_LINEAR );
+            pDevice->SetSamplerState ( i, D3DSAMP_MINFILTER, D3DTEXF_LINEAR );
+            pDevice->SetSamplerState ( i, D3DSAMP_MIPFILTER, D3DTEXF_LINEAR );
+        }
+    }
+
     // Create a state block.
     IDirect3DStateBlock9 * pDeviceState = NULL;
     pDevice->CreateStateBlock ( D3DSBT_ALL, &pDeviceState );
