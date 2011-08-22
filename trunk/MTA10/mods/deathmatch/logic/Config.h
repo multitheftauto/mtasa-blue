58fd9ce173146326a8ea4512d9052451ca9b4ac4
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2011-08-22 04:23:23 +0000

    Set voice to off by default. Set max players to 1024.

--- trunk/MTA10/mods/deathmatch/logic/Config.h
+++ trunk/MTA10/mods/deathmatch/logic/Config.h
@@ -157,7 +157,7 @@ public:
 #define MAX_VALID_WEATHER 255
 
 // Upper player limit
-#define MAX_PLAYER_COUNT 65535
+#define MAX_PLAYER_COUNT 1024
 #if MAX_PLAYER_COUNT > 65535
     #error MAX_PLAYER_COUNT "macro can't exceed 65535"
 #endif
