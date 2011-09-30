30c4c4e28f613f5f1402ad7452ea7c66108d4292
tvc@tvcdev.net <tvc@tvcdev.net@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2011-09-30 02:07:46 +0000

    Fixed #5875 (Incorrect usage of guiCreateTab causes crash)

--- trunk/MTA10/mods/deathmatch/logic/CStaticFunctionDefinitions.cpp
+++ trunk/MTA10/mods/deathmatch/logic/CStaticFunctionDefinitions.cpp
@@ -4254,6 +4254,10 @@ CClientGUIElement* CStaticFunctionDefinitions::GUICreateTab ( CLuaMain& LuaMain,
     if ( !pParent ) return NULL;
 
     CGUIElement *pGUIParent = pParent->GetCGUIElement ();
+
+    // Make sure the parent element is a TabPanel
+    if ( pGUIParent->GetType () != CGUI_TABPANEL ) return NULL;
+
     CGUIElement *pTab = static_cast < CGUITabPanel* > ( pGUIParent ) -> CreateTab ( szCaption );
     CClientGUIElement *pGUIElement = new CClientGUIElement ( m_pManager, &LuaMain, pTab );
     pGUIElement->SetParent ( pParent ? pParent : LuaMain.GetResource()->GetResourceGUIEntity()  );
