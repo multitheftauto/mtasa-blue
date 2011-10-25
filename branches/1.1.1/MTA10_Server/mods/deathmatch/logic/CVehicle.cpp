b035300e2109aa2a44c4dcd931689cef3756a270
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2011-10-25 21:25:45 +0000

    [1.1.1] Backported r3324 (Disabled vehicle resync packets when lightsync is disabled.)

--- branches/1.1.1/MTA10_Server/mods/deathmatch/logic/CVehicle.cpp
+++ branches/1.1.1/MTA10_Server/mods/deathmatch/logic/CVehicle.cpp
@@ -491,8 +491,11 @@ bool CVehicle::SetOccupant ( CPed* pPed, unsigned int uiSeat )
         // Set the Player
         if ( m_pOccupants [uiSeat] != pPed )
         {
-            if ( uiSeat == 0 && m_pOccupants[0] && IS_PLAYER(m_pOccupants[0]) )
-                g_pGame->GetPlayerManager()->BroadcastOnlyJoined ( CVehicleResyncPacket(this), NULL, 0x1a );
+            if ( g_pBandwidthSettings->bLightSyncEnabled )
+            {
+                if ( uiSeat == 0 && m_pOccupants[0] && IS_PLAYER(m_pOccupants[0]) )
+                    g_pGame->GetPlayerManager()->BroadcastOnlyJoined ( CVehicleResyncPacket(this), NULL, 0x1a );
+            }
             m_pOccupants [uiSeat] = pPed;
         }
 
