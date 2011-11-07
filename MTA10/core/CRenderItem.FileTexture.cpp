fdb7c9ea4554c6560e25fb924e364581fb1c6922
ccw808 <ccw808@googlemail.com>
2011-11-07 00:09:51 +0000

    Reduced memory usage of radar map

--- MTA10/core/CRenderItem.FileTexture.cpp
+++ MTA10/core/CRenderItem.FileTexture.cpp
@@ -18,12 +18,12 @@
 //
 //
 ////////////////////////////////////////////////////////////////
-void CFileTextureItem::PostConstruct ( CRenderItemManager* pManager, const SString& strFilename )
+void CFileTextureItem::PostConstruct ( CRenderItemManager* pManager, const SString& strFilename, bool bMipMaps, uint uiSizeX, uint uiSizeY, ERenderFormat format )
 {
     Super::PostConstruct ( pManager );
 
     // Initial creation of d3d data
-    CreateUnderlyingData ( strFilename );
+    CreateUnderlyingData ( strFilename, bMipMaps, uiSizeX, uiSizeY, format );
 }
 
 
@@ -87,13 +87,20 @@ void CFileTextureItem::OnResetDevice ( void )
 //
 //
 ////////////////////////////////////////////////////////////////
-void CFileTextureItem::CreateUnderlyingData ( const SString& strFilename )
+void CFileTextureItem::CreateUnderlyingData ( const SString& strFilename, bool bMipMaps, uint uiSizeX, uint uiSizeY, ERenderFormat format )
 {
     assert ( !m_pD3DTexture );
 
     D3DXIMAGE_INFO imageInfo;
     if ( FAILED ( D3DXGetImageInfoFromFile( strFilename, &imageInfo ) ) )
         return;
+
+    D3DFORMAT D3DFormat = (D3DFORMAT)format;
+    int iMipMaps = bMipMaps ? D3DX_DEFAULT : 1;
+    if ( uiSizeX != D3DX_DEFAULT )
+        imageInfo.Width = uiSizeX;
+    if ( uiSizeY != D3DX_DEFAULT )
+        imageInfo.Height = uiSizeY;
 
     m_uiSizeX = imageInfo.Width;
     m_uiSizeY = imageInfo.Height;
@@ -103,20 +110,20 @@ void CFileTextureItem::CreateUnderlyingData ( const SString& strFilename )
     if ( imageInfo.ResourceType == D3DRTYPE_VOLUMETEXTURE )
     {
         // It's a volume texture!
-        if ( FAILED( D3DXCreateVolumeTextureFromFile ( m_pDevice, strFilename, (IDirect3DVolumeTexture9**)&m_pD3DTexture ) ) )
+        if ( FAILED( D3DXCreateVolumeTextureFromFileEx ( m_pDevice, strFilename, uiSizeX, uiSizeY, D3DX_DEFAULT, iMipMaps, 0, D3DFormat, D3DPOOL_MANAGED, D3DX_DEFAULT, D3DX_DEFAULT, 0, NULL, NULL, (IDirect3DVolumeTexture9**)&m_pD3DTexture ) ) )
             return;
     }
     else
     if ( imageInfo.ResourceType == D3DRTYPE_CUBETEXTURE )
     {
         // It's a cubemap texture!
-        if ( FAILED( D3DXCreateCubeTextureFromFile ( m_pDevice, strFilename, (IDirect3DCubeTexture9**)&m_pD3DTexture ) ) )
+        if ( FAILED( D3DXCreateCubeTextureFromFileEx ( m_pDevice, strFilename, uiSizeX, iMipMaps, 0, D3DFormat, D3DPOOL_MANAGED, D3DX_DEFAULT, D3DX_DEFAULT, 0, NULL, NULL, (IDirect3DCubeTexture9**)&m_pD3DTexture ) ) )
             return;
     }
     else
     {
         // It's none of the above!
-        if ( FAILED( D3DXCreateTextureFromFile ( m_pDevice, strFilename, (IDirect3DTexture9**)&m_pD3DTexture ) ) )
+        if ( FAILED( D3DXCreateTextureFromFileEx ( m_pDevice, strFilename, uiSizeX, uiSizeY, iMipMaps, 0, D3DFormat, D3DPOOL_MANAGED, D3DX_DEFAULT, D3DX_DEFAULT, 0, NULL, NULL, (IDirect3DTexture9**)&m_pD3DTexture ) ) )
             return;
 
         // Update surface size if it's a normal texture
