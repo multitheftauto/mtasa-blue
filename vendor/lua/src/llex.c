a9e2d19d721e11057a3f901f864657319bec3307
ccw808 <ccw808@googlemail.com>
2011-11-08 22:25:16 +0000

    Fixed #5535 (Add 'continue' to lua) patch from onestone

--- vendor/lua/src/llex.c
+++ vendor/lua/src/llex.c
@@ -35,7 +35,7 @@
 
 /* ORDER RESERVED */
 const char *const luaX_tokens [] = {
-    "and", "break", "do", "else", "elseif",
+    "and", "break", "continue", "do", "else", "elseif",
     "end", "false", "for", "function", "if",
     "in", "local", "nil", "not", "or", "repeat",
     "return", "then", "true", "until", "while",
