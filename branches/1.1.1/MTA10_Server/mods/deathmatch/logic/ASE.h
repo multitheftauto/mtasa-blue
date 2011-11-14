701ac522ed884262881397737504ed1cca9ad43e
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2011-11-14 13:49:38 +0000

    [1.1.1] Added some bounds checks

--- branches/1.1.1/MTA10_Server/mods/deathmatch/logic/ASE.h
+++ branches/1.1.1/MTA10_Server/mods/deathmatch/logic/ASE.h
@@ -38,6 +38,12 @@ class ASE;
 
 #include <list>
 
+#define MAX_ASE_GAME_TYPE_LENGTH    200
+#define MAX_ASE_MAP_NAME_LENGTH     200
+#define MAX_RULE_KEY_LENGTH         200
+#define MAX_RULE_VALUE_LENGTH       200
+#define MAX_ANNOUNCE_VALUE_LENGTH   200
+
 class CASERule;
 
 class ASE
