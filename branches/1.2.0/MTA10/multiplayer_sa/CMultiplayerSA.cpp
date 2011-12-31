f166dcba3bb02da444d98c3edb727c5fa37d1bd2
Cazomino05 <Cazomino05@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2011-12-31 19:27:50 +0000

    [1.2.0] backported r3613 - "Building removal now supports breakable objects and things like KAC gates.
    If you couldn't remove it before you can now."

--- branches/1.2.0/MTA10/multiplayer_sa/CMultiplayerSA.cpp
+++ branches/1.2.0/MTA10/multiplayer_sa/CMultiplayerSA.cpp
@@ -339,6 +339,7 @@ DWORD CALL_LoadIPLInstance   =                                     0x538090;
 DWORD RETURN_LoadIPLInstance =                                     0x04061ED;
 
 #define HOOKPOS_CWorld_LOD_SETUP                                  0x406224
+#define HOOKPOS_CWorld_LOD_SETUP2                                 0x406326
 DWORD CALL_CWorld_LODSETUP   =                                    0x404C90;
 
 #define HOOKPOS_CBuilding_DTR                                     0x404180
@@ -347,9 +348,29 @@ DWORD JMP_CBuilding_DTR   =                                       0x535E90;
 #define HOOKPOS_CDummy_DTR                                        0x532566
 DWORD JMP_CDummy_DTR   =                                          0x535E90;
 
+#define HOOKPOS_CObject_DTR                                       0x59F680
+DWORD JMP_CObject_DTR  =                                          0x59F686;
+
 #define HOOKPOS_AddBuildingInstancesToWorld_CWorldAdd             0x5B5348
-DWORD CALL_CWorldAdd   =                                          0x563220;
+DWORD JMP_CWorld_Add_AddBuildingInstancesToWorld_CALL_CWorldAdd = 0x563220;
 DWORD RETURN_AddBuildingInstancesToWorld_CWorldAdd =              0x5B534D;
+
+
+#define HOOKPOS_CWorld_Remove_CPopulation_ConvertToDummyObject    0x6146F8
+
+#define HOOKPOS_CWorld_ADD_CPopulation_ConvertToRealObject              0x6145C7
+DWORD JMP_CWorld_Add_CPopulation_ConvertToRealObject_Retn =             0x6145CC;
+DWORD JMP_CWorld_Add_CPopulation_ConvertToRealObject_CallCWorldAdd =    0x563220;
+
+#define HOOKPOS_ConvertToObject_CPopulationManageDummy            0x616091
+DWORD CALL_Convert_To_Real_Object_CPopulation_ManageDummy = 0x614580;
+DWORD JMP_RETN_Called_CPopulation_ManageDummy = 0x616097;
+DWORD JMP_RETN_Cancel_CPopulation_ManageDummy = 0x616098;
+
+#define HOOKPOS_CWorld_ADD_CPopulation_ConvertToDummyObject       0x61470C
+DWORD CALL_CWorld_Add_CPopulation_ConvertToDummyObject = 0x563220;
+DWORD JMP_RETN_Called_CPopulation_ConvertToDummyObject = 0x614712;
+DWORD JMP_RETN_Cancelled_CPopulation_ConvertToDummyObject = 0x614715;
 
 CPed* pContextSwitchedPed = 0;
 CVector vecCenterOfWorld;
@@ -522,9 +543,20 @@ void HOOK_CWorld_LOD_SETUP ();
 
 void Hook_AddBuildingInstancesToWorld ( );
 
+void HOOK_CWorld_Remove_CPopulation_ConvertToDummyObject ( );
+
+void HOOK_CWorld_Add_CPopulation_ConvertToDummyObject ( );
+
+void Hook_CWorld_ADD_CPopulation_ConvertToRealObject ( );
+
+void HOOK_ConvertToObject_CPopulationManageDummy ( );
+
 void Hook_CBuilding_DTR ( );
 
 void Hook_CDummy_DTR ( );
+
+void Hook_CObject_DTR ( );
+
 
 
 CMultiplayerSA::CMultiplayerSA()
@@ -712,15 +744,33 @@ void CMultiplayerSA::InitHooks()
     HookInstallCall ( CALL_CTrafficLights_GetPrimaryLightState, (DWORD)HOOK_CTrafficLights_GetPrimaryLightState);
     HookInstallCall ( CALL_CTrafficLights_GetSecondaryLightState, (DWORD)HOOK_CTrafficLights_GetSecondaryLightState);
 
+
+
+    // Start of Building removal hooks
     HookInstallCall ( HOOKPOS_LoadIPLInstance, (DWORD)HOOK_LoadIPLInstance );
 
-    HookInstallCall ( HOOKPOS_CWorld_LOD_SETUP, (DWORD)HOOK_CWorld_LOD_SETUP );;
+    HookInstallCall ( HOOKPOS_CWorld_LOD_SETUP, (DWORD)HOOK_CWorld_LOD_SETUP );
+
+    HookInstallCall ( HOOKPOS_CWorld_LOD_SETUP2, (DWORD)HOOK_CWorld_LOD_SETUP );
 
     HookInstall ( HOOKPOS_CBuilding_DTR, (DWORD)Hook_CBuilding_DTR, 5 );
 
     HookInstall ( HOOKPOS_CDummy_DTR, (DWORD)Hook_CDummy_DTR, 5 );
 
+    HookInstall ( HOOKPOS_CObject_DTR, (DWORD)Hook_CObject_DTR, 6 );
+
     HookInstallCall ( HOOKPOS_AddBuildingInstancesToWorld_CWorldAdd, (DWORD)Hook_AddBuildingInstancesToWorld );
+
+    HookInstallCall( HOOKPOS_CWorld_ADD_CPopulation_ConvertToRealObject, (DWORD)Hook_CWorld_ADD_CPopulation_ConvertToRealObject );
+
+    HookInstallCall( HOOKPOS_CWorld_Remove_CPopulation_ConvertToDummyObject, (DWORD)HOOK_CWorld_Remove_CPopulation_ConvertToDummyObject );
+
+    HookInstall ( HOOKPOS_CWorld_ADD_CPopulation_ConvertToDummyObject, (DWORD)HOOK_CWorld_Add_CPopulation_ConvertToDummyObject, 6 );
+    
+    HookInstall ( HOOKPOS_ConvertToObject_CPopulationManageDummy, (DWORD)HOOK_ConvertToObject_CPopulationManageDummy, 6 );
+
+    // End of building removal hooks
+
     // Disable GTA setting g_bGotFocus to false when we minimize
     MemSet ( (void *)ADDR_GotFocus, 0x90, pGameInterface->GetGameVersion () == VERSION_EU_10 ? 6 : 10 );
 
@@ -6661,12 +6711,13 @@ second:
 
     }
 }
+
 // Variables
 SIPLInst* pEntityWorldAdd = NULL;
 CEntitySAInterface * pLODInterface = NULL; 
 bool bNextHookSetModel = false;
 
-bool CheckRemovedModel ( )
+bool CheckRemovedModelNoSet ( )
 {
     // Init our variables
     bNextHookSetModel = false;
@@ -6682,13 +6733,17 @@ bool CheckRemovedModel ( )
             if ( pGameInterface->GetWorld ( )->IsRemovedModelInRadius ( pEntityWorldAdd ) )
             {
                 // if it is next hook remove it from the world
-                bNextHookSetModel = true;
                 return true;
             }
         }
         return false;
     }
     return false;
+}
+bool CheckRemovedModel ( )
+{
+    bNextHookSetModel = CheckRemovedModelNoSet ( );
+    return bNextHookSetModel;
 }
 // Hook 1
 void _declspec(naked) HOOK_LoadIPLInstance ()
@@ -6710,7 +6765,6 @@ void _declspec(naked) HOOK_LoadIPLInstance ()
     }
 }
 
-
 void HideEntitySomehow ( )
 {
     // Did we get instructed to set the model
@@ -6721,7 +6775,7 @@ void HideEntitySomehow ( )
         // Grab the removal for the interface
         SBuildingRemoval* pBuildingRemoval = pGameInterface->GetWorld ( )->GetBuildingRemoval ( pInterface );
         // Remove down the LOD tree
-        if ( pInterface && pInterface != NULL && pInterface->bIsProcObject == 0 && ( pInterface->nType == ENTITY_TYPE_BUILDING || pInterface->nType == ENTITY_TYPE_DUMMY ) )
+        if ( pBuildingRemoval && pInterface && pInterface != NULL && pInterface->bIsProcObject == 0 && ( pInterface->nType == ENTITY_TYPE_BUILDING || pInterface->nType == ENTITY_TYPE_DUMMY ) )
         {
             // Add the LOD to the list
             pBuildingRemoval->AddBinaryBuilding ( pInterface );
@@ -6758,6 +6812,7 @@ void StorePointerToBuilding ( )
         pGameInterface->GetWorld ( )->AddDataBuilding ( pBuildingAdd );
     }
 }
+
 // Called when a data entity is added to the world (this happens once when the game loads so we just dump those in a list and we can sift through when someone tries to remove.)
 void _declspec(naked) Hook_AddBuildingInstancesToWorld ( )
 {
@@ -6770,18 +6825,163 @@ void _declspec(naked) Hook_AddBuildingInstancesToWorld ( )
     _asm
     {
         popad
-        jmp CALL_CWorldAdd
+        jmp JMP_CWorld_Add_AddBuildingInstancesToWorld_CALL_CWorldAdd
+    }
+}
+
+bool CheckForRemoval ( )
+{
+    // Did we get instructed to set the model
+    if ( pLODInterface )
+    {
+        // Init pInterface with the Initial model
+        CEntitySAInterface * pInterface = pLODInterface;
+        // Remove down the LOD tree
+        if ( pGameInterface->GetWorld ( )->IsObjectRemoved ( pInterface ) )
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Call to CWorld::Add in CPopulation::ConvertToRealObject we just use this to get a list of pointers to valid objects for instant removal
+void _declspec(naked) Hook_CWorld_ADD_CPopulation_ConvertToRealObject ( )
+{
+    _asm
+    {
+        pushad
+        mov pBuildingAdd, esi
+        mov pLODInterface, esi
+    }
+    StorePointerToBuilding ( );
+    _asm
+    {
+        popad
+        jmp JMP_CWorld_Add_CPopulation_ConvertToRealObject_CallCWorldAdd
+        jmp JMP_CWorld_Add_CPopulation_ConvertToRealObject_Retn
+    }
+}
+
+void RemoveObjectIfNeeded ( )
+{
+    SBuildingRemoval* pBuildingRemoval = pGameInterface->GetWorld ( )->GetBuildingRemoval ( pLODInterface );
+    if ( pBuildingRemoval != NULL )
+    {
+        if ( (DWORD)(pBuildingAdd->vtbl) != VTBL_CPlaceable )
+        {
+            pBuildingRemoval->AddDataBuilding ( pBuildingAdd );
+            pGameInterface->GetWorld ( )->Remove( pBuildingAdd );
+        }
+
+        if ( (DWORD)(pLODInterface->vtbl) != VTBL_CPlaceable )
+        {
+            pBuildingRemoval->AddDataBuilding ( pLODInterface );
+            pGameInterface->GetWorld ( )->Remove( pLODInterface );
+        }
+    }
+}
+
+// on stream in -> create and remove it from the world just after so we can restore easily
+void _declspec (naked) HOOK_ConvertToObject_CPopulationManageDummy ( )
+{
+    _asm
+    {
+        pushad
+        mov pBuildingAdd, edx
+        mov pLODInterface, edx
+    }
+    _asm
+    {
+        popad
+        push edx
+        call CALL_Convert_To_Real_Object_CPopulation_ManageDummy
+        pop ecx
+        mov pLODInterface, ecx
+        pushad
+    }
+    RemoveObjectIfNeeded ( );
+    _asm
+    {
+        popad
+        jmp JMP_RETN_Cancel_CPopulation_ManageDummy
     }
 }
 
 CEntitySAInterface * pBuildingRemove = NULL;
 void RemovePointerToBuilding ( )
 {
-    if ( pBuildingRemove->nType == ENTITY_TYPE_BUILDING || pBuildingRemove->nType == ENTITY_TYPE_DUMMY )
+    if ( pBuildingRemove->nType == ENTITY_TYPE_BUILDING || pBuildingRemove->nType == ENTITY_TYPE_DUMMY || pBuildingRemove->nType == ENTITY_TYPE_OBJECT )
     {
         pGameInterface->GetWorld ( )->RemoveWorldBuildingFromLists ( pBuildingRemove );
     }
 }
+
+DWORD dwCWorldRemove = 0x563280;
+// Call to CWorld::Remove in CPopulation::ConvertToDummyObject this is called just before deleting a CObject so we remove the CObject while we are there and remove the new dummy if we need to do so before returning
+void _declspec(naked) HOOK_CWorld_Remove_CPopulation_ConvertToDummyObject ( )
+{
+    _asm
+    {
+        pushad
+        mov pBuildingRemove, esi
+        mov pBuildingAdd, edi
+        mov pLODInterface, edi
+    }
+    RemovePointerToBuilding ( );
+    StorePointerToBuilding ( );
+    RemoveObjectIfNeeded ( );
+    _asm
+    {
+        popad
+        jmp dwCWorldRemove
+    }
+}
+// if it's replaced get rid of it
+void RemoveDummyIfReplaced ( )
+{
+    SBuildingRemoval* pBuildingRemoval = pGameInterface->GetWorld ( )->GetBuildingRemoval ( pLODInterface );
+    if ( pBuildingRemoval != NULL )
+    {
+        if ( (DWORD)(pBuildingAdd->vtbl) != VTBL_CPlaceable )
+        {
+            pBuildingRemoval->AddDataBuilding ( pBuildingAdd );
+            pGameInterface->GetWorld ( )->Remove( pBuildingAdd );
+        }
+    }
+}
+
+// Function that handles dummy -> object so we can cancel this process if need be
+void _declspec(naked) HOOK_CWorld_Add_CPopulation_ConvertToDummyObject ( )
+{
+    _asm
+    {
+        pushad
+        mov pLODInterface, edi
+        mov pBuildingAdd, edi
+    }
+    StorePointerToBuilding ( );
+    if ( CheckForRemoval ( ) )
+    {
+        _asm
+        {
+            popad
+            jmp JMP_RETN_Cancelled_CPopulation_ConvertToDummyObject
+        }
+    }
+    else
+    {
+        _asm
+        {
+            popad
+            push edi
+            call CALL_CWorld_Add_CPopulation_ConvertToDummyObject
+            jmp JMP_RETN_Called_CPopulation_ConvertToDummyObject
+        }
+    }
+}
+
+// Destructors to catch element deletion so we can delete their entries
 void _declspec(naked) Hook_CBuilding_DTR ( )
 {
     _asm
@@ -6809,5 +7009,22 @@ void _declspec(naked) Hook_CDummy_DTR ( )
     {
         popad
         jmp JMP_CDummy_DTR
+    }
+}
+
+DWORD dwObjectVtbl = 0x866F60;
+void _declspec(naked) Hook_CObject_DTR ( )
+{
+    _asm
+    {
+        pushad
+        mov pBuildingRemove, esi
+    }
+    RemovePointerToBuilding ( );
+    _asm
+    {
+        popad
+        mov dword ptr [esi], offset dwObjectVtbl
+        jmp JMP_CObject_DTR
     }
 }
