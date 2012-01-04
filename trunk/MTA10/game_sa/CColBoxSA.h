8c76d69ffc81d6b5c3875db0648037ea76422690
rydencillo <rydencillo@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2012-01-04 08:26:13 +0000

    Added better documentation for the CColModelSA class.

---
+++ trunk/MTA10/game_sa/CColBoxSA.h
@@ -0,0 +1,16 @@
+/*****************************************************************************
+*
+*  PROJECT:     Multi Theft Auto v1.0
+*  LICENSE:     See LICENSE in the top level directory
+*  FILE:        game_sa/CColBoxSA.h
+*  PURPOSE:     Header file for collision box class
+*  DEVELOPERS:  Alberto Alonso <rydencillo@gmail.com>
+*
+*  Multi Theft Auto is available from http://www.multitheftauto.com/
+*
+*****************************************************************************/
+
+typedef struct
+{
+    BYTE pad0 [ 28 ];
+} CColBoxSA;
---
+++ trunk/MTA10/game_sa/CColSphereSA.h
@@ -0,0 +1,16 @@
+/*****************************************************************************
+*
+*  PROJECT:     Multi Theft Auto v1.0
+*  LICENSE:     See LICENSE in the top level directory
+*  FILE:        game_sa/CColSphereSA.h
+*  PURPOSE:     Header file for collision sphere class
+*  DEVELOPERS:  Alberto Alonso <rydencillo@gmail.com>
+*
+*  Multi Theft Auto is available from http://www.multitheftauto.com/
+*
+*****************************************************************************/
+
+typedef struct
+{
+    BYTE pad0 [ 20 ];
+} CColSphereSA;
