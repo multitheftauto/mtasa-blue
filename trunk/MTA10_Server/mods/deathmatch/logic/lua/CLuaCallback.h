53513f54b49e9e529be7192b686c7219348b58e9
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2012-01-27 14:13:05 +0000

    Forward ported r3756 ([1.3.0] Fixed #6835 (Server logic/lua/CLuaFunctionRef.cpp:72 assertion error)) r3755 ([1.3.0] Fixed #6833 (getZoneName citiesonly is not working correct)) r3754 ([1.3.0] Fixed #6834: ("Player not found." console spam causes lag)) r3748 ([1.3.0] Addendum to r3747) r3747 ([1.3.0] Test for #6826: (Timeout on map change))

--- trunk/MTA10_Server/mods/deathmatch/logic/lua/CLuaCallback.h
+++ trunk/MTA10_Server/mods/deathmatch/logic/lua/CLuaCallback.h
@@ -24,11 +24,52 @@ public:
 
     void Call ( void )
     {
-        m_Arguments.Call ( m_pLuaMain, m_iLuaFunction );
+        if ( m_pLuaMain )
+            m_Arguments.Call ( m_pLuaMain, m_iLuaFunction );
+    }
+
+    void OnLuaMainDestroy ( CLuaMain* pLuaMain )
+    {
+        if ( pLuaMain == m_pLuaMain )
+        {
+            m_pLuaMain = NULL;
+            m_iLuaFunction = CLuaFunctionRef ();
+            m_Arguments = CLuaArguments ();
+        }
     }
 
 protected:
     CLuaMain*       m_pLuaMain;
     CLuaFunctionRef m_iLuaFunction;
     CLuaArguments   m_Arguments;
+};
+
+
+//
+// For managing Lua callbacks
+//
+class CLuaCallbackManager
+{
+public:
+
+    CLuaCallback* CreateCallback ( CLuaMain* pLuaMain, CLuaFunctionRef iLuaFunction, const CLuaArguments& Args )
+    {
+        m_CallbackList.push_back ( new CLuaCallback ( pLuaMain, iLuaFunction, Args ) );
+        return m_CallbackList.back ();
+    }
+
+    void DestroyCallback ( CLuaCallback* pCallback )
+    {
+        ListRemove ( m_CallbackList, pCallback );
+        delete pCallback;
+    }
+
+    void OnLuaMainDestroy ( CLuaMain* pLuaMain )
+    {
+        for ( uint i = 0 ; i < m_CallbackList.size () ; i++ )
+            m_CallbackList[i]->OnLuaMainDestroy ( pLuaMain );
+    }
+
+protected:
+    std::vector < CLuaCallback* >   m_CallbackList;
 };
