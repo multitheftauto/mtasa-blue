a334afbc8caada8c238338a4c309c6a3b9b75de5
lil.Toady@gmail.com <lil.Toady@gmail.com@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2012-02-10 14:18:22 +0000

    Fixed #6150: Console can logout, but cannt log back in.

--- trunk/MTA10_Server/mods/deathmatch/logic/CAccountManager.cpp
+++ trunk/MTA10_Server/mods/deathmatch/logic/CAccountManager.cpp
@@ -726,6 +726,13 @@ bool CAccountManager::LogOut ( CClient* pClient, CClient* pEchoClient )
         return false;
     }
 
+    if ( pClient->GetClientType () == CClient::CLIENT_CONSOLE )
+    {
+        if ( pEchoClient )
+            pEchoClient->SendEcho ( "logout: Console may not log out" );
+        return false;
+    }
+
     CAccount* pCurrentAccount = pClient->GetAccount ();
     pCurrentAccount->SetClient ( NULL );
 
