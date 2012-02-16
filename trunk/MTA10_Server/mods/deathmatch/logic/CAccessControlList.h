888df752231db01c080eac0279ee1ce24b9a74dc
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2012-02-16 22:15:53 +0000

    Fixed #6828 (Hacking the server is easy if all resources have admin rights (i.e. for kicking))
    Using wildcards to block resources is now possible. e.g.:
    <acl name="InRPC">
        <right name="general.http" access="true" />
        <right name="resource.examplechat" access="true" />
        <right name="resource.*" access="false" />
    </acl>

--- trunk/MTA10_Server/mods/deathmatch/logic/CAccessControlList.h
+++ trunk/MTA10_Server/mods/deathmatch/logic/CAccessControlList.h
@@ -30,7 +30,7 @@ public:
     inline const char*                          GetName                     ( void )        { return m_szACLName; };
 
     CAccessControlListRight*                    AddRight                    ( const char* szRightName, CAccessControlListRight::ERightType eRightType, bool bAccess );
-    CAccessControlListRight*                    GetRight                    ( const char* szRightName, CAccessControlListRight::ERightType eRightType );
+    CAccessControlListRight*                    GetRight                    ( const char* szRightName, CAccessControlListRight::ERightType eRightType, bool bAllowWildcardMatch = false );
     bool                                        RemoveRight                 ( const char* szRightName, CAccessControlListRight::ERightType eRightType );
 
     void                                        WriteToXMLNode              ( CXMLNode* pNode );
