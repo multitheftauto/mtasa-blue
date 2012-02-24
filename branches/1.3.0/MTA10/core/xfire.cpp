fd0af77afe09c6e689ce3b93e0602771e81f29b8
Cazomino05 <Cazomino05@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2012-02-24 18:56:53 +0000

    [1.3.0] Reverted previous commit

--- branches/1.3.0/MTA10/core/xfire.cpp
+++ branches/1.3.0/MTA10/core/xfire.cpp
@@ -1,10 +1,10 @@
-#define WIN32_LEAN_AND_MEAN
+#include "StdInc.h"
+
 #include <windows.h>
 #include <tlhelp32.h>
 
-#include "xfiregameclient.h"
-
 static HMODULE g_toucan_dll = NULL;
+static CElapsedTime g_NextToucanDLLSearchTime;
 static void HelperInit();
 static HMODULE HelperGetToucanDLL();
 
@@ -32,88 +32,94 @@ static XfireSetCustomGameDataUTF8Function ptr_XfireSetCustomGameDataUTF8 = NULL;
 
 int XfireIsLoaded()
 {
-	HelperInit();
-	if (ptr_XfireSetCustomGameDataA &&
-		ptr_XfireSetCustomGameDataW &&
-		ptr_XfireSetCustomGameDataUTF8)
-		return 1;
-	return 0;
+    HelperInit();
+    if (ptr_XfireSetCustomGameDataA &&
+        ptr_XfireSetCustomGameDataW &&
+        ptr_XfireSetCustomGameDataUTF8)
+        return 1;
+    return 0;
 }
 
 int XfireSetCustomGameDataA(int num_keys, const char **keys, const char **values)
 {
-	HelperInit();
-	if (ptr_XfireSetCustomGameDataA)
-		return ptr_XfireSetCustomGameDataA(num_keys, keys, values);
-	return 1;
+    HelperInit();
+    if (ptr_XfireSetCustomGameDataA)
+        return ptr_XfireSetCustomGameDataA(num_keys, keys, values);
+    return 1;
 }
 
 int XfireSetCustomGameDataW(int num_keys, const wchar_t **keys, const wchar_t **values)
 {
-	HelperInit();
-	if (ptr_XfireSetCustomGameDataW)
-		return ptr_XfireSetCustomGameDataW(num_keys, keys, values);
-	return 1;
+    HelperInit();
+    if (ptr_XfireSetCustomGameDataW)
+        return ptr_XfireSetCustomGameDataW(num_keys, keys, values);
+    return 1;
 }
 
 int XfireSetCustomGameDataUTF8(int num_keys, const char **keys, const char **values)
 {
-	HelperInit();
-	if (ptr_XfireSetCustomGameDataUTF8)
-		return ptr_XfireSetCustomGameDataUTF8(num_keys, keys, values);
-	return 1;
+    HelperInit();
+    if (ptr_XfireSetCustomGameDataUTF8)
+        return ptr_XfireSetCustomGameDataUTF8(num_keys, keys, values);
+    return 1;
 }
 
 /* ------------------------------------------------------------------------- */
 static void HelperInit()
 {
-	if (!ptr_XfireSetCustomGameDataA ||
-		!ptr_XfireSetCustomGameDataW ||
-		!ptr_XfireSetCustomGameDataUTF8)
-	{
-		HMODULE toucan_dll = HelperGetToucanDLL();
-		if (toucan_dll)
-		{
-			ptr_XfireSetCustomGameDataA = (XfireSetCustomGameDataAFunction)::GetProcAddress(toucan_dll, "ToucanSendGameClientDataA_V1");
-			ptr_XfireSetCustomGameDataW = (XfireSetCustomGameDataWFunction)::GetProcAddress(toucan_dll, "ToucanSendGameClientDataW_V1");
-			ptr_XfireSetCustomGameDataUTF8 = (XfireSetCustomGameDataUTF8Function)::GetProcAddress(toucan_dll, "ToucanSendGameClientDataUTF8_V1");
-		}
-	}
+    if (!ptr_XfireSetCustomGameDataA ||
+        !ptr_XfireSetCustomGameDataW ||
+        !ptr_XfireSetCustomGameDataUTF8)
+    {
+        HMODULE toucan_dll = HelperGetToucanDLL();
+        if (toucan_dll)
+        {
+            ptr_XfireSetCustomGameDataA = (XfireSetCustomGameDataAFunction)::GetProcAddress(toucan_dll, "ToucanSendGameClientDataA_V1");
+            ptr_XfireSetCustomGameDataW = (XfireSetCustomGameDataWFunction)::GetProcAddress(toucan_dll, "ToucanSendGameClientDataW_V1");
+            ptr_XfireSetCustomGameDataUTF8 = (XfireSetCustomGameDataUTF8Function)::GetProcAddress(toucan_dll, "ToucanSendGameClientDataUTF8_V1");
+        }
+    }
 }
 
 
 static HMODULE HelperGetToucanDLL()
 {
-	if (g_toucan_dll)
-		return g_toucan_dll;
+    if (g_toucan_dll)
+        return g_toucan_dll;
 
-	/*
-	** We need to enumerate the DLLs loaded to find toucan dll.
-	** This is done because the toucan dll changes with each update.
-	** The toucan dll has the following format. "xfire_toucan_{BUILD_NUMBER}.dll"
-	** We simply try to find a dll w/ the prefix "xfire_toucan"
-	*/
-	HANDLE snapshot_handle = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, GetCurrentProcessId());
-	if (snapshot_handle != INVALID_HANDLE_VALUE)
-	{
-		MODULEENTRY32 module_entry;
-		module_entry.dwSize = sizeof(MODULEENTRY32); 
+    // Only scan once every five seconds
+    if ( g_NextToucanDLLSearchTime.Get () < 5000 )
+        return NULL;
 
-		BOOL result = Module32First(snapshot_handle, &module_entry);
-		char module_name[] = "xfire_toucan";
-		DWORD module_name_len = sizeof(module_name)-1;
-		while (result)
-		{
-			if (CompareStringA(LOCALE_USER_DEFAULT, NORM_IGNORECASE, module_entry.szModule, module_name_len, module_name, module_name_len) == CSTR_EQUAL)
-			{
-				g_toucan_dll = module_entry.hModule;
-				break;
-			}
-			result = Module32Next(snapshot_handle, &module_entry);
-		}
+    g_NextToucanDLLSearchTime.Reset ();
 
-		CloseHandle(snapshot_handle);
-	}
+    /*
+    ** We need to enumerate the DLLs loaded to find toucan dll.
+    ** This is done because the toucan dll changes with each update.
+    ** The toucan dll has the following format. "xfire_toucan_{BUILD_NUMBER}.dll"
+    ** We simply try to find a dll w/ the prefix "xfire_toucan"
+    */
+    HANDLE snapshot_handle = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, GetCurrentProcessId());
+    if (snapshot_handle != INVALID_HANDLE_VALUE)
+    {
+        MODULEENTRY32 module_entry;
+        module_entry.dwSize = sizeof(MODULEENTRY32); 
 
-	return g_toucan_dll;
+        BOOL result = Module32First(snapshot_handle, &module_entry);
+        char module_name[] = "xfire_toucan";
+        DWORD module_name_len = sizeof(module_name)-1;
+        while (result)
+        {
+            if (CompareStringA(LOCALE_USER_DEFAULT, NORM_IGNORECASE, module_entry.szModule, module_name_len, module_name, module_name_len) == CSTR_EQUAL)
+            {
+                g_toucan_dll = module_entry.hModule;
+                break;
+            }
+            result = Module32Next(snapshot_handle, &module_entry);
+        }
+
+        CloseHandle(snapshot_handle);
+    }
+
+    return g_toucan_dll;
 }
