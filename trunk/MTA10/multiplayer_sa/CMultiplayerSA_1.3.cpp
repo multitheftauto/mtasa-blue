84677d4343d58ef645388bf38fd2f662ca884be0
Cazomino05 <Cazomino05@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2012-02-29 21:48:26 +0000

    forward ported r3866 and r3867

---
+++ trunk/MTA10/multiplayer_sa/CMultiplayerSA_1.3.cpp
@@ -0,0 +1,29 @@
+/*****************************************************************************
+*
+*  PROJECT:     Multi Theft Auto v1.0
+*  LICENSE:     See LICENSE in the top level directory
+*  FILE:        multiplayer_sa/CMultiplayerSA.cpp
+*  PURPOSE:     Multiplayer module class 1.3
+*  DEVELOPERS:  Cazomino05 <>
+*
+*  Multi Theft Auto is available from http://www.multitheftauto.com/
+*
+*****************************************************************************/
+
+#include "StdInc.h"
+
+void CMultiplayerSA::Init_13 ( void )
+{
+    InitHooks_13 ( );
+    InitMemoryCopies_13 ( );
+}
+
+void CMultiplayerSA::InitHooks_13 ( void )
+{
+    // HookInstalls go here
+}
+
+void CMultiplayerSA::InitMemoryCopies_13 ( void )
+{
+    // Memory based fixes go here
+}
