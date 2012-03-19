1b505f4b534ed524e0f72592838076858ad94fde
ccw808 <ccw808@googlemail.com>
2012-03-19 03:04:52 +0000

    Fixed #6949 (Request: client-side pickup events) Patch from John_Michael

--- MTA10/mods/deathmatch/logic/CClientGame.cpp
+++ MTA10/mods/deathmatch/logic/CClientGame.cpp
@@ -2602,6 +2602,8 @@ void CClientGame::AddBuiltInEvents ( void )
     m_Events.AddEvent ( "onClientPlayerVoiceStop", "", NULL, false );
     m_Events.AddEvent ( "onClientPlayerStealthKill", "target", NULL, false );
     m_Events.AddEvent ( "onClientPlayerHitByWaterCannon", "vehicle", NULL, false );
+	m_Events.AddEvent ( "onClientPlayerPickupHit", "pickup, matchingDimension", NULL, false );
+	m_Events.AddEvent ( "onClientPlayerPickupLeave", "pickup, matchingDimension", NULL, false );
 
     // Ped events
     m_Events.AddEvent ( "onClientPedDamage", "attacker, weapon, bodypart", NULL, false );
