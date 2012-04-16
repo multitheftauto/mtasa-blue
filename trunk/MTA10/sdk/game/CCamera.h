b7dd24b3d4ebc8e882d20543c04712809461ae5d
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2012-04-16 05:15:24 +0000

    Fixed #6946 (getCameraRotation, setCameraRotation) patch from Kernell

--- trunk/MTA10/sdk/game/CCamera.h
+++ trunk/MTA10/sdk/game/CCamera.h
@@ -113,6 +113,8 @@ public:
     virtual VOID                        RestoreWithJumpCut()=0;
     virtual CMatrix                     * GetMatrix ( CMatrix * matrix )=0;
     virtual VOID                        SetMatrix ( CMatrix * matrix )=0;
+    virtual VOID                        GetRotation ( float &fX, float &fY )=0;
+    virtual VOID                        SetRotation ( float fX, float fY )=0;
     virtual VOID                        SetCamPositionForFixedMode ( CVector * vecPosition, CVector * vecUpOffset )=0;
     virtual VOID                        Find3rdPersonCamTargetVector ( FLOAT fDistance, CVector * vecGunMuzzle, CVector * vecSource, CVector * vecTarget )=0;
     virtual float                       Find3rdPersonQuickAimPitch ( void ) = 0;
