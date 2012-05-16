eccd94063ffea102e25a774f5cdff367980edf5b
Cazomino05 <Cazomino05@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2012-05-16 15:41:12 +0000

    [BASS-Effects] Applying effects now restores position and such rather than restarting + fixed lib compile issue.

--- branches/BASS-Effects/MTA10/mods/shared_logic/CClientSound.cpp
+++ branches/BASS-Effects/MTA10/mods/shared_logic/CClientSound.cpp
@@ -446,12 +446,19 @@ float CClientSound::GetMaxDistance ( void )
 
 void CClientSound::ApplyFXModifications ( float fSampleRate, float fTempo, float fPitch, bool bReversed )
 {
+    m_SimulatedPlayPosition.SetLooped ( m_bLoop );
+    m_SimulatedPlayPosition.SetLength ( m_dLength );
+    m_SimulatedPlayPosition.SetPaused ( m_bPaused );
+    m_SimulatedPlayPosition.SetPlaybackSpeed( GetPlaybackSpeed () );
+    m_SimulatedPlayPosition.SetPlayPositionNow ( GetPlayPosition () );
+    m_SimulatedPlayPosition.SetValid ( true );
     Destroy();
     m_bReversed = bReversed;
     m_fSampleRate = fSampleRate;
     m_fTempo = fTempo;
     m_fPitch = fPitch;
     Create();
+    EndSimulationOfPlayPositionAndApply ( );
 }
 
 void CClientSound::GetFXModifications ( float &fSampleRate, float &fTempo, float &fPitch, bool &bReversed )
