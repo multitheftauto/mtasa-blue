34356b7a6fb1a0197c0efcb07679c06ffbed0639
joebullet.htc@gmail.com <joebullet.htc@gmail.com@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2012-06-19 15:25:58 +0000

    [jb-contrib] Added CSeaAnimalSA

---
+++ branches/jb-contrib/MTA10/game_sa/CSeaAnimalSA.h
@@ -0,0 +1,22 @@
+/*****************************************************************************
+*
+*  PROJECT:     Multi Theft Auto v1.0
+*  LICENSE:     See LICENSE in the top level directory
+*  FILE:        sdk/game/CPathFind.h
+*  PURPOSE:     Path finder interface
+*
+*  Multi Theft Auto is available from http://www.multitheftauto.com/
+*
+*****************************************************************************/
+
+#pragma once
+
+class CSeaAnimal
+{
+public:
+    uint32 uiObjectID;
+    uint8 Min;
+    uint8 Max;
+    uint8 pad1[2];
+    uint32 pad2[9];
+};
