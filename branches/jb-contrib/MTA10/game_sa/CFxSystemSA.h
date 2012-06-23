11519d0ab4509b44695879f60c16bcb6ca2fc418
joebullet.htc@gmail.com <joebullet.htc@gmail.com@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2012-06-23 20:13:37 +0000

    [jb-contrib] Some more cleanup.

--- branches/jb-contrib/MTA10/game_sa/CFxSystemSA.h
+++ branches/jb-contrib/MTA10/game_sa/CFxSystemSA.h
@@ -16,12 +16,26 @@ class CFxSystemSAInterface
 {
 public:
     uint32 pad1;
-    uint32 pad2;
-    uint32 uiFxSystemHash;
-    uint32 pad3[3];
-    uint16 pad4;
+    uint32 pNext;
+    void* pUnk; // must be initialized before this class is used
+    RwMatrix* pMatrixPos;
+    RwMatrix transformMatrix;
+    uint8 pad2[4];
+    uint32 pad3[2];
+    uint16 pad4[3];
+    uint8 flags;
     uint8 pad5;
-    uint8 uiParticleEmitterManagerCount;
-    uint32 pad6[4];
+    uint32 pad6;
+    CVector vecUnk;
+    uint32 pad7;
+    class CParticleEmitterManager** pParticleEmitterManagers; // allocated array of pointers
+    uint32 pad8[34];
 };
-// C_ASSERT(sizeof(CFxSystemSAInterface) == 0x???); find correct size
+C_ASSERT(sizeof(CFxSystemSAInterface) == 0x104);
+
+class CFxSystem
+{
+public:
+private:
+    CFxSystemSAInterface * m_pInterface;
+};
