7c7f9179a45f04d26b1327087592481fd91b50bd
ccw808 <ccw808@googlemail.com>
2012-07-05 01:38:49 +0000

    Fixed duplicate building removals

--- MTA10_Server/mods/deathmatch/logic/CBuildingRemovalManager.cpp
+++ MTA10_Server/mods/deathmatch/logic/CBuildingRemovalManager.cpp
@@ -4,7 +4,7 @@
 *               (Shared logic for modifications)
 *  LICENSE:     See LICENSE in the top level directory
 *  FILE:        mods/deathmatch/logic/CBuildingRemovalManager.h
-*  PURPOSE:     source file for the custom weapon stats manager.
+*  PURPOSE:
 *  DEVELOPERS:  Cazomino05 < Cazomino05@gmail.com >
 *
 *****************************************************************************/
@@ -24,11 +24,32 @@ CBuildingRemovalManager::~CBuildingRemovalManager ( void )
     }
 }
 
-CBuildingRemoval * CBuildingRemovalManager::CreateBuildingRemoval ( unsigned short usModel, float fRadius, CVector vecPos )
+void CBuildingRemovalManager::CreateBuildingRemoval ( unsigned short usModel, float fRadius, const CVector& vecPos )
 {
+    // Check if this removal has already been covered
+    std::pair < std::multimap < unsigned short, CBuildingRemoval* >::iterator, std::multimap < unsigned short, CBuildingRemoval* >::iterator> iterators = m_BuildingRemovals.equal_range ( usModel );
+    std::multimap < unsigned short, CBuildingRemoval* > ::iterator iter = iterators.first;
+    for ( ; iter != iterators.second ; ++iter )
+    {
+        CBuildingRemoval* pFind = iter->second;
+        if ( pFind ) 
+        {
+            // Sphere A is completely inside sphere B if distance from A to B <= (radius B - radius A)
+            const CVector& vecPosB = pFind->GetPosition ();
+            float fRadiusB = pFind->GetRadius ();
+
+            float fDistance = ( vecPos - vecPosB ).Length ();
+
+            if ( fDistance <= fRadiusB - fRadius )
+            {
+                // Already covered by existing removal
+                return;
+            }
+        }
+    }
+
     CBuildingRemoval * pBuildingRemoval = new CBuildingRemoval ( usModel, fRadius, vecPos );
     m_BuildingRemovals.insert( std::pair < unsigned short, CBuildingRemoval * > ( usModel, pBuildingRemoval ) );
-    return pBuildingRemoval;
 }
 
 void CBuildingRemovalManager::ClearBuildingRemovals ( void )
@@ -41,7 +62,7 @@ void CBuildingRemovalManager::ClearBuildingRemovals ( void )
     m_BuildingRemovals.clear ( );
 }
 
-void CBuildingRemovalManager::RestoreWorldModel ( unsigned short usModel, float fRadius, CVector vecPos )
+void CBuildingRemovalManager::RestoreWorldModel ( unsigned short usModel, float fRadius, const CVector& vecPos )
 {
 
     CBuildingRemoval * pFind = NULL;
@@ -52,7 +73,7 @@ void CBuildingRemovalManager::RestoreWorldModel ( unsigned short usModel, float 
         pFind = (*iter).second;
         if ( pFind ) 
         {
-            CVector vecRemovalPos = pFind->GetPosition ( );
+            const CVector& vecRemovalPos = pFind->GetPosition ( );
             // Grab distances across each axis
             float fDistanceX = vecPos.fX - vecRemovalPos.fX;
             float fDistanceY = vecPos.fY - vecRemovalPos.fY;
