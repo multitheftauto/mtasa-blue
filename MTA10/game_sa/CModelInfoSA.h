45905e202ff4da2275a66d818f0f2d1c6420f644
ccw808 <ccw808@googlemail.com>
2012-07-08 21:56:36 +0000

    Fixed custom object collisions getting randomly reset

--- MTA10/game_sa/CModelInfoSA.h
+++ MTA10/game_sa/CModelInfoSA.h
@@ -154,14 +154,14 @@ public:
     unsigned char           dwUnknownFlag16: 1;
 
     // Flags used by CBaseModelInfo
-    unsigned char           bDoWeOwnTheColModel: 1;     // +18
+    unsigned char           bHasBeenPreRendered: 1;     // +18
     unsigned char           bIsBackfaceCulled: 1;
     unsigned char           bIsLod: 1;
     unsigned char           bDontCastShadowsOn: 1;
     unsigned char           bDontWriteZBuffer: 1;
     unsigned char           bDrawAdditive: 1;
     unsigned char           bDrawLast: 1;
-    unsigned char           bHasBeenPreRendered: 1;
+    unsigned char           bDoWeOwnTheColModel: 1;
 
     unsigned char           dwUnknownFlag25: 1;         // +19
     unsigned char           dwUnknownFlag26: 1;
@@ -238,8 +238,9 @@ protected:
     static std::map < unsigned short, int > ms_RestreamTxdIDMap;
     static std::map < DWORD, float > ms_ModelDefaultLodDistanceMap;
     bool                            m_bAddedRefForCollision;
-
 public:
+    static std::set < uint >        ms_ReplacedColModels;
+
                                     CModelInfoSA            ( void );
                                     CModelInfoSA            ( DWORD dwModelID );
 
@@ -283,6 +284,7 @@ public:
     static void                     StaticResetLodDistances ( void );
     void                            RestreamIPL             ( void );
     static void                     StaticFlushPendingRestreamIPL ( void );
+    static void                     StaticSetHooks          ( void );
 
     void                            ModelAddRef             ( EModelRequestType requestType, const char* szTag );
     int                             GetRefCount             ( void );
