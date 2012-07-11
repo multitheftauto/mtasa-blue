d2cf71391a38e648e55c53297ea6f6e2da1c8d31
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2012-07-11 17:54:12 +0000

    [1.3.0] Fixed version detection for using long strings

--- branches/1.3.0/MTA10_Server/mods/deathmatch/logic/lua/CLuaArgument.cpp
+++ branches/1.3.0/MTA10_Server/mods/deathmatch/logic/lua/CLuaArgument.cpp
@@ -561,7 +561,7 @@ bool CLuaArgument::ReadFromBitStream ( NetBitStreamInterface& bitStream, std::ve
                     ReadString ( "" );
 
                 // Enforce min_mta_version version rule
-                if ( uiLength > 65535 && g_pGame->GetConfig()->GetMinClientVersion () < LONG_STRING_MIN_VERSION )
+                if ( uiLength > 65535 && g_pGame->CalculateMinClientRequirement () < LONG_STRING_MIN_VERSION )
                 {
                     LogUnableToPacketize ( "#### Couldn't packetize argument list. Invalid string specified, limit is 65535 characters."
                                            " To use longer strings, set script <min_mta_version> to " LONG_STRING_MIN_VERSION " or higher." );
@@ -681,7 +681,7 @@ bool CLuaArgument::WriteToBitStream ( NetBitStreamInterface& bitStream, std::map
                 }
             }
             else
-            if ( sizeTemp > 65535 && bitStream.Version () >= 0x027 && g_pGame->GetConfig()->GetMinClientVersion () >= LONG_STRING_MIN_VERSION )
+            if ( sizeTemp > 65535 && bitStream.Version () >= 0x027 && g_pGame->CalculateMinClientRequirement () >= LONG_STRING_MIN_VERSION )
             {
                 // This is a long string argument
                 type.data.ucType = LUA_TSTRING_LONG;
