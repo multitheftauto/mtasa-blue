d955a00d2d836e969b62d74be4064f4dd63cdcd6
Cazomino05 <Cazomino05@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2012-08-19 23:49:53 +0000

    [Weapon-Creation] Fixed setVehicleSirens crash.

--- branches/weapon-creation/MTA10_Server/mods/deathmatch/logic/lua/CLuaFunctionDefinitions.cpp
+++ branches/weapon-creation/MTA10_Server/mods/deathmatch/logic/lua/CLuaFunctionDefinitions.cpp
@@ -4228,7 +4228,7 @@ int CLuaFunctionDefinitions::SetVehicleSirens( lua_State* luaVM )
 
     argStream.ReadUserData ( pVehicle );
     argStream.ReadNumber ( ucSirenID );
-    if ( ucSirenID > 0 )
+    if ( ucSirenID > 0 && ucSirenID < 8 )
     {
         // Array indicies start at 0 so compensate here. This way all code works properly and we get nice 1-8 numbers for API
         ucSirenID--;
