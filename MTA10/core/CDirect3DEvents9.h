d5bdc45276787961e8c4853c971c2fa5334c6c50
ccw808 <ccw808@googlemail.com>
2012-09-01 16:40:36 +0000

    Fixed #6531 (Crash when taking a screenshot and minimizing then restoring)

--- MTA10/core/CDirect3DEvents9.h
+++ MTA10/core/CDirect3DEvents9.h
@@ -41,6 +41,7 @@ class CDirect3DEvents9
     static HRESULT      SetStreamSource             ( IDirect3DDevice9 *pDevice, UINT StreamNumber,IDirect3DVertexBuffer9* pStreamData,UINT OffsetInBytes,UINT Stride );
     static HRESULT      CreateVertexDeclaration     ( IDirect3DDevice9 *pDevice, CONST D3DVERTEXELEMENT9* pVertexElements, IDirect3DVertexDeclaration9** ppDecl );
     static HRESULT      SetVertexDeclaration        ( IDirect3DDevice9 *pDevice, IDirect3DVertexDeclaration9* pDecl );
+    static void         TidyScreenShotSave          ( bool bWait );
 
     static IDirect3DVertexBuffer9*  GetRealVertexBuffer ( IDirect3DVertexBuffer9* pVertexBuffer );
     static IDirect3DIndexBuffer9*   GetRealIndexBuffer  ( IDirect3DIndexBuffer9* pIndexBuffer );
