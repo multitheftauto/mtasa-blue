96e484cf643b7b70b2aa1c65604939f0d1ea929f
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2012-09-02 19:23:05 +0000

    [1.3.1] Updated version number

--- branches/1.3.1/MTA10_Server/version.h
+++ branches/1.3.1/MTA10_Server/version.h
@@ -17,7 +17,7 @@
 // New version info
 #define MTASA_VERSION_MAJOR         1
 #define MTASA_VERSION_MINOR         3
-#define MTASA_VERSION_MAINTENANCE   0
+#define MTASA_VERSION_MAINTENANCE   1
 #define MTASA_VERSION_TYPE          VERSION_TYPE_RELEASE
 #define MTASA_VERSION_BUILD         0
 
