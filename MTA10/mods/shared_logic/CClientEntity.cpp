65a97221507fb5bd2781babbce997bd1f53c812a
ccw808 <ccw808@googlemail.com>
2012-09-16 05:42:42 +0000

    Fixed setMatrix and getMatrix returning false for streamed out objects

--- MTA10/mods/shared_logic/CClientEntity.cpp
+++ MTA10/mods/shared_logic/CClientEntity.cpp
@@ -558,10 +558,16 @@ bool CClientEntity::GetMatrix ( CMatrix& matrix ) const
     CEntity* pEntity = const_cast < CEntity* > ( GetGameEntity () );
     if ( pEntity )
     {
-        if ( pEntity->GetMatrix ( &matrix ) ) return true;
+        if ( pEntity->GetMatrix ( &matrix ) )
+            return true;
     }
 
-    return false;
+    // When streamed out
+    CVector vecRotation;
+    GetRotationRadians ( vecRotation );
+    g_pMultiplayer->ConvertEulerAnglesToMatrix ( matrix, vecRotation.fX, vecRotation.fY, vecRotation.fZ );
+    GetPosition ( matrix.vPos );
+    return true;
 }
 
 
@@ -574,7 +580,12 @@ bool CClientEntity::SetMatrix ( const CMatrix& matrix )
         return true;
     }
 
-    return false;
+    // When streamed out
+    SetPosition ( matrix.vPos );
+    CVector vecRotation;
+    g_pMultiplayer->ConvertMatrixToEulerAngles ( matrix, vecRotation.fX, vecRotation.fY, vecRotation.fZ );
+    SetRotationRadians ( vecRotation );
+    return true;
 }
 
 
@@ -592,6 +603,29 @@ void CClientEntity::SetPositionRelative ( CClientEntity * pOrigin, const CVector
     pOrigin->GetPosition ( vecOrigin );
     SetPosition ( vecOrigin + vecPosition );
 }
+
+void CClientEntity::GetRotationRadians ( CVector& vecOutRadians ) const
+{
+    vecOutRadians = CVector ();
+}
+
+void CClientEntity::GetRotationDegrees ( CVector& vecOutDegrees ) const
+{
+    GetRotationRadians ( vecOutDegrees );
+    ConvertRadiansToDegrees ( vecOutDegrees );
+}
+
+void CClientEntity::SetRotationRadians ( const CVector& vecRadians )
+{
+}
+
+void CClientEntity::SetRotationDegrees ( const CVector& vecDegrees )
+{
+    CVector vecTemp = vecDegrees;
+    ConvertDegreesToRadians ( vecTemp );
+    SetRotationRadians ( vecTemp );
+}
+
 
 bool CClientEntity::IsOutOfBounds ( void )
 {
