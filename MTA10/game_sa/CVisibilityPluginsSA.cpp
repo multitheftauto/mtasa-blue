ea3d0b93c9ecf6aa020aa0c171e093363db1ed0f
ccw808 <ccw808@googlemail.com>
2013-01-22 16:09:22 +0000

    Fixed #7466 (setVehicleComponentVisible was bugged)

--- MTA10/game_sa/CVisibilityPluginsSA.cpp
+++ MTA10/game_sa/CVisibilityPluginsSA.cpp
@@ -23,3 +23,31 @@ void CVisibilityPluginsSA::SetClumpAlpha ( RpClump * pClump, int iAlpha )
         add     esp, 0x8
     }
 }
+
+// Some AtomicId bits are:
+//      0x0001 - Has two versions, and this is the undamaged one
+//      0x0002 - Has two versions, and this is the damaged one
+//
+// Some AtomicId bits might be:
+//      0x0004 - Left door?
+//      0x0008 - Right door? (or mirrored?)
+//      0x0010 - Front part?
+//      0x0020 - Rear part?
+//      0x0040 - Has alpha?
+//      0x0080 - Some door flag?
+//      0x0100 - Front door flag?
+//      0x0200 - Rear door flag?
+//      0x8000 - Some door flag?
+int CVisibilityPluginsSA::GetAtomicId ( RwObject * pAtomic )
+{
+    DWORD dwFunc = FUNC_CVisibilityPlugins_GetAtomicId;
+    int iResult = 0;
+    _asm
+    {
+        push    pAtomic
+        call    dwFunc
+        add     esp, 0x4
+        mov     iResult, eax
+    }
+    return iResult;
+}
