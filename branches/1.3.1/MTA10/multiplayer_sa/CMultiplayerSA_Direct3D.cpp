4aad069c751625a1eb5f86419438a293e4a259e2
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2013-02-06 00:10:25 +0000

    [1.3.1] Merged revision(s) 4988 from trunk: Changed D3D hook system to avoid graphic driver conflicts.

---
+++ branches/1.3.1/MTA10/multiplayer_sa/CMultiplayerSA_Direct3D.cpp
@@ -0,0 +1,133 @@
+/*****************************************************************************
+*
+*  PROJECT:     Multi Theft Auto v1.0
+*  LICENSE:     See LICENSE in the top level directory
+*  FILE:        multiplayer_sa/CMultiplayerSA_Direct3D.cpp
+*
+*  Multi Theft Auto is available from http://www.multitheftauto.com/
+*
+*****************************************************************************/
+
+#include "StdInc.h"
+extern CCoreInterface* g_pCore;
+
+namespace
+{
+    IDirect3D9* ms_pDirect3D = NULL;
+    UINT ms_Adapter = 0;
+    IDirect3DDevice9** ms_ppReturnedDeviceInterface = NULL;
+    D3DPRESENT_PARAMETERS* ms_pPresentationParameters = NULL;
+    HWND ms_hFocusWindow = NULL;
+}
+
+////////////////////////////////////////////////////////////////
+//
+// OnPreCreateDevice
+//
+// Called before GTA creates the D3D device
+//
+////////////////////////////////////////////////////////////////
+void _cdecl OnPreCreateDevice( IDirect3D9* pDirect3D, UINT Adapter, D3DDEVTYPE DeviceType, HWND hFocusWindow, DWORD* BehaviorFlags, D3DPRESENT_PARAMETERS* pPresentationParameters, IDirect3DDevice9** ppReturnedDeviceInterface )
+{
+    ms_pDirect3D                 = pDirect3D;
+    ms_Adapter                   = Adapter;
+    ms_ppReturnedDeviceInterface = ppReturnedDeviceInterface;
+    ms_pPresentationParameters   = pPresentationParameters;
+    ms_hFocusWindow              = hFocusWindow;
+    g_pCore->OnPreCreateDevice( pDirect3D, Adapter, DeviceType, hFocusWindow, *BehaviorFlags, pPresentationParameters );
+}
+
+// Hook info
+#define HOOKPOS_PreCreateDevice_US          0x07F677A
+#define HOOKPOS_PreCreateDevice_EU          0x07F67BA
+#define HOOKSIZE_PreCreateDevice_US         6
+#define HOOKSIZE_PreCreateDevice_EU         6
+DWORD RETURN_PreCreateDevice_US =           0x07F6781;
+DWORD RETURN_PreCreateDevice_EU =           0x07F67C1;
+DWORD RETURN_PreCreateDevice_BOTH =         0;
+void _declspec(naked) HOOK_PreCreateDevice()
+{
+    _asm
+    {
+        // Replaced code
+        mov     eax, ds:0x0C97C24       // __RwD3DAdapterIndex
+        push    eax
+        push    ecx
+
+        pushad
+        push    [esp+32+4*6]
+        push    [esp+32+4*6]
+        lea     eax,[esp+32+4*6]    // Turn BehaviorFlags into a pointer so we can modify it
+        push    eax
+        push    [esp+32+4*6]
+        push    [esp+32+4*6]
+        push    [esp+32+4*6]
+        push    [esp+32+4*6]
+        call    OnPreCreateDevice
+        add     esp, 4*7
+        popad
+
+        // Continue
+        jmp     RETURN_PreCreateDevice_BOTH
+    }
+}
+
+
+////////////////////////////////////////////////////////////////
+//
+// OnPostCreateDevice
+//
+// Called after GTA creates the D3D device
+//
+////////////////////////////////////////////////////////////////
+void _cdecl OnPostCreateDevice( HRESULT hResult )
+{
+    g_pCore->OnPostCreateDevice( hResult, ms_pDirect3D, ms_Adapter, ms_hFocusWindow, ms_pPresentationParameters, ms_ppReturnedDeviceInterface );
+}
+
+// Hook info
+#define HOOKPOS_PostCreateDevice_US         0x07F6784
+#define HOOKPOS_PostCreateDevice_EU         0x07F67C4
+#define HOOKSIZE_PostCreateDevice_US        6
+#define HOOKSIZE_PostCreateDevice_EU        6
+DWORD RETURN_PostCreateDevice_US =          0x07F678A;
+DWORD RETURN_PostCreateDevice_EU =          0x07F67CA;
+DWORD RETURN_PostCreateDevice_BOTH =        0;
+DWORD RETURN_PostCreateDeviceB_US =         0x07F6799;
+DWORD RETURN_PostCreateDeviceB_EU =         0x07F67D9;
+DWORD RETURN_PostCreateDeviceB_BOTH =       0;
+void _declspec(naked) HOOK_PostCreateDevice()
+{
+    _asm
+    {
+        // Replaced code
+        pushad
+        push    eax
+        call    OnPostCreateDevice
+        add     esp, 4*1
+        popad
+
+        // Continue
+        test    eax, eax
+        jge     ok
+        xor     eax, eax
+        jmp     RETURN_PostCreateDevice_BOTH
+
+ok:
+        jmp     RETURN_PostCreateDeviceB_BOTH
+    }
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//
+// CMultiplayerSA::InitHooks_Direct3D
+//
+// Setup hook
+//
+//////////////////////////////////////////////////////////////////////////////////////////
+void CMultiplayerSA::InitHooks_Direct3D ( void )
+{
+    EZHookInstall ( PreCreateDevice );
+    EZHookInstall ( PostCreateDevice );
+}
