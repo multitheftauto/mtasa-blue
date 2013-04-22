5b84768d41d60766ba521479015dc2f034390f0a
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2013-04-22 20:46:19 +0000

    Added dump file encryption for the client

--- trunk/MTA10/core/CCore.h
+++ trunk/MTA10/core/CCore.h
@@ -228,6 +228,7 @@ public:
     void                    RequestNewNickOnStart           ( void ) { m_bWaitToSetNick = true; };
     bool                    WillRequestNewNickOnStart       ( void ) { return m_bWaitToSetNick; };
     bool                    WasLaunchedWithConnectURI       ( void );
+    void                    HandleCrashDumpEncryption       ( void );
 
     //XFire
     SString                 UpdateXfire                     ( void );
