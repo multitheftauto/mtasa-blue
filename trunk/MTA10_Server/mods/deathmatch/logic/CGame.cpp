af64cb9ac30616ff5026bf76eeaef09bd07a7ee3
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2013-05-02 20:37:04 +0000

    Reverted r5151 (Fixed #7570 (warpPedIntoVehicle after cancelEvent() of onVehicleStartEnter causes network trouble)) due to possible issues

--- trunk/MTA10_Server/mods/deathmatch/logic/CGame.cpp
+++ trunk/MTA10_Server/mods/deathmatch/logic/CGame.cpp
@@ -2828,14 +2828,9 @@ void CGame::Packet_Vehicle_InOut ( CVehicleInOutPacket& Packet )
                                                     }
                                                     else
                                                     {
-                                                        // Has he been warped to a vehicle in the mean time? (by lua f.e.)
-                                                        if ( !pPlayer->GetOccupiedVehicle () )
-                                                        {
-                                                            pPlayer->SetOccupiedVehicle ( NULL, 0 );
-                                                            pVehicle->SetOccupant ( NULL, 0 );
-                                                        }
-                                                        
+                                                        pPlayer->SetOccupiedVehicle ( NULL, 0 );
                                                         pPlayer->SetVehicleAction ( CPlayer::VEHICLEACTION_NONE );
+                                                        pVehicle->SetOccupant ( NULL, 0 );
                                                         failReason = FAIL_SCRIPT;
                                                     }
                                                 }
@@ -2939,13 +2934,9 @@ void CGame::Packet_Vehicle_InOut ( CVehicleInOutPacket& Packet )
                                                     }
                                                     else
                                                     {
-                                                        // Has he been warped to a vehicle in the mean time? (by lua f.e.)
-                                                        if ( !pPlayer->GetOccupiedVehicle () )
-                                                        {
-                                                            pPlayer->SetOccupiedVehicle ( NULL, 0 );
-                                                            pVehicle->SetOccupant ( NULL, ucSeat );
-                                                        }
+                                                        pPlayer->SetOccupiedVehicle ( NULL, 0 );
                                                         pPlayer->SetVehicleAction ( CPlayer::VEHICLEACTION_NONE );
+                                                        pVehicle->SetOccupant ( NULL, ucSeat );
                                                         failReason = FAIL_SCRIPT;
                                                     }
                                                 }
