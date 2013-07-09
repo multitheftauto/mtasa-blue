2197e5c29857a9f0eee5c2ec8a602883b374ae60
Cazomino05 <Cazomino05@gmail.com>
2013-07-09 15:21:05 +0000

    Add missing files and fixed compile errors (cherry picked from commit e2c5fcb83fa7df633f040dc6b0c048e2cd72bbb3)

---
+++ MTA10/sdk/game/CTrainTrack.h
@@ -0,0 +1,35 @@
+/*****************************************************************************
+*
+*  PROJECT:     Multi Theft Auto v1.0
+*  LICENSE:     See LICENSE in the top level directory
+*  FILE:        sdk/game/CTrackTrackManager.h
+*  PURPOSE:     Train Track Interface
+
+*  DEVELOPERS:  Cazomino05 < Cazomino05@gmail.com >
+*
+*  Multi Theft Auto is available from http://www.multitheftauto.com/
+*
+*****************************************************************************/
+
+#ifndef __CTRAINTRACK_H
+#define __CTRAINTRACK_H
+
+class CTrainTrack
+{
+public:
+    virtual bool                            SetRailNodePosition             ( unsigned int uiNode, CVector vecPosition ) = 0;
+    virtual bool                            GetRailNodePosition             ( unsigned int uiNode, CVector& vecPosition ) = 0;
+
+    virtual bool                            SetTrackLength                  ( float fLength ) = 0;
+    virtual float                           GetTrackLength                  ( void ) = 0;
+
+    virtual bool                            SetNumberOfNodes                ( unsigned int uiNumberOfNodes ) = 0;
+    virtual unsigned int                    GetNumberOfNodes                ( void ) = 0;
+
+    virtual unsigned char                   GetTrackID                      ( void ) = 0;
+
+    virtual void                            SetLastNodesLinked              ( bool bLinked ) = 0;
+
+};
+
+#endif
