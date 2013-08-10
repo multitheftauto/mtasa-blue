6f6137cc23175e0adf996074082ceee6d19acabf
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2013-08-10 02:29:23 +0000

    Fixed Binds joystick detection for some weirdo drivers

--- trunk/MTA10/core/CProxyDirectInputDevice8.h
+++ trunk/MTA10/core/CProxyDirectInputDevice8.h
@@ -58,8 +58,6 @@ class CProxyDirectInputDevice8 : public IDirectInputDevice8A
     virtual HRESULT __stdcall    SetActionMap               ( LPDIACTIONFORMATA a, LPCSTR b, DWORD c );
     virtual HRESULT __stdcall    GetImageInfo               ( LPDIDEVICEIMAGEINFOHEADERA a);
 
-    private:
-
     IDirectInputDevice8A *   m_pDevice;
     bool                     m_bDropDataIfInputGoesToGUI;
 };
