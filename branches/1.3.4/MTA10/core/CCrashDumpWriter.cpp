3d74b1f50cebd023c2b3573435e49d99b5b26041
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2013-11-17 06:00:07 +0000

    [1.3.4] Merged revision(s) 5930 from trunk: Fixed a server exit crash. Tweaked client launcher trouble detection.

--- branches/1.3.4/MTA10/core/CCrashDumpWriter.cpp
+++ branches/1.3.4/MTA10/core/CCrashDumpWriter.cpp
@@ -145,6 +145,9 @@ long WINAPI CCrashDumpWriter::HandleExceptionGlobal ( _EXCEPTION_POINTERS* pExce
 
 void CCrashDumpWriter::DumpCoreLog ( CExceptionInformation* pExceptionInformation )
 {
+    // Write crash flag for next launch (Simple flag in case of double faults later)
+    fclose( fopen ( CalcMTASAPath ( "mta\\core.log.flag" ), "w" ) );
+
     // Write a log with the generic exception information
     FILE* pFile = fopen ( CalcMTASAPath ( "mta\\core.log" ), "a+" );
     if ( pFile )
