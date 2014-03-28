56dc26171eacfdc7587414284b9b3b2377848fce
jusonex96@gmail.com <jusonex96@gmail.com@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2014-03-28 16:31:06 +0000

    Fixed not working tab key for scrollpanes and staticimages (to switch between edits and memos)

--- trunk/MTA10/gui/CGUIScrollPane_Impl.h
+++ trunk/MTA10/gui/CGUIScrollPane_Impl.h
@@ -17,7 +17,7 @@
 #include <gui/CGUIScrollPane.h>
 #include "CGUIElement_Impl.h"
 
-class CGUIScrollPane_Impl : public CGUIScrollPane, public CGUIElement_Impl
+class CGUIScrollPane_Impl : public CGUIScrollPane, public CGUIElement_Impl, public CGUITabList
 {
 public:
                             CGUIScrollPane_Impl         ( class CGUI_Impl* pGUI, CGUIElement* pParent = NULL );
