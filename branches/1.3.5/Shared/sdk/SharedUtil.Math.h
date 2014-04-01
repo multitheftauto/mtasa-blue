f7eee0d7104f47cd9cb6cb33cceb90be00d487f6
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2014-04-01 04:21:05 +0000

    [1.3.5] Merged revision(s) 6273 from trunk: Fixed #8128 (Integer data change when sending it between server and client)

--- branches/1.3.5/Shared/sdk/SharedUtil.Math.h
+++ branches/1.3.5/Shared/sdk/SharedUtil.Math.h
@@ -31,8 +31,32 @@ namespace SharedUtil
     // Remove possible crap after casting a float to a double
     inline double RoundFromFloatSource( double dValue )
     {
-        double dFactor = pow( 10.0, 7 - ceil( log10( fabs( dValue ) ) ) );
+        double dFactor = pow( 10.0, 8 - ceil( log10( fabs( dValue ) ) ) );
         return floor( dValue * dFactor + 0.5 ) / dFactor;
+    }
+
+    // Determine if value would be better as an int or float.
+    // piNumber is set if result is true
+    inline bool ShouldUseInt( double dValue, int* piNumber )
+    {
+        if ( dValue > -0x1000000 && dValue < 0x1000000 )
+        {
+            // float more accurate with this range, but check if we can use int as it is better for compressing
+            *piNumber = static_cast < int > ( dValue );
+            return ( dValue == *piNumber );
+        }
+        else
+        if ( dValue >= -0x7FFFFFFF && dValue <= 0x7FFFFFFF )
+        {
+            // int more accurate with this range
+            *piNumber = Round( dValue );
+            return true;
+        }
+        else
+        {
+            // Value too large for int
+            return false;
+        }
     }
 
     inline float DegreesToRadians( float fValue )
