3f6382f8f5706137c9758658c495ae3e53d6dd76
ccw808 <ccw808@googlemail.com>
2014-04-23 22:15:32 +0000

    Fixed #8174 (nil/false in Lua to NULL in SQL) - Needs testing

--- MTA10_Server/mods/deathmatch/logic/CDatabaseTypeMySql.cpp
+++ MTA10_Server/mods/deathmatch/logic/CDatabaseTypeMySql.cpp
@@ -341,6 +341,12 @@ SString InsertQueryArgumentsMySql ( const SString& strQuery, CLuaArguments* pArg
                 if ( !bUnquotedStrings ) strParsedQuery += '\'';
             }
             else
+            if ( type == LUA_TNIL )
+            {
+                // Nil becomes NULL
+                strParsedQuery += "NULL";
+            }
+            else
             {
                 // If we don't have any content, put just output 2 quotes to indicate an empty variable
                 strParsedQuery += "\'\'";
