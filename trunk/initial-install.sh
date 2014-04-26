7b8de8091d267d6e4c9112c39d7af589b0e5713c
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2014-04-26 20:06:10 +0000

    Changed Linux build script

--- trunk/initial-install.sh
+++ trunk/initial-install.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+# Remove previous build files
+make clean
+
 # generate autotools build system files
 autoreconf -fiv
 
@@ -9,12 +12,19 @@ autoreconf -fiv
 # on a 64bit system, while there's no 64bit net.so
 # and probably some unknown issues you should compile and link
 # 32bit objects like so:
-export PKG_CONFIG_PATH=/usr/lib32/pkgconfig
-./configure LDFLAGS="-m32" CPPFLAGS="-m32" CFLAGS="-m32" CXXFLAGS="-m32" --enable-silent-rules $@
+#export PKG_CONFIG_PATH=/usr/lib32/pkgconfig
+#./configure LDFLAGS="-m32" CPPFLAGS="-m32" CFLAGS="-m32" CXXFLAGS="-m32" --enable-silent-rules $@
+
+./configure CFLAGS='-O3 -fPIC -DPIC' CXXFLAGS='-O3 -fPIC -DPIC' CPPFLAGS='-O3 -fPIC -DPIC' --with-pic --disable-system-pcre
+
+# Remove previous build files #2
+make clean
+rm ./MTA10_Server/core/core.la
+rm ./MTA10_Server/dbconmy/dbconmy.la
+rm ./MTA10_Server/mods/deathmatch/deathmatch.la
+rm ./Shared/XML/xmll.la
+rm ./MTA10_Server/net/net.la
 
 # then you have makefiles and the source can be compiled :)
 # building in parallel mode (use -j<JOBS>)
-make -j2
-
-# then install mta
-./mta-install.sh
+make CFLAGS='-O3 -fPIC -DPIC' CXXFLAGS='-O3 -fPIC -DPIC' CPPFLAGS='-O3 -fPIC -DPIC' >_make.log
