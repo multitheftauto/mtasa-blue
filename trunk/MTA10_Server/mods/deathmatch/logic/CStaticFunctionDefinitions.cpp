c40da26ed0e855ff1b2d29eadb7753c3e8e7be52
sbx320@sbx320.net <sbx320@sbx320.net@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2014-06-03 18:09:06 +0000

    Fixed a crash when using setElementParent with an element without parent as new parent

--- trunk/MTA10_Server/mods/deathmatch/logic/CStaticFunctionDefinitions.cpp
+++ trunk/MTA10_Server/mods/deathmatch/logic/CStaticFunctionDefinitions.cpp
@@ -1136,7 +1136,7 @@ bool CStaticFunctionDefinitions::SetElementParent ( CElement* pElement, CElement
     CElement * pTemp = pParent;
     CElement * pRoot = m_pMapManager->GetRootElement();
     bool bValidParent = false;
-    while ( pTemp != pRoot )
+    while ( pTemp != pRoot && pTemp != NULL)
     {
         const char * szTypeName = pTemp->GetTypeName ().c_str ();
         if ( szTypeName && strcmp(szTypeName, "map") == 0 )
