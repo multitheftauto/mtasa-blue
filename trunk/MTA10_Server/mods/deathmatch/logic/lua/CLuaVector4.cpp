f78751fe3747cf6a5e784524adfdbe39fee94db3
Cazomino05 <Cazomino05@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2014-06-07 13:38:42 +0000

    Added missing vector classes from r6512 in the server.

---
+++ trunk/MTA10_Server/mods/deathmatch/logic/lua/CLuaVector4.cpp
@@ -0,0 +1,43 @@
+/*****************************************************************************
+*
+*  PROJECT:     Multi Theft Auto v1.0
+*  LICENSE:     See LICENSE in the top level directory
+*  FILE:        mods/shared_logic/logic/lua/CLuaVector4.cpp
+*  PURPOSE:     Lua vector4 class
+*  DEVELOPERS:  Stanislav Bobrov <lil_toady@hotmail.com>
+*
+*  Multi Theft Auto is available from http://www.multitheftauto.com/
+*
+*****************************************************************************/
+
+#include <StdInc.h>
+
+CLuaVector4D::CLuaVector4D ( void ) :
+    CVector ()
+{
+    m_uiScriptID = CIdArray::PopUniqueId ( this, EIdClass::VECTOR4 );
+}
+
+CLuaVector4D::CLuaVector4D ( float fX, float fY, float fZ ) :
+    CVector ( fX, fY, fZ )
+{
+    m_uiScriptID = CIdArray::PopUniqueId ( this, EIdClass::VECTOR4 );
+}
+
+CLuaVector4D::CLuaVector4D ( const CVector & vector ) :
+    CVector ( vector )
+{
+    m_uiScriptID = CIdArray::PopUniqueId ( this, EIdClass::VECTOR4 );
+}
+
+CLuaVector4D::~CLuaVector4D ( void )
+{
+    CIdArray::PushUniqueId ( this, EIdClass::VECTOR4, m_uiScriptID );
+    m_uiScriptID = INVALID_ARRAY_ID;
+}
+
+CLuaVector4D* CLuaVector4D::GetFromScriptID ( unsigned int uiScriptID )
+{
+    CLuaVector4D* pLuaVector = ( CLuaVector4D* ) CIdArray::FindEntry ( uiScriptID, EIdClass::VECTOR4 );
+    return pLuaVector;
+}
