5f76627de2ee6082625ed2f22ef05aa49208f64f
Cazomino05 <Cazomino05@gmail.com>
2014-06-07 13:38:42 +0000

    Added missing vector classes from r6512 in the server.

---
+++ MTA10_Server/mods/deathmatch/logic/lua/CLuaVector4.h
@@ -0,0 +1,36 @@
+/*****************************************************************************
+*
+*  PROJECT:     Multi Theft Auto v1.0
+*  LICENSE:     See LICENSE in the top level directory
+*  FILE:        mods/shared_logic/logic/lua/CLuaVector4.h
+*  PURPOSE:     Lua vector class
+*  DEVELOPERS:  Stanislav Bobrov <lil_toady@hotmail.com>
+*
+*  Multi Theft Auto is available from http://www.multitheftauto.com/
+*
+*****************************************************************************/
+
+#ifndef __CLUAVECTOR4_H
+#define __CLUAVECTOR4_H
+
+class CLuaVector4D : public CVector
+{
+public:
+
+                            CLuaVector4D        ( void );
+                            CLuaVector4D        ( const CVector & vector );
+                            CLuaVector4D        ( float fX, float fY, float fZ );
+
+                            ~CLuaVector4D       ( void );
+
+    unsigned int            GetScriptID         ( void ) const                      { return m_uiScriptID; }
+
+    static CLuaVector4D*    GetFromScriptID     ( unsigned int uiScriptID );
+
+private:
+
+    unsigned int    m_uiScriptID;
+
+};
+
+#endif
