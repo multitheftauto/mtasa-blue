ad49bfc91470e077e06f58025687a7f5703a508a
Cazomino05 <Cazomino05@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2014-06-13 23:05:29 +0000

    Added server side Element.rotation set property

--- trunk/MTA10_Server/mods/deathmatch/logic/lua/oopdefs/CLuaOOPFunctionDefs.Element.cpp
+++ trunk/MTA10_Server/mods/deathmatch/logic/lua/oopdefs/CLuaOOPFunctionDefs.Element.cpp
@@ -80,3 +80,49 @@ int CLuaOOPDefs::GetElementRotation ( lua_State* luaVM )
     return 1;
 }
 
+int CLuaOOPDefs::SetElementRotation ( lua_State* luaVM )
+{
+    CElement* pElement = NULL;
+    CVector vecRotation;
+    SString strRotationOrder;
+    bool bNewWay;
+    CScriptArgReader argStream ( luaVM );
+    argStream.ReadUserData ( pElement );
+    argStream.ReadVector3D ( vecRotation );
+    /*argStream.ReadString ( strRotationOrder, "default" );
+    argStream.ReadBool ( bNewWay, false );*/
+
+    if ( !argStream.HasErrors ( ) )
+    {
+        CMatrix matrix;
+
+        // fill in our matrix
+        pElement->GetMatrix ( matrix );
+
+        // degrees to radians
+        ConvertDegreesToRadians ( vecRotation );
+
+        // set the matrix rotation (takes radians)
+        matrix.SetRotation ( vecRotation );
+
+        // set the element matrix using the element specific SetMatrix function
+        pElement->SetMatrix ( matrix );
+
+        // get the rotation (outputs radians)
+        pElement->GetRotation( vecRotation );
+
+        // convert radians to degrees
+        ConvertRadiansToDegrees ( vecRotation );
+
+        if ( CStaticFunctionDefinitions::SetElementRotation ( pElement, vecRotation, strRotationOrder, bNewWay ) )
+        {
+            lua_pushboolean ( luaVM, true );
+            return 1;
+        }
+    }
+    else
+        m_pScriptDebugging->LogCustom ( luaVM, argStream.GetFullErrorMessage ( ) );
+
+    lua_pushboolean ( luaVM, false );
+    return 1;
+}
