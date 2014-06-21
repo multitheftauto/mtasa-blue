fd4cc3fe2ef65b9b9961c67fdb9e6631ff78d13f
Cazomino05 <Cazomino05@gmail.com>
2014-06-21 16:30:46 +0000

    Fixed an issue with createBlip where it would wrap around if used like the wiki example (using 999999 visible to distance) and would therefore set the visible distance to garbage.

--- MTA10_Server/mods/deathmatch/logic/packets/CEntityAddPacket.cpp
+++ MTA10_Server/mods/deathmatch/logic/packets/CEntityAddPacket.cpp
@@ -711,8 +711,8 @@ bool CEntityAddPacket::Write ( NetBitStreamInterface& BitStream ) const
                     // Write the ordering id
                     BitStream.WriteCompressed ( pBlip->m_sOrdering );
 
-                    // Write the visible distance
-                    SIntegerSync < unsigned short, 14 > visibleDistance ( pBlip->m_usVisibleDistance );
+                    // Write the visible distance - 14 bits allows 16383.
+                    SIntegerSync < unsigned short, 14 > visibleDistance ( Min ( pBlip->m_usVisibleDistance, (unsigned short)16383 ) );
                     BitStream.Write ( &visibleDistance );
 
                     // Write the icon
