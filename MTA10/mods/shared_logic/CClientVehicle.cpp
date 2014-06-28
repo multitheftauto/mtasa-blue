8827db2bb034034cbce9a82d3a6f0008b4349a0d
Cazomino05 <Cazomino05@gmail.com>
2014-06-28 09:11:13 +0000

    Fixed fbi truck and fbi rancher police radio, fixed resuming when switching from radio off.

--- MTA10/mods/shared_logic/CClientVehicle.cpp
+++ MTA10/mods/shared_logic/CClientVehicle.cpp
@@ -3859,6 +3859,8 @@ bool CClientVehicle::HasPoliceRadio ( void )
         case VT_POLMAV:
         case VT_COPBIKE:
         case VT_SWATVAN:
+        case VT_FBIRANCH:
+        case VT_FBITRUCK:
             return true;
             break;
         default:
