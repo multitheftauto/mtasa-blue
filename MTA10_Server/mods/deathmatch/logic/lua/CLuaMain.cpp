4c18e17befb0d1302f8ef67a89c0ab564a6d3fbf
Qais Patankar <qaisjp@gmail.com>
2014-07-12 05:12:52 +0000

    OOP: Fixed account:getFromPlayer(p) incorrect mapping typo
    Thanks MarioKart!

--- MTA10_Server/mods/deathmatch/logic/lua/CLuaMain.cpp
+++ MTA10_Server/mods/deathmatch/logic/lua/CLuaMain.cpp
@@ -503,7 +503,7 @@ void CLuaMain::AddAccountClass ( lua_State* luaVM )
     
     lua_classfunction ( luaVM, "getAll", "getAccounts" );
     lua_classfunction ( luaVM, "getAllBySerial", "getAccountsBySerial" );
-    lua_classfunction ( luaVM, "getFromPlayer", "getPlayerAccount" );
+    lua_classfunction ( luaVM, "getFromPlayer", "getAccountPlayer" );
     lua_classfunction ( luaVM, "logPlayerOut", "logOut" );
 
     lua_classfunction ( luaVM, "create", "getAccount" );
