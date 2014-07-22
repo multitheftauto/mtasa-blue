324f97d73f9aa2ac61172ca16790516c3e7fae10
ccw808 <ccw808@googlemail.com>
2014-07-22 19:52:17 +0000

    Vehicle freeze test

--- MTA10/mods/shared_logic/CClientVehicle.h
+++ MTA10/mods/shared_logic/CClientVehicle.h
@@ -133,7 +133,7 @@ public:
 
     void                        GetPosition             ( CVector& vecPosition ) const;
     void                        SetPosition             ( const CVector& vecPosition )      { SetPosition ( vecPosition, true ); }
-    void                        SetPosition             ( const CVector& vecPosition, bool bResetInterpolation );
+    void                        SetPosition             ( const CVector& vecPosition, bool bResetInterpolation, bool bAllowGroundLoadFreeze = true );
 
     void                        UpdatePedPositions      ( const CVector& vecPosition );
 
