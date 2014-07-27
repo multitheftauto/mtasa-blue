e9b3e138213094bb4fe8e04ea6e18267de398b3c
Cazomino05 <Cazomino05@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2014-07-27 13:39:18 +0000

    [LuaJIT] Sped up server ACL hooks Test code run local t = getTickCount() for i=1,5e6 do getServerPort() end outputServerLog( getTickCount()-t )
    Before:
    368
    After:
    278

--- branches/LuaJIT/MTA10_Server/mods/deathmatch/logic/luadefs/CLuaDefs.cpp
+++ branches/LuaJIT/MTA10_Server/mods/deathmatch/logic/luadefs/CLuaDefs.cpp
@@ -141,12 +141,29 @@ int CLuaDefs::CanUseFunction ( lua_CFunction f, lua_State* luaVM )
     }
 
     // Get associated resource
-    CResource* pResource = m_pResourceManager->GetResourceFromLuaState( luaVM );
-    if ( !pResource )
+    CLuaMain* pLuaMain = g_pGame->GetScriptDebugging()->GetTopLuaMain();
+    if ( !pLuaMain )
         return true;
 
+    CResource * pResource = pLuaMain->GetResource ( );
+
+    // just double check what we have is right
+    if ( pResource->GetVirtualMachine ( )->GetVM ( ) != luaVM )
+    {
+        // probably coroutine so revert to old code
+        // This does happen, see admin resource for a reliable way to reproduce this section of code
+
+        // get the resource from the Lua state
+        pResource = m_pResourceManager->GetResourceFromLuaState ( luaVM );
+        if ( pResource != NULL )
+        {
+            // update our lua main
+            pLuaMain = pResource->GetVirtualMachine ( );
+        }
+    }
+
     // Update execution time check
-    pResource->GetVirtualMachine()->CheckExecutionTime();
+    pLuaMain->CheckExecutionTime();
 
     // Check function right cache in resource
     bool bAllowed;
