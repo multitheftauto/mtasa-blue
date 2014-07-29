ca568814031421ea33a061046a2f6e2c5634c6dd
ccw808 <ccw808@googlemail.com>
2014-07-29 01:00:35 +0000

    Updated translation build scripts #3

--- utils/pull_pootle_catalogs.py
+++ utils/pull_pootle_catalogs.py
@@ -32,14 +32,14 @@ def myurlopen(url,urltimeout):
     return content
 
 # urlopen with over complex handling of connection issues
-def myurlopenDeluxe(url,urltimeout):
+def myurlopenDeluxe(url,urltimeout,attempts):
     print ( "Trying '%s'"%(url) )
-    for x in range(0, 3):
+    for x in range(0, attempts):
         # Get file first
         content = myurlopen(url,urltimeout)
         if ( len(content) > 0 ):
             print ( "content size %d"%(len(content)) )
-            for y in range(0, 3):
+            for y in range(0, attempts):
                 # Get file second
                 content2 = myurlopen(url,urltimeout)
                 if ( len(content2) > 0 ):
@@ -77,6 +77,10 @@ parser.add_option("-r", "--rmdir",action="store_true", dest="rmdir", default=Fal
             help="Clear the output directory before starting")
 parser.add_option("-t", "--timeout",type="float", dest="urltimeout", default=30,
             help="HTTP fetch timeout")
+parser.add_option("-i", "--index",type="int", dest="langindex", default=-1,
+            help="If set, only do one language. Returns 2 if index out of range")
+parser.add_option("-a", "--attempts",type="int", dest="attempts", default=3,
+            help="Number of download retry attempts before giving up")
 
 (options, args) = parser.parse_args()
 
@@ -87,8 +91,18 @@ if options.rmdir:
         shutil.rmtree(options.output)
     print ( "Cleared output directory: '%s'"%(options.output) )
 
-# Let's split our languages string into a Python list, and loop it
-for lang in (options.languages).replace(" ","").split(","):
+# Let's split our languages string into a Python list
+langlist = (options.languages).replace(" ","").split(",")
+
+# Select one language if required
+if options.langindex > -1:
+    if options.langindex >= len(langlist):
+        print ( "Index out of range" )
+        sys.exit(2)
+    langlist = [langlist[options.langindex]]
+
+# Loop over the list
+for lang in (langlist):
     # Let's create our full Pootle export URL depending on whether we're GNU or not
     url = ""
     output = ""
@@ -99,7 +113,7 @@ for lang in (options.languages).replace(" ","").split(","):
         url = urlparse.urljoin(options.url, "export/%s/%s/%s.po"%(options.project,lang,options.project))
         output = os.path.join(options.output,"%s/%s.po"%(lang,options.project))
 
-    content = myurlopenDeluxe(url,options.urltimeout)
+    content = myurlopenDeluxe(url,options.urltimeout,options.attempts)
 
     path,tail = os.path.split(output)
     if ( not os.path.exists(path) ):
