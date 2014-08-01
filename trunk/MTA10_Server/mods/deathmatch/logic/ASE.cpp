be58f383a71640ebc7b8143bcc9f191232b4ec4e
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2014-08-01 13:34:32 +0000

    Updated ASE protocol datum

--- trunk/MTA10_Server/mods/deathmatch/logic/ASE.cpp
+++ trunk/MTA10_Server/mods/deathmatch/logic/ASE.cpp
@@ -27,6 +27,7 @@ ASE::ASE ( CMainConfig* pMainConfig, CPlayerManager* pPlayerManager, unsigned sh
     : m_QueryDosProtect( 5, 6000, 7000 )        // Max of 5 queries per 6 seconds, then 7 second ignore
 {
     _instance = this;
+    m_tStartTime = time( NULL );
 
     m_usPortBase = usPort;
 
@@ -410,6 +411,7 @@ std::string ASE::QueryLight ( void )
     g_pNetServer->GetNetRoute ( &strNetRouteFixed );
     SString strPingStatus = (const char*)strPingStatusFixed;
     SString strNetRoute = (const char*)strNetRouteFixed;
+    SString strUpTime( "%d", (uint)( time( NULL ) - m_tStartTime ) );
 
     reply << "EYE2";
     // game
@@ -425,7 +427,7 @@ std::string ASE::QueryLight ( void )
     reply << ( unsigned char ) ( m_strGameType.length() + 1 );
     reply << m_strGameType;
     // map name with backwardly compatible large player count, build type and build number
-    reply << ( unsigned char ) ( m_strMapName.length() + 1 + strPlayerCount.length () + 1 + strBuildType.length () + 1 + strBuildNumber.length () + 1 + strPingStatus.length () + 1 + strNetRoute.length () + 1 );
+    reply << ( unsigned char ) ( m_strMapName.length() + 1 + strPlayerCount.length () + 1 + strBuildType.length () + 1 + strBuildNumber.length () + 1 + strPingStatus.length () + 1 + strNetRoute.length () + 1 + strUpTime.length() + 1 );
     reply << m_strMapName;
     reply << ( unsigned char ) 0;
     reply << strPlayerCount;
@@ -437,6 +439,8 @@ std::string ASE::QueryLight ( void )
     reply << strPingStatus;
     reply << ( unsigned char ) 0;
     reply << strNetRoute;
+    reply << ( unsigned char ) 0;
+    reply << strUpTime;
     // version
     std::string temp = MTA_DM_ASE_VERSION;
     reply << ( unsigned char ) ( temp.length() + 1 );
