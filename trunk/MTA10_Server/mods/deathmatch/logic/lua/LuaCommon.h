be13503ec181fe8fe85b81937e7decb19ce686cd
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2014-11-13 20:43:30 +0000

    Fixed Linux compile error

--- trunk/MTA10_Server/mods/deathmatch/logic/lua/LuaCommon.h
+++ trunk/MTA10_Server/mods/deathmatch/logic/lua/LuaCommon.h
@@ -88,7 +88,7 @@ enum
 struct SLuaDebugInfo
 {
     SLuaDebugInfo( void ) : iLine( INVALID_LINE_NUMBER ), infoType( DEBUG_INFO_NONE ) {}
-    SLuaDebugInfo( const SString& strFile, int iLine, const SString& strShortSrc = "" ) : strFile( strFile ), iLine( iLine ), strShortSrc( strShortSrc ), infoType( DEBUG_INFO_FILE_AND_LINE ) {}
+    SLuaDebugInfo( const SString& strFile, int iLine, const SString& strShortSrc = "" ) : strFile( strFile ), strShortSrc( strShortSrc ), iLine( iLine ), infoType( DEBUG_INFO_FILE_AND_LINE ) {}
     SString     strFile;
     SString     strShortSrc;
     int         iLine;
