c1c310c933c1a39286f556c4ab1e5f196a0f4f68
ccw808 <ccw808@c8a8ddca-64a2-a3fd-dc76-42152cdbfb0b>
2015-01-28 21:02:29 +0000

    Addendum to r7048 (Applied source patch #8737 (PointLight Creation) by Lex128.)

---
+++ trunk/MTA10/mods/shared_logic/CClientPointLightsManager.cpp
@@ -0,0 +1,105 @@
+/*****************************************************************************
+*
+*  PROJECT:     Multi Theft Auto v1.0
+*               (Shared logic for modifications)
+*  LICENSE:     See LICENSE in the top level directory
+*  FILE:        mods/shared_logic/CClientPointLightsManager.cpp
+*  PURPOSE:     PointLights entity manager class
+*  DEVELOPERS:  Alexander Romanov <lexr128@gmail.com>
+*
+*****************************************************************************/
+
+#include "StdInc.h"
+
+using std::list;
+
+CClientPointLightsManager::CClientPointLightsManager ( CClientManager* pManager )
+{
+    // Init
+    m_pManager = pManager;
+    m_bDontRemoveFromList = false;
+    m_usDimension = 0;
+}
+
+
+CClientPointLightsManager::~CClientPointLightsManager ( void )
+{
+    // Make sure all the lights are deleted
+    DeleteAll ();
+}
+
+
+CClientPointLights* CClientPointLightsManager::Create ( ElementID EntityID )
+{
+    if ( !Get ( EntityID ) )
+    {
+        return new CClientPointLights ( m_pManager, EntityID );
+    }
+
+    return NULL;
+}
+
+
+void CClientPointLightsManager::Delete ( CClientPointLights* pLight )
+{
+    delete pLight;
+}
+
+
+void CClientPointLightsManager::DeleteAll ( void )
+{
+    // Delete all the lights
+    m_bDontRemoveFromList = true;
+    list < CClientPointLights* > ::const_iterator iter = m_List.begin ();
+    for ( ; iter != m_List.end (); iter++ )
+    {
+        delete *iter;
+    }
+    m_bDontRemoveFromList = false;
+
+    // Clear the list
+    m_List.clear ();
+}
+
+CClientPointLights* CClientPointLightsManager::Get ( ElementID ID )
+{
+    // Grab the element with the given id. Check its type.
+    CClientEntity* pEntity = CElementIDs::GetElement ( ID );
+    if ( pEntity && pEntity->GetType () == CCLIENTPOINTLIGHTS )
+    {
+        return static_cast < CClientPointLights* > ( pEntity );
+    }
+
+    return NULL;
+}
+
+
+void CClientPointLightsManager::SetDimension ( unsigned short usDimension )
+{
+    list < CClientPointLights* > ::iterator iter = m_List.begin ();
+    for ( ; iter != m_List.end (); iter++ )
+    {
+        (*iter)->RelateDimension ( usDimension );
+    }
+
+    m_usDimension = usDimension;
+}
+
+
+void CClientPointLightsManager::DoPulse ( void )
+{
+    // Pulse each light
+    list < CClientPointLights* > ::const_iterator iter = m_List.begin ();
+    for ( ; iter != m_List.end (); ++iter )
+    {
+        (*iter)->DoPulse ();
+    }
+}
+
+void CClientPointLightsManager::RemoveFromList ( CClientPointLights* pLight )
+{
+    if ( !m_bDontRemoveFromList )
+    {
+        if ( !m_List.empty() ) m_List.remove ( pLight );
+    }
+}
