57c2a867739ae8a09ad1d13878ebd4326d340f3c
Jusonex <jusonex96@gmail.com>
2015-08-17 18:25:18 +0200

    Fixed #8989 (File libcef.dll wasn't found)

--- MTA10/ceflauncher/Main.cpp
+++ MTA10/ceflauncher/Main.cpp
@@ -10,11 +10,23 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include "CCefApp.h"
+#include <string>
 
 int WINAPI WinMain ( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdShow, int nCmdShow )
 {
-    CefMainArgs mainArgs ( hInstance );
-    CefRefPtr<CCefApp> app ( new CCefApp );
+    // Get absolute CEFLauncher.exe path
+    TCHAR buffer[MAX_PATH];
+    GetModuleFileName ( NULL, buffer, MAX_PATH );
+    std::wstring currentFileName ( buffer );
 
-    return CefExecuteProcess ( mainArgs, app, NULL );
+    // Extract MTA path and set DLL directory (absolute path is required here)
+    size_t pos = currentFileName.find_last_of ( L'\\' );
+    std::wstring mtaPath = currentFileName.substr ( 0, pos - 3 ); // Strip "CEF"
+    SetDllDirectory ( mtaPath.c_str () );
+
+    // Load CEF
+    CefMainArgs mainArgs ( hInstance );
+    CefRefPtr<CCefApp> app { new CCefApp };
+
+    return CefExecuteProcess ( mainArgs, app, nullptr );
 }
