773a918a7946c7b4aa98407214cfa8638d9066d8
ccw <ccw808@googlemail.com>
2015-10-16 17:58:23 +0100

    Added autofix for black screen on startup #2

--- Shared/sdk/SharedUtil.Game.h
+++ Shared/sdk/SharedUtil.Game.h
@@ -131,4 +131,5 @@ namespace SharedUtil
     #define WD_SECTION_IS_QUITTING                          "Q0"                // Open during quit 
     #define WD_COUNTER_CRASH_CHAIN_BEFORE_ONLINE_GAME       "CR1"               // Counts consecutive crashes before the online game starts
     #define WD_COUNTER_CRASH_CHAIN_BEFORE_LOADING_SCREEN    "CR2"               // Counts consecutive crashes before the loading screen is shown
+    #define WD_COUNTER_CRASH_CHAIN_BEFORE_USED_MAIN_MENU    "CR3"               // Counts consecutive crashes before the main menu is used
 }
