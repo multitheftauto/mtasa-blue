4eeaee63a358521ad4aa0e376ddb68838f8c56c8
ccw <ccw808@googlemail.com>
2015-11-14 14:20:36 +0000

    Improved server dll version check

--- MTA10_Server/core/CDynamicLibrary.cpp
+++ MTA10_Server/core/CDynamicLibrary.cpp
@@ -99,6 +99,12 @@ bool CDynamicLibrary::Load ( const char* szFilename )
         }
     #endif
 
+    // Check for version mismatch
+    if ( !CheckMtaVersion( ExtractFilename( szFilename ) ) )
+    {
+        return false;
+    }
+
     // Return whether we succeeded or not
     return m_hModule != 0;
 }
@@ -149,4 +155,35 @@ FuncPtr_t CDynamicLibrary::GetProcedureAddress ( const char* szProcName )
     }
 
     return NULL;
+}
+
+
+bool CDynamicLibrary::CheckMtaVersion( const char* szLibName )
+{
+#if MTASA_VERSION_TYPE == VERSION_TYPE_RELEASE
+// define MTASA_SKIP_VERSION_CHECKS in "build_overrides_s.h" to skip version checks
+#ifndef MTASA_SKIP_VERSION_CHECKS
+
+    if ( m_hModule == 0 )
+        return false;
+
+    char buffer[256];
+    buffer[0] = 0;
+    GetLibMtaVersion( buffer, sizeof( buffer ) );
+    SString strVersionCore = buffer;
+
+    buffer[0] = 0;
+    FUNC_GetMtaVersion* pfnGetMtaVersion = (FUNC_GetMtaVersion*) ( GetProcedureAddress ( "GetLibMtaVersion" ) );
+    if ( pfnGetMtaVersion )
+        pfnGetMtaVersion( buffer, sizeof( buffer ) );
+    if ( strVersionCore != buffer )
+    {
+        Print( "ERROR: '%s' library version is '%s' (Expected '%s')\n", szLibName, buffer, *strVersionCore );
+        Print( "Reinstall MTA\n" );
+        return false;
+    }
+
+#endif
+#endif
+    return true;
 }
