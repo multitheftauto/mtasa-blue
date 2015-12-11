c4f2ea45b6ac8956d77f2e491833319422c2ebea
Qais Patankar <qaisjp@gmail.com>
2015-12-11 23:43:10 +0000

    Clean up client File luadefs

--- MTA10/mods/shared_logic/lua/CLuaMain.cpp
+++ MTA10/mods/shared_logic/lua/CLuaMain.cpp
@@ -1383,37 +1383,6 @@ void CLuaMain::AddTimerClass ( lua_State* luaVM )
 }
 
 
-void CLuaMain::AddFileClass ( lua_State* luaVM )
-{
-    lua_newclass ( luaVM );
-
-    lua_classfunction ( luaVM, "create", "fileOpen" );
-    lua_classfunction ( luaVM, "destroy", "fileClose" );
-    lua_classfunction ( luaVM, "close", "fileClose" );
-    lua_classfunction ( luaVM, "new", "fileCreate" );
-    
-    lua_classfunction ( luaVM, "delete", "fileDelete" );
-    lua_classfunction ( luaVM, "exists", "fileExists" );
-    lua_classfunction ( luaVM, "flush", "fileFlush" );
-    lua_classfunction ( luaVM, "getPos", "fileGetPos" );
-    lua_classfunction ( luaVM, "getSize", "fileGetSize" );
-    lua_classfunction ( luaVM, "isEOF", "fileIsEOF" );
-    lua_classfunction ( luaVM, "read", "fileRead" );
-    lua_classfunction ( luaVM, "rename", "fileRename" );
-    lua_classfunction ( luaVM, "setPos", "fileSetPos" );
-    lua_classfunction ( luaVM, "write", "fileWrite" );
-    lua_classfunction ( luaVM, "copy", "fileCopy" );
-    lua_classfunction ( luaVM, "getPath", "fileGetPath" );
-
-    lua_classvariable ( luaVM, "pos", "fileSetPos", "fileGetPos" );
-    lua_classvariable ( luaVM, "size", NULL, "fileGetSize" );
-    lua_classvariable ( luaVM, "isEOF", NULL, "fileIsEOF" );
-    lua_classvariable ( luaVM, "path", NULL, "fileGetPath" );
-    
-    lua_registerclass ( luaVM, "File" );
-}
-
-
 void CLuaMain::AddXMLClass ( lua_State* luaVM )
 {
     lua_newclass ( luaVM );
@@ -1905,7 +1874,6 @@ void CLuaMain::InitClasses ( lua_State* luaVM )
 
     AddResourceClass            ( luaVM );
     AddTimerClass               ( luaVM );
-    AddFileClass                ( luaVM );
     AddXMLClass                 ( luaVM );
 
     AddEngineClass              ( luaVM );
@@ -1923,6 +1891,7 @@ void CLuaMain::InitClasses ( lua_State* luaVM )
     
     AddCameraClass              ( luaVM );
 
+    CLuaFileDefs::AddClass ( luaVM );
     CLuaSearchLightDefs::AddClass ( luaVM );
 }
 
