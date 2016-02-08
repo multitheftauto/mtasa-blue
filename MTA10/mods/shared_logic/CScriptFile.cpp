multitheftauto/mtasa-blue

commit 23f4001f36fc7a5ad50ec85eb9c722c8e52e1ac6

Fixed crash when using fileRead with a large count argument

MTA10/mods/shared_logic/CScriptFile.cpp
@@ -162,15 +162,25 @@ void CScriptFile::Flush ( void )
 }
 
 
-long CScriptFile::Read ( unsigned long ulSize, char* pData )
+long CScriptFile::Read ( unsigned long ulSize, CBuffer& outBuffer )
 {
     if ( !m_pFile )
         return -1;
 
     DoResourceFileCheck();
 
-    // Try to read data into the given block. Return number of bytes we read.
-    return m_pFile->FRead ( pData, ulSize );
+    // If read size is large, limit it to how many bytes can be read (avoid memory problems with over allocation)
+    if ( ulSize > 10000 )
+    {
+        long lCurrentPos = m_pFile->FTell ();
+        m_pFile->FSeek ( 0, SEEK_END );
+        long lFileSize = m_pFile->FTell ();
+        m_pFile->FSeek ( lCurrentPos, SEEK_SET );
+        ulSize = Min < unsigned long > ( lFileSize - lCurrentPos, ulSize );
+    }
+
+    outBuffer.SetSize( ulSize );
+    return m_pFile->FRead ( outBuffer.GetData(), ulSize );
 }
 
 

MTA10_Server/mods/deathmatch/logic/CScriptFile.cpp
@@ -181,13 +181,23 @@ void CScriptFile::Flush ( void )
 }
 
 
-long CScriptFile::Read ( unsigned long ulSize, char* pData )
+long CScriptFile::Read ( unsigned long ulSize, CBuffer& outBuffer )
 {
     if ( !m_pFile )
         return -1;
 
-    // Try to read data into the given block. Return number of bytes we read.
-    return fread ( pData, 1, ulSize, m_pFile );
+    // If read size is large, limit it to how many bytes can be read (avoid memory problems with over allocation)
+    if ( ulSize > 10000 )
+    {
+        long lCurrentPos = ftell ( m_pFile );
+        fseek ( m_pFile, 0, SEEK_END );
+        long lFileSize = ftell ( m_pFile );
+        fseek ( m_pFile, lCurrentPos, SEEK_SET );
+        ulSize = Min < unsigned long > ( lFileSize - lCurrentPos, ulSize );
+    }
+
+    outBuffer.SetSize( ulSize );
+    return fread ( outBuffer.GetData(), 1, ulSize, m_pFile );
 }
 
 

MTA10/mods/shared_logic/CScriptFile.h
@@ -59,7 +59,7 @@ public:
     long                    SetPointer              ( unsigned long ulPosition );
 
     void                    Flush                   ( void );
-    long                    Read                    ( unsigned long ulSize, char* pData );
+    long                    Read                    ( unsigned long ulSize, CBuffer& outBuffer );
     long                    Write                   ( unsigned long ulSize, const char* pData );
 
 private:

MTA10/mods/shared_logic/luadefs/CLuaFileDefs.cpp
@@ -409,23 +409,20 @@ int CLuaFileDefs::fileRead ( lua_State* luaVM )
             if ( ulCount > 0 )
             {
                 // Allocate a buffer to read the stuff into and read some shit into it
-                char* pReadContent = new char [ulCount + 1];
-                long lBytesRead = pFile->Read ( ulCount, pReadContent );
+                CBuffer buffer;
+                long lBytesRead = pFile->Read ( ulCount, buffer );
 
                 if ( lBytesRead != -1 )
                 {
                     // Push the string onto the lua stack. Use pushlstring so we are binary
                     // compatible. Normal push string takes zero terminated strings.
-                    lua_pushlstring ( luaVM, pReadContent, lBytesRead );
+                    lua_pushlstring ( luaVM, buffer.GetData(), lBytesRead );
                 }
                 else
                 {
                     m_pScriptDebugging->LogBadPointer ( luaVM, "file", 1 );
                     lua_pushnil ( luaVM );
                 }
-
-                // Delete our read content. Lua should've stored it
-                delete[] pReadContent;
 
                 // We're returning the result string
                 return 1;

MTA10_Server/mods/deathmatch/logic/CScriptFile.h
@@ -53,7 +53,7 @@ public:
     void                    SetSize                 ( unsigned long ulNewSize );
 
     void                    Flush                   ( void );
-    long                    Read                    ( unsigned long ulSize, char* pData );
+    long                    Read                    ( unsigned long ulSize, CBuffer& outBuffer );
     long                    Write                   ( unsigned long ulSize, const char* pData );
 
 private:

MTA10_Server/mods/deathmatch/logic/luadefs/CLuaFileDefs.cpp
@@ -405,24 +405,20 @@ int CLuaFileDefs::fileRead ( lua_State* luaVM )
     {
         if ( ulCount > 0 )
         {
-            // Allocate a buffer to read the stuff into and read some shit into it
-            char* pReadContent = new char [ulCount + 1];
-            long lBytesRead = pFile->Read ( ulCount, pReadContent );
+            CBuffer buffer;
+            long lBytesRead = pFile->Read ( ulCount, buffer );
 
             if ( lBytesRead != -1 )
             {
                 // Push the string onto the lua stack. Use pushlstring so we are binary
                 // compatible. Normal push string takes zero terminated strings.
-                lua_pushlstring ( luaVM, pReadContent, lBytesRead );
+                lua_pushlstring ( luaVM, buffer.GetData(), lBytesRead );
             }
             else
             {
                 m_pScriptDebugging->LogBadPointer ( luaVM, "file", 1 );
                 lua_pushnil ( luaVM );
             }
-
-            // Delete our read content. Lua should've stored it
-            delete [] pReadContent;
 
             // We're returning the result string
             return 1;

commit 9dd823a1eb53cd6af96f2dd762c1d5f812f72792

Fixed #9140 (Wrong behavior of fileRead function if bytes request exceed 10000) Bug introduced in 23f4001f36fc7a5ad50ec85eb9c722c8e52e1ac6 (Fixed crash when using fileRead with a large count argument)

MTA10/mods/shared_logic/CScriptFile.cpp
@@ -176,7 +176,8 @@ long CScriptFile::Read ( unsigned long ulSize, CBuffer& outBuffer )
         m_pFile->FSeek ( 0, SEEK_END );
         long lFileSize = m_pFile->FTell ();
         m_pFile->FSeek ( lCurrentPos, SEEK_SET );
-        ulSize = Min < unsigned long > ( lFileSize - lCurrentPos, ulSize );
+        ulSize = Min < unsigned long > ( 1 + lFileSize - lCurrentPos, ulSize );
+        // Note: Read extra byte at end so EOF indicator gets set
     }
 
     outBuffer.SetSize( ulSize );

MTA10_Server/mods/deathmatch/logic/CScriptFile.cpp
@@ -193,7 +193,8 @@ long CScriptFile::Read ( unsigned long ulSize, CBuffer& outBuffer )
         fseek ( m_pFile, 0, SEEK_END );
         long lFileSize = ftell ( m_pFile );
         fseek ( m_pFile, lCurrentPos, SEEK_SET );
-        ulSize = Min < unsigned long > ( lFileSize - lCurrentPos, ulSize );
+        ulSize = Min < unsigned long > ( 1 + lFileSize - lCurrentPos, ulSize );
+        // Note: Read extra byte at end so EOF indicator gets set
     }
 
     outBuffer.SetSize( ulSize );