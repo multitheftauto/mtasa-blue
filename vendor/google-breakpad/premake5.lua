98dc41354f8c30cb80ba7fa584966c3cc8cfa878
sbx320 <sbx320@sbx320.net>
2016-08-01 00:50:04 +0200

    Update premake5.lua

--- vendor/google-breakpad/premake5.lua
+++ vendor/google-breakpad/premake5.lua
@@ -4,7 +4,6 @@ project "breakpad"
 	targetname "breakpad"
 	
 	includedirs { "src", "src/third_party/glog/src" }
-	buildoptions { "-fpermissive" }
 	vpaths { 
 		["Headers/*"] = "src/**.h",
 		["Sources/*"] = {"src/**.cc", "src/**.c"},
