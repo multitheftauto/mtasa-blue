fbc1628a9491e3b6342195025e01be3b8ee74269
ccw <ccw808@googlemail.com>
2016-08-01 04:44:54 +0100

    Fixed xcopy wanting input if dir not exists. Changed include path order for technical reasons.

--- premake5.lua
+++ premake5.lua
@@ -16,8 +16,8 @@ workspace "MTASA"
 	
 	dxdir = os.getenv("DXSDK_DIR") or ""
 	includedirs { 
+		"vendor",
 		"Shared/sdk", 
-		"vendor"
 	}
 
 	defines { 
@@ -28,7 +28,7 @@ workspace "MTASA"
 		
 	-- Helper function for output path 
 	buildpath = function(p) return "%{wks.location}../Bin/"..p.."/" end
-	copy = function(p) return "{COPY} %{cfg.buildtarget.abspath} %{wks.location}../Bin/"..p end 
+	copy = function(p) return "{COPY} %{cfg.buildtarget.abspath} %{wks.location}../Bin/"..p.."/" end 
 	
 	filter "platforms:x86"
 		architecture "x86"
