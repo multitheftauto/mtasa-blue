dd057b30a322845d5b85b98bb1c70ca4cddf53d5
sbx320 <sbx320@sbx320.net>
2016-08-01 05:50:16 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -28,7 +28,7 @@ workspace "MTASA"
 		
 	-- Helper function for output path 
 	buildpath = function(p) return "%{wks.location}../Bin/"..p.."/" end
-	copy = function(p) return "{COPY} %{cfg.buildtarget.abspath} %{wks.location}../Bin/"..p end 
+	copy = function(p) return "{COPY} %{cfg.buildtarget.abspath} %{wks.location}../Bin/"..p.."/" end 
 	
 	filter "platforms:x86"
 		architecture "x86"
