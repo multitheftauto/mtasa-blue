a0e9f3b00accb65b031de8ebbb34c2cc73de31e7
ccw <ccw808@googlemail.com>
2016-08-22 20:25:02 +0100

    Fixed pcre include path for Linux build

--- vendor/ehs/premake5.lua
+++ vendor/ehs/premake5.lua
@@ -2,7 +2,7 @@ project "ehs"
 	language "C++"
 	kind "StaticLib"
 	targetname "ehs"
-	includedirs { "../pme" }
+	includedirs { "../pcre", "../pme" }
 
 	defines { "WIN32_LEAN_AND_MEAN", "_LIB" }
 	disablewarnings { "4800" }
@@ -25,4 +25,4 @@ project "ehs"
 	}
 	
 	filter "system:windows"
-		includedirs { "../pcre", "../pthreads/include" }
+		includedirs { "../pthreads/include" }
