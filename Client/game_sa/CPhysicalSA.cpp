multitheftauto/mtasa-blue

commit a88ca7c22778a23946403e0c20d0e93815ca4b42

Do not recreate object when changing frozen state

Client/game_sa/CPhysicalSA.cpp
@@ -370,3 +370,10 @@ void CPhysicalSA::SetLighting ( float fLighting )
     CPhysicalSAInterface * pInterface = (CPhysicalSAInterface *)this->GetInterface();
     pInterface->m_fLighting = fLighting;
 }
+
+void CPhysicalSA::SetFrozen ( bool bFrozen )
+{
+    CPhysicalSAInterface * pInterface = (CPhysicalSAInterface *)this->GetInterface();
+    pInterface->bDisableMovement = bFrozen;
+    pInterface->bDisableFriction = bFrozen;
+}

Client/game_sa/CPhysicalSA.h
@@ -153,6 +153,8 @@ public:
     float       GetLighting                 ( void );
     void        SetLighting                 ( float fLighting );
 
+    void        SetFrozen                   ( bool bFrozen );
+
     /*
     VOID        SetMassMultiplier(FLOAT fMassMultiplier);
     FLOAT       GetMassMultiplier();

Client/mods/deathmatch/logic/CStaticFunctionDefinitions.cpp
@@ -898,7 +898,7 @@ bool CStaticFunctionDefinitions::IsElementFrozen ( CClientEntity& Entity, bool& 
         case CCLIENTOBJECT:
         {
             CClientObject& Object = static_cast < CClientObject& > ( Entity );
-            bFrozen = Object.IsStatic ();
+            bFrozen = Object.IsFrozen ();
             break;
         }
         default: return false;
@@ -3579,7 +3579,7 @@ bool CStaticFunctionDefinitions::SetElementFrozen ( CClientEntity& Entity, bool 
         case CCLIENTOBJECT:
         {
             CClientObject& Object = static_cast < CClientObject& > ( Entity );
-            Object.SetStatic ( bFrozen );
+            Object.SetFrozen ( bFrozen );
             break;
         }
         default: return false;
@@ -3713,13 +3713,6 @@ CClientObject* CStaticFunctionDefinitions::CreateObject ( CResource& Resource, u
     }
 
     return NULL;
-}
-
-
-bool CStaticFunctionDefinitions::IsObjectStatic ( CClientObject & Object, bool & bStatic )
-{
-    bStatic = Object.IsStatic ();
-    return true;
 }
 
 

Client/mods/deathmatch/logic/CClientObject.cpp
@@ -38,7 +38,7 @@ CClientObject::CClientObject ( CClientManager* pManager, ElementID ID, unsigned 
 
     m_usModel = usModel;
     m_bIsVisible = true;
-    m_bIsStatic = false;
+    m_bIsFrozen = false;
     m_bUsesCollision = true;
     m_ucAlpha = 255;
     m_vecScale = CVector ( 1.0f, 1.0f, 1.0f );
@@ -339,10 +339,18 @@ void CClientObject::Render ( void )
 }
 
 
-void CClientObject::SetStatic ( bool bStatic )
+void CClientObject::SetFrozen ( bool bFrozen )
 {
-    m_bIsStatic = bStatic;
-    StreamOutForABit ( );
+    m_bIsFrozen = bFrozen;
+    if ( m_pObject )
+        m_pObject->SetFrozen ( bFrozen );
+
+    if ( bFrozen )
+    {
+        CVector vecZero;
+        SetMoveSpeed ( vecZero );
+        SetTurnSpeed ( vecZero );
+    }
 }
 
 
@@ -502,9 +510,6 @@ void CClientObject::Create ( void )
                 // Add XRef
                 g_pClientGame->GetGameEntityXRefManager ()->AddEntityXRef ( this, m_pObject );
 
-                // If set to true,this has the effect of forcing the object to be static at all times
-                m_pObject->SetStaticWaitingForCollision ( m_bIsStatic );
-
                 // Apply our data to the object
                 m_pObject->Teleport ( m_vecPosition.fX, m_vecPosition.fY, m_vecPosition.fZ );
                 m_pObject->SetOrientation ( m_vecRotation.fX, m_vecRotation.fY, m_vecRotation.fZ );
@@ -512,6 +517,7 @@ void CClientObject::Create ( void )
                 m_pObject->ProcessCollision ();
                 #endif
                 m_pObject->SetupLighting ();
+                m_pObject->SetFrozen ( m_bIsFrozen );
 
                 UpdateVisibility ();
                 if ( !m_bUsesCollision ) SetCollisionEnabled ( false );
@@ -612,9 +618,8 @@ void CClientObject::StreamedInPulse ( void )
         UpdateVisibility ();
     }
 
-
-    // Are we not a static object (allowed to move by physics)
-    if ( !m_bIsStatic )
+    // Are we not frozen
+    if ( !m_bIsFrozen )
     {
         // Grab our actual position (as GTA moves it too)
         CVector vecPosition = *m_pObject->GetPosition ();
@@ -651,6 +656,28 @@ void CClientObject::SetMoveSpeed ( const CVector& vecMoveSpeed )
         m_pObject->SetMoveSpeed ( const_cast < CVector* > ( &vecMoveSpeed ) );
     }
     m_vecMoveSpeed = vecMoveSpeed;
+}
+
+
+void CClientObject::GetTurnSpeed ( CVector& vecTurnSpeed ) const
+{
+    if ( m_pObject )
+    {
+        m_pObject->GetTurnSpeed ( &vecTurnSpeed );
+    }
+    else
+    {
+        vecTurnSpeed = m_vecTurnSpeed;
+    }
+}
+
+void CClientObject::SetTurnSpeed ( const CVector& vecTurnSpeed )
+{
+    if ( m_pObject )
+    {
+        m_pObject->SetTurnSpeed ( const_cast < CVector* > ( &vecTurnSpeed ) );
+    }
+    m_vecTurnSpeed = vecTurnSpeed;
 }
 
 

Client/mods/deathmatch/logic/CClientObject.h
@@ -56,6 +56,9 @@ public:
     void                            GetMoveSpeed            ( CVector& vecMoveSpeed ) const;
     void                            SetMoveSpeed            ( const CVector& vecMoveSpeed );
 
+    void                            GetTurnSpeed            ( CVector& vecTurnSpeed ) const;
+    void                            SetTurnSpeed            ( const CVector& vecTurnSpeed );
+
     void                            GetOrientation          ( CVector& vecPosition, CVector& vecRotationRadians );
     virtual void                    SetOrientation          ( const CVector& vecPosition, const CVector& vecRotationRadians );
 
@@ -75,8 +78,8 @@ public:
 
     void                            Render                  ( void );
 
-    inline bool                     IsStatic                ( void )                            { return m_bIsStatic; }
-    void                            SetStatic               ( bool bStatic );
+    inline bool                     IsFrozen                ( void )                            { return m_bIsFrozen; }
+    void                            SetFrozen               ( bool bFrozen );
     
     inline unsigned char            GetAlpha                ( void )                            { return m_ucAlpha; }   
     void                            SetAlpha                ( unsigned char ucAlpha );
@@ -125,7 +128,7 @@ protected:
     CVector                             m_vecPosition;
     CVector                             m_vecRotation;
     bool                                m_bIsVisible;
-    bool                                m_bIsStatic;
+    bool                                m_bIsFrozen;
     bool                                m_bUsesCollision;
     unsigned char                       m_ucAlpha;
     CVector                             m_vecScale;
@@ -136,6 +139,7 @@ protected:
     float                               m_fMass;
 
     CVector                             m_vecMoveSpeed;
+    CVector                             m_vecTurnSpeed;
 
     const bool                          m_bIsLowLod;            // true if this object is low LOD
     CClientObject*                      m_pLowLodObject;        // Pointer to low LOD version of this object

Client/mods/deathmatch/logic/CStaticFunctionDefinitions.h
@@ -269,7 +269,6 @@ public:
 
     // Object get funcs
     static CClientObject*               CreateObject                        ( CResource& Resource, unsigned short usModelID, const CVector& vecPosition, const CVector& vecRotation, bool bLowLod );
-    static bool                         IsObjectStatic                      ( CClientObject& Object, bool & bStatic );
     static bool                         GetObjectScale                      ( CClientObject& Object, CVector& vecScale );
     static bool                         IsObjectBreakable                   ( CClientObject& Object, bool& bBreakable );
     static bool                         GetObjectMass                       ( CClientObject& Object, float& fMass );

Client/mods/deathmatch/logic/luadefs/CLuaObjectDefs.cpp
@@ -123,7 +123,7 @@ int CLuaObjectDefs::IsObjectStatic ( lua_State* luaVM )
     if ( !argStream.HasErrors () )
     {
         bool bStatic;
-        if ( CStaticFunctionDefinitions::IsObjectStatic ( *pObject, bStatic ) )
+        if ( CStaticFunctionDefinitions::IsElementFrozen ( *pObject, bStatic ) )
         {
             lua_pushboolean ( luaVM, bStatic );
             return 1;

Client/sdk/game/CPhysical.h
@@ -51,6 +51,8 @@ public:
     virtual float       GetLighting                 ( void ) = 0;
     virtual void        SetLighting                 ( float fLighting ) = 0;
 
+    virtual void        SetFrozen                   ( bool bFrozen ) = 0;
+
 /*  virtual VOID        SetMass(FLOAT fMass)=0;
     virtual FLOAT       GetMass()=0;
     virtual VOID        SetTurnMass(FLOAT fTurnMass)=0;

commit 7807d6717f1e8c355fd51ae30f3a2e71c29c2625

Fixed #9418 (setElementFrozen directly after resource start not working)
Fixed collision issue with local player and frozen object
Issuses introduced in a88ca7c

Client/game_sa/CPhysicalSA.cpp
@@ -366,7 +366,7 @@ void CPhysicalSA::SetFrozen ( bool bFrozen )
 {
     CPhysicalSAInterface * pInterface = (CPhysicalSAInterface *)this->GetInterface();
     // Set movement ability
-    pInterface->bDisableMovement = bFrozen;
+    pInterface->bDontApplySpeed = bFrozen;
     // Set rotation movement ability
     pInterface->bDisableFriction = bFrozen;
 }

Client/game_sa/CPhysicalSA.h
@@ -50,7 +50,7 @@ public:
     uint32 bBroken : 1;
     uint32 b0x800 : 1; // ref @ 0x6F5CF0
     uint32 b0x1000 : 1;//
-    uint32 b0x2000 : 1;//
+    uint32 bDontApplySpeed : 1;//
     uint32 b0x4000 : 1;//
     uint32 b0x8000 : 1;//
 

Client/mods/deathmatch/logic/CPacketHandler.cpp
@@ -2911,9 +2911,9 @@ void CPacketHandler::Packet_EntityAdd ( NetBitStreamInterface& bitStream )
                         }
                         pObject->SetScale ( vecScale );
 
-                        bool bStatic;
-                        if ( bitStream.ReadBit ( bStatic ) )
-                            pObject->SetStatic ( bStatic );
+                        bool bFrozen;
+                        if ( bitStream.ReadBit ( bFrozen ) )
+                            pObject->SetFrozen ( bFrozen );
 
                         SObjectHealthSync health;
                         if ( bitStream.Read ( &health ) )

Client/mods/deathmatch/logic/CStaticFunctionDefinitions.cpp
@@ -3842,7 +3842,7 @@ bool CStaticFunctionDefinitions::SetObjectStatic ( CClientEntity& Entity, bool b
     if ( IS_OBJECT ( &Entity ) )
     {
         CDeathmatchObject& Object = static_cast < CDeathmatchObject& > ( Entity );
-        Object.SetStatic ( bStatic );
+        Object.SetFrozen ( bStatic );
         return true;
     }
 

Server/mods/deathmatch/logic/CObject.cpp
@@ -33,7 +33,7 @@ CObject::CObject ( CElement* pParent, CXMLNode* pNode, CObjectManager* pObjectMa
     m_fHealth = 1000.0f;
     m_bSyncable = true;
     m_pSyncer = NULL;
-    m_bIsStatic = false;
+    m_bIsFrozen = false;
 
     m_bCollisionsEnabled = true;
 
@@ -171,7 +171,7 @@ bool CObject::ReadSpecialData ( void )
 
     bool bFrozen;
     if ( GetCustomDataBool ( "frozen", bFrozen, true ) )
-        m_bIsStatic = bFrozen;
+        m_bIsFrozen = bFrozen;
 
     // Success
     return true;

Server/mods/deathmatch/logic/CObject.h
@@ -62,8 +62,8 @@ public:
     inline bool                 GetCollisionEnabled     ( void )                        { return m_bCollisionsEnabled; }
     inline void                 SetCollisionEnabled     ( bool bCollisionEnabled )      { m_bCollisionsEnabled = bCollisionEnabled; }
 
-    inline bool                 IsStatic                ( void )                        { return m_bIsStatic; }
-    inline void                 SetStatic               ( bool bStatic )                { m_bIsStatic = bStatic; }
+    inline bool                 IsFrozen                ( void )                        { return m_bIsFrozen; }
+    inline void                 SetFrozen               ( bool bFrozen )                { m_bIsFrozen = bFrozen; }
 
     inline float                GetHealth               ( void )                        { return m_fHealth; }
     inline void                 SetHealth               ( float fHealth )               { m_fHealth = fHealth; }
@@ -84,7 +84,7 @@ private:
     unsigned char               m_ucAlpha;
     unsigned short              m_usModel;
     CVector                     m_vecScale;
-    bool                        m_bIsStatic;
+    bool                        m_bIsFrozen;
     float                       m_fHealth;
     bool                        m_bBreakable;
     bool                        m_bSyncable;

Server/mods/deathmatch/logic/CStaticFunctionDefinitions.cpp
@@ -924,7 +924,7 @@ bool CStaticFunctionDefinitions::IsElementFrozen ( CElement* pElement, bool &bFr
         case CElement::OBJECT:
         {
             CObject* pObject = static_cast < CObject* > ( pElement );
-            bFrozen = pObject->IsStatic ();
+            bFrozen = pObject->IsFrozen ();
             break;
         }
         default: return false;
@@ -1938,7 +1938,7 @@ bool CStaticFunctionDefinitions::SetElementFrozen ( CElement* pElement, bool bFr
         case CElement::OBJECT:
         {
             CObject * pObject = static_cast < CObject* > ( pElement );
-            pObject->SetStatic ( bFrozen );
+            pObject->SetFrozen ( bFrozen );
             break;
         }
         default: return false;

Server/mods/deathmatch/logic/packets/CEntityAddPacket.cpp
@@ -275,9 +275,9 @@ bool CEntityAddPacket::Write ( NetBitStreamInterface& BitStream ) const
                         BitStream.Write( vecScale.fX );
                     }
 
-                    // Static
-                    bool bStatic = pObject->IsStatic ();
-                    BitStream.WriteBit ( bStatic );
+                    // Frozen
+                    bool bFrozen = pObject->IsFrozen ();
+                    BitStream.WriteBit ( bFrozen );
 
                     // Health
                     SObjectHealthSync health;