multitheftauto/mtasa-blue

commit fcc1ed4e68c697566ef3204c8f9454d7ec9b7c37

Fixed #9477 (Goggles stay after player is killed)

Client/mods/deathmatch/logic/CClientPed.cpp
@@ -1870,6 +1870,10 @@ void CClientPed::Kill ( eWeaponType weaponType, unsigned char ucBodypart, bool b
     // Stop pressing buttons
     SetControllerState ( CControllerState () );
 
+    // Remove goggles #9477
+    if ( IsWearingGoggles ( ) )
+        m_pPlayerPed->SetGogglesState ( false );
+
     m_bDead = true;
 }
 

commit 4735bab72132dc4370b9865134fb751ed6f4b395

Fix crash introduced in fcc1ed4e68c697566ef3204c8f9454d7ec9b7c37

Client/mods/deathmatch/logic/CClientPed.cpp
@@ -1872,7 +1872,7 @@ void CClientPed::Kill ( eWeaponType weaponType, unsigned char ucBodypart, bool b
 
     // Remove goggles #9477
     if ( IsWearingGoggles ( ) )
-        m_pPlayerPed->SetGogglesState ( false );
+        SetWearingGoggles ( false, false );
 
     m_bDead = true;
 }
@@ -4304,7 +4304,7 @@ bool CClientPed::IsChoking ( void )
 }
 
 
-void CClientPed::SetWearingGoggles ( bool bWearing )
+void CClientPed::SetWearingGoggles ( bool bWearing, bool animationEnabled )
 {
     if ( m_pPlayerPed )
     {
@@ -4314,10 +4314,13 @@ void CClientPed::SetWearingGoggles ( bool bWearing )
             m_pPlayerPed->SetGogglesState ( bWearing );
 
             // Are our goggle anims loaded?
-            CAnimBlock * pBlock = g_pGame->GetAnimManager ()->GetAnimationBlock ( "GOGGLES" );
-            if ( pBlock->IsLoaded () )
+            if (animationEnabled)
             {
-                BlendAnimation ( ANIM_GROUP_GOGGLES, ANIM_ID_GOGGLES_ON, 4.0f );
+                CAnimBlock* pBlock = g_pGame->GetAnimManager()->GetAnimationBlock("GOGGLES");
+                if (pBlock->IsLoaded())
+                {
+                    BlendAnimation(ANIM_GROUP_GOGGLES, ANIM_ID_GOGGLES_ON, 4.0f);
+                }
             }
         }
     }

Client/mods/deathmatch/logic/CClientPed.h
@@ -301,7 +301,7 @@ public:
     void                        SetChoking                  ( bool bChoking );
     bool                        IsChoking                   ( void );
   
-    void                        SetWearingGoggles           ( bool bWearing );
+    void                        SetWearingGoggles           ( bool bWearing, bool animationEnabled = true );
     bool                        IsWearingGoggles            ( bool bCheckMoving = false );
     bool                        IsMovingGoggles             ( bool & bPuttingOn );
 
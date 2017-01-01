4735bab72132dc4370b9865134fb751ed6f4b395
Jusonex <jusonex96@gmail.com>
2017-01-01 19:10:57 +0100

    Fix crash introduced in fcc1ed4e68c697566ef3204c8f9454d7ec9b7c37

--- Client/mods/deathmatch/logic/CClientPed.cpp
+++ Client/mods/deathmatch/logic/CClientPed.cpp
@@ -1872,7 +1872,7 @@ void CClientPed::Kill ( eWeaponType weaponType, unsigned char ucBodypart, bool b
 
     // Remove goggles #9477
     if ( IsWearingGoggles ( ) )
-        m_pPlayerPed->SetGogglesState ( false );
+        SetWearingGoggles ( false, false );
 
     m_bDead = true;
 }
@@ -4304,7 +4304,7 @@ bool CClientPed::IsChoking ( void )
 }
 
 
-void CClientPed::SetWearingGoggles ( bool bWearing )
+void CClientPed::SetWearingGoggles ( bool bWearing, bool animationEnabled )
 {
     if ( m_pPlayerPed )
     {
@@ -4314,10 +4314,13 @@ void CClientPed::SetWearingGoggles ( bool bWearing )
             m_pPlayerPed->SetGogglesState ( bWearing );
 
             // Are our goggle anims loaded?
-            CAnimBlock * pBlock = g_pGame->GetAnimManager ()->GetAnimationBlock ( "GOGGLES" );
-            if ( pBlock->IsLoaded () )
+            if (animationEnabled)
             {
-                BlendAnimation ( ANIM_GROUP_GOGGLES, ANIM_ID_GOGGLES_ON, 4.0f );
+                CAnimBlock* pBlock = g_pGame->GetAnimManager()->GetAnimationBlock("GOGGLES");
+                if (pBlock->IsLoaded())
+                {
+                    BlendAnimation(ANIM_GROUP_GOGGLES, ANIM_ID_GOGGLES_ON, 4.0f);
+                }
             }
         }
     }
