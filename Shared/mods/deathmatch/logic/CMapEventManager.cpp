3824f7779950b4d9e863400b321bb040c19c4b1a
sbx320 <sbx320@sbx320.net>
2017-01-03 14:31:24 +0100

    Linux compile fix #4 Addendum to 34bea4d67c8c9e94f2525d34be8f121147ba6af8

--- Shared/mods/deathmatch/logic/CMapEventManager.cpp
+++ Shared/mods/deathmatch/logic/CMapEventManager.cpp
@@ -46,7 +46,7 @@ bool CMapEventManager::Add(CLuaMain* pLuaMain, const std::string& strName, const
     }
 
     // Emplace to the map
-    m_EventsMap.emplace_hint(iter, strName, new CMapEvent(pLuaMain, strName, iLuaFunction, bPropagated, eventPriority, fPriorityMod));
+    m_EventsMap.emplace_hint(iter, strName, std::unique_ptr<CMapEvent>(new CMapEvent(pLuaMain, strName, iLuaFunction, bPropagated, eventPriority, fPriorityMod)));
     m_bHasEvents = true;
     return true;
 }
