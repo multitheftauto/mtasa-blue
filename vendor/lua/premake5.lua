486f46c3a0aa1a16cf2b1bcbf8e852a35d640a2c
Jusonex <Jusonex@users.noreply.github.com>
2017-03-25 11:21:48 +0100

    Premakify publicsdk and build additional shared library on Linux (#120)
    * Premakify publicsdk
    Also remove deprecated files
    
    * Unfinished linux fixes
    
    * Clean up link stuff
    
    * Linux fixes
    
    * Fix comment

--- vendor/lua/premake5.lua
+++ vendor/lua/premake5.lua
@@ -1,8 +1,10 @@
 project "Lua_Server"
 	language "C++"
 	targetname "lua5.1"
+	targetdir(buildpath("server/mods/deathmatch"))
+	kind "SharedLib"
 
-	vpaths { 
+	vpaths {
 		["Headers"] = "**.h",
 		["Sources"] = "**.c",
 		["*"] = "premake5.lua"
@@ -14,16 +16,27 @@ project "Lua_Server"
 		"src/**.h",
 	}
 
+	local luapath = _SCRIPT_DIR.."/src/"
+	local targetpath = "%{wks.location}/../Shared/publicsdk/include/"
+
+	postbuildcommands {
+		"{COPY} "..luapath.."lua.h "..targetpath,
+		"{COPY} "..luapath.."luaconf.h "..targetpath,
+		"{COPY} "..luapath.."lauxlib.h "..targetpath,
+		"{COPY} "..luapath.."lualib.h "..targetpath
+	}
+
 	filter "system:windows"
 		defines { "LUA_BUILD_AS_DLL" }
-		kind "SharedLib"
-		targetdir(buildpath("server/mods/deathmatch"))
 
-	filter "system:not windows"
-		kind "StaticLib"
-
-	filter {"system:windows", "platforms:x64"}
+	filter "platforms:x64"
 		targetdir(buildpath("server/x64"))
+
+	filter {"system:linux", "platforms:x86"}
+		postbuildcommands { "{COPY} "..buildpath("server/mods/deathmatch").."lua5.1.so %{wks.location}/../Shared/publicsdk/lib/" }
+	
+	filter {"system:linux", "platforms:x64"}
+		postbuildcommands { "{COPY} "..buildpath("server/x64").."lua5.1.so %{wks.location}/../Shared/publicsdk/lib/" }
 
 
 if os.get() == "windows" then
@@ -33,7 +46,7 @@ if os.get() == "windows" then
 		targetname "lua5.1c"
 		targetdir(buildpath("mods/deathmatch"))
 
-		vpaths { 
+		vpaths {
 			["Headers"] = "**.h",
 			["Sources"] = "**.c",
 			["*"] = "premake5.lua"
@@ -55,3 +68,20 @@ if os.get() == "windows" then
         filter "platforms:x64"
             flags { "ExcludeFromBuild" } 
 end
+
+-- Build static version for Linux module backwards compatibility
+project "Lua_Server_Static"
+	language "C++"
+	kind "StaticLib"
+
+	vpaths {
+		["Headers"] = "**.h",
+		["Sources"] = "**.c",
+		["*"] = "premake5.lua"
+	}
+	
+	files {
+		"premake5.lua",
+		"src/**.c",
+		"src/**.h",
+	}
