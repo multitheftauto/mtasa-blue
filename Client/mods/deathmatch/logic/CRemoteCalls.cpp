multitheftauto/mtasa-blue

commit ffa943c043ddd9d4e3a49b55a13218091862e02b

Added to fetchRemote: request method, request headers and response headers

Client/mods/deathmatch/logic/CRemoteCalls.cpp
@@ -49,6 +49,12 @@ void CRemoteCalls::Call ( const char * szURL, CLuaArguments * fetchArguments, co
     m_calls.back ()->MakeCall ();
 }
 
+void CRemoteCalls::Call ( const char * szURL, CLuaArguments * fetchArguments, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, const SHttpRequestOptions& options )
+{
+    m_calls.push_back ( new CRemoteCall ( szURL, fetchArguments, luaMain, iFunction, strQueueName, options ) );
+    m_calls.back ()->MakeCall ();
+}
+
 void CRemoteCalls::Remove ( CLuaMain * lua )
 {
     list<CRemoteCall *> trash;
@@ -144,14 +150,14 @@ CRemoteCall::CRemoteCall ( const char * szServerHost, const char * szResourceNam
     m_VM = luaMain;
     m_iFunction = iFunction;
 
-    arguments->WriteToJSONString ( m_strData, true );
-    m_bPostBinary = false;
+    arguments->WriteToJSONString ( m_options.strPostData, true );
+    m_options.bPostBinary = false;
     m_bIsFetch = false;
 
     m_strURL = SString ( "http://%s/%s/call/%s", szServerHost, szResourceName, szFunctionName );
     m_strQueueName = strQueueName;
-    m_uiConnectionAttempts = uiConnectionAttempts;
-    m_uiConnectTimeoutMs = uiConnectTimeoutMs;
+    m_options.uiConnectionAttempts = uiConnectionAttempts;
+    m_options.uiConnectTimeoutMs = uiConnectTimeoutMs;
 }
 
 //arbitary URL version
@@ -160,14 +166,14 @@ CRemoteCall::CRemoteCall ( const char * szURL, CLuaArguments * arguments, CLuaMa
     m_VM = luaMain;
     m_iFunction = iFunction;
 
-    arguments->WriteToJSONString ( m_strData, true );
-    m_bPostBinary = false;
+    arguments->WriteToJSONString ( m_options.strPostData, true );
+    m_options.bPostBinary = false;
     m_bIsFetch = false;
 
     m_strURL = szURL;
     m_strQueueName = strQueueName;
-    m_uiConnectionAttempts = uiConnectionAttempts;
-    m_uiConnectTimeoutMs = uiConnectTimeoutMs;
+    m_options.uiConnectionAttempts = uiConnectionAttempts;
+    m_options.uiConnectTimeoutMs = uiConnectTimeoutMs;
 }
 
 //Fetch version
@@ -177,14 +183,26 @@ CRemoteCall::CRemoteCall ( const char * szURL, CLuaArguments * fetchArguments, c
     m_VM = luaMain;
     m_iFunction = iFunction;
 
-    m_strData = strPostData;
-    m_bPostBinary = bPostBinary;
+    m_options.strPostData = strPostData;
+    m_options.bPostBinary = bPostBinary;
     m_bIsFetch = true;
 
     m_strURL = szURL;
     m_strQueueName = strQueueName;
-    m_uiConnectionAttempts = uiConnectionAttempts;
-    m_uiConnectTimeoutMs = uiConnectTimeoutMs;
+    m_options.uiConnectionAttempts = uiConnectionAttempts;
+    m_options.uiConnectTimeoutMs = uiConnectTimeoutMs;
+}
+
+//Fetch version #2
+CRemoteCall::CRemoteCall ( const char * szURL, CLuaArguments * fetchArguments, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, const SHttpRequestOptions& options )
+    : m_FetchArguments ( *fetchArguments )
+{
+    m_VM = luaMain;
+    m_iFunction = iFunction;
+    m_bIsFetch = true;
+    m_strURL = szURL;
+    m_strQueueName = strQueueName;
+    m_options = options;
 }
 
 
@@ -198,7 +216,7 @@ void CRemoteCall::MakeCall()
     bool bAnyHost = (g_pCore->GetWebCore()->GetDomainState(g_pCore->GetWebCore()->GetDomainFromURL(m_strURL)) == eURLState::WEBPAGE_ALLOWED);
     EDownloadModeType downloadMode = g_pClientGame->GetRemoteCalls()->GetDownloadModeForQueueName(m_strQueueName, bAnyHost);
     CNetHTTPDownloadManagerInterface* pDownloadManager = g_pNet->GetHTTPDownloadManager(downloadMode);
-    pDownloadManager->QueueFile(m_strURL, NULL, m_strData.c_str(), m_strData.length(), m_bPostBinary, this, DownloadFinishedCallback, false, m_uiConnectionAttempts, m_uiConnectTimeoutMs);
+    pDownloadManager->QueueFile(m_strURL, NULL, this, DownloadFinishedCallback, false, m_options, false, false);
 }
 
 void CRemoteCall::DownloadFinishedCallback(const SHttpDownloadResult& result)
@@ -208,20 +226,48 @@ void CRemoteCall::DownloadFinishedCallback(const SHttpDownloadResult& result)
         return;
 
     CLuaArguments arguments;
-    if (result.bSuccess)
+    if (pCall->IsLegacy())
     {
-        if (pCall->IsFetch())
+        if (result.bSuccess)
         {
-            arguments.PushString(std::string(result.pData, result.dataSize));
-            arguments.PushNumber(0);
+            if (pCall->IsFetch())
+            {
+                arguments.PushString(std::string(result.pData, result.dataSize));
+                arguments.PushNumber(0);
+            }
+            else
+                arguments.ReadFromJSONString(result.pData);
         }
         else
-            arguments.ReadFromJSONString(result.pData);
+        {
+            arguments.PushString("ERROR");
+            arguments.PushNumber(result.iErrorCode);
+        }
+
     }
     else
     {
-        arguments.PushString("ERROR");
-        arguments.PushNumber(result.iErrorCode);
+        // Append response body
+        arguments.PushString(std::string(result.pData, result.dataSize));
+
+        // Append info table
+        CLuaArguments info;
+        info.PushString("success");
+        info.PushBoolean(result.iErrorCode >= 200 && result.iErrorCode <= 299);
+        info.PushString("statusCode");
+        info.PushNumber(result.iErrorCode);
+
+        // Headers as a subtable
+        CLuaArguments headers;
+        for (auto iter : result.headers )
+        {
+            headers.PushString(iter.first);
+            headers.PushString(iter.second);
+        }
+        info.PushString("headers");
+        info.PushTable(&headers);
+
+        arguments.PushTable(&info);
     }
 
     // Append stored arguments

Server/mods/deathmatch/logic/CRemoteCalls.cpp
@@ -49,6 +49,12 @@ void CRemoteCalls::Call ( const char * szURL, CLuaArguments * fetchArguments, co
     m_calls.back ()->MakeCall ();
 }
 
+void CRemoteCalls::Call ( const char * szURL, CLuaArguments * fetchArguments, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, const SHttpRequestOptions& options )
+{
+    m_calls.push_back ( new CRemoteCall ( szURL, fetchArguments, luaMain, iFunction, strQueueName, options ) );
+    m_calls.back ()->MakeCall ();
+}
+
 void CRemoteCalls::Remove ( CLuaMain * lua )
 {
     list<CRemoteCall *> trash;
@@ -142,14 +148,14 @@ CRemoteCall::CRemoteCall ( const char * szServerHost, const char * szResourceNam
     m_VM = luaMain;
     m_iFunction = iFunction;
 
-    arguments->WriteToJSONString ( m_strData, true );
-    m_bPostBinary = false;
+    arguments->WriteToJSONString ( m_options.strPostData, true );
+    m_options.bPostBinary = false;
     m_bIsFetch = false;
 
     m_strURL = SString ( "http://%s/%s/call/%s", szServerHost, szResourceName, szFunctionName );
     m_strQueueName = strQueueName;
-    m_uiConnectionAttempts = uiConnectionAttempts;
-    m_uiConnectTimeoutMs = uiConnectTimeoutMs;
+    m_options.uiConnectionAttempts = uiConnectionAttempts;
+    m_options.uiConnectTimeoutMs = uiConnectTimeoutMs;
 }
 
 //arbitary URL version
@@ -158,14 +164,14 @@ CRemoteCall::CRemoteCall ( const char * szURL, CLuaArguments * arguments, CLuaMa
     m_VM = luaMain;
     m_iFunction = iFunction;
 
-    arguments->WriteToJSONString ( m_strData, true );
-    m_bPostBinary = false;
+    arguments->WriteToJSONString ( m_options.strPostData, true );
+    m_options.bPostBinary = false;
     m_bIsFetch = false;
 
     m_strURL = szURL;
     m_strQueueName = strQueueName;
-    m_uiConnectionAttempts = uiConnectionAttempts;
-    m_uiConnectTimeoutMs = uiConnectTimeoutMs;
+    m_options.uiConnectionAttempts = uiConnectionAttempts;
+    m_options.uiConnectTimeoutMs = uiConnectTimeoutMs;
 }
 
 //Fetch version
@@ -175,14 +181,26 @@ CRemoteCall::CRemoteCall ( const char * szURL, CLuaArguments * fetchArguments, c
     m_VM = luaMain;
     m_iFunction = iFunction;
 
-    m_strData = strPostData;
-    m_bPostBinary = bPostBinary;
+    m_options.strPostData = strPostData;
+    m_options.bPostBinary = bPostBinary;
     m_bIsFetch = true;
 
     m_strURL = szURL;
     m_strQueueName = strQueueName;
-    m_uiConnectionAttempts = uiConnectionAttempts;
-    m_uiConnectTimeoutMs = uiConnectTimeoutMs;
+    m_options.uiConnectionAttempts = uiConnectionAttempts;
+    m_options.uiConnectTimeoutMs = uiConnectTimeoutMs;
+}
+
+//Fetch version #2
+CRemoteCall::CRemoteCall ( const char * szURL, CLuaArguments * fetchArguments, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, const SHttpRequestOptions& options )
+    : m_FetchArguments ( *fetchArguments )
+{
+    m_VM = luaMain;
+    m_iFunction = iFunction;
+    m_bIsFetch = true;
+    m_strURL = szURL;
+    m_strQueueName = strQueueName;
+    m_options = options;
 }
 
 
@@ -192,9 +210,9 @@ CRemoteCall::~CRemoteCall ()
 
 void CRemoteCall::MakeCall()
 {
-    EDownloadModeType downloadMode = g_pGame->GetRemoteCalls()->GetDownloadModeForQueueName( m_strQueueName );
-    CNetHTTPDownloadManagerInterface * downloadManager = g_pNetServer->GetHTTPDownloadManager( downloadMode );
-    downloadManager->QueueFile ( m_strURL, NULL, m_strData.c_str (), m_strData.length (), m_bPostBinary, this, DownloadFinishedCallback, false, m_uiConnectionAttempts, m_uiConnectTimeoutMs );
+    EDownloadModeType downloadMode = g_pGame->GetRemoteCalls()->GetDownloadModeForQueueName(m_strQueueName);
+    CNetHTTPDownloadManagerInterface* pDownloadManager = g_pNetServer->GetHTTPDownloadManager(downloadMode);
+    pDownloadManager->QueueFile( m_strURL, NULL, this, DownloadFinishedCallback, false, m_options, false, false );
 }
 
 void CRemoteCall::DownloadFinishedCallback(const SHttpDownloadResult& result)
@@ -204,20 +222,48 @@ void CRemoteCall::DownloadFinishedCallback(const SHttpDownloadResult& result)
         return;
 
     CLuaArguments arguments;
-    if (result.bSuccess)
+    if (pCall->IsLegacy())
     {
-        if (pCall->IsFetch())
+        if (result.bSuccess)
         {
-            arguments.PushString(std::string(result.pData, result.dataSize));
-            arguments.PushNumber(0);
+            if (pCall->IsFetch())
+            {
+                arguments.PushString(std::string(result.pData, result.dataSize));
+                arguments.PushNumber(0);
+            }
+            else
+                arguments.ReadFromJSONString(result.pData);
         }
         else
-            arguments.ReadFromJSONString(result.pData);
+        {
+            arguments.PushString("ERROR");
+            arguments.PushNumber(result.iErrorCode);
+        }
+
     }
     else
     {
-        arguments.PushString("ERROR");
-        arguments.PushNumber(result.iErrorCode);
+        // Append response body
+        arguments.PushString(std::string(result.pData, result.dataSize));
+
+        // Append info table
+        CLuaArguments info;
+        info.PushString("success");
+        info.PushBoolean(result.iErrorCode >= 200 && result.iErrorCode <= 299);
+        info.PushString("statusCode");
+        info.PushNumber(result.iErrorCode);
+
+        // Headers as a subtable
+        CLuaArguments headers;
+        for (auto iter : result.headers )
+        {
+            headers.PushString(iter.first);
+            headers.PushString(iter.second);
+        }
+        info.PushString("headers");
+        info.PushTable(&headers);
+
+        arguments.PushTable(&info);
     }
 
     // Append stored arguments

Client/mods/deathmatch/logic/CRemoteCalls.h
@@ -22,26 +22,25 @@ i.e. the http client has downloaded all the data returned
 class CRemoteCall
 {
 private:
-    std::string         m_strData;
-    bool                m_bPostBinary;
     bool                m_bIsFetch;
     class CLuaMain *    m_VM;
     CLuaFunctionRef     m_iFunction;
     SString             m_strURL;
     SString             m_strQueueName;
-    uint                m_uiConnectionAttempts;
-    uint                m_uiConnectTimeoutMs;
     CLuaArguments       m_FetchArguments;
+    SHttpRequestOptions m_options; 
 
 public:
                         CRemoteCall ( const char * szServerHost, const char * szResourceName, const char * szFunctionName, CLuaArguments * arguments, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, uint uiConnectionAttempts, uint uiConnectTimeoutMs );
                         CRemoteCall ( const char * szURL, CLuaArguments * arguments, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, uint uiConnectionAttempts, uint uiConnectTimeoutMs );
                         CRemoteCall ( const char * szURL, CLuaArguments * fetchArguments, const SString& strPostData, bool bPostBinary, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, uint uiConnectionAttempts, uint uiConnectTimeoutMs );
+                        CRemoteCall ( const char * szURL, CLuaArguments * fetchArguments, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, const SHttpRequestOptions& options );
                         ~CRemoteCall ();
     void                MakeCall();
     static void         DownloadFinishedCallback( const SHttpDownloadResult& result );
     CLuaMain *          GetVM() {return m_VM;};
     bool                IsFetch() {return m_bIsFetch;}
+    bool                IsLegacy() {return m_options.bIsLegacy;}
     CLuaArguments&      GetFetchArguments() {return m_FetchArguments;}
 };
 
@@ -62,6 +61,7 @@ public:
     void                Call ( const char * szServerHost, const char * szResourceName, const char * szFunctionName, CLuaArguments * arguments, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, uint uiConnectionAttempts, uint uiConnectTimeoutMs );
     void                Call ( const char * szURL, CLuaArguments * arguments, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, uint uiConnectionAttempts, uint uiConnectTimeoutMs );
     void                Call ( const char * szURL, CLuaArguments * fetchArguments, const SString& strPostData, bool bPostBinary, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, uint uiConnectionAttempts, uint uiConnectTimeoutMs );
+    void                Call ( const char * szURL, CLuaArguments * fetchArguments, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, const SHttpRequestOptions& options );
     void                Remove ( CLuaMain * luaMain );
     void                Remove ( CRemoteCall * call );
     bool                CallExists ( CRemoteCall * call );

Client/mods/deathmatch/logic/lua/CLuaFunctionDefs.Util.cpp
@@ -433,7 +433,8 @@ int CLuaFunctionDefs::AddDebugHook ( lua_State* luaVM )
     CScriptArgReader argStream( luaVM );
     argStream.ReadEnumString( hookType );
     argStream.ReadFunction( callBack );
-    argStream.ReadStringTable( allowedNames, true );
+    if ( argStream.NextIsTable() )
+        argStream.ReadStringTable( allowedNames );
     argStream.ReadFunctionComplete ();
 
     if ( !argStream.HasErrors() )

Client/mods/deathmatch/logic/lua/CLuaFunctionDefs.World.cpp
@@ -21,6 +21,7 @@
 
 #define MIN_CLIENT_REQ_CALLREMOTE_QUEUE_NAME                "1.5.3-9.11270"
 #define MIN_CLIENT_REQ_FETCHREMOTE_CONNECT_TIMEOUT          "1.3.5"
+#define MIN_CLIENT_REQ_CALLREMOTE_OPTIONS_TABLE             "1.5.4-9.11334"
 
 int CLuaFunctionDefs::CreateExplosion ( lua_State* luaVM )
 {
@@ -1844,34 +1845,73 @@ int CLuaFunctionDefs::GetFPSLimit ( lua_State* luaVM )
 int CLuaFunctionDefs::FetchRemote ( lua_State* luaVM )
 {
 //  bool fetchRemote ( string URL [, string queueName ][, int connectionAttempts = 10, int connectTimeout = 10000 ], callback callbackFunction, [ string postData, bool bPostBinary, arguments... ] )
+//  bool fetchRemote ( string URL [, table options ], callback callbackFunction[, table callbackArguments ] )
     CScriptArgReader argStream ( luaVM );
-    SString strURL; SString strQueueName; CLuaFunctionRef iLuaFunction; SString strPostData; bool bPostBinary; CLuaArguments args; uint uiConnectionAttempts; uint uiConnectTimeoutMs;
+    SString strURL; SHttpRequestOptions httpRequestOptions; SString strQueueName; CLuaFunctionRef iLuaFunction; CLuaArguments callbackArguments;
 
     argStream.ReadString ( strURL );
-    if ( argStream.NextIsString () )
-        MinClientReqCheck ( argStream, MIN_CLIENT_REQ_CALLREMOTE_QUEUE_NAME, "'queue name' is being used" );
-    argStream.ReadIfNextIsString ( strQueueName, CALL_REMOTE_DEFAULT_QUEUE_NAME );
-    argStream.ReadIfNextIsNumber ( uiConnectionAttempts, 10 );
-    if ( argStream.NextIsNumber () )
-        MinClientReqCheck ( argStream, MIN_CLIENT_REQ_FETCHREMOTE_CONNECT_TIMEOUT, "'connect timeout' is being used" );
-    argStream.ReadIfNextIsNumber ( uiConnectTimeoutMs, 10000 );
-    argStream.ReadFunction ( iLuaFunction );
-    argStream.ReadString ( strPostData, "" );
-    argStream.ReadBool ( bPostBinary, false );
-    argStream.ReadLuaArguments ( args );
-    argStream.ReadFunctionComplete ();
-
-    if ( !argStream.HasErrors () )
+    if (!argStream.NextIsTable())
     {
-        CLuaMain * luaMain = m_pLuaManager->GetVirtualMachine ( luaVM );
-        if ( luaMain )
+        if ( argStream.NextIsString () )
+            MinClientReqCheck ( argStream, MIN_CLIENT_REQ_CALLREMOTE_QUEUE_NAME, "'queue name' is being used" );
+        argStream.ReadIfNextIsString ( strQueueName, CALL_REMOTE_DEFAULT_QUEUE_NAME );
+        argStream.ReadIfNextIsNumber ( httpRequestOptions.uiConnectionAttempts, 10 );
+        if ( argStream.NextIsNumber () )
+            MinClientReqCheck ( argStream, MIN_CLIENT_REQ_FETCHREMOTE_CONNECT_TIMEOUT, "'connect timeout' is being used" );
+        argStream.ReadIfNextIsNumber ( httpRequestOptions.uiConnectTimeoutMs, 10000 );
+        argStream.ReadFunction ( iLuaFunction );
+        argStream.ReadString ( httpRequestOptions.strPostData, "" );
+        argStream.ReadBool ( httpRequestOptions.bPostBinary, false );
+        argStream.ReadLuaArguments ( callbackArguments );
+        argStream.ReadFunctionComplete ();
+
+        if ( !argStream.HasErrors () )
         {
-            g_pClientGame->GetRemoteCalls()->Call ( strURL, &args, strPostData, bPostBinary, luaMain, iLuaFunction, strQueueName, uiConnectionAttempts, uiConnectTimeoutMs );
-            lua_pushboolean ( luaVM, true );
-            return 1;
+            CLuaMain * luaMain = m_pLuaManager->GetVirtualMachine ( luaVM );
+            if ( luaMain )
+            {
+                httpRequestOptions.bIsLegacy = true;
+                g_pClientGame->GetRemoteCalls()->Call(strURL, &callbackArguments, luaMain, iLuaFunction, strQueueName, httpRequestOptions);
+                lua_pushboolean ( luaVM, true );
+                return 1;
+            }
         }
     }
     else
+    {
+        MinClientReqCheck(argStream, MIN_CLIENT_REQ_CALLREMOTE_OPTIONS_TABLE, "'options' table is being used");
+        CStringMap optionsMap;
+
+        argStream.ReadStringMap(optionsMap);
+        argStream.ReadFunction(iLuaFunction);
+        if ( argStream.NextIsTable() )
+            argStream.ReadLuaArgumentsTable(callbackArguments);
+        argStream.ReadFunctionComplete();
+
+        optionsMap.ReadNumber("connectionAttempts", httpRequestOptions.uiConnectionAttempts, 10);
+        optionsMap.ReadNumber("connectTimeout", httpRequestOptions.uiConnectTimeoutMs, 10000);
+        optionsMap.ReadString("method", httpRequestOptions.strRequestMethod, "");
+        optionsMap.ReadString("queueName", strQueueName, CALL_REMOTE_DEFAULT_QUEUE_NAME);
+        optionsMap.ReadString("postData", httpRequestOptions.strPostData, "");
+        optionsMap.ReadBool("postIsBinary", httpRequestOptions.bPostBinary, false);
+
+        CStringMap headersMap;
+        optionsMap.ReadStringMap("headers", headersMap);
+        httpRequestOptions.requestHeaders.insert(headersMap.begin(), headersMap.end());
+
+        if (!argStream.HasErrors())
+        {
+            CLuaMain* luaMain = m_pLuaManager->GetVirtualMachine(luaVM);
+            if (luaMain)
+            {
+                g_pClientGame->GetRemoteCalls ()->Call(strURL, &callbackArguments, luaMain, iLuaFunction, strQueueName, httpRequestOptions);
+                lua_pushboolean(luaVM, true);
+                return 1;
+            }
+        }
+    }
+    
+    if ( argStream.HasErrors () )
         m_pScriptDebugging->LogCustom ( luaVM, argStream.GetFullErrorMessage () );
 
     lua_pushboolean ( luaVM, false );

Client/sdk/net/CNetHTTPDownloadManagerInterface.h
@@ -12,7 +12,16 @@
 #ifndef __CNETHTTPDOWNLOADMANAGERINTERFACE_H
 #define __CNETHTTPDOWNLOADMANAGERINTERFACE_H
 
-#include <windows.h>
+struct SHttpRequestOptions
+{
+    bool bIsLegacy = false;         // true = old error behaviour
+    SString strPostData;
+    bool bPostBinary = false;       // false = truncate data to first null character and send as text/plain (true = send as application/octet-stream)
+    uint uiConnectionAttempts = 10;
+    uint uiConnectTimeoutMs = 10000;
+    SString strRequestMethod;
+    std::map<SString,SString> requestHeaders;
+};
 
 struct SHttpDownloadResult
 {
@@ -21,12 +30,15 @@ struct SHttpDownloadResult
     void* pObj;
     bool bSuccess;
     int iErrorCode;
-    const char* szHeaders;  // new line separated list of HTTP headers
+    std::map<SString,SString> headers;
 };
 
 // PFN_DOWNLOAD_FINISHED_CALLBACK is called once at the end of the download.
 // If bSuccess is true, then pData+dataSize will be set or the output file will be ready.
 // If bSuccess is false, then iErrorCode and CNetHTTPDownloadManagerInterface->GetError() will reveal the problem.
+// When iErrorCode is 400-599:
+//      If bIsLegacy is true, then bSuccess is false and pData+dataSize contain nothing
+//      If bIsLegacy is false, then bSuccess is true and pData+dataSize contain the server response
 typedef void (*PFN_DOWNLOAD_FINISHED_CALLBACK) ( const SHttpDownloadResult& result );
 
 class CNetHTTPDownloadManagerInterface
@@ -44,8 +56,8 @@ public:
     virtual bool            ProcessQueuedFiles  ( void ) = 0;
 
     // Queue a file to download
-    // szRequestHeaders is a new line separated list of HTTP headers. Examples at https://en.wikipedia.org/wiki/List_of_HTTP_header_fields
-    virtual bool            QueueFile           ( const char* szURL, const char* szOutputFile, const char* szPostData = NULL, unsigned int uiPostSize = 0, bool bPostBinary = false, void * objectPtr = NULL, PFN_DOWNLOAD_FINISHED_CALLBACK pfnDownloadFinishedCallback = NULL, bool bIsLocal = false, uint uiConnectionAttempts = 10, uint uiConnectTimeoutMs = 10000, bool bCheckContents = false, bool bResumeFile = false, const char* szRequestHeaders = nullptr, const char* szRequestMethod = nullptr ) = 0;
+    virtual bool            QueueFile           ( const char* szURL, const char* szOutputFile, const char* szPostData = NULL, unsigned int uiPostSize = 0, bool bPostBinary = false, void * objectPtr = NULL, PFN_DOWNLOAD_FINISHED_CALLBACK pfnDownloadFinishedCallback = NULL, bool bIsLocal = false, uint uiConnectionAttempts = 10, uint uiConnectTimeoutMs = 10000, bool bCheckContents = false, bool bResumeFile = false ) = 0;
+    virtual bool            QueueFile           ( const char* szURL, const char* szOutputFile, void * objectPtr = NULL, PFN_DOWNLOAD_FINISHED_CALLBACK pfnDownloadFinishedCallback = NULL, bool bIsLocal = false, const SHttpRequestOptions& options = SHttpRequestOptions(), bool bCheckContents = false, bool bResumeFile = false ) = 0;
 
     // Limit number of concurrent http client connections
     virtual void            SetMaxConnections   ( int iMaxConnections ) = 0;

Client/version.h
@@ -75,7 +75,7 @@
 
 #define _ASE_VERSION QUOTE_DEFINE(MTASA_VERSION_MAJOR) "." QUOTE_DEFINE(MTASA_VERSION_MINOR)
 #define _NETCODE_VERSION_BRANCH_ID      0x4         // Use 0x1 - 0xF to indicate an incompatible branch is being used (0x0 is reserved, 0x4 is trunk)
-#define _CLIENT_NET_MODULE_VERSION      0x0A5       // (0x000 - 0xfff) Lvl9 wizards only
+#define _CLIENT_NET_MODULE_VERSION      0x0A6       // (0x000 - 0xfff) Lvl9 wizards only
 #define _NETCODE_VERSION                0x1DA       // (0x000 - 0xfff) Increment when net messages change (pre-release)
 #define MTA_DM_BITSTREAM_VERSION        0x06A       // (0x000 - 0xfff) Increment when net messages change (post-release). (Changing will also require additional backward compatibility code).
 

Server/mods/deathmatch/logic/CRemoteCalls.h
@@ -22,26 +22,25 @@ i.e. the http client has downloaded all the data returned
 class CRemoteCall
 {
 private:
-    std::string         m_strData;
-    bool                m_bPostBinary;
     bool                m_bIsFetch;
     class CLuaMain *    m_VM;
     CLuaFunctionRef     m_iFunction;
     SString             m_strURL;
     SString             m_strQueueName;
-    uint                m_uiConnectionAttempts;
-    uint                m_uiConnectTimeoutMs;
     CLuaArguments       m_FetchArguments;
+    SHttpRequestOptions m_options; 
 
 public:
                         CRemoteCall ( const char * szServerHost, const char * szResourceName, const char * szFunctionName, CLuaArguments * arguments, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, uint uiConnectionAttempts, uint uiConnectTimeoutMs );
                         CRemoteCall ( const char * szURL, CLuaArguments * arguments, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, uint uiConnectionAttempts, uint uiConnectTimeoutMs );
                         CRemoteCall ( const char * szURL, CLuaArguments * fetchArguments, const SString& strPostData, bool bPostBinary, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, uint uiConnectionAttempts, uint uiConnectTimeoutMs );
+                        CRemoteCall ( const char * szURL, CLuaArguments * fetchArguments, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, const SHttpRequestOptions& options );
                         ~CRemoteCall ();
     void                MakeCall();
     static void         DownloadFinishedCallback( const SHttpDownloadResult& result );
     CLuaMain *          GetVM() {return m_VM;};
     bool                IsFetch() {return m_bIsFetch;}
+    bool                IsLegacy() {return m_options.bIsLegacy;}
     CLuaArguments&      GetFetchArguments() {return m_FetchArguments;}
 };
 
@@ -62,6 +61,7 @@ public:
     void                Call ( const char * szServerHost, const char * szResourceName, const char * szFunctionName, CLuaArguments * arguments, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, uint uiConnectionAttempts, uint uiConnectTimeoutMs );
     void                Call ( const char * szURL, CLuaArguments * arguments, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, uint uiConnectionAttempts, uint uiConnectTimeoutMs );
     void                Call ( const char * szURL, CLuaArguments * fetchArguments, const SString& strPostData, bool bPostBinary, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, uint uiConnectionAttempts, uint uiConnectTimeoutMs );
+    void                Call ( const char * szURL, CLuaArguments * fetchArguments, CLuaMain * luaMain, const CLuaFunctionRef& iFunction, const SString& strQueueName, const SHttpRequestOptions& options );
     void                Remove ( CLuaMain * luaMain );
     void                Remove ( CRemoteCall * call );
     bool                CallExists ( CRemoteCall * call );

commit 02200370ab80ec20a3bac492216bedbec716465a

Fixed callRemote() callback arguments. Bug introduced in ffa943c (Added to fetchRemote: request method, request headers and response headers)

Client/mods/deathmatch/logic/CRemoteCalls.cpp
@@ -152,6 +152,7 @@ CRemoteCall::CRemoteCall ( const char * szServerHost, const char * szResourceNam
 
     arguments->WriteToJSONString ( m_options.strPostData, true );
     m_options.bPostBinary = false;
+    m_options.bIsLegacy = true;
     m_bIsFetch = false;
 
     m_strURL = SString ( "http://%s/%s/call/%s", szServerHost, szResourceName, szFunctionName );
@@ -168,6 +169,7 @@ CRemoteCall::CRemoteCall ( const char * szURL, CLuaArguments * arguments, CLuaMa
 
     arguments->WriteToJSONString ( m_options.strPostData, true );
     m_options.bPostBinary = false;
+    m_options.bIsLegacy = true;
     m_bIsFetch = false;
 
     m_strURL = szURL;
@@ -185,6 +187,7 @@ CRemoteCall::CRemoteCall ( const char * szURL, CLuaArguments * fetchArguments, c
 
     m_options.strPostData = strPostData;
     m_options.bPostBinary = bPostBinary;
+    m_options.bIsLegacy = true;
     m_bIsFetch = true;
 
     m_strURL = szURL;

Server/mods/deathmatch/logic/CRemoteCalls.cpp
@@ -150,6 +150,7 @@ CRemoteCall::CRemoteCall ( const char * szServerHost, const char * szResourceNam
 
     arguments->WriteToJSONString ( m_options.strPostData, true );
     m_options.bPostBinary = false;
+    m_options.bIsLegacy = true;
     m_bIsFetch = false;
 
     m_strURL = SString ( "http://%s/%s/call/%s", szServerHost, szResourceName, szFunctionName );
@@ -166,6 +167,7 @@ CRemoteCall::CRemoteCall ( const char * szURL, CLuaArguments * arguments, CLuaMa
 
     arguments->WriteToJSONString ( m_options.strPostData, true );
     m_options.bPostBinary = false;
+    m_options.bIsLegacy = true;
     m_bIsFetch = false;
 
     m_strURL = szURL;
@@ -183,6 +185,7 @@ CRemoteCall::CRemoteCall ( const char * szURL, CLuaArguments * fetchArguments, c
 
     m_options.strPostData = strPostData;
     m_options.bPostBinary = bPostBinary;
+    m_options.bIsLegacy = true;
     m_bIsFetch = true;
 
     m_strURL = szURL;