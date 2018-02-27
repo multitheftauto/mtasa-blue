894b4f6336e45042f89a0728d61aaaea7269f8e0
idanish <danishroar@gmail.com>
2018-02-28 04:48:04 +0500

    Displays Console. I'm only using it for testing

--- Client/core/Core.cpp
+++ Client/core/Core.cpp
@@ -31,6 +31,15 @@ int WINAPI DllMain(HINSTANCE hModule, DWORD dwReason, PVOID pvNothing)
         WriteDebugEvent( SString( "DLL_PROCESS_ATTACH %08x", pvNothing ) );
         if ( IsGTAProcess() )
         {
+            if (AllocConsole())
+            {
+                freopen("CONIN$", "r", stdin);
+                freopen("CONOUT$", "w", stdout);
+                freopen("CONOUT$", "w", stderr);
+                printf("Okay, enjoy your console :)\n");
+            }
+
+
             WriteDebugEvent( SString( "ModuleFileName: %s", *GetLaunchPathFilename() ) );
 
             AddUtf8FileHooks();
