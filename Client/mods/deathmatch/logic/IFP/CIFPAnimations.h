412a42a99e816746acf769a367fcb49df46c0e84
idanish <danishroar@gmail.com>
2018-03-27 04:28:01 +0500

    Added IFP_Animation struct and removed unused members.

--- Client/mods/deathmatch/logic/IFP/CIFPAnimations.h
+++ Client/mods/deathmatch/logic/IFP/CIFPAnimations.h
@@ -1,17 +1,19 @@
 #pragma
 #ifndef __CIFPANIMATIONS_H
 #define __CIFPANIMATIONS_H
-#include "../mods/deathmatch/logic/IFP/IFPLoader.h"
+#include "../Client/mods/deathmatch/logic/CClientIFP.h"
 
 class CIFPAnimations
 {
     public:
-    // This is set to true when unloadIFP function is called, it means that animations
-    // can be unloaded when iReferences reaches zero, if this boolean is set to true
-	bool  bUnloadOnZeroReferences; 
-    // incremented when a new CAnimBlendAssociationSAInteface is created and decremented 
-    // when destructor of that animation is called 
-	DWORD iReferences; 
+
+    struct IFP_Animation
+    {
+        SString                           Name;
+        CAnimBlendHierarchySAInterface    Hierarchy;
+        char *                            pSequencesMemory;
+    };
+
 	std::vector < IFP_Animation > vecAnimations;	
 
 public:
