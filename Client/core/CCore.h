70bdef2c35d28c0b5da287461c6b78d542e3f15a
ccw808 <ccw808@googlemail.com>
2018-05-22 05:16:53 +0100

    Automated copyright date

--- Client/core/CCore.h
+++ Client/core/CCore.h
@@ -46,7 +46,7 @@ class CCore;
 #include <dinput.h>
 
 #define BLUE_VERSION_STRING     "Multi Theft Auto v" MTA_DM_BUILDTAG_LONG
-#define BLUE_COPYRIGHT_STRING    _td("Copyright (C) 2003 - 2018 Multi Theft Auto")
+#define BLUE_COPYRIGHT_STRING   "Copyright (C) 2003 - %BUILD_YEAR% Multi Theft Auto"
 
 // Configuration file path (relative to MTA install directory)
 #define MTA_CONFIG_PATH             "mta/config/coreconfig.xml"
@@ -272,6 +272,7 @@ public:
     const char* GetProductVersion(void) { return SharedUtil::GetProductVersion(); }
     void        SetFakeLagCommandEnabled(bool bEnabled) { m_bFakeLagCommandEnabled = bEnabled; }
     bool        IsFakeLagCommandEnabled(void) { return m_bFakeLagCommandEnabled; }
+    SString     GetBlueCopyrightString(void);
 
 private:
     // Core devices.
