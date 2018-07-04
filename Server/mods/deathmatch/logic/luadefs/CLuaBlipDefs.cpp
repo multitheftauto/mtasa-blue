9da68cc70fb03254f1e10de46a3abfd93e820462
CrosRoad95 <sebajura1234@gmail.com>
2018-07-04 17:34:26 +0200

    fix

--- Server/mods/deathmatch/logic/luadefs/CLuaBlipDefs.cpp
+++ Server/mods/deathmatch/logic/luadefs/CLuaBlipDefs.cpp
@@ -165,7 +165,7 @@ int CLuaBlipDefs::CreateBlipAttachedTo(lua_State* luaVM)
                 }
 
                 CLuaArguments Arguments;
-                pBlip->CallEvent("onElementCreated", Arguments, false);
+                pBlip->CallEvent("onElementCreated", Arguments);
 
                 lua_pushelement(luaVM, pBlip);
                 return 1;
