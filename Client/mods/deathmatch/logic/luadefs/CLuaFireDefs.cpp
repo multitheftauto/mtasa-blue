eeff04bb7be1c44a40cf3cf49d47288fc2091316
Patrik Juvonen <patrik@scope.studio>
2018-07-23 00:02:23 +0300

    CLuaFireDefs: unify extinguish functions as extinguishFire

--- Client/mods/deathmatch/logic/luadefs/CLuaFireDefs.cpp
+++ Client/mods/deathmatch/logic/luadefs/CLuaFireDefs.cpp
@@ -13,8 +13,7 @@
 void CLuaFireDefs::LoadFunctions(void)
 {
     CLuaCFunctions::AddFunction("createFire", CLuaFireDefs::CreateFire);
-    CLuaCFunctions::AddFunction("extinguishFireInRadius", CLuaFireDefs::ExtinguishFireInRadius);
-    CLuaCFunctions::AddFunction("extinguishAllFires", CLuaFireDefs::ExtinguishAllFires);
+    CLuaCFunctions::AddFunction("extinguishFire", CLuaFireDefs::ExtinguishFire);
 }
 
 int CLuaFireDefs::CreateFire(lua_State* luaVM)
@@ -42,34 +41,34 @@ int CLuaFireDefs::CreateFire(lua_State* luaVM)
     return 1;
 }
 
-int CLuaFireDefs::ExtinguishFireInRadius(lua_State* luaVM)
+int CLuaFireDefs::ExtinguishFire(lua_State* luaVM)
 {
-    // bool extinguishFireInRadius ( float x, float y, float z [, float radius = 1.0 ] )
-    CVector vecPosition;
-    float   fRadius;
-
+    // bool extinguishFire ( [ float x, float y, float z [, float radius = 1.0 ] ] )
     CScriptArgReader argStream(luaVM);
-    argStream.ReadVector3D(vecPosition);
-    argStream.ReadNumber(fRadius, 1.0f);
 
-    if (!argStream.HasErrors())
+    if (argStream.NextIsVector3D())
     {
-        if (CStaticFunctionDefinitions::ExtinguishFireInRadius(vecPosition, fRadius))
+        CVector vecPosition;
+        float   fRadius;
+
+        argStream.ReadVector3D(vecPosition);
+        argStream.ReadNumber(fRadius, 1.0f);
+
+        if (!argStream.HasErrors())
         {
-            lua_pushboolean(luaVM, true);
-            return 1;
+            if (CStaticFunctionDefinitions::ExtinguishFireInRadius(vecPosition, fRadius))
+            {
+                lua_pushboolean(luaVM, true);
+                return 1;
+            }
         }
+        else
+            m_pScriptDebugging->LogCustom(luaVM, argStream.GetFullErrorMessage());
+
+        lua_pushboolean(luaVM, false);
+        return 1;
     }
-    else
-        m_pScriptDebugging->LogCustom(luaVM, argStream.GetFullErrorMessage());
 
-    lua_pushboolean(luaVM, false);
-    return 1;
-}
-
-int CLuaFireDefs::ExtinguishAllFires(lua_State* luaVM)
-{
-    // bool extinguishAllFires ( )
     lua_pushboolean(luaVM, CStaticFunctionDefinitions::ExtinguishAllFires());
     return 1;
 }
