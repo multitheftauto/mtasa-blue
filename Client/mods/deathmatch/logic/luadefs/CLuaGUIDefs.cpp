56bf4663a981a5c0393bb47a4b9b5ec5341cc4c7
CrosRoad95 <sebajura1234@gmail.com>
2018-07-23 18:09:40 +0200

    update cluaguidefs.cpp

--- Client/mods/deathmatch/logic/luadefs/CLuaGUIDefs.cpp
+++ Client/mods/deathmatch/logic/luadefs/CLuaGUIDefs.cpp
@@ -1,13 +1,13 @@
 /*****************************************************************************
- *
- *  PROJECT:     Multi Theft Auto
- *  LICENSE:     See LICENSE in the top level directory
- *  FILE:        mods/shared_logic/luadefs/CLuaGUIDefs.cpp
- *  PURPOSE:     Lua definitions class
- *
- *  Multi Theft Auto is available from http://www.multitheftauto.com/
- *
- *****************************************************************************/
+*
+*  PROJECT:     Multi Theft Auto
+*  LICENSE:     See LICENSE in the top level directory
+*  FILE:        mods/shared_logic/luadefs/CLuaGUIDefs.cpp
+*  PURPOSE:     Lua definitions class
+*
+*  Multi Theft Auto is available from http://www.multitheftauto.com/
+*
+*****************************************************************************/
 
 #include "StdInc.h"
 
@@ -24,7 +24,6 @@ void CLuaGUIDefs::LoadFunctions(void)
     CLuaCFunctions::AddFunction("setDebugViewActive", GUISetDebugViewActive);
     CLuaCFunctions::AddFunction("isMainMenuActive", GUIIsMainMenuActive);
     CLuaCFunctions::AddFunction("isMTAWindowActive", GUIIsMTAWindowActive);
-
     CLuaCFunctions::AddFunction("isTransferBoxActive", GUIIsTransferBoxActive);
     CLuaCFunctions::AddFunction("isTransferBoxEnabled", GUIIsTransferBoxEnabled);
     CLuaCFunctions::AddFunction("setTransferBoxEnabled", GUISetTransferBoxEnabled);
@@ -131,14 +130,10 @@ void CLuaGUIDefs::LoadFunctions(void)
     CLuaCFunctions::AddFunction("guiEditSetMasked", GUIEditSetMasked);
     CLuaCFunctions::AddFunction("guiEditSetMaxLength", GUIEditSetMaxLength);
     CLuaCFunctions::AddFunction("guiEditSetReadOnly", GUIEditSetReadOnly);
-    CLuaCFunctions::AddFunction("guiEditIsReadOnly", GUIEditIsReadOnly);
 
     CLuaCFunctions::AddFunction("guiMemoSetCaretIndex", GUIMemoSetCaretIndex);
     CLuaCFunctions::AddFunction("guiMemoGetCaretIndex", GUIMemoGetCaretIndex);
     CLuaCFunctions::AddFunction("guiMemoSetReadOnly", GUIMemoSetReadOnly);
-    CLuaCFunctions::AddFunction("guiMemoIsReadOnly", GUIMemoIsReadOnly);
-    CLuaCFunctions::AddFunction("guiMemoSetVerticalScrollPosition", GUIMemoSetVerticalScrollPosition);
-    CLuaCFunctions::AddFunction("guiMemoGetVerticalScrollPosition", GUIMemoGetVerticalScrollPosition);
 
     CLuaCFunctions::AddFunction("guiLabelSetColor", GUILabelSetColor);
     CLuaCFunctions::AddFunction("guiLabelGetColor", GUILabelGetColor);
@@ -283,7 +278,7 @@ void CLuaGUIDefs::AddGuiEditClass(lua_State* luaVM)
     lua_classfunction(luaVM, "setMaxLength", "guiEditSetMaxLength");
 
     lua_classvariable(luaVM, "caretIndex", "guiEditSetCaretIndex", "guiEditGetCaretIndex");
-    lua_classvariable(luaVM, "readOnly", "guiEditSetReadOnly", "guiEditIsReadOnly");
+    lua_classvariable(luaVM, "readOnly", "guiEditSetReadOnly", NULL);
     lua_classvariable(luaVM, "masked", "guiEditSetMasked", NULL);
     lua_classvariable(luaVM, "maxLength", "guiEditSetMaxLength", NULL);
 
@@ -321,15 +316,12 @@ void CLuaGUIDefs::AddGuiMemoClass(lua_State* luaVM)
     lua_classfunction(luaVM, "create", "guiCreateMemo");
 
     lua_classfunction(luaVM, "getCaretIndex", "guiMemoGetCaretIndex");
-    lua_classfunction(luaVM, "getVerticalScrollPosition", "guiMemoGetVerticalScrollPosition");
 
     lua_classfunction(luaVM, "setCaretIndex", "guiMemoSetCaretIndex");
-    lua_classfunction(luaVM, "setVerticalScrollPosition", "guiMemoSetVerticalScrollPosition");
     lua_classfunction(luaVM, "setReadOnly", "guiMemoSetReadOnly");
 
     lua_classvariable(luaVM, "caretIndex", "guiMemoSetCaretIndex", "guiMemoGetCaretIndex");
-    lua_classvariable(luaVM, "verticalScrollPosition", "guiMemoSetVerticalScrollPosition", "guiMemoGetVerticalScrollPosition");
-    lua_classvariable(luaVM, "readOnly", "guiMemoSetReadOnly", "guiMemoIsReadOnly");
+    lua_classvariable(luaVM, "readOnly", "guiMemoSetReadOnly", NULL);
 
     lua_registerclass(luaVM, "GuiMemo", "GuiElement");
 }
@@ -639,6 +631,12 @@ int CLuaGUIDefs::GUIIsMTAWindowActive(lua_State* luaVM)
         (g_pCore->IsChatInputEnabled() || g_pCore->IsMenuVisible() || g_pCore->GetConsole()->IsVisible() || g_pClientGame->GetTransferBox()->IsVisible());
 
     lua_pushboolean(luaVM, bActive);
+    return 1;
+}
+
+int CLuaGUIDefs::GUIIsTransferBoxActive(lua_State* luaVM)
+{
+    lua_pushboolean(luaVM, g_pClientGame->GetTransferBox()->IsVisible());
     return 1;
 }
 
@@ -2989,28 +2987,6 @@ int CLuaGUIDefs::GUIEditSetReadOnly(lua_State* luaVM)
     return 1;
 }
 
-int CLuaGUIDefs::GUIEditIsReadOnly(lua_State* luaVM)
-{
-    // bool guiEditIsReadOnly( element editField )
-    CClientGUIElement* editField;
-
-    CScriptArgReader argStream(luaVM);
-    argStream.ReadUserData<CGUIEdit>(editField);
-
-    if (!argStream.HasErrors())
-    {
-        bool readOnly = static_cast<CGUIEdit*>(editField->GetCGUIElement())->IsReadOnly();
-        lua_pushboolean(luaVM, readOnly);
-        return 1;
-    }
-    else
-        m_pScriptDebugging->LogCustom(luaVM, argStream.GetFullErrorMessage());
-
-    // error: bad arguments
-    lua_pushnil(luaVM);
-    return 1;
-}
-
 int CLuaGUIDefs::GUIMemoSetReadOnly(lua_State* luaVM)
 {
     //  bool guiMemoSetReadOnly ( gui-memo theMemo, bool status )
@@ -3032,28 +3008,6 @@ int CLuaGUIDefs::GUIMemoSetReadOnly(lua_State* luaVM)
 
     // error: bad arguments
     lua_pushboolean(luaVM, false);
-    return 1;
-}
-
-int CLuaGUIDefs::GUIMemoIsReadOnly(lua_State* luaVM)
-{
-    // bool guiMemoIsReadOnly( gui-memo theMemo )
-    CClientGUIElement* theMemo;
-
-    CScriptArgReader argStream(luaVM);
-    argStream.ReadUserData<CGUIMemo>(theMemo);
-
-    if (!argStream.HasErrors())
-    {
-        bool readOnly = static_cast<CGUIMemo*>(theMemo->GetCGUIElement())->IsReadOnly();
-        lua_pushboolean(luaVM, readOnly);
-        return 1;
-    }
-    else
-        m_pScriptDebugging->LogCustom(luaVM, argStream.GetFullErrorMessage());
-
-    // error: bad arguments
-    lua_pushnil(luaVM);
     return 1;
 }
 
@@ -3174,29 +3128,6 @@ int CLuaGUIDefs::GUIMemoSetCaretIndex(lua_State* luaVM)
     return 1;
 }
 
-int CLuaGUIDefs::GUIMemoSetVerticalScrollPosition(lua_State* luaVM)
-{
-    //  bool guiMemoSetVerticalScrollPosition ( gui-memo theMemo, float fPosition )
-    CClientGUIElement* theMemo;
-    float              fPosition;
-
-    CScriptArgReader argStream(luaVM);
-    argStream.ReadUserData<CGUIMemo>(theMemo);
-    argStream.ReadNumber(fPosition);
-
-    if (!argStream.HasErrors())
-    {
-        CStaticFunctionDefinitions::GUIMemoSetVerticalScrollPosition(*theMemo, fPosition);
-        lua_pushboolean(luaVM, true);
-        return 1;
-    }
-    else
-        m_pScriptDebugging->LogCustom(luaVM, argStream.GetFullErrorMessage());
-
-    lua_pushboolean(luaVM, false);
-    return 1;
-}
-
 int CLuaGUIDefs::GUIMemoGetCaretIndex(lua_State* luaVM)
 {
     //  bool guiMemoGetCaretIndex ( gui-memo theMemo )
@@ -3208,29 +3139,6 @@ int CLuaGUIDefs::GUIMemoGetCaretIndex(lua_State* luaVM)
     if (!argStream.HasErrors())
     {
         lua_pushnumber(luaVM, static_cast<CGUIMemo*>(theMemo->GetCGUIElement())->GetCaretIndex());
-        return 1;
-    }
-    else
-        m_pScriptDebugging->LogCustom(luaVM, argStream.GetFullErrorMessage());
-
-    // error: bad arguments
-    lua_pushboolean(luaVM, false);
-    return 1;
-}
-
-int CLuaGUIDefs::GUIMemoGetVerticalScrollPosition(lua_State* luaVM)
-{
-    //  float guiMemoGetVerticalScrollPosition ( gui-memo theMemo )
-    CClientGUIElement* theMemo;
-
-    CScriptArgReader argStream(luaVM);
-    argStream.ReadUserData<CGUIMemo>(theMemo);
-
-    if (!argStream.HasErrors())
-    {
-        CGUIMemo* guiMemo = static_cast<CGUIMemo*>(theMemo->GetCGUIElement());
-        float     fPos = guiMemo->GetVerticalScrollPosition() / guiMemo->GetMaxVerticalScrollPosition() * 100.0f;
-        lua_pushnumber(luaVM, fPos);
         return 1;
     }
     else
