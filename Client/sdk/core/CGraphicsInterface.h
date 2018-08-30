893c134cad9fdd431bafb441ee6d789710c01603
Kamil Marciniak <kammar3@st.amu.edu.pl>
2018-08-31 00:55:29 +0200

    Queued dxDrawPrimitive and dxDrawMaterialPrimitive (#339)
    * Auto stash before merge of "dxDrawPrimitive" and "multitheftauto/master"
    
    Basic dxDrawPrimitive
    
    * Added material caching, so no settings are changed needlessly
    
    * Small fixes
    
    * Naming changes, formatting
    
    * Fixed conditions checking for Pointlist, cleaned up classes a bit
    
    - dxDrawPrimitive doesn't require you to draw at least 2 points now
    - Batcher classes did draw only a single point at all times anyways tho, fixed that
    - Cleaned up CPrimitiveBatcher.cpp
    - Replaced NULLs with nullptrs
    
    * Fix for pointlist checking
    
    Noticed that with last commit it would be possible to try draw 0 points without error
    
    * linestrip condition checking
    
    * hopefully no more bad stuff
    
    * gitignore again
    
    Co-authored-by: CrosRoad95 <sebajura1234@gmail.com>

--- Client/sdk/core/CGraphicsInterface.h
+++ Client/sdk/core/CGraphicsInterface.h
@@ -13,6 +13,22 @@
 #define __CGRAPHICSINTERFACE_H
 
 #include "CVector.h"
+#include <d3d9.h>
+
+// Vertex type used by the primitives batchers
+struct PrimitiveVertice
+{
+    static const uint FNV = D3DFVF_XYZ | D3DFVF_DIFFUSE;
+    float             fX, fY, fZ;
+    D3DCOLOR          Color;
+};
+struct PrimitiveMaterialVertice
+{
+    static const uint FNV = D3DFVF_XYZ | D3DFVF_DIFFUSE | D3DFVF_TEX1;
+    float             fX, fY, fZ;
+    D3DCOLOR          Color;
+    float             u, v;
+};
 
 struct ID3DXFont;
 struct IDirect3DDevice9;
@@ -127,7 +143,11 @@ public:
                                   float fScaleY, unsigned long ulFormat, ID3DXFont* pDXFont, bool bPostGUI, bool bColorCoded = false,
                                   bool bSubPixelPositioning = false, float fRotation = 0, float fRotationCenterX = 0, float fRotationCenterY = 0) = 0;
 
-    virtual void DrawCircleQueued(float fX, float fY, float fRadius, float fStartAngle, float fStopAngle, unsigned long ulColor, unsigned long ulColorCenter, short siSegments, float fRatio, bool bPostGUI) = 0;
+    virtual void DrawPrimitiveQueued(const std::vector<PrimitiveVertice>& vertices, D3DPRIMITIVETYPE type, bool bPostGUI) = 0;
+    virtual void DrawMaterialPrimitiveQueued(const std::vector<PrimitiveMaterialVertice>& vertices, D3DPRIMITIVETYPE type, CMaterialItem* pMaterial,
+                                             bool bPostGUI) = 0;
+    virtual void DrawCircleQueued(float fX, float fY, float fRadius, float fStartAngle, float fStopAngle, unsigned long ulColor, unsigned long ulColorCenter,
+                                  short siSegments, float fRatio, bool bPostGUI) = 0;
 
     // Subsystems
     virtual CRenderItemManagerInterface* GetRenderItemManager(void) = 0;
