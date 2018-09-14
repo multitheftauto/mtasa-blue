fe2ee205531cbaa8ba019935caacb2e3b788d604
Qais Patankar <qaisjp@gmail.com>
2018-09-14 16:16:40 +0100

    Format CAnimBlendAssociationSAInterface

--- Client/game_sa/CAnimBlendAssociationSA.h
+++ Client/game_sa/CAnimBlendAssociationSA.h
@@ -37,40 +37,51 @@ public:
     float                           fTimeStep;                      // 40
     short                           sAnimID;                        // 44
     union {
-        struct {
-            unsigned short m_bPlaying : 1;                   // Animation will stop playing if flag is not set       
-            unsigned short m_bLooped : 1;                    // Animation will always restart when it completes 
-            unsigned short m_bFreezeLastFrame : 1;           // Animation will freeze on last frame           
-            unsigned short m_bLockLastFrame : 1;             // When this flag is Set, IF m_bLooped == false, m_bPlaying == true, and anim progress >= total anim time
-                                                             // THEN m_bFreezeLastFrame is set to TRUE and m_fBlendDelta is set to -4.0.
-                                                             // Result: https://i.imgur.com/idw0jsX.png
+        struct
+        {
+            unsigned short m_bPlaying : 1;                    // Anim will stop playing if flag is not set
+            unsigned short m_bLooped : 1;                     // Anim will always restart when it completes
+            unsigned short m_bFreezeLastFrame : 1;            // Anim will freeze on last frame
 
-            unsigned short m_bPartial : 1;                   // Partial animations run along other animations, like 
-            unsigned short m_bEnableMovement : 1;            // blends all playing animations together if set
+            // When this flag is Set, IF m_bLooped == false, m_bPlaying == true, and anim progress >= total anim time
+            // THEN m_bFreezeLastFrame is set to TRUE and m_fBlendDelta is set to -4.0.
+            // Result: https://i.imgur.com/idw0jsX.png
+            unsigned short m_bLockLastFrame : 1;
 
-                                                             // Camera will not move with ped, and ped will go back to the initial position when animation is over 
+            unsigned short m_bPartial : 1;                   // Partial anims run along other anims
+            unsigned short m_bEnableMovement : 1;            // blends all playing anims together if set
+
+            // Camera will not move with ped, and ped will go back to the initial position when anim is over
             unsigned short m_bLockLastX : 1;
-            unsigned short m_bLockLastY : 1;                 // only applies if m_bLockLastX is set             
+            unsigned short m_bLockLastY : 1;            // only applies if m_bLockLastX is set
 
-            unsigned short m_bf9 : 1;                        // doesn't seem to be used  
-            unsigned short m_bf10 : 1;                       // doesn't seem to be used  
-            unsigned short m_bAddAnimBlendToTotalBlend : 1;  // If set to TRUE, then result:
-                                                             // Before = https://i.imgur.com/c8T7xNK.png | AFTER = https://i.imgur.com/4gqlA4n.png
-            unsigned short m_bf12 : 1;                       // doesn't seem to be used  
-            unsigned short m_bSecondaryTaskAnim : 1;         // doesn't seem to be used          
-            unsigned short m_bFreezeTranslation : 1;         // Animation will play. Translation values will be ignored for animation (m_bEnableTranslation is ignored if set), and
-                                                             // it only applies to some animations though
-            unsigned short m_bBlockReferenced : 1;           // Animation block can't be unloaded if it's referenced by an animation.  
-            unsigned short m_bIndestructible : 1;            // Animation will not be destroyed. It will be played simultaneously with other animations (multiple animations at once)
-                                                             // if you set this flag while sitting in a car, you'll still be seated even if you exit the vehicle or play other animations,
-                                                             // like walking, running, crouching, etc, like this: https://i.imgur.com/VgWn5fl.png
+            unsigned short m_bf9 : 1;             // doesn't seem to be used
+            unsigned short m_bf10 : 1;            // doesn't seem to be used
+
+            // If set to TRUE, then result:
+            // Before = https://i.imgur.com/c8T7xNK.png | AFTER = https://i.imgur.com/4gqlA4n.png
+            unsigned short m_bAddAnimBlendToTotalBlend : 1;
+
+            unsigned short m_bf12 : 1;                          // doesn't seem to be used
+            unsigned short m_bSecondaryTaskAnim : 1;            // doesn't seem to be used
+
+            // Anim will play. Translation values will be ignored for anim (m_bEnableTranslation
+            // is ignored if set), and it only applies to some anims though
+            unsigned short m_bFreezeTranslation : 1;
+            unsigned short m_bBlockReferenced : 1;            // anim block can't be unloaded if it's referenced by an anim.
+
+            // Anim will not be destroyed. It will be played simultaneously with other anims
+            // (multiple anims at once) if you set this flag while sitting in a car, you'll still be
+            // seated even if you exit the vehicle or play other anims, like walking, running, crouching,
+            // etc, like this: https://i.imgur.com/VgWn5fl.png
+            unsigned short m_bIndestructible : 1;
         };
         unsigned short m_nFlags;
     };
-    unsigned int                    uiCallbackType;                 // 48:  1 means finished and 2 means delete
-    DWORD*                          pCallbackFunc;                  // 52
-    DWORD*                          pCallbackData;                  // 56
-                                                                    // Total: 60 bytes
+    unsigned int uiCallbackType;            // 48:  1 means finished and 2 means delete
+    DWORD*       pCallbackFunc;             // 52
+    DWORD*       pCallbackData;             // 56
+                                            // Total: 60 bytes
 };
 static_assert(sizeof(CAnimBlendAssociationSAInterface) == 0x3C, "Incorrect class size: CAnimBlendAssociationSAInterface");
 
