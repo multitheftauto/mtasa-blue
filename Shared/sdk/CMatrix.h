multitheftauto/mtasa-blue

commit 339eb59c5f254fd3bdeb0c4854ccd796023e1063

Add possibility to modify vehicle components scale (#361)
* Initial version of vehicle component scale modifications

* Added OOP vehicle component scale functions

* fixup of conflict resolution

Shared/sdk/CMatrix.h
@@ -38,6 +38,20 @@ public:
 
     CMatrix(const CVector& vecPosition, const CVector& vecRotation)
     {
+        // Initialize as identity, so rotation isn't broken by no scale set.
+        vRight = CVector(1.0f, 0.0f, 0.0f);
+        vFront = CVector(0.0f, 1.0f, 0.0f);
+        vUp = CVector(0.0f, 0.0f, 1.0f);
+        SetRotation(vecRotation);
+        SetPosition(vecPosition);
+    }
+
+    CMatrix(const CVector& vecPosition, const CVector& vecRotation, const CVector& vecScale)
+    {
+        // Initialize with scale set
+        vRight = CVector(vecScale.fX, 0.0f, 0.0f);
+        vFront = CVector(0.0f, vecScale.fY, 0.0f);
+        vUp = CVector(0.0f, 0.0f, vecScale.fZ);
         SetRotation(vecRotation);
         SetPosition(vecPosition);
     }
@@ -130,6 +144,17 @@ public:
         return matResult;
     }
 
+    CMatrix Clone(void) const
+    {
+        CMatrix matResult;
+        matResult.vRight = vRight.Clone();
+        matResult.vFront = vFront.Clone();
+        matResult.vUp = vUp.Clone();
+        matResult.vPos = vPos.Clone();
+        
+        return matResult;
+    }
+
     void Invert()
     {
         // Transpose the rotation matrix and negate the position
@@ -184,17 +209,32 @@ public:
         }
     }
 
+    CMatrix GetRotationMatrix() const
+    {
+        // Operate only on rotation, ignore scale.
+        CMatrix matClone = Clone();
+        CVector vecScale = GetScale();
+        matClone.vRight /= vecScale.fX;
+        matClone.vFront /= vecScale.fY;
+        matClone.vUp /= vecScale.fZ;
+        matClone.vPos = CVector(0, 0, 0);
+        return matClone;
+    }
+
     // Get matrix rotation as angles
     // Inverted to match MTAized rotations for vehicles and players (and objects on the server)
     // Should produce the same results as ( CVector(0,0,0) - ConvertToEulerAngles() )
     CVector GetRotation(void) const
     {
-        float fRotY = atan2(vRight.fZ, sqrtf(Square(vRight.fX) + Square(vRight.fY)));
-        float fRotZ = atan2(vRight.fY, vRight.fX);
+        // Operate only on rotation, ignore scale.
+        CMatrix matRot = GetRotationMatrix();
+
+        float fRotY = atan2(matRot.vRight.fZ, sqrtf(Square(matRot.vRight.fX) + Square(matRot.vRight.fY)));
+        float fRotZ = atan2(matRot.vRight.fY, matRot.vRight.fX);
 
         float fSinZ = -sin(fRotZ);
         float fCosZ = cos(fRotZ);
-        float fRotX = atan2(vUp.fX * fSinZ + vUp.fY * fCosZ, vFront.fX * fSinZ + vFront.fY * fCosZ);
+        float fRotX = atan2(matRot.vUp.fX * fSinZ + matRot.vUp.fY * fCosZ, matRot.vFront.fX * fSinZ + matRot.vFront.fY * fCosZ);
         return CVector(-fRotX, -fRotY, fRotZ);
     }
 
@@ -210,17 +250,23 @@ public:
         float fSinY = sin(-vecRotation.fY);
         float fSinZ = sin(vecRotation.fZ);
 
+        // Keep current scale even after rotation.
+        CVector vecScale = GetScale();
+
         vRight.fX = fCosY * fCosZ;
         vRight.fY = fCosY * fSinZ;
         vRight.fZ = fSinY;
+        vRight *= vecScale.fX;
 
         vFront.fX = fSinX * fSinY * fCosZ - fCosX * fSinZ;
         vFront.fY = fSinX * fSinY * fSinZ + fCosX * fCosZ;
         vFront.fZ = -fSinX * fCosY;
+        vFront *= vecScale.fY;
 
         vUp.fX = -(fCosX * fSinY * fCosZ + fSinX * fSinZ);
         vUp.fY = fCosZ * fSinX - fCosX * fSinY * fSinZ;
         vUp.fZ = fCosX * fCosY;
+        vUp *= vecScale.fZ;
     }
 
     // Get matrix translational part
@@ -229,6 +275,18 @@ public:
     // Set matrix translational part
     void SetPosition(const CVector& vecPosition) { vPos = vecPosition; }
 
+    const CVector& GetScale() const
+    {
+        return CVector(vRight.Length(), vFront.Length(), vUp.Length());
+    }
+
+    void SetScale(const CVector& vecScale) 
+    {
+        CMatrix matRot = GetRotationMatrix();
+        vRight = matRot.vRight * vecScale.fX;
+        vFront = matRot.vFront * vecScale.fY;
+        vUp = matRot.vUp * vecScale.fZ;
+    }
     //
     // Get reference to component axis by index
     //

Client/game_sa/CVehicleSA.cpp
@@ -37,15 +37,14 @@ namespace
     void RwMatrixGetRotation(const RwMatrix& rwMatrix, CVector& vecOutRotation)
     {
         CMatrix matTemp;
-        matTemp.vRight = (CVector&)rwMatrix.right;
-        matTemp.vFront = (CVector&)rwMatrix.up;
-        matTemp.vUp = (CVector&)rwMatrix.at;
+        RwMatrixToCMatrix(rwMatrix, matTemp);
         vecOutRotation = matTemp.GetRotation();
     }
 
     void RwMatrixSetRotation(RwMatrix& rwInOutMatrix, const CVector& vecRotation)
     {
         CMatrix matTemp;
+        RwMatrixToCMatrix(rwInOutMatrix, matTemp);
         matTemp.SetRotation(vecRotation);
         rwInOutMatrix.right = (RwV3d&)matTemp.vRight;
         rwInOutMatrix.up = (RwV3d&)matTemp.vFront;
@@ -55,6 +54,23 @@ namespace
     void RwMatrixGetPosition(const RwMatrix& rwMatrix, CVector& vecOutPosition) { vecOutPosition = (CVector&)rwMatrix.pos; }
 
     void RwMatrixSetPosition(RwMatrix& rwInOutMatrix, const CVector& vecPosition) { rwInOutMatrix.pos = (RwV3d&)vecPosition; }
+
+    void RwMatrixGetScale(const RwMatrix& rwMatrix, CVector& vecOutScale)
+    {
+        CMatrix matTemp;
+        RwMatrixToCMatrix(rwMatrix, matTemp);
+        vecOutScale = matTemp.GetScale();
+    }
+
+    void RwMatrixSetScale(RwMatrix& rwInOutMatrix, const CVector& vecScale)
+    {
+        CMatrix matTemp;
+        RwMatrixToCMatrix(rwInOutMatrix, matTemp);
+        matTemp.SetScale(vecScale);
+        rwInOutMatrix.right = (RwV3d&)matTemp.vRight;
+        rwInOutMatrix.up = (RwV3d&)matTemp.vFront;
+        rwInOutMatrix.at = (RwV3d&)matTemp.vUp;
+    }
 
     bool ClumpDumpCB(RpAtomic* pAtomic, void* data)
     {
@@ -2409,6 +2425,28 @@ bool CVehicleSA::GetComponentPosition(const SString& vehicleComponent, CVector& 
     if (pComponent && pComponent->pFrame != NULL)
     {
         RwMatrixGetPosition(pComponent->pFrame->modelling, vecPositionModelling);
+        return true;
+    }
+    return false;
+}
+
+bool CVehicleSA::SetComponentScale(const SString& vehicleComponent, const CVector& vecScale)
+{
+    SVehicleFrame* pComponent = GetVehicleComponent(vehicleComponent);
+    if(pComponent && pComponent->pFrame != NULL)
+    {
+        RwMatrixSetScale(pComponent->pFrame->modelling, vecScale);
+        return true;
+    }
+    return false;
+}
+
+bool CVehicleSA::GetComponentScale(const SString& vehicleComponent, CVector& vecScaleModelling)
+{
+    SVehicleFrame* pComponent = GetVehicleComponent(vehicleComponent);
+    if(pComponent && pComponent->pFrame != NULL)
+    {
+        RwMatrixGetScale(pComponent->pFrame->modelling, vecScaleModelling);
         return true;
     }
     return false;

Client/game_sa/CVehicleSA.h
@@ -783,6 +783,8 @@ public:
     bool                              GetComponentRotation(const SString& vehicleComponent, CVector& vecPositionModelling);
     bool                              SetComponentPosition(const SString& vehicleComponent, const CVector& vecPosition);
     bool                              GetComponentPosition(const SString& vehicleComponent, CVector& vecPositionModelling);
+    bool                              SetComponentScale(const SString & vehicleComponent, const CVector & vecScale);
+    bool                              GetComponentScale(const SString & vehicleComponent, CVector & vecScaleModelling);
     bool                              IsComponentPresent(const SString& vehicleComponent);
     bool                              SetComponentMatrix(const SString& vehicleComponent, const CMatrix& matOrientation);
     bool                              GetComponentMatrix(const SString& vehicleComponent, CMatrix& matOutOrientation);

Client/mods/deathmatch/logic/CClientVehicle.cpp
@@ -882,6 +882,17 @@ void CClientVehicle::Fix(void)
                 SetComponentRotation(strTemp, (*iter).second.m_vecComponentRotation);
             }
         }
+        // is our scale changed?
+        if((*iter).second.m_bScaleChanged)
+        {
+            // Make sure it's different
+            if((*iter).second.m_vecOriginalComponentScale != (*iter).second.m_vecComponentScale)
+            {
+                // apply our new rotation
+                SetComponentScale(strTemp, (*iter).second.m_vecComponentScale);
+            }
+        }
+
         // set our visibility
         SetComponentVisible(strTemp, (*iter).second.m_bVisible);
     }
@@ -2791,10 +2802,12 @@ void CClientVehicle::Create(void)
                 // Grab our start position
                 GetComponentPosition((*iter).first, vehicleComponentData.m_vecComponentPosition);
                 GetComponentRotation((*iter).first, vehicleComponentData.m_vecComponentRotation);
+                GetComponentScale((*iter).first, vehicleComponentData.m_vecComponentScale);
 
                 // copy it into our original positions
                 vehicleComponentData.m_vecOriginalComponentPosition = vehicleComponentData.m_vecComponentPosition;
                 vehicleComponentData.m_vecOriginalComponentRotation = vehicleComponentData.m_vecComponentRotation;
+                vehicleComponentData.m_vecOriginalComponentScale = vehicleComponentData.m_vecComponentScale;
 
                 // insert it into our component data list
                 m_ComponentData.insert(std::pair<SString, SVehicleComponentData>((*iter).first, vehicleComponentData));
@@ -2826,14 +2839,24 @@ void CClientVehicle::Create(void)
                     SetComponentPosition(strTemp, (*iter).second.m_vecComponentPosition);
                 }
             }
-            // is our position changed?
+            // is our rotation changed?
             if ((*iter).second.m_bRotationChanged)
             {
                 // Make sure it's different
                 if ((*iter).second.m_vecOriginalComponentRotation != (*iter).second.m_vecComponentRotation)
                 {
-                    // apple our new position
+                    // apple our new rotation
                     SetComponentRotation(strTemp, (*iter).second.m_vecComponentRotation);
+                }
+            }
+            // is our scale changed?
+            if((*iter).second.m_bScaleChanged)
+            {
+                // Make sure it's different
+                if((*iter).second.m_vecOriginalComponentScale != (*iter).second.m_vecComponentScale)
+                {
+                    // apple our new scale
+                    SetComponentScale(strTemp, (*iter).second.m_vecComponentScale);
                 }
             }
             // set our visibility
@@ -4517,6 +4540,74 @@ bool CClientVehicle::GetComponentRotation(const SString& vehicleComponent, CVect
     return false;
 }
 
+bool CClientVehicle::SetComponentScale(const SString& vehicleComponent, CVector vecScale, EComponentBaseType inputBase)
+{
+    // Ensure scale is parent relative
+    ConvertComponentScaleBase(vehicleComponent, vecScale, inputBase, EComponentBase::PARENT);
+
+    if(m_pVehicle)
+    {
+        // set our rotation on the model
+        if(m_pVehicle->SetComponentScale(vehicleComponent, vecScale))
+        {
+            // update our cache
+            m_ComponentData[vehicleComponent].m_vecComponentScale = vecScale;
+            m_ComponentData[vehicleComponent].m_bScaleChanged = true;
+
+            return true;
+        }
+    }
+    else
+    {
+        if(m_ComponentData.find(vehicleComponent) != m_ComponentData.end())
+        {
+            // update our cache
+            m_ComponentData[vehicleComponent].m_vecComponentScale = vecScale;
+            m_ComponentData[vehicleComponent].m_bScaleChanged = true;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool CClientVehicle::GetComponentScale(const SString& vehicleComponent, CVector& vecScale , EComponentBaseType outputBase)
+{
+    if(m_pVehicle)
+    {
+        // fill our scale from the actual rotation
+        bool bResult = m_pVehicle->GetComponentScale(vehicleComponent, vecScale);
+
+        // Convert to required base
+        ConvertComponentScaleBase(vehicleComponent, vecScale, EComponentBase::PARENT, outputBase);
+
+        return bResult;
+    }
+    else
+    {
+        if(m_ComponentData.find(vehicleComponent) != m_ComponentData.end())
+        {
+            // fill our rotation from the cached rotation
+            vecScale = m_ComponentData[vehicleComponent].m_vecComponentScale;
+
+            // Convert to required base
+            ConvertComponentScaleBase(vehicleComponent, vecScale, EComponentBase::PARENT, outputBase);
+            return true;
+        }
+    }
+    return false;
+}
+
+bool CClientVehicle::ResetComponentScale(const SString& vehicleComponent)
+{
+    // set our rotation on the model
+    if(SetComponentScale(vehicleComponent, m_ComponentData[vehicleComponent].m_vecOriginalComponentScale))
+    {
+        // update our cache
+        m_ComponentData[vehicleComponent].m_bScaleChanged = false;
+        return true;
+    }
+    return false;
+}
 bool CClientVehicle::ResetComponentRotation(const SString& vehicleComponent)
 {
     // set our rotation on the model
@@ -4565,7 +4656,7 @@ void CClientVehicle::GetComponentParentToRootMatrix(const SString& vehicleCompon
         CMatrix matCombo;
         for (uint i = 0; pComponentData && i < 10; i++)
         {
-            CMatrix matFrame(pComponentData->m_vecComponentPosition, pComponentData->m_vecComponentRotation);
+            CMatrix matFrame(pComponentData->m_vecComponentPosition, pComponentData->m_vecComponentRotation, pComponentData->m_vecComponentScale);
             matCombo = matCombo * matFrame;
             pComponentData = MapFind(m_ComponentData, pComponentData->m_strParentName);
         }
@@ -4596,6 +4687,17 @@ void CClientVehicle::ConvertComponentPositionBase(const SString& vehicleComponen
         CMatrix matTemp(vecPosition);
         ConvertComponentMatrixBase(vehicleComponent, matTemp, inputBase, outputBase);
         vecPosition = matTemp.GetPosition();
+    }
+}
+
+void CClientVehicle::ConvertComponentScaleBase(const SString& vehicleComponent, CVector& vecScale, EComponentBaseType inputBase,
+    EComponentBaseType outputBase)
+{
+    if(inputBase != outputBase)
+    {
+        CMatrix matTemp(CVector(), CVector(), vecScale);
+        ConvertComponentMatrixBase(vehicleComponent, matTemp, inputBase, outputBase);
+        vecScale = matTemp.GetScale();
     }
 }
 

Client/mods/deathmatch/logic/CClientVehicle.h
@@ -108,15 +108,19 @@ struct SVehicleComponentData
     {
         m_bPositionChanged = false;
         m_bRotationChanged = false;
+        m_bScaleChanged    = false;
         m_bVisible = true;
     }
     SString m_strParentName;
     CVector m_vecComponentPosition;                    // Parent relative
     CVector m_vecComponentRotation;                    // Parent relative radians
+    CVector m_vecComponentScale;                       // Parent relative
     CVector m_vecOriginalComponentPosition;            // Parent relative
     CVector m_vecOriginalComponentRotation;            // Parent relative radians
+    CVector m_vecOriginalComponentScale;               // Parent relative
     bool    m_bPositionChanged;
     bool    m_bRotationChanged;
+    bool    m_bScaleChanged;
     bool    m_bVisible;
 };
 class CClientProjectile;
@@ -472,6 +476,10 @@ public:
     bool SetComponentRotation(const SString& vehicleComponent, CVector vecRotation, EComponentBaseType base = EComponentBase::PARENT);
     bool GetComponentRotation(const SString& vehicleComponent, CVector& vecRotation, EComponentBaseType base = EComponentBase::PARENT);
 
+    bool ResetComponentScale(const SString & vehicleComponent);
+    bool SetComponentScale(const SString & vehicleComponent, CVector vecScale, EComponentBaseType base = EComponentBase::PARENT);
+    bool GetComponentScale(const SString & vehicleComponent, CVector& vecScale, EComponentBaseType base = EComponentBase::PARENT);
+
     bool                                               SetComponentVisible(const SString& vehicleComponent, bool bVisible);
     bool                                               GetComponentVisible(const SString& vehicleComponent, bool& bVisible);
     std::map<SString, SVehicleComponentData>::iterator ComponentsBegin(void) { return m_ComponentData.begin(); }
@@ -488,6 +496,7 @@ public:
 protected:
     void ConvertComponentRotationBase(const SString& vehicleComponent, CVector& vecInOutRotation, EComponentBaseType inputBase, EComponentBaseType outputBase);
     void ConvertComponentPositionBase(const SString& vehicleComponent, CVector& vecInOutPosition, EComponentBaseType inputBase, EComponentBaseType outputBase);
+    void ConvertComponentScaleBase(const SString & vehicleComponent, CVector & vecScale, EComponentBaseType inputBase, EComponentBaseType outputBase);
     void ConvertComponentMatrixBase(const SString& vehicleComponent, CMatrix& matInOutOrientation, EComponentBaseType inputBase, EComponentBaseType outputBase);
     void GetComponentParentToRootMatrix(const SString& vehicleComponent, CMatrix& matOutParentToRoot);
 

Client/mods/deathmatch/logic/luadefs/CLuaVehicleDefs.cpp
@@ -77,6 +77,7 @@ void CLuaVehicleDefs::LoadFunctions(void)
         {"isVehicleWindowOpen", IsVehicleWindowOpen},
         {"getVehicleComponentPosition", GetVehicleComponentPosition},
         {"getVehicleComponentRotation", GetVehicleComponentRotation},
+        {"getVehicleComponentScale", GetVehicleComponentScale},
         {"getVehicleComponentVisible", GetVehicleComponentVisible},
         {"getVehicleComponents", GetVehicleComponents},
         {"getVehicleModelExhaustFumesPosition", GetVehicleModelExhaustFumesPosition},
@@ -124,8 +125,10 @@ void CLuaVehicleDefs::LoadFunctions(void)
         {"setVehicleSirens", SetVehicleSirens},
         {"setVehicleComponentPosition", SetVehicleComponentPosition},
         {"setVehicleComponentRotation", SetVehicleComponentRotation},
+        {"setVehicleComponentScale", SetVehicleComponentScale},
         {"resetVehicleComponentPosition", ResetVehicleComponentPosition},
         {"resetVehicleComponentRotation", ResetVehicleComponentRotation},
+        {"resetVehicleComponentScale", ResetVehicleComponentScale },
         {"setVehicleComponentVisible", SetVehicleComponentVisible},
         {"setVehicleNitroActivated", SetVehicleNitroActivated},
         {"setVehicleNitroCount", SetVehicleNitroCount},
@@ -211,6 +214,7 @@ void CLuaVehicleDefs::AddClass(lua_State* luaVM)
     lua_classfunction(luaVM, "getComponentPosition", OOP_GetVehicleComponentPosition);
     lua_classfunction(luaVM, "getComponentVisible", "getVehicleComponentVisible");
     lua_classfunction(luaVM, "getComponentRotation", OOP_GetVehicleComponentRotation);
+    lua_classfunction(luaVM, "getComponentScale", OOP_GetVehicleComponentScale);
     lua_classfunction(luaVM, "getUpgrades", "getVehicleUpgrades");
     lua_classfunction(luaVM, "getUpgradeSlotName", "getVehicleUpgradeSlotName");
     lua_classfunction(luaVM, "getCompatibleUpgrades", "getVehicleCompatibleUpgrades");
@@ -223,6 +227,7 @@ void CLuaVehicleDefs::AddClass(lua_State* luaVM)
     lua_classfunction(luaVM, "setSirens", "setVehicleSirens");
     lua_classfunction(luaVM, "setComponentPosition", "setVehicleComponentPosition");
     lua_classfunction(luaVM, "setComponentRotation", "setVehicleComponentRotation");
+    lua_classfunction(luaVM, "setComponentScale", "setVehicleComponentScale");
     lua_classfunction(luaVM, "setLocked", "setVehicleLocked");
     lua_classfunction(luaVM, "setDamageProof", "setVehicleDamageProof");
     lua_classfunction(luaVM, "setHelicopterRotorSpeed", "setHelicopterRotorSpeed");
@@ -262,6 +267,7 @@ void CLuaVehicleDefs::AddClass(lua_State* luaVM)
 
     lua_classfunction(luaVM, "resetComponentPosition", "resetVehicleComponentPosition");
     lua_classfunction(luaVM, "resetComponentRotation", "resetVehicleComponentRotation");
+    lua_classfunction(luaVM, "resetComponentScale", "resetVehicleComponentScale");
 
     lua_classfunction(luaVM, "attachTrailer", "attachTrailerToVehicle");
     lua_classfunction(luaVM, "detachTrailer", "detachTrailerFromVehicle");
@@ -3341,6 +3347,91 @@ int CLuaVehicleDefs::OOP_GetVehicleComponentRotation(lua_State* luaVM)
     return 1;
 }
 
+int CLuaVehicleDefs::SetVehicleComponentScale(lua_State* luaVM)
+{
+    //  bool setVehicleComponentScale ( vehicle theVehicle, string theComponent, float rotX, float rotY, float rotZ [, string base = "parent"] )
+    SString            strComponent;
+    CClientVehicle*    pVehicle = NULL;
+    CVector            vecScale;
+    EComponentBaseType inputBase;
+
+    CScriptArgReader argStream(luaVM);
+    argStream.ReadUserData(pVehicle);
+    argStream.ReadString(strComponent);
+    argStream.ReadVector3D(vecScale);
+    argStream.ReadEnumString(inputBase, EComponentBase::PARENT);
+
+    if(!argStream.HasErrors())
+    {
+        pVehicle->SetComponentScale(strComponent, vecScale, inputBase);
+        lua_pushboolean(luaVM, true);
+        return 1;
+    }
+    else
+        m_pScriptDebugging->LogCustom(luaVM, argStream.GetFullErrorMessage());
+
+    lua_pushboolean(luaVM, false);
+    return 1;
+}
+
+int CLuaVehicleDefs::GetVehicleComponentScale(lua_State* luaVM)
+{
+    // float, float, float getVehicleComponentScale ( vehicle theVehicle, string theComponent [, string base = "parent"]  )
+    SString            strComponent;
+    CClientVehicle*    pVehicle = NULL;
+    EComponentBaseType outputBase;
+
+    CScriptArgReader argStream(luaVM);
+    argStream.ReadUserData(pVehicle);
+    argStream.ReadString(strComponent);
+    argStream.ReadEnumString(outputBase, EComponentBase::PARENT);
+
+    if(!argStream.HasErrors())
+    {
+        CVector vecScale;
+        if(pVehicle->GetComponentScale(strComponent, vecScale, outputBase))
+        {
+            lua_pushnumber(luaVM, vecScale.fX);
+            lua_pushnumber(luaVM, vecScale.fY);
+            lua_pushnumber(luaVM, vecScale.fZ);
+            return 3;
+        }
+    }
+    else
+        m_pScriptDebugging->LogCustom(luaVM, argStream.GetFullErrorMessage());
+
+    lua_pushboolean(luaVM, false);
+    return 1;
+}
+
+int CLuaVehicleDefs::OOP_GetVehicleComponentScale(lua_State* luaVM)
+{
+    // float, float, float getVehicleComponentScale ( vehicle theVehicle, string theComponent [, string base = "parent"]  )
+    SString            strComponent;
+    CClientVehicle*    pVehicle = NULL;
+    EComponentBaseType outputBase;
+
+    CScriptArgReader argStream(luaVM);
+    argStream.ReadUserData(pVehicle);
+    argStream.ReadString(strComponent);
+    argStream.ReadEnumString(outputBase, EComponentBase::PARENT);
+
+    if(!argStream.HasErrors())
+    {
+        CVector vecScale;
+        if(pVehicle->GetComponentScale(strComponent, vecScale, outputBase))
+        {
+            lua_pushvector(luaVM, vecScale);
+            return 1;
+        }
+    }
+    else
+        m_pScriptDebugging->LogCustom(luaVM, argStream.GetFullErrorMessage());
+
+    lua_pushboolean(luaVM, false);
+    return 1;
+}
+
 int CLuaVehicleDefs::ResetVehicleComponentPosition(lua_State* luaVM)
 {
     CScriptArgReader argStream(luaVM);
@@ -3375,6 +3466,29 @@ int CLuaVehicleDefs::ResetVehicleComponentRotation(lua_State* luaVM)
     if (!argStream.HasErrors())
     {
         if (pVehicle->ResetComponentRotation(strComponent))
+        {
+            lua_pushboolean(luaVM, true);
+            return 1;
+        }
+    }
+    else
+        m_pScriptDebugging->LogCustom(luaVM, argStream.GetFullErrorMessage());
+
+    lua_pushboolean(luaVM, false);
+    return 1;
+}
+
+int CLuaVehicleDefs::ResetVehicleComponentScale(lua_State* luaVM)
+{
+    CScriptArgReader argStream(luaVM);
+    SString          strComponent;
+    CClientVehicle*  pVehicle = NULL;
+    argStream.ReadUserData(pVehicle);
+    argStream.ReadString(strComponent);
+
+    if(!argStream.HasErrors())
+    {
+        if(pVehicle->ResetComponentScale(strComponent))
         {
             lua_pushboolean(luaVM, true);
             return 1;

Client/mods/deathmatch/logic/luadefs/CLuaVehicleDefs.h
@@ -147,8 +147,11 @@ public:
     LUA_DECLARE_OOP(GetVehicleComponentPosition);
     LUA_DECLARE(SetVehicleComponentRotation);
     LUA_DECLARE_OOP(GetVehicleComponentRotation);
+    LUA_DECLARE(SetVehicleComponentScale);
+    LUA_DECLARE_OOP(GetVehicleComponentScale);
     LUA_DECLARE(ResetVehicleComponentPosition);
     LUA_DECLARE(ResetVehicleComponentRotation);
+    LUA_DECLARE(ResetVehicleComponentScale);
     LUA_DECLARE(SetVehicleComponentVisible);
     LUA_DECLARE(GetVehicleComponentVisible);
     LUA_DECLARE(GetVehicleComponents);

Client/sdk/game/CVehicle.h
@@ -303,6 +303,8 @@ public:
     virtual bool                              GetComponentRotation(const SString& vehicleComponent, CVector& vecRotation) = 0;
     virtual bool                              SetComponentPosition(const SString& vehicleComponent, const CVector& vecPosition) = 0;
     virtual bool                              GetComponentPosition(const SString& vehicleComponent, CVector& vecPositionModelling) = 0;
+    virtual bool                              SetComponentScale(const SString& vehicleComponent, const CVector& vecScale) = 0;
+    virtual bool                              GetComponentScale(const SString& vehicleComponent, CVector& vecScaleModelling) = 0;
     virtual bool                              IsComponentPresent(const SString& vehicleComponent) = 0;
     virtual bool                              SetComponentMatrix(const SString& vehicleComponent, const CMatrix& matOrientation) = 0;
     virtual bool                              GetComponentMatrix(const SString& vehicleComponent, CMatrix& matOutOrientation) = 0;

commit 01213c76a1b5cb53f1b82487d0ff59f4ba45056e

Fix Linux crash in getElementMatrix introduced in 339eb59c5f254fd3bdeb0c4854ccd796023e1063

Shared/sdk/CMatrix.h
@@ -275,7 +275,7 @@ public:
     // Set matrix translational part
     void SetPosition(const CVector& vecPosition) { vPos = vecPosition; }
 
-    const CVector& GetScale() const
+    CVector GetScale() const
     {
         return CVector(vRight.Length(), vFront.Length(), vUp.Length());
     }