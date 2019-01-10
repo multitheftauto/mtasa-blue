ca13977c74600fdbdeed66674c463ea64b176262
Marek Kulik <mk.botder@gmail.com>
2019-01-10 22:53:30 +0100

    Revert "Revert: Refactor frame rate limiter"

--- Client/mods/deathmatch/logic/CClientGame.h
+++ Client/mods/deathmatch/logic/CClientGame.h
@@ -238,7 +238,6 @@ public:
     void DoPulses(void);
     void DoPulses2(bool bCalledFromIdle);
 
-    uint GetFrameTimeSlice(void) { return m_uiFrameTimeSlice; }
     uint GetFrameCount(void) { return m_uiFrameCount; }
 
     void        HandleException(CExceptionInformation* pExceptionInformation);
@@ -764,9 +763,8 @@ private:
     unsigned long m_ulBigPacketBytesReceivedBase;
     CTransferBox* m_pBigPacketTransferBox;
 
-    uint m_uiFrameTimeSlice;            // how long it took (in ms) to process the current frame
-    uint m_uiLastFrameTick;             // time at which the previous frame was processed
-    uint m_uiFrameCount;                // Frame counter
+    CElapsedTimeHD m_TimeSliceTimer;
+    uint           m_uiFrameCount;
 
     long long m_llLastTransgressionTime;
     SString   m_strLastDiagnosticStatus;
