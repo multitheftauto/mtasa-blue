5ef6a7cda7eceed67c76789bf7a1dfaf64d5df7a
Neproify <yamasaki3214@gmail.com>
2019-05-26 21:12:12 +0200

    Sync player even if it is dead. Should resolve desyncs.

--- Client/mods/deathmatch/logic/CNetAPI.cpp
+++ Client/mods/deathmatch/logic/CNetAPI.cpp
@@ -299,7 +299,7 @@ void CNetAPI::DoPulse()
 
         // Grab the local player
         CClientPlayer* pPlayer = m_pPlayerManager->GetLocalPlayer();
-        if (pPlayer && !pPlayer->IsDeadOnNetwork())
+        if (pPlayer)
         {
             unsigned long ulCurrentTime = CClientTime::GetTime();
 
