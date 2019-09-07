multitheftauto/mtasa-blue

commit 5d743da34d9941e448a16b8379f439603003df38

Fix default track sync

Client/mods/deathmatch/logic/CClientTrainTrackManager.cpp
@@ -8,7 +8,27 @@
 *
 *****************************************************************************/
 #include <StdInc.h>
-#include "CClientTrainTrackManager.h"
+#include <TrackNodes.h>
+#include <game/CTrainTrackManager.h>
+
+void CClientTrainTrackManager::Reset()
+{
+    // Reset game
+    g_pGame->GetTrainTrackManager()->Reset();
+
+    // Create default tracks
+    for (std::size_t i = 0; i < 4; ++i)
+    {
+        // Delete old default tracks if exist
+        CClientTrainTrack* pTrainTrack = GetTrainTrackByIndex(i);
+        if (pTrainTrack)
+            delete pTrainTrack;
+
+        // Create train tracks
+        pTrainTrack = new CClientTrainTrack(INVALID_ELEMENT_ID, OriginalTrackNodes[i], true);
+        pTrainTrack->MakeSystemEntity();
+    }
+}
 
 CClientTrainTrack* CClientTrainTrackManager::Get(ElementID ID)
 {

Client/mods/deathmatch/logic/CNetAPI.cpp
@@ -1348,22 +1348,33 @@ void CNetAPI::ReadVehiclePuresync ( CClientPlayer* pPlayer, CClientVehicle* pVeh
             // Train specific data
             float fPosition;
             bool bDirection;
-            ElementID trainTrackID;
             float fSpeed;
             BitStream.Read(fPosition);
             BitStream.ReadBit(bDirection);
-            BitStream.Read(trainTrackID);
             BitStream.Read(fSpeed);
 
-            CClientTrainTrack* pTrainTrack = g_pClientGame->GetManager()->GetTrainTrackManager()->Get(trainTrackID);
+            CClientTrainTrack* pTrainTrack = nullptr;
+            if (BitStream.ReadBit())
+            {
+                uint trackIndex;
+                BitStream.Read(trackIndex);
+
+                pTrainTrack = g_pClientGame->GetManager()->GetTrainTrackManager()->GetTrainTrackByIndex(trackIndex);
+            }
+            else
+            {
+                ElementID trackID;
+                BitStream.Read(trackID);
+
+                pTrainTrack = g_pClientGame->GetManager()->GetTrainTrackManager()->Get(trackID);
+            }
+
             if (vehicleType == CLIENTVEHICLE_TRAIN)
             {
                 if ( !pVehicle->IsStreamedIn ( ) )
                     pVehicle->SetPosition ( position.data.vecPosition, true );
 
                 pVehicle->SetTrainTrack(pTrainTrack);
-                pVehicle->SetDerailed(pTrainTrack == nullptr);
-
                 pVehicle->SetTrainPosition ( fPosition, false );
                 pVehicle->SetTrainDirection ( bDirection );
                 pVehicle->SetTrainSpeed ( fSpeed );
@@ -1626,8 +1637,24 @@ void CNetAPI::WriteVehiclePuresync ( CClientPed* pPlayerModel, CClientVehicle* p
 
         BitStream.Write(fPosition);
         BitStream.WriteBit(bDirection);
-        BitStream.Write(pTrainTrack ? pTrainTrack->GetID() : INVALID_ELEMENT_ID);
         BitStream.Write(fSpeed);
+        
+        if (pTrainTrack && pTrainTrack->GetID() == INVALID_ELEMENT_ID)
+        {
+            BitStream.WriteBit(true); // is default track
+            BitStream.Write(pTrainTrack->GetTrackIndex());
+        }
+        else if (!pTrainTrack || pVehicle->IsDerailed())
+        {
+            // place derailed tracks on track 0
+            BitStream.WriteBit(true);
+            BitStream.Write((uint)0);
+        }
+        else
+        {
+            BitStream.WriteBit(false); // isn't default track
+            BitStream.Write(pTrainTrack->GetID());
+        }
     }
 
     // Write the camera orientation

Client/game_sa/CTrainTrackManagerSA.cpp
@@ -10,14 +10,11 @@
 *
 *****************************************************************************/
 #include "StdInc.h"
-#include <TrackNodes.h>
 #include <algorithm>
 #include "CTrainTrackSA.h"
 
 CTrainTrackManagerSA::CTrainTrackManagerSA()
 {
-    Reset();
-
     // Disable CTrain::InitTrains
     MemPut(0x6F7440, 0xC3); // Write retn
 }
@@ -94,13 +91,6 @@ void CTrainTrackManagerSA::Reset()
     m_TrackLengths = nullptr;
     m_NumberOfTrackNodes = nullptr;
     m_CurrentTrackNodeSize = 0;
-
-    // Create default tracks
-    for (std::size_t i = 0; i < 4; ++i)
-    {
-        // Create train tracks
-        CreateTrainTrack(OriginalTrackNodes[i], true);
-    }
 }
 
 uint CTrainTrackManagerSA::AllocateTrainTrackIndex()

Client/mods/deathmatch/logic/CClientGame.cpp
@@ -3617,6 +3617,9 @@ void CClientGame::Event_OnIngame ( void )
     // Make sure we never get tired
     g_pGame->GetPlayerInfo ()->SetDoesNotGetTired ( true );
 
+    // Reset default train tracks
+    m_pManager->GetTrainTrackManager()->Reset();
+
     // Tell doggy we got the game running
     WatchDogCompletedSection ( "L1" );
 }
@@ -6504,11 +6507,6 @@ void CClientGame::OutputServerInfo( void )
                         );
 
         strTotalOutput += SString( "Tick rates: %s\n", *strTickRates );
-    }
-
-    if ( g_pGame->GetTrainTrackManager () )
-    {
-        g_pGame->GetTrainTrackManager ()->Reset ();
     }
 
     {

Client/mods/deathmatch/logic/CClientTrainTrackManager.h
@@ -13,6 +13,8 @@ class CClientTrainTrack;
 class CClientTrainTrackManager
 {
 public:
+    void Reset();
+
     static CClientTrainTrack* Get(ElementID ID);
     CClientTrainTrack* Get(CTrainTrack* pTrainTrack);
 

Client/mods/deathmatch/premake5.lua
@@ -44,7 +44,8 @@ project "Client Deathmatch"
 			"../../../vendor/lua/src", 
 			"../../../Shared/mods/deathmatch/logic", 
 			"../../../Shared/animation", 
-			"../../../vendor/sparsehash/src/"
+			"../../../vendor/sparsehash/src",
+			"../../../Shared/gta"
 	}
 	
 	files {

Server/mods/deathmatch/logic/CTrainTrack.cpp
@@ -11,7 +11,7 @@
 *****************************************************************************/
 #include "StdInc.h"
 
-CTrainTrack::CTrainTrack(CTrainTrackManager* pManager, const std::vector<STrackNode>& nodes, bool linkLastNodes, CElement* pParent, CXMLNode* pNode, bool defaultTrack)
+CTrainTrack::CTrainTrack(CTrainTrackManager* pManager, const std::vector<STrackNode>& nodes, bool linkLastNodes, CElement* pParent, CXMLNode* pNode, uchar defaultTrackId)
     : CElement(pParent, pNode), m_pManager(pManager)
 {
     m_iType = CElement::TRAINTRACK;
@@ -19,7 +19,7 @@ CTrainTrack::CTrainTrack(CTrainTrackManager* pManager, const std::vector<STrackN
 
     m_LinkLastNodes = linkLastNodes;
     m_Nodes = nodes;
-    m_Default = defaultTrack;
+    m_DefaultTrackId = defaultTrackId;
 }
 
 CTrainTrack::~CTrainTrack()

Server/mods/deathmatch/logic/CTrainTrack.h
@@ -32,7 +32,7 @@ struct STrackNode
 class CTrainTrack : public CElement
 {
 public:
-    CTrainTrack(CTrainTrackManager* pManager, const std::vector<STrackNode>& nodes, bool linkLastNodes, CElement* pParent, CXMLNode* pNode, bool defaultTrack = false);
+    CTrainTrack(CTrainTrackManager* pManager, const std::vector<STrackNode>& nodes, bool linkLastNodes, CElement* pParent, CXMLNode* pNode, uchar defaultTrackId = 0xFF);
     // TODO: Add move constructor
     virtual ~CTrainTrack();
 
@@ -45,7 +45,8 @@ public:
     inline void SetLastNodesLinked(bool link) { m_LinkLastNodes = link; }
     inline bool GetLastNodesLinked() { return m_LinkLastNodes; }
 
-    inline bool IsDefault() { return m_Default; }
+    inline bool IsDefault() { return m_DefaultTrackId != 0xFF; }
+    inline uchar GetDefaultTrackId() { return m_DefaultTrackId; }
 
     // CClientEntity methods
     virtual void Unlink() override {}
@@ -57,4 +58,5 @@ private:
     std::vector<STrackNode> m_Nodes;
     bool m_LinkLastNodes;
     bool m_Default;
+    uchar m_DefaultTrackId;
 };

commit c1758fab4d3ed3512b41f10e8ec0fda957c054ad

Fix default track sync issue introduced in 5d743da34d9
Previously this line would get triggered:
- https://github.com/multitheftauto/mtasa-blue/blob/fdc13c28006816a/Shared/sdk/net/bitstream.h#L325

This happens because of:
- https://github.com/multitheftauto/mtasa-blue/blob/e11685cab4beb7958ab202261f9c9d9b4ce71e58/Client/mods/deathmatch/logic/CClientEntity.cpp#L33-L37

Client/mods/deathmatch/logic/CClientTrainTrack.cpp
@@ -11,11 +11,11 @@
 #include <StdInc.h>
 #include "CClientTrainTrack.h"
 
-CClientTrainTrack::CClientTrainTrack(ElementID ID, const std::vector<STrackNode>& trackNodes, bool linkLastNode) : CClientEntity(ID)
+CClientTrainTrack::CClientTrainTrack(ElementID ID, const std::vector<STrackNode>& trackNodes, bool linkLastNode, bool isDefault) : CClientEntity(ID)
 {
     SetTypeName("train-track");
     m_pTrainTrack = g_pGame->GetTrainTrackManager()->CreateTrainTrack(trackNodes, linkLastNode);
-
+    m_defaultTrack = isDefault;
     g_pClientGame->GetManager()->GetTrainTrackManager()->RegisterTrainTrack(this);
 }
 

Client/mods/deathmatch/logic/CClientTrainTrack.h
@@ -9,12 +9,13 @@
 *
 *****************************************************************************/
 #pragma once
-#include "CClientEntity.h"
+
+class CClientEntity;
 
 class CClientTrainTrack : public CClientEntity
 {
 public:
-    CClientTrainTrack(ElementID ID, const std::vector<STrackNode>& trackNodes, bool linkLastNode);
+    CClientTrainTrack(ElementID ID, const std::vector<STrackNode>& trackNodes, bool linkLastNode, bool isDefault);
     virtual ~CClientTrainTrack();
 
     virtual eClientEntityType GetType() const { return CCLIENTTRAINTRACK; }
@@ -29,6 +30,7 @@ public:
     size_t GetNumberOfNodes();
 
     void SetLastNodesLinked(bool linked);
+    bool IsDefault() { return m_defaultTrack; }
 
     // Sorta a hack that these are required by CClientEntity
     void Unlink () {}
@@ -37,4 +39,5 @@ public:
 
 private:
     CTrainTrack* m_pTrainTrack;
+    bool         m_defaultTrack = false;
 };

Client/mods/deathmatch/logic/CClientTrainTrackManager.cpp
@@ -25,7 +25,7 @@ void CClientTrainTrackManager::Reset()
             delete pTrainTrack;
 
         // Create train tracks
-        pTrainTrack = new CClientTrainTrack(INVALID_ELEMENT_ID, OriginalTrackNodes[i], true);
+        pTrainTrack = new CClientTrainTrack(INVALID_ELEMENT_ID, OriginalTrackNodes[i], true, true);
         pTrainTrack->MakeSystemEntity();
     }
 }

Client/mods/deathmatch/logic/CNetAPI.cpp
@@ -1585,7 +1585,7 @@ void CNetAPI::WriteVehiclePuresync(CClientPed* pPlayerModel, CClientVehicle* pVe
         BitStream.WriteBit(bDirection);
         BitStream.Write(fSpeed);
         
-        if (pTrainTrack && pTrainTrack->GetID() == INVALID_ELEMENT_ID)
+        if (pTrainTrack && pTrainTrack->IsDefault())
         {
             BitStream.WriteBit(true); // is default track
             BitStream.Write(pTrainTrack->GetTrackIndex());

Client/mods/deathmatch/logic/CPacketHandler.cpp
@@ -4001,7 +4001,7 @@ retry:
                         trackNode.SetPosition(position);
                         trackNodes.push_back(std::move(trackNode));
                     }
-                    pEntity = new CClientTrainTrack(EntityID, trackNodes, linkLastNode);
+                    pEntity = new CClientTrainTrack(EntityID, trackNodes, linkLastNode, false);
                     break;
                 }
 