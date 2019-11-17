36b90641f0e02ca0754b518981c09818c3d0e383
Dutchman101 <supernep@live.nl>
2019-11-17 09:03:28 +0100

    Revert "Update CEF to 78.3.1+g8819d2e+chromium-78.0.3904.87"
    This reverts commit 0da4deba77b98704a915460edbb169c956ea5231.
    
    Requested by Jusonex (due to a potential new crash, and him not being around for a couple days)

--- utils/buildactions/install_cef.lua
+++ utils/buildactions/install_cef.lua
@@ -7,7 +7,7 @@ local CEF_PATH = "vendor/cef3/"
 local CEF_URL_PREFIX = "https://mirror.mtasa.com/bdata/cef/"
 local CEF_URL_SUFFIX = "_windows32_minimal.tar.bz2"
 
-local CEF_VERSION = "78.3.1+g8819d2e+chromium-78.0.3904.87" -- Change here to update CEF version
+local CEF_VERSION = "77.1.12+gc63c001+chromium-77.0.3865.90" -- Change here to update CEF version
 
 function make_cef_download_url()
 	return CEF_URL_PREFIX..http.escapeUrlParam(CEF_VERSION)..CEF_URL_SUFFIX
