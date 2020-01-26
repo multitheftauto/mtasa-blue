4c36d37056d2a1496904f394bf96303dd0f2b4c5
Qais Patankar <qaisjp@gmail.com>
2020-01-26 18:51:53 +0000

    Fix #880: push setPedAnimationSpeed limit

--- Client/mods/deathmatch/logic/luadefs/CLuaPedDefs.cpp
+++ Client/mods/deathmatch/logic/luadefs/CLuaPedDefs.cpp
@@ -2080,7 +2080,7 @@ int CLuaPedDefs::SetPedAnimationSpeed(lua_State* luaVM)
 
     if (!argStream.HasErrors())
     {
-        if (!strAnimName.empty() && fSpeed >= 0.0f && fSpeed <= 1.0f)
+        if (!strAnimName.empty() && fSpeed >= 0.0f && fSpeed <= 10.0f)
         {
             if (CStaticFunctionDefinitions::SetPedAnimationSpeed(*pEntity, strAnimName, fSpeed))
             {
