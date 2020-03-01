multitheftauto/mtasa-blue

commit dfa9b5086a256aeb8eea01ee5aba2414cc14c964

Fix engineReplaceModel memory leak (#359)

Client/game_sa/CFileLoaderSA.cpp
@@ -1,0 +1,90 @@
+#include "StdInc.h"
+#include "gamesa_renderware.h"
+#include "CFileLoaderSA.h"
+
+CFileLoaderSA::CFileLoaderSA()
+{
+    InstallHooks();
+}
+
+CFileLoaderSA::~CFileLoaderSA()
+{
+
+}
+
+void CFileLoaderSA::InstallHooks()
+{
+    HookInstall(0x537150, (DWORD)CFileLoader_SetRelatedModelInfoCB, 5);
+}
+
+class CAtomicModelInfo
+{
+public:
+
+    void CAtomicModelInfo::DeleteRwObject()
+    {
+        ((void(__thiscall *)(CAtomicModelInfo*))(*(void ***)this)[8])(this);
+    }
+
+    void CAtomicModelInfo::SetAtomic(RpAtomic* atomic)
+    {
+        ((void(__thiscall *)(CAtomicModelInfo*, RpAtomic*))(*(void ***)this)[15])(this, atomic);
+    }
+
+};
+
+class CDamagableModelInfo
+{
+public:
+    void CDamagableModelInfo::SetDamagedAtomic(RpAtomic* atomic)
+    {
+        ((void(__thiscall *)(CDamagableModelInfo*, RpAtomic*))0x4C48D0)(this, atomic);
+    }
+};
+
+static char* GetFrameNodeName(RwFrame* frame) {
+    return ((char* (__cdecl*)(RwFrame*))0x72FB30)(frame);
+}
+
+static void GetNameAndDamage(char const* nodeName, char* outName, bool& outDamage) {
+    return ((void(__cdecl*)(char const*, char*, bool&))0x5370A0)(nodeName, outName, outDamage);
+}
+
+static void CVisibilityPlugins_SetAtomicRenderCallback(RpAtomic *pRpAtomic, RpAtomic * (*renderCB)(RpAtomic *)) {
+    return ((void(__cdecl*)(RpAtomic *, RpAtomic * (*renderCB)(RpAtomic *)))0x7328A0)(pRpAtomic, renderCB);
+}
+
+static void CVisibilityPlugins_SetAtomicId(RpAtomic* pRpAtomic, int id) {
+    return ((void(__cdecl*)(RpAtomic*, int))0x732230)(pRpAtomic, id);
+}
+
+static RpAtomic* CFileLoader_SetRelatedModelInfoCB(RpAtomic* atomic, RpClump* clump)
+{
+    static auto CModelInfo_ms_modelInfoPtrs = (CBaseModelInfoSAInterface**)ARRAY_ModelInfo;
+    static unsigned int& gAtomicModelId = *reinterpret_cast<unsigned int*>(DWORD_AtomicsReplacerModelID);
+
+    char name[24];
+    auto pAtomicModelInfo = reinterpret_cast<CAtomicModelInfo*>(CModelInfo_ms_modelInfoPtrs[gAtomicModelId]);
+    char* frameNodeName = GetFrameNodeName((RwFrame*)atomic->object.object.parent);
+    bool bDamage = false;
+    GetNameAndDamage(frameNodeName, (char*)&name, bDamage);
+    CVisibilityPlugins_SetAtomicRenderCallback(atomic, 0);
+
+    // Fix #359: engineReplaceModel memory leak
+    // Delete the current atomic before setting a new one.
+    pAtomicModelInfo->DeleteRwObject();
+    if (bDamage)
+    {
+        auto pDamagableModelInfo = reinterpret_cast<CDamagableModelInfo*>(pAtomicModelInfo);
+        pDamagableModelInfo->SetDamagedAtomic(atomic);
+    }
+    else
+    {
+        pAtomicModelInfo->SetAtomic(atomic);
+    }
+    RpClumpRemoveAtomic(clump, atomic);
+    RwFrame* newFrame = RwFrameCreate();
+    RpAtomicSetFrame(atomic, newFrame);
+    CVisibilityPlugins_SetAtomicId(atomic, gAtomicModelId);
+    return atomic;
+}

Client/game_sa/CFileLoaderSA.h
@@ -1,0 +1,12 @@
+#pragma once
+
+class CFileLoaderSA
+{
+public:
+    CFileLoaderSA();
+    ~CFileLoaderSA();
+
+    void InstallHooks();
+};
+
+RpAtomic* CFileLoader_SetRelatedModelInfoCB(RpAtomic* atomic, RpClump* clump);

Client/game_sa/CGameSA.h
@@ -14,6 +14,7 @@
 #include "CModelInfoSA.h"
 #include "CObjectGroupPhysicalPropertiesSA.h"
 #include "CFxManagerSA.h"
+#include "CFileLoaderSA.h"
 
 #define     MAX_MEMORY_OFFSET_1_0           0xCAF008
 
@@ -106,6 +107,7 @@ private:
     CWeaponInfo* WeaponInfos[NUM_WeaponInfosTotal];
     CModelInfoSA ModelInfo[MODELINFO_MAX];
     CObjectGroupPhysicalPropertiesSA ObjectGroupsInfo[OBJECTDYNAMICINFO_MAX];
+    CFileLoaderSA FileLoader;
 
 public:
     ZERO_ON_NEW

commit 275eae917eb0f39abd69f7e0133ffed02f318335

Fix upgrade crash introduced in dfa9b5086a256aeb8eea01ee5aba2414cc14c964

Client/game_sa/CFileLoaderSA.cpp
@@ -14,6 +14,7 @@ CFileLoaderSA::~CFileLoaderSA()
 
 void CFileLoaderSA::InstallHooks()
 {
+    HookInstall(0x5371F0, (DWORD)CFileLoader_LoadAtomicFile, 5);
     HookInstall(0x537150, (DWORD)CFileLoader_SetRelatedModelInfoCB, 5);
 }
 
@@ -58,13 +59,70 @@ static void CVisibilityPlugins_SetAtomicId(RpAtomic* pRpAtomic, int id) {
     return ((void(__cdecl*)(RpAtomic*, int))0x732230)(pRpAtomic, id);
 }
 
-static RpAtomic* CFileLoader_SetRelatedModelInfoCB(RpAtomic* atomic, RpClump* clump)
+static void CVehicleModelInfo_UseCommonVehicleTexDicationary()
 {
-    static auto CModelInfo_ms_modelInfoPtrs = (CBaseModelInfoSAInterface**)ARRAY_ModelInfo;
-    static unsigned int& gAtomicModelId = *reinterpret_cast<unsigned int*>(DWORD_AtomicsReplacerModelID);
+    ((void(__cdecl*)())0x4C75A0)();
+}
 
+static void CVehicleModelInfo_StopUsingCommonVehicleTexDicationary()
+{
+    ((void(__cdecl*)())0x4C75C0)();
+}
+
+static auto CModelInfo_ms_modelInfoPtrs = (CBaseModelInfoSAInterface**)ARRAY_ModelInfo;
+static unsigned int& gAtomicModelId = *reinterpret_cast<unsigned int*>(DWORD_AtomicsReplacerModelID);
+
+bool CFileLoader_LoadAtomicFile(RwStream *stream, unsigned int modelId) 
+{
+    CBaseModelInfoSAInterface* pBaseModelInfo = CModelInfo_ms_modelInfoPtrs[modelId];
+    auto pAtomicModelInfo = reinterpret_cast<CAtomicModelInfo*>(pBaseModelInfo);
+
+    bool bUseCommonVehicleTexDictionary = false;
+    if (pAtomicModelInfo && pBaseModelInfo->bWetRoadReflection)
+    {
+        bUseCommonVehicleTexDictionary = true;
+        CVehicleModelInfo_UseCommonVehicleTexDicationary();
+    }
+
+    const unsigned int rwID_CLUMP = 16;
+    if (RwStreamFindChunk(stream, rwID_CLUMP, nullptr, nullptr))
+    {
+        RpClump* pReadClump = RpClumpStreamRead(stream);
+        if (!pReadClump)
+        {
+            if (bUseCommonVehicleTexDictionary)
+            {
+                CVehicleModelInfo_StopUsingCommonVehicleTexDicationary();
+            }
+            return false;
+        }
+
+        gAtomicModelId = modelId;
+        SRelatedModelInfo relatedModelInfo;
+        relatedModelInfo.pClump = pReadClump;
+        relatedModelInfo.bDeleteOldRwObject = false;
+
+        RpClumpForAllAtomics(pReadClump, (RpClumpForAllAtomicsCB_t)CFileLoader_SetRelatedModelInfoCB, &relatedModelInfo);
+        RpClumpDestroy(pReadClump);
+    }
+
+    if (!pBaseModelInfo->pRwObject)
+    {
+        return false;
+    }
+
+    if (bUseCommonVehicleTexDictionary)
+    {
+        CVehicleModelInfo_StopUsingCommonVehicleTexDicationary();
+    }
+    return true;
+}
+
+RpAtomic* CFileLoader_SetRelatedModelInfoCB(RpAtomic* atomic, SRelatedModelInfo* pRelatedModelInfo)
+{
     char name[24];
-    auto pAtomicModelInfo = reinterpret_cast<CAtomicModelInfo*>(CModelInfo_ms_modelInfoPtrs[gAtomicModelId]);
+    CBaseModelInfoSAInterface* pBaseModelInfo = CModelInfo_ms_modelInfoPtrs[gAtomicModelId];
+    auto pAtomicModelInfo = reinterpret_cast<CAtomicModelInfo*>(pBaseModelInfo);
     char* frameNodeName = GetFrameNodeName((RwFrame*)atomic->object.object.parent);
     bool bDamage = false;
     GetNameAndDamage(frameNodeName, (char*)&name, bDamage);
@@ -72,7 +130,14 @@ static RpAtomic* CFileLoader_SetRelatedModelInfoCB(RpAtomic* atomic, RpClump* cl
 
     // Fix #359: engineReplaceModel memory leak
     // Delete the current atomic before setting a new one.
-    pAtomicModelInfo->DeleteRwObject();
+    if (pRelatedModelInfo->bDeleteOldRwObject && pBaseModelInfo->pRwObject)
+    {
+        // DeleteRwObject will decrement the reference count. 
+        // We don't want the texture to get unloaded, so let's increment it.
+        CTxdStore_AddRef(pBaseModelInfo->usTextureDictionary);
+        pAtomicModelInfo->DeleteRwObject();
+    }
+
     if (bDamage)
     {
         auto pDamagableModelInfo = reinterpret_cast<CDamagableModelInfo*>(pAtomicModelInfo);
@@ -82,7 +147,7 @@ static RpAtomic* CFileLoader_SetRelatedModelInfoCB(RpAtomic* atomic, RpClump* cl
     {
         pAtomicModelInfo->SetAtomic(atomic);
     }
-    RpClumpRemoveAtomic(clump, atomic);
+    RpClumpRemoveAtomic(pRelatedModelInfo->pClump, atomic);
     RwFrame* newFrame = RwFrameCreate();
     RpAtomicSetFrame(atomic, newFrame);
     CVisibilityPlugins_SetAtomicId(atomic, gAtomicModelId);

Client/game_sa/CFileLoaderSA.h
@@ -1,5 +1,11 @@
 #pragma once
 
+struct SRelatedModelInfo
+{
+    RpClump* pClump;
+    bool bDeleteOldRwObject;
+};
+
 class CFileLoaderSA
 {
 public:
@@ -9,4 +15,5 @@ public:
     void InstallHooks();
 };
 
-RpAtomic* CFileLoader_SetRelatedModelInfoCB(RpAtomic* atomic, RpClump* clump);
+bool CFileLoader_LoadAtomicFile(RwStream *stream, unsigned int modelId);
+RpAtomic* CFileLoader_SetRelatedModelInfoCB(RpAtomic* atomic, SRelatedModelInfo* pRelatedModelInfo);

Client/game_sa/CRenderWareSA.cpp
@@ -17,6 +17,7 @@
 #include "gamesa_renderware.h"
 #include "gamesa_renderware.hpp"
 #include "CRenderWareSA.ShaderMatching.h"
+#include "CFileLoaderSA.h"
 
 extern CGameSA* pGame;
 
@@ -478,8 +479,13 @@ typedef struct
 bool AtomicsReplacer(RpAtomic* pAtomic, void* data)
 {
     SAtomicsReplacer* pData = reinterpret_cast<SAtomicsReplacer*>(data);
-    ((void (*)(RpAtomic*, void*))FUNC_AtomicsReplacer)(pAtomic, pData->pClump);
-    // The above function adds a reference to the model's TXD. Remove it again.
+    SRelatedModelInfo relatedModelInfo;
+    relatedModelInfo.pClump = pData->pClump;
+    relatedModelInfo.bDeleteOldRwObject = true;
+    CFileLoader_SetRelatedModelInfoCB(pAtomic, &relatedModelInfo);
+
+    // The above function adds a reference to the model's TXD by either 
+    // calling CAtomicModelInfo::SetAtomic or CDamagableModelInfo::SetDamagedAtomic. Remove it again.
     CTxdStore_RemoveRef(pData->usTxdID);
     return true;
 }