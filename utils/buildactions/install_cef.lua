0b148377aff89147556c0c4fe9c03dfbe3c13b65
Qais Patankar <qaisjp@gmail.com>
2020-03-27 09:15:07 +0000

    Download CEF from opensource.spotify.com (#1298)
    * Download CEF from opensource.spotify.com
    
    * Switch CEF from online SHA1 to offline SHA256
    
    * Add error handling when downloading CEF
    
    * Use sha256 for discord too

--- utils/buildactions/install_cef.lua
+++ utils/buildactions/install_cef.lua
@@ -4,10 +4,12 @@ premake.modules.install_cef = {}
 
 -- Config variables
 local CEF_PATH = "vendor/cef3/"
-local CEF_URL_PREFIX = "https://mirror.mtasa.com/bdata/cef/"
+local CEF_URL_PREFIX = "http://opensource.spotify.com/cefbuilds/cef_binary_"
 local CEF_URL_SUFFIX = "_windows32_minimal.tar.bz2"
 
-local CEF_VERSION = "80.0.4+g74f7b0c+chromium-80.0.3987.122" -- Change here to update CEF version
+-- Change here to update CEF version
+local CEF_VERSION = "80.0.4+g74f7b0c+chromium-80.0.3987.122"
+local CEF_HASH = "8FD8E24AF196F00FEAAA1553496BAE99D8196BA023D0DD0FE44EFEEE93B04DFC"
 
 function make_cef_download_url()
 	return CEF_URL_PREFIX..http.escapeUrlParam(CEF_VERSION)..CEF_URL_SUFFIX
@@ -21,28 +23,24 @@ newaction {
 		-- Only execute on Windows
 		if os.host() ~= "windows" then return end
 
-		-- Download md5
-		local correct_checksum, result_string = http.get(make_cef_download_url()..".md5")
-		if result_string ~= "OK" and result_string then
-			print("Could not check CEF checksum: "..result_string)
-			return -- Do nothing and rely on earlier installed files (to allow working offline)
-
-			-- TODO(jusonex): It might make sense to fallback to spotify cef mirror
-		end
-
-		-- Trim whitespace
-		correct_checksum = correct_checksum:gsub("[%s%c]", "")
-
-		-- Check md5
+		-- Check file hash
 		local archive_path = CEF_PATH.."temp.tar.bz2"
-		if os.isfile(archive_path) and os.md5_file(archive_path) == correct_checksum then
+		if os.isfile(archive_path) and os.sha256_file(archive_path) == CEF_HASH then
 			print("CEF consistency checks succeeded")
 			return
 		end
 
 		-- Download CEF
 		print("Downloading CEF...")
-		http.download(make_cef_download_url(), archive_path)
+		local result_str, response_code = http.download(make_cef_download_url(), archive_path)
+		if result_str ~= "OK" and response_code ~= 200 then
+			term.pushColor(term.red)
+			io.write(("Could not download CEF with status code %d: "):format(response_code))
+			term.setTextColor(term.purple)
+			print(result_str)
+			term.popColor()
+			return
+		end
 
 		-- Delete old CEF files
 		-- TODO: It might be better to download the files into a new folder and delete this folder at once
@@ -56,10 +54,10 @@ newaction {
 		-- Extract first bz2 and then tar
 		os.extract_archive(archive_path, CEF_PATH, true) -- Extract .tar.bz2 to .tar
 		os.extract_archive(CEF_PATH.."temp.tar", CEF_PATH, true) -- Extract .tar
-		
+
 		-- Move all files from cef_binary*/* to ./
 		os.expanddir_wildcard(CEF_PATH.."cef_binary*", CEF_PATH)
-		
+
 		-- Delete .tar archive, but keep .tar.bz2 for checksumming
 		os.remove(CEF_PATH.."temp.tar")
 	end
