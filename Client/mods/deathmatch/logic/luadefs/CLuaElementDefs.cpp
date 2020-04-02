1d182b35138c8b53ff26d47d7bbb773f4c6e0b0a
Qais Patankar <qaisjp@gmail.com>
2020-04-02 10:56:58 +0100

    Update Client/mods/deathmatch/logic/luadefs/CLuaElementDefs.cpp

--- Client/mods/deathmatch/logic/luadefs/CLuaElementDefs.cpp
+++ Client/mods/deathmatch/logic/luadefs/CLuaElementDefs.cpp
@@ -1779,7 +1779,7 @@ int CLuaElementDefs::SetElementData(lua_State* luaVM)
         {
             // Warn and truncate if key is too long
             if (strKey.length() > MAX_CUSTOMDATA_NAME_LENGTH)
-            { 
+            {
                 m_pScriptDebugging->LogCustom(luaVM, SString("Truncated argument @ '%s' [%s]", lua_tostring(luaVM, lua_upvalueindex(1)),
                                                              *SString("string length reduced to %d characters at argument 2", MAX_CUSTOMDATA_NAME_LENGTH)));
                 strKey = strKey.Left(MAX_CUSTOMDATA_NAME_LENGTH);
