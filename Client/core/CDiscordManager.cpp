multitheftauto/mtasa-blue

commit a4f447ae8047b6d2d65077f093adff69b16b3246

Fix #1336: Discord Rich Presence not respecting CVars load (#1345)

Client/core/CDiscordManager.cpp
@@ -18,11 +18,7 @@
 
 CDiscordManager::CDiscordManager() : m_DiscordCore(nullptr), m_Suicide(false), m_WaitingForServerName(false), m_StoredActivity{}
 {
-    Reconnect(true);            // Try to interact with discord on construction
-    m_Thread = new CThreadHandle(CDiscordManager::DiscordThread, this);
-
-    m_StoredActivity.GetAssets().SetLargeImage("mta_logo_round");            // Always thing
-    m_StoredActivity.GetAssets().SetLargeText("Playing MTA:SA");
+    
 }
 
 CDiscordManager::~CDiscordManager()
@@ -37,6 +33,15 @@ CDiscordManager::~CDiscordManager()
 
     delete m_Thread;
     SAFE_DELETE(m_DiscordCore);
+}
+
+void CDiscordManager::Initialize()
+{
+    Reconnect(true);            // Try to interact with discord on construction
+    m_Thread = new CThreadHandle(CDiscordManager::DiscordThread, this);
+
+    m_StoredActivity.GetAssets().SetLargeImage("mta_logo_round");            // Always thing
+    m_StoredActivity.GetAssets().SetLargeText("Playing MTA:SA");
 }
 
 // Establish connection with discord

Client/core/CDiscordManager.h
@@ -24,6 +24,7 @@ public:
     static void  OnActivityJoin(const char* joinSecret);
     static void* DiscordThread(void* arg);
 
+    void Initialize();
     void Reconnect(bool bOnInitialization = false);
     void DoPulse();
 

Client/core/CCore.cpp
@@ -1006,6 +1006,9 @@ void CCore::CreateXML()
 
     // Load XML-dependant subsystems
     m_ClientVariables.Load();
+
+    // Initialize discord rich presence after client variables have been loaded completely as it is dependant on CVars
+    m_DiscordManager->Initialize();
 }
 
 void CCore::DestroyGame()

commit 3d92acdd0027bdb3a0aa378d8d0e6e4144b0f402

Fix #1349 crash introduced in PR #1345 (#1350)
* Fix a bug with discord rich presence not respecting CVars load

* Fix crash at https://github.com/multitheftauto/mtasa-blue/issues/1349

* Fix coding style

Client/core/CDiscordManager.cpp
@@ -16,7 +16,7 @@
 #define DISCORD_CLIENT_ID 468493322583801867
 #endif
 
-CDiscordManager::CDiscordManager() : m_DiscordCore(nullptr), m_Suicide(false), m_WaitingForServerName(false), m_StoredActivity{}
+CDiscordManager::CDiscordManager() : m_DiscordCore(nullptr), m_Suicide(false), m_WaitingForServerName(false), m_StoredActivity{}, m_Initialized(false)
 {
     
 }
@@ -42,6 +42,8 @@ void CDiscordManager::Initialize()
 
     m_StoredActivity.GetAssets().SetLargeImage("mta_logo_round");            // Always thing
     m_StoredActivity.GetAssets().SetLargeText("Playing MTA:SA");
+
+    m_Initialized = true;
 }
 
 // Establish connection with discord
@@ -173,6 +175,8 @@ void* CDiscordManager::DiscordThread(void* arg)
 // establishing connection with discord is sometimes time-consuming, especially when it's not running
 void CDiscordManager::DoPulse()
 {
+    if (!m_Initialized) return; // Wait until initialization
+
     if (!m_DiscordCore)
     {
         // Discord is not initialized, maybe it's not installed or not yet running

Client/core/CDiscordManager.h
@@ -56,6 +56,7 @@ private:
     discord::Activity m_StoredActivity;
 
     bool m_WaitingForServerName;
+    bool m_Initialized;
 
     volatile bool m_Suicide;            // Thread kill command
 