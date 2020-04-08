multitheftauto/mtasa-blue

commit a4f447ae8047b6d2d65077f093adff69b16b3246

Fix #1336: Discord Rich Presence not respecting CVars load (#1345)

Client/core/CCore.cpp
@@ -1006,6 +1006,9 @@ void CCore::CreateXML()
 
     // Load XML-dependant subsystems
     m_ClientVariables.Load();
+
+    // Initialize discord rich presence after client variables have been loaded completely as it is dependant on CVars
+    m_DiscordManager->Initialize();
 }
 
 void CCore::DestroyGame()

Client/core/CDiscordManager.cpp
@@ -18,11 +18,7 @@
 
 CDiscordManager::CDiscordManager() : m_DiscordCore(nullptr), m_Suicide(false), m_WaitingForServerName(false), m_StoredActivity{}
 {
-    Reconnect(true);            // Try to interact with discord on construction
-    m_Thread = new CThreadHandle(CDiscordManager::DiscordThread, this);
-
-    m_StoredActivity.GetAssets().SetLargeImage("mta_logo_round");            // Always thing
-    m_StoredActivity.GetAssets().SetLargeText("Playing MTA:SA");
+    
 }
 
 CDiscordManager::~CDiscordManager()
@@ -37,6 +33,15 @@ CDiscordManager::~CDiscordManager()
 
     delete m_Thread;
     SAFE_DELETE(m_DiscordCore);
+}
+
+void CDiscordManager::Initialize()
+{
+    Reconnect(true);            // Try to interact with discord on construction
+    m_Thread = new CThreadHandle(CDiscordManager::DiscordThread, this);
+
+    m_StoredActivity.GetAssets().SetLargeImage("mta_logo_round");            // Always thing
+    m_StoredActivity.GetAssets().SetLargeText("Playing MTA:SA");
 }
 
 // Establish connection with discord

Client/core/CDiscordManager.h
@@ -24,6 +24,7 @@ public:
     static void  OnActivityJoin(const char* joinSecret);
     static void* DiscordThread(void* arg);
 
+    void Initialize();
     void Reconnect(bool bOnInitialization = false);
     void DoPulse();
 

commit 16d249db4e10d84c91f65cbe979e0a85bb431362

Fix #1357: discord crashes from a4f447ae8047b6d2d65077f093adff69 (#1358)
* Fix a bug with discord rich presence not respecting CVars load

* Fix crash at https://github.com/multitheftauto/mtasa-blue/issues/1349

* Fix coding style

* Fix all the bugs which caused crash after a4f447ae8047b6d2d65077f093adff69b16b3246

* Move all the initialization values to class declaration

* Addendum ea42a3c1f8c68fe61c21f77d57949486c4ad1b4a

Client/core/CCore.cpp
@@ -57,6 +57,9 @@ CCore::CCore() : m_DiscordManager(new CDiscordManager())
     ApplyCoreInitSettings();
     g_pLocalization = new CLocalization;
 
+    // Initialize discord manager
+    m_DiscordManager->Initialize();
+
     // Create a logger instance.
     m_pConsoleLogger = new CConsoleLogger();
 
@@ -1006,9 +1009,6 @@ void CCore::CreateXML()
 
     // Load XML-dependant subsystems
     m_ClientVariables.Load();
-
-    // Initialize discord rich presence after client variables have been loaded completely as it is dependant on CVars
-    m_DiscordManager->Initialize();
 }
 
 void CCore::DestroyGame()

Client/core/CDiscordManager.cpp
@@ -16,11 +16,6 @@
 #define DISCORD_CLIENT_ID 468493322583801867
 #endif
 
-CDiscordManager::CDiscordManager() : m_DiscordCore(nullptr), m_Suicide(false), m_WaitingForServerName(false), m_StoredActivity{}, m_Initialized(false)
-{
-    
-}
-
 CDiscordManager::~CDiscordManager()
 {
     m_Suicide = true;
@@ -37,6 +32,8 @@ CDiscordManager::~CDiscordManager()
 
 void CDiscordManager::Initialize()
 {
+    if (m_Initialized) return;
+
     Reconnect(true);            // Try to interact with discord on construction
     m_Thread = new CThreadHandle(CDiscordManager::DiscordThread, this);
 
@@ -175,10 +172,11 @@ void* CDiscordManager::DiscordThread(void* arg)
 // establishing connection with discord is sometimes time-consuming, especially when it's not running
 void CDiscordManager::DoPulse()
 {
-    if (!m_Initialized) return; // Wait until initialization
-
+    m_ThreadSafety.lock();
     if (!m_DiscordCore)
     {
+        m_ThreadSafety.unlock();
+
         // Discord is not initialized, maybe it's not installed or not yet running
         // So every 15sec we will check if the player got discord running
         if (m_TimeForReconnection.Get() >= 15000)
@@ -189,6 +187,7 @@ void CDiscordManager::DoPulse()
 
         return;
     }
+    m_ThreadSafety.unlock();
 
     std::lock_guard<std::mutex> guardian(m_ThreadSafety);
 
@@ -362,13 +361,6 @@ void CDiscordManager::Disconnect()
 {
     std::lock_guard<std::mutex> guardian(m_ThreadSafety);
     SAFE_DELETE(m_DiscordCore);
-}
-
-void CDiscordManager::DisconnectNotification()
-{
-    std::lock_guard<std::mutex> guardian(m_ThreadSafety);
-    m_WaitingForServerName = false;            // No longer wait
-    m_QueryReceiver.InvalidateSocket();
 }
 
 SString CDiscordManager::GetJoinSecret()

Client/core/CDiscordManager.h
@@ -17,16 +17,9 @@
 class CDiscordManager : public CDiscordManagerInterface
 {
 public:
-    CDiscordManager();
     ~CDiscordManager();
 
-    static void  DiscordLogCallback(discord::LogLevel level, const char* message);
-    static void  OnActivityJoin(const char* joinSecret);
-    static void* DiscordThread(void* arg);
-
     void Initialize();
-    void Reconnect(bool bOnInitialization = false);
-    void DoPulse();
 
     // ActivityManager
     void UpdateActivity(SDiscordActivity& activity, std::function<void(EDiscordRes)> callback);            // Change it all, or ...
@@ -41,27 +34,30 @@ public:
     void RegisterPlay(bool connected);
     void Disconnect();
 
-    discord::Activity GetStoredActivity() const { return m_StoredActivity; }            // For retrieving stored information in rich presence
-
-    bool NeedsSuicide() const { return m_Suicide; }
-    void SetDead() { m_Suicide = false; }
-    void DisconnectNotification();
-
     SString GetJoinSecret();
 
 private:
+    void Reconnect(bool bOnInitialization = false);
+    void DoPulse();
     void Restore();
 
-    discord::Core*    m_DiscordCore;
-    discord::Activity m_StoredActivity;
+    static void  DiscordLogCallback(discord::LogLevel level, const char* message);
+    static void  OnActivityJoin(const char* joinSecret);
+    static void* DiscordThread(void* arg);
 
-    bool m_WaitingForServerName;
-    bool m_Initialized;
+    bool NeedsSuicide() const { return m_Suicide; }
+    void SetDead() { m_Suicide = false; }
 
-    volatile bool m_Suicide;            // Thread kill command
+    discord::Core*    m_DiscordCore = nullptr;
+    discord::Activity m_StoredActivity{};
+
+    bool m_WaitingForServerName = false;
+    bool m_Initialized = false;
+
+    volatile bool m_Suicide = false;            // Thread kill command
 
     std::mutex                 m_ThreadSafety;
-    SharedUtil::CThreadHandle* m_Thread;
+    SharedUtil::CThreadHandle* m_Thread = nullptr;
 
     CElapsedTime   m_TimeForReconnection;
     CQueryReceiver m_QueryReceiver;