multitheftauto/mtasa-blue

commit a4f447ae8047b6d2d65077f093adff69b16b3246

Fix #1336: Discord Rich Presence not respecting CVars load (#1345)

Client/core/CDiscordManager.cpp
@@ -18,11 +18,7 @@
 
 CDiscordManager::CDiscordManager() : m_DiscordCore(nullptr), m_Suicide(false), m_WaitingForServerName(false), m_StoredActivity{}
 {
-    Reconnect(true);            // Try to interact with discord on construction
-    m_Thread = new CThreadHandle(CDiscordManager::DiscordThread, this);
-
-    m_StoredActivity.GetAssets().SetLargeImage("mta_logo_round");            // Always thing
-    m_StoredActivity.GetAssets().SetLargeText("Playing MTA:SA");
+    
 }
 
 CDiscordManager::~CDiscordManager()
@@ -37,6 +33,15 @@ CDiscordManager::~CDiscordManager()
 
     delete m_Thread;
     SAFE_DELETE(m_DiscordCore);
+}
+
+void CDiscordManager::Initialize()
+{
+    Reconnect(true);            // Try to interact with discord on construction
+    m_Thread = new CThreadHandle(CDiscordManager::DiscordThread, this);
+
+    m_StoredActivity.GetAssets().SetLargeImage("mta_logo_round");            // Always thing
+    m_StoredActivity.GetAssets().SetLargeText("Playing MTA:SA");
 }
 
 // Establish connection with discord

Client/core/CCore.cpp
@@ -1006,6 +1006,9 @@ void CCore::CreateXML()
 
     // Load XML-dependant subsystems
     m_ClientVariables.Load();
+
+    // Initialize discord rich presence after client variables have been loaded completely as it is dependant on CVars
+    m_DiscordManager->Initialize();
 }
 
 void CCore::DestroyGame()

Client/core/CDiscordManager.h
@@ -24,6 +24,7 @@ public:
     static void  OnActivityJoin(const char* joinSecret);
     static void* DiscordThread(void* arg);
 
+    void Initialize();
     void Reconnect(bool bOnInitialization = false);
     void DoPulse();
 

commit d8c3ff5f5a9872d8aff448d58fb9fe6d0df1f1d5

Fix #1329: discord presence party info is kept after disconnect (#1359)
* Fix a bug with discord rich presence not respecting CVars load

* Fix crash at https://github.com/multitheftauto/mtasa-blue/issues/1349

* Fix coding style

* Fix all the bugs which caused crash after a4f447ae8047b6d2d65077f093adff69b16b3246

* Move all the initialization values to class declaration

* Addendum ea42a3c1f8c68fe61c21f77d57949486c4ad1b4a

* Fix issue #1329

Client/core/CDiscordManager.cpp
@@ -239,7 +239,11 @@ void CDiscordManager::UpdateActivity(std::function<void(EDiscordRes)> callback)
     if (!m_DiscordCore)
         return;
 
-    m_DiscordCore->ActivityManager().UpdateActivity(m_StoredActivity, [=](discord::Result res) { callback(static_cast<EDiscordRes>(res)); });
+    m_DiscordCore->ActivityManager().UpdateActivity(m_StoredActivity, [=](discord::Result res)
+    {
+        callback(static_cast<EDiscordRes>(res));
+        if (res != discord::Result::Ok) WriteDebugEvent(SString("[DISCORD]: Updating activity failed, code: %i", static_cast<int>(res)));
+    });
 }
 
 void CDiscordManager::SetType(EDiscordActivityT type, std::function<void(EDiscordRes)> callback)
@@ -283,6 +287,16 @@ void CDiscordManager::SetJoinParameters(const char* joinSecret, const char* part
     if (!joinSecret || !partyId)
         return;
 
+    if (!strlen(joinSecret) || !strlen(partyId))
+    {
+        // Delete join state
+        m_StoredActivity.GetSecrets().SetJoin("");
+        m_StoredActivity.GetParty().SetId("");
+        m_StoredActivity.GetParty().GetSize().SetCurrentSize(0);
+        m_StoredActivity.GetParty().GetSize().SetMaxSize(0);
+        return;
+    }
+
     SString      ipaddr;
     unsigned int port;
     CVARS_GET("host", ipaddr);
@@ -314,6 +328,10 @@ void CDiscordManager::RegisterPlay(bool connected)
     {
         m_StoredActivity.GetAssets().SetSmallText("");
         m_StoredActivity.GetAssets().SetSmallImage("");
+        m_StoredActivity.GetSecrets().SetJoin("");
+        m_StoredActivity.GetParty().SetId("");
+        m_StoredActivity.GetParty().GetSize().SetCurrentSize(0);
+        m_StoredActivity.GetParty().GetSize().SetMaxSize(0);
         UpdateActivity([=](EDiscordRes res) {});
         return;
     }