multitheftauto/mtasa-blue

commit 5dd446be50c5c8dcbb2f81a6186c2bc3841429d3

Fix #863 warpPedIntoVehicle fails when there are too many vehicles streamed in (#1431)
* Fix #863 warpPedIntoVehicle fails when there are too many vehicles streamed in

* Fix typo: wrap to warp (thanks ZeoN)

Co-authored-by: saml1er <danishroar@gmail.com>

Client/mods/deathmatch/logic/CClientPed.cpp
@@ -1347,6 +1347,8 @@ void CClientPed::GetIntoVehicle(CClientVehicle* pVehicle, unsigned int uiSeat, u
 
 void CClientPed::WarpIntoVehicle(CClientVehicle* pVehicle, unsigned int uiSeat)
 {
+    SetWarpInToVehicleRequired(true);
+
     // Ensure vehicle model is loaded
     CModelInfo* pModelInfo = pVehicle->GetModelInfo();
     if (g_pGame->IsASyncLoadingEnabled() && !pModelInfo->IsLoaded())
@@ -4165,6 +4167,7 @@ void CClientPed::InternalWarpIntoVehicle(CVehicle* pGameVehicle)
             pInTask->SetIsWarpingPedIntoCar();
             pInTask->ProcessPed(m_pPlayerPed);
             pInTask->Destroy();
+            SetWarpInToVehicleRequired(false);
         }
 
         // If we're a remote player
@@ -4180,6 +4183,8 @@ void CClientPed::InternalRemoveFromVehicle(CVehicle* pGameVehicle)
 {
     if (m_pPlayerPed && m_pTaskManager)
     {
+        SetWarpInToVehicleRequired(false);
+
         // Reset whatever task
         m_pTaskManager->RemoveTask(TASK_PRIORITY_PRIMARY);
 

Client/mods/deathmatch/logic/CClientGame.cpp
@@ -1793,7 +1793,7 @@ void CClientGame::UpdateVehicleInOut()
             }
 
             // Are we supposed to be in a vehicle? But aren't?
-            if (pOccupiedVehicle && !pVehicle)
+            if (pOccupiedVehicle && !pVehicle && !m_pLocalPlayer->IsWarpInToVehicleRequired())
             {
                 // Jax: this happens when we try to warp into a streamed out vehicle, including when we use CClientVehicle::StreamInNow
                 // ..maybe we need a different way to detect bike falls?

Client/mods/deathmatch/logic/CClientPed.h
@@ -569,6 +569,9 @@ public:
     void      SetTaskTypeToBeRestoredOnAnimEnd(eTaskType taskType) { m_eTaskTypeToBeRestoredOnAnimEnd = taskType; }
     eTaskType GetTaskTypeToBeRestoredOnAnimEnd() { return m_eTaskTypeToBeRestoredOnAnimEnd; }
 
+    bool IsWarpInToVehicleRequired() { return m_bWarpInToVehicleRequired; }
+    void SetWarpInToVehicleRequired(bool warp) { m_bWarpInToVehicleRequired = warp; }
+
     void NotifyCreate();
     void NotifyDestroy();
 
@@ -733,4 +736,5 @@ public:
     ReplacedAnim_type m_mapOfReplacedAnimations;
     bool              m_bTaskToBeRestoredOnAnimEnd;
     eTaskType         m_eTaskTypeToBeRestoredOnAnimEnd;
+    bool              m_bWarpInToVehicleRequired = false;
 };

commit 0ee554b1cd5dc30c2c9ed348ab5ab94feb9e1ba4

Fix ped falling off bike desync bug
Bug was caused by 5dd446be50c5c8dcbb2f81a6186c2bc3841429d3

Client/mods/deathmatch/logic/CClientPed.cpp
@@ -1512,13 +1512,15 @@ void CClientPed::WarpIntoVehicle(CClientVehicle* pVehicle, unsigned int uiSeat)
 
     RemoveTargetPosition();
 
+    if (!pVehicle->IsStreamedIn() || !m_pPlayerPed)
+        SetWarpInToVehicleRequired(true);
+
     // Make peds stream in when they warp to a vehicle
     CVector vecInVehiclePosition;
     GetPosition(vecInVehiclePosition);
     UpdateStreamPosition(vecInVehiclePosition);
     if (pVehicle->IsStreamedIn() && !m_pPlayerPed)
         StreamIn(true);
-    SetWarpInToVehicleRequired(true);
 }
 
 void CClientPed::ResetToOutOfVehicleWeapon()