1791a2c9cf6e06a11e7c47569ca9801c7049d9e0
Pirulax <patrikjankovics7@gmail.com>
2020-05-20 04:09:11 +0200

    trying to add CLuaArgument as pushable, but i need help(someone who knows how CLuaArgs::PushAsTable works)
    So, the thing is, idk how many elements it pushes on the stack and its required to be known. So i need to modify CLuaArgs::PushAsTable to return how many items it pushed onto the stack. Thanks!

--- Shared/mods/deathmatch/logic/lua/LuaBasic.h
+++ Shared/mods/deathmatch/logic/lua/LuaBasic.h
@@ -77,6 +77,24 @@ namespace lua
         return 1;
     }
 
+    inline int Push(lua_State* L, const CLuaArgument* const arg)
+    {
+        if (arg->GetType() == LUA_TNONE)
+            return 0;
+
+        arg->Push(L);
+        return 1;
+    }
+
+    inline int Push(lua_State* L, const CLuaArgument& const arg)
+    {
+        if (arg.GetType() == LUA_TNONE)
+            return 0;
+
+        arg.Push(L);
+        return 1;
+    }
+
     template <typename... Ts>
     int Push(lua_State* L, const std::variant<Ts...>&& val)
     {
