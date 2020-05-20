d1ed5239324f028d5e60c80b6e10a51492f64e5f
Pirulax <patrikjankovics7@gmail.com>
2020-05-20 23:37:50 +0200

    Do changes to the macro in shared element defs as suggested

--- Shared/mods/deathmatch/logic/luadefs/CLuaElementDefsShared.cpp
+++ Shared/mods/deathmatch/logic/luadefs/CLuaElementDefsShared.cpp
@@ -1,13 +1,15 @@
 #include "StdInc.h"
 
-// Warn and truncate if key is too long(luaVM must be in a var luaVM)
-// is undef-ed at the end of the element data func defs
-#define EDATA_KEY_WARN_TRUNCATE(key) \
-if ((key).length() > MAX_CUSTOMDATA_NAME_LENGTH) \
-{ \
-    m_pScriptDebugging->LogCustom(luaVM, SString("Truncated argument @ '%s' [string length reduced to " QUOTE_DEFINE(MAX_CUSTOMDATA_NAME_LENGTH) " characters at argument 2]", lua_tostring(luaVM, lua_upvalueindex(1))).c_str()); \
-    (key) = (key).substr(0, MAX_CUSTOMDATA_NAME_LENGTH); \
-} \
+// Warn and truncate if key is too long
+static void TruncateStringAndWarn(std::string& key, lua_State* const luaVM, CScriptDebugging* const scriptDebugging)
+{
+    if (key.length() > MAX_CUSTOMDATA_NAME_LENGTH)
+    {
+        static const char* formatString = "Truncated argument @ '%s' [string length reduced to " QUOTE_DEFINE(MAX_CUSTOMDATA_NAME_LENGTH) " characters at argument 2. Key: %s]";
+        key = key.substr(0, MAX_CUSTOMDATA_NAME_LENGTH);
+        scriptDebugging->LogCustom(luaVM, SString(formatString, lua_tostring(luaVM, lua_upvalueindex(1)), key.c_str()).c_str());
+    }
+}
 
 
 #ifdef MTA_CLIENT
@@ -16,7 +18,7 @@ std::variant<bool, CLuaArgument*> CLuaElementDefs::GetElementData(lua_State* con
 std::variant<bool, CLuaArgument*> CLuaElementDefs::getElementData(lua_State* const luaVM, CElement* const element, std::string key, const std::optional<bool> inherit)
 #endif
 {
-    EDATA_KEY_WARN_TRUNCATE(key);
+    TruncateStringAndWarn(key, luaVM, m_pScriptDebugging);
 
 #ifdef MTA_CLIENT
     CLuaArgument* const pVariable = element->GetCustomData(key.c_str(), inherit.value_or(true));
@@ -48,8 +50,8 @@ bool CLuaElementDefs::setElementData(lua_State* const luaVM, CElement* const ele
 
     LogWarningIfPlayerHasNotJoinedYet(luaVM, element);
 #endif
-    EDATA_KEY_WARN_TRUNCATE(key);
 
+    TruncateStringAndWarn(key, luaVM, m_pScriptDebugging);
 
 #ifdef MTA_CLIENT
     return CStaticFunctionDefinitions::SetElementData(*element, key.c_str(), newValue, optionalIsSynced.value_or(true));
@@ -65,7 +67,7 @@ bool CLuaElementDefs::HasElementData(lua_State* const luaVM, CClientEntity* cons
 bool CLuaElementDefs::hasElementData(lua_State* const luaVM, CElement* const element, std::string key, const std::optional<bool> inherit)
 #endif
 {
-    EDATA_KEY_WARN_TRUNCATE(key);
+    TruncateStringAndWarn(key, luaVM, m_pScriptDebugging);
 
 #ifdef MTA_CLIENT
     return element->GetCustomData(key.c_str(), inherit.value_or(true)) != nullptr;
@@ -74,5 +76,3 @@ bool CLuaElementDefs::hasElementData(lua_State* const luaVM, CElement* const ele
 #endif
 
 }
-
-#undef EDATA_KEY_WARN_TRUNCATE
