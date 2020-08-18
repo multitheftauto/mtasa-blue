9588f99b7645a4817b56b22d66f18fe2dd0d83be
saml1er <danishroar@gmail.com>
2020-08-19 04:27:16 +0500

    Fix 1617: Vehicle DFF leak fix disabled

--- Client/game_sa/CRenderWareSA.cpp
+++ Client/game_sa/CRenderWareSA.cpp
@@ -391,7 +391,8 @@ void CRenderWareSA::ReplaceModel(RpClump* pNew, unsigned short usModelID, DWORD 
 
             // CClumpModelInfo::SetClump will increment CTxdStore reference count.
             // We must remove it again to avoid TXD leaks.
-            CTxdStore_RemoveRef(pModelInfoInterface->usTextureDictionary);
+            // UPDATE: Fix disabled due to crash reported in issue #1617
+            // CTxdStore_RemoveRef(pModelInfoInterface->usTextureDictionary);
             RpClumpDestroy(pOldClump);
         }
     }
--- Client/mods/deathmatch/logic/luadefs/CLuaEngineDefs.cpp
+++ Client/mods/deathmatch/logic/luadefs/CLuaEngineDefs.cpp
@@ -513,7 +513,8 @@ int CLuaEngineDefs::EngineReplaceModel(lua_State* luaVM)
         ushort usModelID = CModelNames::ResolveModelID(strModelName);
         if (usModelID != INVALID_MODEL_ID)
         {
-            m_pDFFManager->RestoreModel(usModelID);
+            // Fix disabled due to crash reported in issue #1617
+            // m_pDFFManager->RestoreModel(usModelID);
             if (pDFF->ReplaceModel(usModelID, bAlphaTransparency))
             {
                 lua_pushboolean(luaVM, true);
