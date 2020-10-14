f260a3786aaa1d276c9989188c6c869cad529834
saml1er <danishroar@gmail.com>
2020-10-14 12:24:18 +0500

    Fix 1708: Force people to opt back into Default release type

--- Client/core/CVersionUpdater.cpp
+++ Client/core/CVersionUpdater.cpp
@@ -130,6 +130,7 @@ public:
     int DoPollDownload();
     int DoSendPostToNextServer();
     int DoPollPost();
+    void OnMainMenuFullyVisible();
 
     static void* StaticThreadProc(void* pContext);
     void*        ThreadProc();
@@ -3376,4 +3377,24 @@ int CVersionUpdater::DoPollPost()
         return RES_POLL;
     }
     return RES_OK;
+}
+
+// Issue: #1708
+// Set the update settings to stable build if the user is on 1.5.8.
+// We do this only once, this is why we need to write to registry.
+void CVersionUpdater::OnMainMenuFullyVisible()
+{
+    const SString requiredValue = "discord-rich-presence";
+    SString       lastSubKey = "version-revert-reason";
+    if (GetApplicationSetting(lastSubKey) != requiredValue)
+    {
+        SetApplicationSetting(lastSubKey, requiredValue);
+        // check if the MTA client version is 1.5.8
+        if (MTASA_VERSION_MINOR == 5 && MTASA_VERSION_MAINTENANCE == 8)
+        {
+            CVARS_SET("update_build_type", 0);
+            SString strMessage = _("We've reset you back to Default, please go change your settings again if you want Nightly updates.\n");
+            g_pCore->ShowMessageBox(_("VERSION UPDATE INFORMATION"), strMessage, MB_BUTTON_OK | MB_ICON_INFO);
+        }
+    }
 }
