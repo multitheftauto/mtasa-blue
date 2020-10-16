a708e682b12517993a440af04ead9252b0ac4a7b
Emiliano Castro <emilianocast98@gmail.com>
2020-10-15 21:00:50 -0300

    Save server row index instead of finding it (#1716)
    * Save server row index instead of finding it
    
    * Fix typo indexes -> indices
    
    Co-authored-by: Qais Patankar <qaisjp@gmail.com>

--- Client/core/ServerBrowser/CServerBrowser.h
+++ Client/core/ServerBrowser/CServerBrowser.h
@@ -95,7 +95,7 @@ public:
     CServerListItem* FindServerFromRow(ServerBrowserType Type, int iRow);
     CServerListItem* FindServer(const std::string& strHost, unsigned short usPort);
     unsigned short   FindServerHttpPort(const std::string& strHost, unsigned short usPort);
-    int              FindRowFromServer(ServerBrowserType Type, const CServerListItem* pServer);
+    void             UpdateRowIndexMembers(ServerBrowserType Type);
     void             UpdateSelectedServerPlayerList(ServerBrowserType Type);
     void             GetVisibleEndPointList(std::vector<SAddressPort>& outEndpointList);
 
@@ -207,7 +207,7 @@ private:
     void         UpdateServerList(ServerBrowserType Type, bool bClearServerList = false);
     void         UpdateHistoryList();
     CServerList* GetServerList(ServerBrowserType Type);
-    void         AddServerToList(const CServerListItem* pServer, const ServerBrowserType Type);
+    void         AddServerToList(CServerListItem* pServer, const ServerBrowserType Type);
     bool         RemoveSelectedServerFromRecentlyPlayedList();
 
     bool OnClick(CGUIElement* pElement);
