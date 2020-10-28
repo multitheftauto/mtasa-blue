832af0553c1b760259c8439a0945434736ed3e45
Pirulax <patrikjankovics7@gmail.com>
2020-10-28 21:46:54 +0100

    rename lua_pushquery to lua_pushobject

--- Server/mods/deathmatch/logic/luadefs/CLuaDatabaseDefs.cpp
+++ Server/mods/deathmatch/logic/luadefs/CLuaDatabaseDefs.cpp
@@ -371,7 +371,7 @@ int CLuaDatabaseDefs::DbQuery(lua_State* luaVM)
         }
         // Add debug info incase query result does not get collected
         pJobData->SetLuaDebugInfo(g_pGame->GetScriptDebugging()->GetLuaDebugInfo(luaVM));
-        lua_pushquery(luaVM, pJobData);
+        lua_pushobject(luaVM, pJobData);
         return 1;
     }
     else
@@ -431,7 +431,7 @@ int CLuaDatabaseDefs::OOP_DbQuery(lua_State* luaVM)
         }
         // Add debug info incase query result does not get collected
         pJobData->SetLuaDebugInfo(g_pGame->GetScriptDebugging()->GetLuaDebugInfo(luaVM));
-        lua_pushquery(luaVM, pJobData);
+        lua_pushobject(luaVM, pJobData);
         return 1;
     }
     else
