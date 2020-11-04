5d22f2aab2781b45d04093b7d5f936c80fcfdd74
Pirulax <patrikjankovics7@gmail.com>
2020-11-04 10:10:27 +0100

    client

--- Client/mods/deathmatch/logic/CClientVehicle.h
+++ Client/mods/deathmatch/logic/CClientVehicle.h
@@ -125,7 +125,7 @@ struct SVehicleComponentData
 };
 class CClientProjectile;
 
-class CClientVehicle final : public CClientStreamElement
+class CClientVehicle : public CClientStreamElement
 {
     DECLARE_CLASS(CClientVehicle, CClientStreamElement)
     friend class CClientCamera;
