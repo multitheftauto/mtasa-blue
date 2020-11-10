57ba5a2785f2a1076a8868d7de4839828aa5c5a9
Pirulax <patrikjankovics7@gmail.com>
2020-11-10 23:27:06 +0100

    fixup

--- Shared/mods/deathmatch/logic/CCustomData.h
+++ Shared/mods/deathmatch/logic/CCustomData.h
@@ -52,7 +52,7 @@ public:
     {
         auto& data = m_Data[name];
         data.Variable = Variable;
-        data.bSynchronized = syncType;
+        data.bSynchronized = bSynchronized;
     }
 #else
     void Set(const std::string& name, const CLuaArgument& Variable, ESyncType syncType = ESyncType::BROADCAST)
@@ -64,18 +64,22 @@ public:
 
     // Server only
     CXMLNode* OutputToXML(CXMLNode* pNode);
-
-    const size_t CountSynced() const
-    {
-        return std::count_if(m_Data.begin(), m_Data.end(), [](const auto& kvpair) {
-            return kvpair.second.syncType != ESyncType::LOCAL;
-        });
-    }
 #endif
 
     // Shared functions
+    const size_t Count() const noexcept { return m_Data.size(); }
+    const size_t CountSynced() const
+    {
+        return std::count_if(m_Data.begin(), m_Data.end(), [](const auto& kvpair) {
+#ifdef MTA_CLIENT
+            return kvpair.second.bSynchronized;
+#else
+            return kvpair.second.syncType != ESyncType::LOCAL;
+#endif
+        });
+    }
+    
     bool Delete(const std::string& name) { return (bool)m_Data.erase(name); }
-    const size_t Count() const noexcept { return m_Data.size(); } 
     const auto& GetAll() const noexcept { return m_Data; }
 private:
     SharedUtil::CFastHashMap<std::string, SCustomData> m_Data;
