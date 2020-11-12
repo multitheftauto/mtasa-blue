fec6a70fdd1d64fdefd6a81b68c6983a4a40d3fc
Pirulax <patrikjankovics7@gmail.com>
2020-11-12 03:19:15 +0100

    remove CAutoRefedPointer

--- Shared/sdk/SharedUtil.Misc.h
+++ Shared/sdk/SharedUtil.Misc.h
@@ -1660,49 +1660,6 @@ namespace SharedUtil
 
         T* GetData() { return pData; }
     };
-
-    //
-    // Smart pointer with reference count.
-    //
-    template <typename T>
-    class CAutoRefedPointer
-    {
-    private:
-        CRefedPointer<T>* pPointer;
-
-    public:
-        CAutoRefedPointer() { pPointer = new CRefedPointer<T>(); }
-
-        CAutoRefedPointer(const CAutoRefedPointer<T>& other)
-        {
-            pPointer = other.pPointer;
-            pPointer->AddRef();
-        }
-
-        ~CAutoRefedPointer() { pPointer->Release(); }
-
-        CAutoRefedPointer<T>& operator=(const CAutoRefedPointer<T>& other)
-        {
-            // Assignment operator
-            if (this != &other)            // Avoid self assignment
-            {
-                CRefedPointer<T>* pOldPointer = pPointer;
-
-                // Copy the data and reference pointer
-                // and increment the reference count
-                pPointer = other.pPointer;
-                pPointer->AddRef();
-
-                // Decrement the old reference count
-                pOldPointer->Release();
-            }
-            return *this;
-        }
-
-        T* operator->() { return pPointer->GetData(); }
-
-        const T* operator->() const { return pPointer->GetData(); }
-    };
 };            // namespace SharedUtil
 
 using namespace SharedUtil;
