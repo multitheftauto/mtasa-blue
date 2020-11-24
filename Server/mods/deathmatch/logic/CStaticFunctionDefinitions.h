49d32b16f7af11efa9d3b90756e873cdfd668f9d
Pirulax <patrikjankovics7@gmail.com>
2020-11-24 23:49:32 +0100

    remove CStaticFunctionDefinitions::GetBans from .h

--- Server/mods/deathmatch/logic/CStaticFunctionDefinitions.h
+++ Server/mods/deathmatch/logic/CStaticFunctionDefinitions.h
@@ -702,7 +702,6 @@ public:
 
     static CBan* AddBan(SString strIP, SString strUsername, SString strSerial, CPlayer* pResponsible = NULL, SString strResponsible = "Console",
                         SString strReason = "", time_t tUnban = 0);
-    static bool  RemoveBan(CBan* pBan, CPlayer* pResponsible = NULL);
 
     static bool GetBans(lua_State* luaVM);
     static bool ReloadBanList();
