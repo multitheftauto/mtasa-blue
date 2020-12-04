multitheftauto/mtasa-blue

commit fe9d419e2564926976490b5e26d4ca6cf8d4ed79

FLA compatibility - possible different file ID and world map sector limits (#1859)
* Compatibility with different world sector limits & ID limits

Compatibility with different world sector limits & ID limits.

This provides a possibility for heavy modding, for someone who would add FLA loading into MTA.

Log:
At least one ID limit is changed. ID limits will be patched.
Count of file IDs is over 32767 and requires unsigned ID. Applying unsigned ID patches.
Count of file IDs is over 65532 and base ID of CStreamingInfo requires int32_t. Applying int32_t base ID patches.
Count of DFF/TXD IDs is over 65532 and requires int32_t. Applying int32_t DFF+TXD ID patches.
COL ID limit is over 256 and requires something more than uint8_t. Applying COL ID uint32_t patches.
IPL ID limit is over 256 and requires something more than uint8_t. Applying IPL ID uint32_t patches.

New ID limits:
0 - 599999 (600000) - DFF models defined within IDE files
600000 - 1099999 (500000) - TXD texture archives.
1100000 - 1599999 (500000) - COL collision archives.
1600000 - 1600299 (300) - IPL Binary IPL files.
1600300 - 1600363 (64) - DAT files limited to nodes*.dat
1600364 - 1600763 (400) - IFP animation archives.
1600764 - 1601263 (500) - RRR car recordings, carrec*.rrr files
1601264 - 1601345 (82) - SCM scripts
1601346 - 1601347 (2) - Loaded list
1601348 - 1601349 (2) - Requested list
1601350 - count of all file IDs
--------------------------
Renderware world map size set to 28000 x 28000
World map size set to 24000 x 24000

World map info info:
World map size: 24000 x 24000

World sectors:
CWorld::ms_aSectors
type: buildings, dummies
count: 480 * 480 = 230400
sector area: 50 x 50

CWorld::ms_aLodPtrLists
type: lod
count: 120 * 120 = 14400
sector area: 200 x 200

CWorld::ms_aRepeatSectors
type: vehicles, peds, object
count: 16 * 16 = 256

The FLA is not a part of official MTA so far.

Client/game_sa/CGameSA.h
@@ -14,6 +14,7 @@
 #include "CModelInfoSA.h"
 #include "CObjectGroupPhysicalPropertiesSA.h"
 #include "CFxManagerSA.h"
+#include <game/CStreaming.h>
 
 #define     MAX_MEMORY_OFFSET_1_0           0xCAF008
 
@@ -33,8 +34,7 @@
 #define     NUM_WeaponInfosOtherSkill       11
 #define     NUM_WeaponInfosTotal            (NUM_WeaponInfosStdSkill + (3*NUM_WeaponInfosOtherSkill)) // std, (poor, pro, special)
 
-#define     MODELINFO_MAX                   26000       // Actual max is 25755
-#define     OBJECTDYNAMICINFO_MAX           160
+extern unsigned int OBJECTDYNAMICINFO_MAX;  // default: 160
 
 #define     FUNC_GetLevelFromPosition       0x4DD300
 
@@ -107,8 +107,8 @@ class CGameSA : public CGame
 
 private:
     CWeaponInfo* WeaponInfos[NUM_WeaponInfosTotal];
-    CModelInfoSA ModelInfo[MODELINFO_MAX];
-    CObjectGroupPhysicalPropertiesSA ObjectGroupsInfo[OBJECTDYNAMICINFO_MAX];
+    CModelInfoSA* ModelInfo;
+    CObjectGroupPhysicalPropertiesSA* ObjectGroupsInfo;
 public:
     ZERO_ON_NEW
 
@@ -308,6 +308,16 @@ public:
     CWeaponInfo*                    GetWeaponInfo(eWeaponType weapon, eWeaponSkill skill = WEAPONSKILL_STD);
     CModelInfo*                     GetModelInfo(DWORD dwModelID, bool bCanBeInvalid = false);
     CObjectGroupPhysicalProperties* GetObjectGroupPhysicalProperties(unsigned char ucObjectGroup);
+
+    int32_t GetBaseIDforDFF() { return 0; }
+    int32_t GetBaseIDforTXD() { return *(int32_t*)(0x407104 + 2); }
+    int32_t GetBaseIDforCOL() { return *(int32_t*)(0x410344 + 2); }
+    int32_t GetBaseIDforIPL() { return *(int32_t*)(0x4044F4 + 2); }
+    int32_t GetBaseIDforDAT() { return *(int32_t*)(0x44D064 + 2); }
+    int32_t GetBaseIDforIFP() { return *(int32_t*)(0x404969 + 2); }
+    int32_t GetBaseIDforRRR() { return *(int32_t*)(0x4594A1 + 2); }
+    int32_t GetBaseIDforSCM() { return *(int32_t*)(0x46A574 + 2); }
+    int32_t GetCountOfAllFileIDs() { return (*(char**)(0x5B8AFA + 2) - *(char**)(0x5B8B08 + 6)) / sizeof(CStreamingInfo); }
 
     DWORD GetSystemTime()
     {

Client/core/CMemStats.cpp
@@ -474,8 +474,11 @@ void CMemStats::SampleState(SMemStatsInfo& memStatsInfo)
     memStatsInfo.iStreamingMemoryUsed = *(int*)0x08E4CB4;
     memStatsInfo.iStreamingMemoryAvailable = *(int*)0x08A5A80;
 
-    uint* pModelInfoArray = (uint*)0x08E4CC0;
-    for (uint i = 0; i < 25755; i++)
+    uint* pModelInfoArray = (uint*)*(void**)(0x403DA4 + 3);
+    CGame* pGame = g_pCore->GetGame();
+    unsigned int RRR_BASE_ID = pGame->GetBaseIDforRRR();
+
+    for (uint i = 0; i < RRR_BASE_ID; i++)
     {
         uint* pModelInfo = pModelInfoArray + 5 * i;
         uint  uiLoadedFlag = pModelInfo[4];
@@ -496,17 +499,17 @@ void CMemStats::SampleState(SMemStatsInfo& memStatsInfo)
                 memStatsInfo.modelInfo.uiUnknown_612_999++;
             else if (i < 1194)
                 memStatsInfo.modelInfo.uiUpgrades_1000_1193++;
-            else if (i < 20000)
+            else if (i < pGame->GetBaseIDforTXD())
                 memStatsInfo.modelInfo.uiUnknown_1194_19999++;
-            else if (i < 25000)
+            else if (i < pGame->GetBaseIDforCOL())
                 memStatsInfo.modelInfo.uiTextures_20000_24999++;
-            else if (i < 25255)
+            else if (i < pGame->GetBaseIDforIPL())
                 memStatsInfo.modelInfo.uiCollisions_25000_25254++;
-            else if (i < 25511)
+            else if (i < pGame->GetBaseIDforDAT())
                 memStatsInfo.modelInfo.uiIpls_25255_25510++;
-            else if (i < 25575)
+            else if (i < pGame->GetBaseIDforIFP())
                 memStatsInfo.modelInfo.uiPaths_25511_25574++;
-            else if (i < 25755)
+            else if (i < pGame->GetBaseIDforRRR())
                 memStatsInfo.modelInfo.uiAnims_25575_25754++;
         }
     }
@@ -657,9 +660,10 @@ void CMemStats::UpdateIntervalStats()
 ///////////////////////////////////////////////////////////////
 void CMemStats::CreateTables()
 {
+    CGame* pGame = g_pCore->GetGame();
     m_TableList.clear();
 
-    //
+//
     // Color setups
     //
     #define YELLOW "#FFFF00"
@@ -894,14 +898,20 @@ void CMemStats::CreateTables()
         table.AddRow(SString("400-611|(Vehicles)|^1~.%d|%d", m_MemStatsDelta.modelInfo.uiVehicles_400_611, m_MemStatsNow.modelInfo.uiVehicles_400_611));
         table.AddRow(SString("612-999| |^1~.%d|%d", m_MemStatsDelta.modelInfo.uiUnknown_612_999, m_MemStatsNow.modelInfo.uiUnknown_612_999));
         table.AddRow(SString("1000-1193|(Upgrades)|^1~.%d|%d", m_MemStatsDelta.modelInfo.uiUpgrades_1000_1193, m_MemStatsNow.modelInfo.uiUpgrades_1000_1193));
-        table.AddRow(SString("1194-19999|(World)|^1~.%d|%d", m_MemStatsDelta.modelInfo.uiUnknown_1194_19999, m_MemStatsNow.modelInfo.uiUnknown_1194_19999));
-        table.AddRow(
-            SString("20000-24999|(Textures)|^1~.%d|%d", m_MemStatsDelta.modelInfo.uiTextures_20000_24999, m_MemStatsNow.modelInfo.uiTextures_20000_24999));
-        table.AddRow(SString("25000-25254|(Collisions)|^1~.%d|%d", m_MemStatsDelta.modelInfo.uiCollisions_25000_25254,
+        table.AddRow(SString("1194-%d|(World)|^1~.%d|%d", pGame->GetBaseIDforTXD() - 1, m_MemStatsDelta.modelInfo.uiUnknown_1194_19999,
+                             m_MemStatsNow.modelInfo.uiUnknown_1194_19999));
+        table.AddRow(SString("%d-%d|(Textures)|^1~.%d|%d", pGame->GetBaseIDforTXD(), pGame->GetBaseIDforCOL() - 1,
+            m_MemStatsDelta.modelInfo.uiTextures_20000_24999,
+                             m_MemStatsNow.modelInfo.uiTextures_20000_24999));
+        table.AddRow(SString("%d-%d|(Collisions)|^1~.%d|%d", pGame->GetBaseIDforCOL(), pGame->GetBaseIDforIPL() - 1,
+            m_MemStatsDelta.modelInfo.uiCollisions_25000_25254,
                              m_MemStatsNow.modelInfo.uiCollisions_25000_25254));
-        table.AddRow(SString("25255-25510|(Ipls)|^1~.%d|%d", m_MemStatsDelta.modelInfo.uiIpls_25255_25510, m_MemStatsNow.modelInfo.uiIpls_25255_25510));
-        table.AddRow(SString("25511-25574|(Paths)|^1~.%d|%d", m_MemStatsDelta.modelInfo.uiPaths_25511_25574, m_MemStatsNow.modelInfo.uiPaths_25511_25574));
-        table.AddRow(SString("25575-25754|(Anims)|^1~.%d|%d", m_MemStatsDelta.modelInfo.uiAnims_25575_25754, m_MemStatsNow.modelInfo.uiAnims_25575_25754));
+        table.AddRow(SString("%d-%d|(Ipls)|^1~.%d|%d", pGame->GetBaseIDforIPL(), pGame->GetBaseIDforDAT() - 1,
+            m_MemStatsDelta.modelInfo.uiIpls_25255_25510, m_MemStatsNow.modelInfo.uiIpls_25255_25510));
+        table.AddRow(SString("%d-%d|(Paths)|^1~.%d|%d", pGame->GetBaseIDforDAT(), pGame->GetBaseIDforIFP() - 1,
+            m_MemStatsDelta.modelInfo.uiPaths_25511_25574, m_MemStatsNow.modelInfo.uiPaths_25511_25574));
+        table.AddRow(SString("%d-%d|(Anims)|^1~.%d|%d", pGame->GetBaseIDforIFP(), pGame->GetBaseIDforRRR() - 1,
+            m_MemStatsDelta.modelInfo.uiAnims_25575_25754, m_MemStatsNow.modelInfo.uiAnims_25575_25754));
         table.AddRow(SString("|Total:|^1~.%d|%d", m_MemStatsDelta.modelInfo.uiTotal, m_MemStatsNow.modelInfo.uiTotal));
     }
 

Client/game_sa/CAnimBlockSA.cpp
@@ -21,7 +21,7 @@ void CAnimBlockSA::Request(EModelRequestType requestType, bool bAllowBlockingFai
     if (IsLoaded())
         return;
 
-    DWORD dwModelID = GetIndex() + 25575;
+    DWORD dwModelID = GetIndex() + pGame->GetBaseIDforIFP();
 
     if (requestType == BLOCKING)
     {

Client/game_sa/CGameSA.cpp
@@ -31,6 +31,8 @@ float*         CGameSA::VAR_OldTimeStep;
 float*         CGameSA::VAR_TimeStep;
 unsigned long* CGameSA::VAR_Framelimiter;
 
+unsigned int OBJECTDYNAMICINFO_MAX = *(uint32_t*)0x59FB4C != 0x90909090 ? *(uint32_t*)0x59FB4C : 160;            // default: 160
+
 /**
  * \todo allow the addon to change the size of the pools (see 0x4C0270 - CPools::Initialise) (in start game?)
  */
@@ -41,6 +43,10 @@ CGameSA::CGameSA()
     m_bAsyncScriptForced = false;
     m_bASyncLoadingSuspended = false;
     m_iCheckStatus = 0;
+
+    const unsigned int modelInfoMax = GetCountOfAllFileIDs(); 
+    ModelInfo = new CModelInfoSA[modelInfoMax];
+    ObjectGroupsInfo = new CObjectGroupPhysicalPropertiesSA[OBJECTDYNAMICINFO_MAX];
 
     SetInitialVirtualProtect();
 
@@ -63,7 +69,7 @@ CGameSA::CGameSA()
     }
 
     // Set the model ids for all the CModelInfoSA instances
-    for (int i = 0; i < MODELINFO_MAX; i++)
+    for (int i = 0; i < modelInfoMax; i++)
     {
         ModelInfo[i].SetModelID(i);
     }
@@ -257,6 +263,9 @@ CGameSA::~CGameSA()
     delete reinterpret_cast<CAEAudioHardwareSA*>(m_pAEAudioHardware);
     delete reinterpret_cast<CAudioContainerSA*>(m_pAudioContainer);
     delete reinterpret_cast<CPointLightsSA*>(m_pPointLights);
+
+    delete[] ModelInfo;
+    delete[] ObjectGroupsInfo;
 }
 
 CWeaponInfo* CGameSA::GetWeaponInfo(eWeaponType weapon, eWeaponSkill skill)
@@ -308,7 +317,7 @@ bool CGameSA::IsInForeground()
 CModelInfo* CGameSA::GetModelInfo(DWORD dwModelID, bool bCanBeInvalid)
 {
     DEBUG_TRACE("CModelInfo * CGameSA::GetModelInfo(DWORD dwModelID, bool bCanBeInvalid)");
-    if (dwModelID < MODELINFO_MAX)
+    if (dwModelID < GetCountOfAllFileIDs())
     {
         if (ModelInfo[dwModelID].IsValid() || bCanBeInvalid)
         {

Client/game_sa/CModelInfoSA.cpp
@@ -14,7 +14,7 @@
 
 extern CGameSA* pGame;
 
-CBaseModelInfoSAInterface** CModelInfoSAInterface::ms_modelInfoPtrs = (CBaseModelInfoSAInterface**)0xA9B0C8;
+CBaseModelInfoSAInterface** CModelInfoSAInterface::ms_modelInfoPtrs = (CBaseModelInfoSAInterface**)ARRAY_ModelInfo;
 CBaseModelInfoSAInterface** ppModelInfo = (CBaseModelInfoSAInterface**)ARRAY_ModelInfo;
 
 std::map<unsigned short, int>                                         CModelInfoSA::ms_RestreamTxdIDMap;
@@ -267,7 +267,7 @@ char* CModelInfoSA::GetNameIfVehicle()
     DEBUG_TRACE("char * CModelInfoSA::GetNameIfVehicle ( )");
     //  if(this->IsVehicle())
     //  {
-    DWORD dwModelInfo = ARRAY_ModelInfo;
+    DWORD dwModelInfo = (DWORD)ARRAY_ModelInfo;
     DWORD dwFunc = FUNC_CText_Get;
     DWORD ModelID = m_dwModelID;
     DWORD dwReturn = 0;
@@ -280,7 +280,7 @@ char* CModelInfoSA::GetNameIfVehicle()
 
             mov     ebx, ModelID
             lea     ebx, [ebx*4]
-            add     ebx, ARRAY_ModelInfo
+            add     ebx, dword ptr[ARRAY_ModelInfo]
             mov     eax, [ebx]
             add     eax, 50
 
@@ -330,11 +330,11 @@ VOID CModelInfoSA::Request(EModelRequestType requestType, const char* szTag)
         uint uiAnimFileIndex = GetAnimFileIndex();
         if (uiAnimFileIndex != 0xffffffff)
         {
-            uint          uiAnimId = uiAnimFileIndex + 25575;
+            uint          uiAnimId = uiAnimFileIndex + pGame->GetBaseIDforIFP();
             CModelInfoSA* pAnim = static_cast<CModelInfoSA*>(pGame->GetModelInfo(uiAnimId));
             if (!pAnim)
             {
-                if (uiAnimId != 25714)
+                if (uiAnimId != pGame->GetBaseIDforIFP() + 139)
                     LogEvent(505, "Model no anim", "", SString("%d (%d)", m_dwModelID, uiAnimId));
             }
             else if (!pAnim->IsLoaded())
@@ -469,7 +469,7 @@ BOOL CModelInfoSA::DoIsLoaded()
     // return (BOOL)*(BYTE *)(ARRAY_ModelLoaded + 20*dwModelID);
     BOOL bLoaded = pGame->GetStreaming()->HasModelLoaded(m_dwModelID);
 
-    if (m_dwModelID < 20000)
+    if (m_dwModelID < pGame->GetBaseIDforTXD())
     {
         m_pInterface = ppModelInfo[m_dwModelID];
 
@@ -515,7 +515,7 @@ CBoundingBox* CModelInfoSA::GetBoundingBox()
 
 bool CModelInfoSA::IsValid()
 {
-    if (m_dwModelID >= 20000 && m_dwModelID < MODELINFO_MAX)
+    if (m_dwModelID >= pGame->GetBaseIDforTXD() && m_dwModelID < pGame->GetCountOfAllFileIDs())
         return true;
 
     if (!ppModelInfo[m_dwModelID])
@@ -534,10 +534,14 @@ float CModelInfoSA::GetDistanceFromCentreOfMassToBaseOfModel()
     DWORD dwModelInfo = 0;
     DWORD ModelID = m_dwModelID;
     FLOAT fReturn = 0;
-    _asm
-    {
+    _asm {
         mov     eax, ModelID
-        mov     eax, ARRAY_ModelInfo[eax*4]
+
+        push    ecx
+        mov     ecx, dword ptr[ARRAY_ModelInfo]
+        mov     eax, dword ptr[ecx + eax*4]
+        pop     ecx
+
         mov     eax, [eax+20]
         cmp     eax, 0
         jz      skip
@@ -772,7 +776,7 @@ void CModelInfoSA::StaticFlushPendingRestreamIPL()
     for (it = removedModels.begin(); it != removedModels.end(); it++)
     {
         ((void(__cdecl*)(unsigned short))FUNC_RemoveModel)(*it);
-        MemPut<BYTE>(ARRAY_ModelLoaded + 20 * (*it), 0);
+        MemPut<BYTE>((char*)CStreaming__ms_aInfoForModel + 20 * (*it), 0);
     }
 }
 
@@ -905,8 +909,14 @@ short CModelInfoSA::GetAvailableVehicleMod(unsigned short usUpgrade)
         _asm
         {
             mov     eax, ModelID
-            movsx   edx, usUpgrade
-            mov     eax, ARRAY_ModelInfo[eax*4]
+
+            push    ecx
+            mov ecx, dword ptr[ARRAY_ModelInfo]
+            mov     eax, dword ptr[ecx + eax*4]
+            pop     ecx
+
+
+            movsx   edx, usUpgrade;
             mov     ax, [eax+edx*2+0x2D6]
             mov     sreturn, ax
         }
@@ -921,7 +931,8 @@ bool CModelInfoSA::IsUpgradeAvailable(eVehicleUpgradePosn posn)
     _asm
     {
         mov     eax, ModelID
-        lea     ecx, ARRAY_ModelInfo[eax*4]
+        mov ecx, dword ptr[ARRAY_ModelInfo]
+        mov     ecx, dword ptr[ecx + eax*4]
 
         mov     eax, posn
         mov     ecx, [ecx+0x5C]
@@ -947,7 +958,12 @@ void CModelInfoSA::SetCustomCarPlateText(const char* szText)
     {
         push    ecx
         mov     ecx, ModelID
-        mov     ecx, ARRAY_ModelInfo[ecx*4]
+
+        push    eax
+        mov     eax, dword ptr[ARRAY_ModelInfo]
+        mov     ecx, dword ptr[eax + ecx*4]
+        pop     eax
+
         add     ecx, 40
         mov     szStoredText, ecx
         pop     ecx
@@ -965,7 +981,12 @@ unsigned int CModelInfoSA::GetNumRemaps()
     _asm
     {
         mov     ecx, ModelID
-        mov     ecx, ARRAY_ModelInfo[ecx*4]
+
+        push    eax
+        mov     eax, dword ptr[ARRAY_ModelInfo]
+        mov     ecx, dword ptr[eax + ecx*4]
+        pop     eax
+
         call    dwFunc
         mov     uiReturn, eax
     }
@@ -1239,7 +1260,12 @@ void CModelInfoSA::SetColModel(CColModel* pColModel)
         _asm
         {
             mov     ecx, ModelID
-            mov     ecx, ARRAY_ModelInfo[ecx*4]
+
+            push    eax
+            mov     eax, dword ptr[ARRAY_ModelInfo]
+            mov     ecx, dword ptr[eax + ecx*4]
+            pop     eax
+
             push    1
             push    pColModelInterface
             call    dwFunc
@@ -1292,7 +1318,12 @@ void CModelInfoSA::RestoreColModel()
             _asm
             {
                 mov     ecx, ModelID
-                mov     ecx, ARRAY_ModelInfo[ecx*4]
+
+                push    eax
+                mov     eax, dword ptr[ARRAY_ModelInfo]
+                mov     ecx, dword ptr[eax + ecx*4]
+                pop     eax
+
                 push    1
                 push    dwOriginalColModelInterface
                 call    dwFunc

Client/game_sa/CModelInfoSA.h
@@ -21,7 +21,9 @@ class CPedModelInfoSAInterface;
 
 #define     RpGetFrame(__c)                 ((RwFrame*)(((RwObject *)(__c))->parent))
 
-#define     ARRAY_ModelLoaded               0x8E4CD0 // ##SA##
+// #define     ARRAY_ModelLoaded               0x8E4CD0 // ##SA##
+static void* CStreaming__ms_aInfoForModel = *(void**)(0x5B8B08 + 6);
+static void* ARRAY_ModelLoaded = (char*)CStreaming__ms_aInfoForModel + 0x10;
 
 #define     FUNC_CStreaming__HasModelLoaded 0x4044C0
 
@@ -33,7 +35,8 @@ class CPedModelInfoSAInterface;
 #define     DWORD_AtomicsReplacerModelID    0xB71840
 #define     FUNC_AtomicsReplacer            0x537150
 
-#define     ARRAY_ModelInfo                 0xA9B0C8
+// #define     ARRAY_ModelInfo                 0xA9B0C8
+static void* ARRAY_ModelInfo = *(void**)(0x403DA4 + 3);
 #define     CLASS_CText                     0xC1B340
 #define     FUNC_CText_Get                  0x6A0050
 #define     FUNC_GetModelFlags              0x4044E0

Client/game_sa/CObjectGroupPhysicalPropertiesSA.cpp
@@ -13,7 +13,7 @@
 #include "CObjectGroupPhysicalPropertiesSA.h"
 extern CGameSA* pGame;
 
-CObjectGroupPhysicalPropertiesSAInterface* pObjectInfo = (CObjectGroupPhysicalPropertiesSAInterface*)ARRAY_ObjectGroupsDynamicInfo;
+CObjectGroupPhysicalPropertiesSAInterface* pObjectInfo = *(CObjectGroupPhysicalPropertiesSAInterface**)(0x59F857 + 6);
 std::unordered_map<unsigned char, std::unique_ptr<CObjectGroupPhysicalPropertiesSAInterface>> CObjectGroupPhysicalPropertiesSA::ms_OriginalGroupProperties;
 
 CObjectGroupPhysicalPropertiesSA::CObjectGroupPhysicalPropertiesSA() : m_pInterface(nullptr)

Client/game_sa/CObjectSA.cpp
@@ -117,7 +117,12 @@ CObjectSA::CObjectSA(DWORD dwModel, bool bBreakingDisabled)
     _asm
     {
         mov     eax, dwModel
-        mov     eax, 0xA9B0C8[eax*4]
+
+        push    ecx
+        mov     ecx, dword ptr[ARRAY_ModelInfo]
+        mov     eax, dword ptr[ecx + eax*4]
+        pop     ecx
+
         mov     eax, [eax+20]
         movzx   eax, byte ptr [eax+40]
         push    eax
@@ -209,7 +214,12 @@ CObjectSA::~CObjectSA()
                 _asm
                 {
                     mov     eax, dwModelID
-                    mov     eax, 0xA9B0C8[eax*4]
+
+                    push    ecx
+                    mov     ecx, dword ptr[ARRAY_ModelInfo]
+                    mov     eax, dword ptr[ecx + eax*4]
+                    pop     ecx
+
                     mov     eax, [eax+20]
                     movzx   eax, byte ptr [eax+40]
                     push    eax

commit f4be845d1f484a1f1063bb239f96495d31527d54

Fix IFP crash caused by fe9d419e2564926976490b5e26d4ca6cf8d4ed79

Client/game_sa/CGameSA.h
@@ -314,7 +314,7 @@ public:
     int32_t GetBaseIDforCOL() { return *(int32_t*)(0x410344 + 2); }
     int32_t GetBaseIDforIPL() { return *(int32_t*)(0x4044F4 + 2); }
     int32_t GetBaseIDforDAT() { return *(int32_t*)(0x44D064 + 2); }
-    int32_t GetBaseIDforIFP() { return *(int32_t*)(0x404969 + 2); }
+    int32_t GetBaseIDforIFP() { return *(int32_t*)(0x407124 + 2); }
     int32_t GetBaseIDforRRR() { return *(int32_t*)(0x4594A1 + 2); }
     int32_t GetBaseIDforSCM() { return *(int32_t*)(0x46A574 + 2); }
     int32_t GetCountOfAllFileIDs() { return (*(char**)(0x5B8AFA + 2) - *(char**)(0x5B8B08 + 6)) / sizeof(CStreamingInfo); }