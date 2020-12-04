29f811c6d38b3cf289ad648a00ec88978bae6317
saml1er <10183157+saml1er@users.noreply.github.com>
2020-12-04 13:06:33 +0500

    Fix IFP crash caused by fe9d419e2564926976490b5e26d4ca6cf8d4ed79 (#1897)
    Co-authored-by: saml1er <danishroar@gmail.com>

--- Client/game_sa/CGameSA.h
+++ Client/game_sa/CGameSA.h
@@ -314,7 +314,7 @@ public:
     int32_t GetBaseIDforCOL() { return *(int32_t*)(0x410344 + 2); }
     int32_t GetBaseIDforIPL() { return *(int32_t*)(0x4044F4 + 2); }
     int32_t GetBaseIDforDAT() { return *(int32_t*)(0x44D064 + 2); }
-    int32_t GetBaseIDforIFP() { return *(int32_t*)(0x404969 + 2); }
+    int32_t GetBaseIDforIFP() { return *(int32_t*)(0x407124 + 2); }
     int32_t GetBaseIDforRRR() { return *(int32_t*)(0x4594A1 + 2); }
     int32_t GetBaseIDforSCM() { return *(int32_t*)(0x46A574 + 2); }
     int32_t GetCountOfAllFileIDs() { return (*(char**)(0x5B8AFA + 2) - *(char**)(0x5B8B08 + 6)) / sizeof(CStreamingInfo); }
