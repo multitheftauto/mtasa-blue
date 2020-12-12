48f55b794d003b700f1475ba053638deb4e3445a
CrosRoad95 <sebajura1234@gmail.com>
2020-12-12 16:52:06 +0100

    fix concurrent stack

--- Shared/sdk/ConcurrentStack.hpp
+++ Shared/sdk/ConcurrentStack.hpp
@@ -9,23 +9,23 @@ namespace SharedUtil
     public:
         void push(const T& item)
         {
-            std::lock_guard<std::mutex> guard(lock);
+            std::lock_guard<std::mutex> guard(m_mutex);
             m_stack.push(item);
         }
         void pop()
         {
-            std::lock_guard<std::mutex> guard(lock);
+            std::lock_guard<std::mutex> guard(m_mutex);
             m_stack.pop();
         }
         T top() const
         {
-            std::lock_guard<std::mutex> guard(lock);
+            std::lock_guard<std::mutex> guard(m_mutex);
             return m_stack.top();
         }
 
         bool empty() const
         {
-            std::lock_guard<std::mutex> guard(lock);
+            std::lock_guard<std::mutex> guard(m_mutex);
             return m_stack.empty();
         }
 
