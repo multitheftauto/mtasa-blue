66b23d79e8d860da5ad7159f44c2ed4c1a767652
saml1er <danishroar@gmail.com>
2020-12-15 01:12:04 +0500

    Addendum to previous commit

--- Client/multiplayer_sa/CMultiplayerSA.cpp
+++ Client/multiplayer_sa/CMultiplayerSA.cpp
@@ -6880,17 +6880,17 @@ void PostCWorld_ProcessPedsAfterPreRender()
         if (!objectEntity)
             continue;
         auto objectInterface = objectEntity->GetObjectInterface();
-        if (objectInterface)
+        if (objectInterface && objectEntity->GetPreRenderRequired())
         {
-            if ((objectInterface->fScale != 1.0f || objectInterface->bUpdateScale) && objectEntity->GetPreRenderRequired())
+            if (objectInterface->fScale != 1.0f || objectInterface->bUpdateScale)
             {
                 objectEntity->SetScaleInternal(*objectEntity->GetScale());
                 objectInterface->bUpdateScale = false;
-                objectEntity->SetPreRenderRequired(false);
-                RpClump* clump = objectInterface->m_pRwObject;
-                if (clump && clump->object.type == RP_TYPE_CLUMP)
-                    objectEntity->UpdateRpHAnim();
             }
+            RpClump* clump = objectInterface->m_pRwObject;
+            if (clump && clump->object.type == RP_TYPE_CLUMP)
+                objectEntity->UpdateRpHAnim();
+            objectEntity->SetPreRenderRequired(false);
         }
     }
 }
