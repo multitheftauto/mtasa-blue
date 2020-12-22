738274f0ac8e9e48c90c884534bc222c213fe86c
pentaflops <kras.russian@gmail.com>
2020-12-22 14:38:19 +0300

    try fix linux server build

--- Server/mods/deathmatch/logic/CModelManager.h
+++ Server/mods/deathmatch/logic/CModelManager.h
@@ -18,14 +18,14 @@ public:
     CModelManager::CModelManager() { }
     ~CModelManager(void) { }
 
-    bool RequestModel(ushort modelID, ushort parentID, eModelType modelType);
+    bool RequestModel(unsigned short modelID, unsigned short parentID, eModelType modelType);
 
-    bool IsValidModel(ushort modelID);
-    ushort GetModelParent(ushort modelID);
-    eModelType GetModelType(ushort modelID);
+    bool IsValidModel(unsigned short modelID);
+    unsigned short GetModelParent(unsigned short modelID);
+    eModelType GetModelType(unsigned short modelID);
 
 private:
-    std::map<ushort, ushort> m_ModelsParent;
-    std::map<ushort, eModelType> m_ModelsType;
+    std::map<unsigned short, unsigned short> m_ModelsParent;
+    std::map<unsigned short, eModelType> m_ModelsType;
     unsigned int m_modelCount = 0;
 };
