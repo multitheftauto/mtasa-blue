b3ba15bad3a0943d83cefb90cddc6f9191667fa8
Marek Kulik <me@botder.com>
2021-01-08 05:24:49 +0100

    Allow peds to enter certain models while in water (fixes #1990)
    Last commit (77a81bd110bbc8093368e244e99c5922fb583d6f) broke entering certain vehicle models that can float on water

--- Client/mods/deathmatch/logic/CClientPed.cpp
+++ Client/mods/deathmatch/logic/CClientPed.cpp
@@ -6439,7 +6439,7 @@ bool CClientPed::EnterVehicle(CClientVehicle* pVehicle, bool bPassenger)
     }
 
     // We dead or in water?
-    if (IsDead() || IsInWater())
+    if (IsDead())
     {
         return false;
     }
@@ -6495,6 +6495,14 @@ bool CClientPed::EnterVehicle(CClientVehicle* pVehicle, bool bPassenger)
     if (!pVehicle->IsEnterable())
     {
         // Stop if the vehicle is not enterable
+        return false;
+    }
+
+    // Stop if the ped is swimming and the vehicle model cannot be entered from water (fixes #1990)
+    unsigned short vehicleModel = pVehicle->GetModel();
+
+    if (IsInWater() && !(vehicleModel == VT_SKIMMER || vehicleModel == VT_SEASPAR || vehicleModel == VT_LEVIATHN || vehicleModel == VT_VORTEX))
+    {
         return false;
     }
 
