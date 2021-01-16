3fa894c8cb6e1e0e2e9b0931b54f041b5bf21088
Sebastian Jura <sebajura1234@gmail.com>
2021-01-16 21:05:31 +0100

    Update Client/mods/deathmatch/logic/CClientColPolygon.h
    Co-authored-by: patrikjuvonen <22572159+patrikjuvonen@users.noreply.github.com>

--- Client/mods/deathmatch/logic/CClientColPolygon.h
+++ Client/mods/deathmatch/logic/CClientColPolygon.h
@@ -35,7 +35,7 @@ public:
     std::vector<CVector2D>::const_iterator IterBegin() { return m_Points.begin(); };
     std::vector<CVector2D>::const_iterator IterEnd() { return m_Points.end(); };
     bool SetHeight(float fFloor, float fCeil);
-    void GetHeight(float &fFloor, float &fCeil) { fFloor = m_fFloor; fCeil = m_fCeil; };
+    void GetHeight(float& fFloor, float& fCeil) const { fFloor = m_fFloor; fCeil = m_fCeil; };
 
 protected:
     std::vector<CVector2D> m_Points;
