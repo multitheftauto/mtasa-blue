cd63d45c950cc128d400cd46252eba570aa08c2f
Marek Kulik <me@botder.com>
2021-01-18 16:45:05 +0100

    Addendum to 0a44921526fd1ba6f72827d08f7ad887dc009e75

--- Client/core/DXHook/CDirect3DHook9.cpp
+++ Client/core/DXHook/CDirect3DHook9.cpp
@@ -10,7 +10,7 @@
  *****************************************************************************/
 
 #include "StdInc.h"
-#include <detours.h>
+#include <SharedUtil.Detours.h>
 
 template <>
 CDirect3DHook9* CSingleton<CDirect3DHook9>::m_pSingleton = NULL;
@@ -38,16 +38,14 @@ bool CDirect3DHook9::ApplyHook()
     // Hook Direct3DCreate9.
     if (!m_pfnDirect3DCreate9)
     {
-        DetourTransactionBegin();
-        DetourUpdateThread(GetCurrentThread());
-        m_pfnDirect3DCreate9 = reinterpret_cast<decltype(m_pfnDirect3DCreate9)>(DetourFindFunction("D3D9.DLL", "Direct3DCreate9"));
-        WriteDebugEvent(SString("Direct3D9 hook applied %08x", m_pfnDirect3DCreate9));
-        DetourAttach(&reinterpret_cast<PVOID&>(m_pfnDirect3DCreate9), API_Direct3DCreate9);
+        bool success = DetourLibraryFunction("d3d9.dll", "Direct3DCreate9", m_pfnDirect3DCreate9, API_Direct3DCreate9);
 
-        if (m_pfnDirect3DCreate9 == nullptr || DetourTransactionCommit() != NO_ERROR)
+        if (m_pfnDirect3DCreate9 == nullptr || !success)
         {
             BrowseToSolution("d3dapplyhook-fail", EXIT_GAME_FIRST | ASK_GO_ONLINE, "There was a problem hooking Direct3DCreate9");
         }
+
+        WriteDebugEvent(SString("Direct3D9 hook applied %08x", m_pfnDirect3DCreate9));
     }
     else
     {
