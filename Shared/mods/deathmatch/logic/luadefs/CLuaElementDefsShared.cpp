94c46cc2a34520d4d3d34b4ae10d239815362804
patrikjuvonen <22572159+patrikjuvonen@users.noreply.github.com>
2021-03-21 21:31:47 +0200

    Add missing file header

--- Shared/mods/deathmatch/logic/luadefs/CLuaElementDefsShared.cpp
+++ Shared/mods/deathmatch/logic/luadefs/CLuaElementDefsShared.cpp
@@ -1,3 +1,13 @@
+/*****************************************************************************
+ *
+ *  PROJECT:     Multi Theft Auto
+ *  LICENSE:     See LICENSE in the top level directory
+ *  FILE:        Shared/mods/deathmatch/logic/luadefs/CLuaElementDefs.cpp
+ *
+ *  Multi Theft Auto is available from https://www.multitheftauto.com/
+ *
+ *****************************************************************************/
+
 #include "StdInc.h"
 
 int CLuaElementDefs::GetElementData(lua_State* luaVM)
