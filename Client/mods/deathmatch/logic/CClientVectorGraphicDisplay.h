708fd14b64d10805d18836588ef4107931f0fbd7
LopSided <40902730+Lpsd@users.noreply.github.com>
2021-03-24 22:02:36 +0000

    Remove unnecessary definitions in CClientVectorGraphicDisplay

--- Client/mods/deathmatch/logic/CClientVectorGraphicDisplay.h
+++ Client/mods/deathmatch/logic/CClientVectorGraphicDisplay.h
@@ -24,26 +24,10 @@ public:
 
     eDisplayType GetType() { return DISPLAY_VECTORGRAPHIC; }
 
-    void SetPosition(const CVector& vecPosition);
-
-    void SetColor(const SColor color);
-    void SetColorAlpha(unsigned char ucAlpha);
-    void SetShadowAlpha(unsigned char ucShadowAlpha);
-
-    float GetScale() { return m_fScale; };
-    void  SetScale(float fScale);
-
-    unsigned long GetFormat() { return m_ulFormat; };
-    void          SetFormat(unsigned long ulFormat);
-
-    void SetVisible(bool bVisible);
-
     void Render();
 
     void UpdateTexture();
     void ClearTexture();
-
-    static void SetGlobalScale(float fScale) { m_fGlobalScale = fScale; }
 
     const bool IsCleared() { return m_bIsCleared; }
     const bool HasUpdated() { return m_bHasUpdated; }
@@ -51,13 +35,6 @@ public:
     void Update();
 private:
     CClientVectorGraphic* m_pVectorGraphic;
-    SString m_strCaption;
-    float   m_fScale;
-
-    unsigned long m_ulFormat;
-    unsigned char m_ucShadowAlpha;
-
-    static float m_fGlobalScale;
 
     bool m_bIsCleared;
     bool m_bHasUpdated;
