1c803a84d5994cd15502eb40e2b8aef05230101f
Pirulax <patrikjankovics7@gmail.com>
2021-04-01 18:15:26 +0200

    add missing function definitions to EventHandlerCallDispatcher.cpp

--- Shared/mods/deathmatch/logic/event/EventHandlerCallDispatcher.cpp
+++ Shared/mods/deathmatch/logic/event/EventHandlerCallDispatcher.cpp
@@ -3,6 +3,27 @@
 #include "EventHandlerCollection.h"
 #include "Event.h"
 
+EventHandlerCollection* EventHandlerCallDispatcher::GetHandlers(const Event& event, bool allowCreate = false)
+{
+    if (event.IsBuiltIn())
+    {
+        const auto& builtin = static_cast<const BuiltInEvent&>(event);
+        return &(*GetHandlers(builtin));
+    }
+    else
+    {
+        const auto custom = static_cast<const CustomEvent&>(event);
+        if (allowCreate)
+            return &m_custom[&custom]; // operator[] will construct object if not in map
+        else
+        {
+            if (auto [it, success] = GetHandlers(custom); success)
+                return &it->second;
+            return nullptr;
+        }
+    }
+}
+
 bool EventHandlerCallDispatcher::Remove(const Event& event, CLuaMain* lmain, const CLuaFunctionRef& fn)
 {
     bool out = false;
@@ -21,7 +42,8 @@ void EventHandlerCallDispatcher::Remove(CLuaMain* lmain)
     });
 }
 
-void EventHandlerCallDispatcher::Remove(const CustomEvent& event) // TODO
+void EventHandlerCallDispatcher::Remove(const CustomEvent& event)
 {
-
+    if (auto [it, success] = GetHandlers(event); success)
+        m_custom.erase(it);
 }
