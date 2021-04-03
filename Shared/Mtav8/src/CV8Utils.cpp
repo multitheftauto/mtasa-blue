d39536a2de0bb80c36c25397a41e5fa4d2a8557e
CrosRoad95 <sebajura1234@gmail.com>
2021-04-03 17:39:29 +0200

    remove utils functions

--- Shared/Mtav8/src/CV8Utils.cpp
+++ Shared/Mtav8/src/CV8Utils.cpp
@@ -21,36 +21,6 @@ Local<Number> CV8Utils::ToV8Number(double number)
     return Number::New(isolate, number);
 }
 
-MaybeLocal<Value> CV8Utils::GetGlobal(const char* name)
-{
-    Isolate* isolate = Isolate::GetCurrent();
-    assert(isolate);
-    MaybeLocal<Value> maybeValue = isolate->GetCurrentContext()->Global()->Get(isolate->GetCurrentContext(), CV8Utils::ToV8String(name));
-    return maybeValue;
-}
-
-MaybeLocal<Value> CV8Utils::GetClassByName(const char* className)
-{
-    Isolate* isolate = Isolate::GetCurrent();
-    assert(isolate);
-    EscapableHandleScope handleScope(isolate);
-    MaybeLocal<Value>    maybeValue = GetGlobal(className);
-    Local<Value>         value;
-    if (maybeValue.ToLocal(&value))
-    {
-        if (value->IsFunction())
-        {
-            Handle<Function> v8Class = Handle<Function>::Cast(value);
-            if (v8Class->IsConstructor())
-            {
-                return handleScope.Escape(value);
-            }
-        }
-    }
-
-    return handleScope.EscapeMaybe(MaybeLocal<Value>());
-}
-
 const char* CV8Utils::ToString(const v8::String::Utf8Value& value)
 {
     return *value ? *value : "<string conversion failed>";
