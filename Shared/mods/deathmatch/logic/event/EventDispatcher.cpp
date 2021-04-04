910a83922e01b216fe8569c9bd3c81cf80501210
Pirulax <patrikjankovics7@gmail.com>
2021-04-04 22:08:24 +0200

    Refactor EventDispatcher to use the new iter functions of CElement

--- Shared/mods/deathmatch/logic/event/EventDispatcher.cpp
+++ Shared/mods/deathmatch/logic/event/EventDispatcher.cpp
@@ -6,39 +6,6 @@
 #include <logic/CPlayer.h>
 #include <logic/CElement.h>
 
-
-void DispatchTo(CElement* to, const Event& event, const CLuaArguments& args, CElement* source, CPlayer* client)
-{
-    to->GetEventHandlerCallDispatcher().Emmit(event, args, source, to, client);
-}
-
-void DispatchToParents(CElement* current, const Event& event, const CLuaArguments& args, CElement* source, CPlayer* client)
-{
-    if (!current)
-        return;
-    DispatchTo(current, event, args, source, client);
-    DispatchToParents(current->GetParentEntity(), event, args, source, client);
-}
-
-void DispatchToChildren(CElement* parent, const Event& event, const CLuaArguments& args, CElement* source, CPlayer* client)
-{
-    if (!parent->CountChildren()) 
-        return; // Doesn't have any alive children
-
-    auto snapshot = parent->GetChildrenListSnapshot();
-    snapshot->AddRef();
-    for (auto child : *snapshot)
-    {
-        if (parent->IsBeingDeleted())
-            continue; // Event handler might've destroyed us
-        if (child->IsBeingDeleted())
-            continue; // ...or our children
-        DispatchTo(child, event, args, source, client);
-        DispatchToChildren(child, event, args, source, client);
-    }
-    snapshot->Release();
-}
-
 void EventDispatcher::PreEventPulse()
 {
 	m_cancelStack.push_back(m_eventCancelled);
@@ -65,8 +32,14 @@ bool EventDispatcher::Call(const Event& event, const CLuaArguments& args, CEleme
 	    return false;
 	PreEventPulse();
 
-    DispatchToParents(source, event, args, source, client); // Also calls event on source
-    DispatchToChildren(source, event, args, source, client);
+    // Call all event handlers (us, ancestors, children)
+    {
+        const auto CallHandlers = [&](auto* elem) {
+            elem->GetEventHandlerCallDispatcher().Emmit(event, args, source, elem, client);
+        };
+        CallHandlers(source);
+        source->IterAncestorsThenChildren(CallHandlers);
+    }
 
 	PostEventPulse();
     g_pGame->GetDebugHookManager()->OnPostEvent(event.GetName(), args, source, client);
