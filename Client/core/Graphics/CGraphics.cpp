d850bcf957d507817f288d3bbc49e32ab8fed2e2
Allerek <allerek22@interia.pl>
2021-04-28 11:33:02 +0200

    Revert "no message"
    This reverts commit 57d0329b6e45d32259f5d16b449aecabc35bb512.

--- Client/core/Graphics/CGraphics.cpp
+++ Client/core/Graphics/CGraphics.cpp
@@ -1098,7 +1098,11 @@ void CGraphics::DrawTextureQueued(float fX, float fY, float fWidth, float fHeigh
 
 void CGraphics::DrawStringQueued(float fLeft, float fTop, float fRight, float fBottom, unsigned long dwColor, const char* szText, float fScaleX, float fScaleY,
                                  unsigned long ulFormat, ID3DXFont* pDXFont, bool bPostGUI, bool bColorCoded, bool bSubPixelPositioning, float fRotation,
+<<<<<<< HEAD
                                  float fRotationCenterX, float fRotationCenterY, const std::optional<float>& lineHeight)
+=======
+                                 float fRotationCenterX, float fRotationCenterY, float fLineHeight)
+>>>>>>> 14824a3641bf81dd67bbe55a12be0accb17caab0
 {
     if (!szText || !m_pDXSprite)
         return;
@@ -1138,7 +1142,8 @@ void CGraphics::DrawStringQueued(float fLeft, float fTop, float fRight, float fB
             fRight = floor(fRight);
             fBottom = floor(fBottom);
         }
-
+        if (fLineHeight == 0.0f)
+            fLineHeight = GetDXFontHeight(fScaleY, pDXFont);
         sDrawQueueItem Item;
         Item.eType = QUEUE_TEXT;
         Item.blendMode = m_ActiveBlendMode;
@@ -1155,6 +1160,7 @@ void CGraphics::DrawStringQueued(float fLeft, float fTop, float fRight, float fB
         Item.Text.fRotation = fRotation;
         Item.Text.fRotationCenterX = fRotationCenterX;
         Item.Text.fRotationCenterY = fRotationCenterY;
+        Item.Text.fLineHeight = fLineHeight;
 
         // Convert to wstring
         Item.wstrText = MbUTF8ToUTF16(szText);
@@ -1179,7 +1185,12 @@ void CGraphics::DrawStringQueued(float fLeft, float fTop, float fRight, float fB
         CSplitStringW splitLines(wstrText, L"\n");
         int           iNumLines = splitLines.size();
 
+<<<<<<< HEAD
         fLineHeight = GetDXFontHeight(fScaleY, pDXFont)
+=======
+        if (fLineHeight == 0.0f)
+            fLineHeight = GetDXFontHeight(fScaleY, pDXFont);
+>>>>>>> 14824a3641bf81dd67bbe55a12be0accb17caab0
         float fTotalHeight = iNumLines * fLineHeight;
 
         // Y position of text
