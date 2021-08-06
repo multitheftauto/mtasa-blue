449c5c329732d77ea36ce0abc9595f1577dd1304
Nikita Obrekht <obrekht@gmail.com>
2021-08-06 13:55:02 +0200

    Add functions to change sound effect parameters (#642)
    * Add setSoundEffectParameter and getSoundEffectParameters functions
    
    Co-authored-by: Sarrum <thesarrum@gmail.com>
    
    Co-authored-by: Qais Patankar <qaisjp@gmail.com>
    Co-authored-by: Pirulax <patrikjankovics7@gmail.com>
    Co-authored-by: patrikjuvonen <22572159+patrikjuvonen@users.noreply.github.com>
    Co-authored-by: Sarrum <thesarrum@gmail.com>

--- Client/mods/deathmatch/logic/CClientSound.h
+++ Client/mods/deathmatch/logic/CClientSound.h
@@ -82,6 +82,9 @@ public:
 
     bool SetFxEffect(uint uiFxEffect, bool bEnable);
     bool IsFxEffectEnabled(uint uiFxEffect);
+    
+    bool SetFxEffectParameters(uint uiFxEffect, void* params);
+    bool GetFxEffectParameters(uint uiFxEffect, void* params);
 
     void Unlink(){};
 
