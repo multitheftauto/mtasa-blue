5f320d7a3280a4340827bb2734d01a20cc57bfc9
patrikjuvonen <22572159+patrikjuvonen@users.noreply.github.com>
2021-08-28 21:17:16 +0300

    Rename non-standard filename and add missing license section

--- Shared/mods/deathmatch/logic/luadefs/CLuaElementDefs.cpp
+++ Shared/mods/deathmatch/logic/luadefs/CLuaElementDefs.cpp
@@ -1,3 +1,12 @@
+/*****************************************************************************
+ *
+ *  PROJECT:     Multi Theft Auto
+ *  LICENSE:     See LICENSE in the top level directory
+ *  FILE:        Shared/mods/logic/luadefs/CLuaElementDefs.cpp
+ *
+ *  Multi Theft Auto is available from http://www.multitheftauto.com/
+ *
+ *****************************************************************************/
 #include "StdInc.h"
 
 int CLuaElementDefs::GetElementData(lua_State* luaVM)
