26565770db2662d84554d68e3680c0acbfc9eeeb
Allerek <allerek22@interia.pl>
2021-09-18 12:15:13 +0200

    Fix #611: Add line spacing to dxDrawText (#2355)

--- Client/core/Graphics/CGraphics.cpp
+++ Client/core/Graphics/CGraphics.cpp
@@ -1098,7 +1098,7 @@ void CGraphics::DrawTextureQueued(float fX, float fY, float fWidth, float fHeigh
 
 void CGraphics::DrawStringQueued(float fLeft, float fTop, float fRight, float fBottom, unsigned long dwColor, const char* szText, float fScaleX, float fScaleY,
                                  unsigned long ulFormat, ID3DXFont* pDXFont, bool bPostGUI, bool bColorCoded, bool bSubPixelPositioning, float fRotation,
-                                 float fRotationCenterX, float fRotationCenterY)
+                                 float fRotationCenterX, float fRotationCenterY, float fLineHeight)
 {
     if (!szText || !m_pDXSprite)
         return;
@@ -1179,7 +1179,8 @@ void CGraphics::DrawStringQueued(float fLeft, float fTop, float fRight, float fB
         CSplitStringW splitLines(wstrText, L"\n");
         int           iNumLines = splitLines.size();
 
-        float fLineHeight = GetDXFontHeight(fScaleY, pDXFont);
+        if (fLineHeight == 0.0f)
+            fLineHeight = GetDXFontHeight(fScaleY, pDXFont);
         float fTotalHeight = iNumLines * fLineHeight;
 
         // Y position of text
