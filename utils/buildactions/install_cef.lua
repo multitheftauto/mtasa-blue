07209470b99788687c4e35488c38aff801b46850
Dutchman101 <12105539+Dutchman101@users.noreply.github.com>
2021-10-31 08:13:22 +0100

    Update CEF to 95.7.14+g9f72f35+chromium-95.0.4638.69
    Compared to our last one, it's a small bump in chromium version, but with important security fixes

--- utils/buildactions/install_cef.lua
+++ utils/buildactions/install_cef.lua
@@ -9,8 +9,8 @@ local CEF_URL_PREFIX = "https://cef-builds.spotifycdn.com/cef_binary_"
 local CEF_URL_SUFFIX = "_windows32_minimal.tar.bz2"
 
 -- Change here to update CEF version
-local CEF_VERSION = "95.7.12+g99c4ac0+chromium-95.0.4638.54"
-local CEF_HASH = "1a7295741ecad5ec5d75b04fd6753d2a6df044d40e4e54b6b093a4b9f8bf35bf"
+local CEF_VERSION = "95.7.14+g9f72f35+chromium-95.0.4638.69"
+local CEF_HASH = "9af28fdf935810f6e55d3eb4dae7eea8eedad3d678009230f17a86cdf9e2e4bb"
 
 function make_cef_download_url()
 	return CEF_URL_PREFIX..http.escapeUrlParam(CEF_VERSION)..CEF_URL_SUFFIX
