8aea207a680712ff3ccb420b24ee55f18a176fb0
LopSided <40902730+Lpsd@users.noreply.github.com>
2021-11-20 23:35:50 +0000

    Fix CXMLNodeImpl::m_bUsingIDs initial value

--- Shared/XML/CXMLNodeImpl.cpp
+++ Shared/XML/CXMLNodeImpl.cpp
@@ -15,9 +15,9 @@ using std::list;
 
 CXMLNodeImpl::CXMLNodeImpl(CXMLFileImpl* pFile, CXMLNodeImpl* pParent, TiXmlElement& Node)
     : m_ulID(INVALID_XML_ID),
-      m_bUsingIDs((!pFile) || pFile && pFile->IsUsingIDs()),
+      m_bUsingIDs(pFile && pFile->IsUsingIDs()),
       m_pNode(&Node),
-      m_Attributes(Node, (!pFile) || pFile && pFile->IsUsingIDs())
+      m_Attributes(Node, pFile && pFile->IsUsingIDs())
 {
     // Init
     m_pFile = pFile;
