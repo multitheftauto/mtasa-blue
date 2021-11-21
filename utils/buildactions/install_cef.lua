9fa0d00664bd21edd73996c157b6e29717dc86a0
Dutchman101 <12105539+Dutchman101@users.noreply.github.com>
2021-11-21 04:31:56 +0100

    Update CEF to 96.0.14+g28ba5c8+chromium-96.0.4664.55

--- utils/buildactions/install_cef.lua
+++ utils/buildactions/install_cef.lua
@@ -9,8 +9,8 @@ local CEF_URL_PREFIX = "https://cef-builds.spotifycdn.com/cef_binary_"
 local CEF_URL_SUFFIX = "_windows32_minimal.tar.bz2"
 
 -- Change here to update CEF version
-local CEF_VERSION = "95.7.14+g9f72f35+chromium-95.0.4638.69"
-local CEF_HASH = "9af28fdf935810f6e55d3eb4dae7eea8eedad3d678009230f17a86cdf9e2e4bb"
+local CEF_VERSION = "96.0.14+g28ba5c8+chromium-96.0.4664.55"
+local CEF_HASH = "a0ce2c76db66262c9134fcf2f9ad9a2716f6409742ab8e67d07aa3ef8b425ed3"
 
 function make_cef_download_url()
 	return CEF_URL_PREFIX..http.escapeUrlParam(CEF_VERSION)..CEF_URL_SUFFIX
