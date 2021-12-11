27d82bb24c1352cb66a925228b89a5c287cf79c8
LopSided <40902730+Lpsd@users.noreply.github.com>
2021-12-11 13:25:11 +0000

    Use .get() on m_callback

--- Client/cefweb/CAjaxResourceHandler.cpp
+++ Client/cefweb/CAjaxResourceHandler.cpp
@@ -33,8 +33,8 @@ void CAjaxResourceHandler::SetResponse(const SString& data)
     m_strResponse = data;
     m_bHasData = true;
 
-    if (m_callback)
-        m_callback->Continue();
+    if (m_callback && m_callback.get())
+        m_callback.get()->Continue();
 }
 
 // CefResourceHandler implementation
