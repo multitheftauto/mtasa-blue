e7e3ba5b337f791203ef977bd083a28226614da7
Inder00 <inder00@wp.pl>
2021-12-21 20:30:24 +0100

    Add RSA support on encodeString (#2412)
    * RSA support
    
    * RSA Support (Tests)
    
    * Removed unnecessary rsa key size check
    
    * Fix typo
    
    * Change enum StringEncryptFunction to StringEncodeFunction
    
    * Pirulax code review suggestions
    
    Co-authored-by: Pirulax <patrikjankovics7@gmail.com>
    
    * Code review fixes
    
    * Replace lua expressions with lua namespace
    
    * Remove unnecessary RSA test
    
    * Remove annoying keysize warning
    
    * Change keysize limitations
    
    * Expose exception cause to debugscript warning
    
    * Inverted result logic inside async block
    
    * Refactor generateRsaKeyPair function
    
    * Adds missing const modifer inside async block
    
    * Refactor generateRsaKeyPair to generateKeyPair for future compatible
    
    * Fix missing references
    
    * Remove key size and expose exception cause
    
    * Fix sync key pair generation
    
    * RSA Debug Tests
    
    Co-authored-by: Pirulax <patrikjankovics7@gmail.com>

--- Shared/mods/deathmatch/logic/luadefs/CLuaCryptDefs.h
+++ Shared/mods/deathmatch/logic/luadefs/CLuaCryptDefs.h
@@ -26,6 +26,8 @@ public:
     static std::string                     TeaDecode(std::string str, std::string key);
     static std::string                     Base64encode(std::string str);
     static std::string                     Base64decode(std::string str);
+    static std::variant<bool, CLuaMultiReturn<SString, SString>> GenerateKeyPair(lua_State* const luaVM, KeyPairAlgorithm algorithm,
+                                                             std::unordered_map<std::string, std::string> options, std::optional<CLuaFunctionRef> callback);
     static std::variant<std::string, bool> PasswordHash(lua_State* luaVM, std::string password, PasswordHashFunction algorithm,
                                                         std::unordered_map<std::string, std::string> options, std::optional<CLuaFunctionRef> callback);
     static std::string                     Sha256(std::string strSourceData);
