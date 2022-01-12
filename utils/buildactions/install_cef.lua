98a0cd6e62862ff15bdbf7022271fd40ce182954
Dutchman101 <12105539+Dutchman101@users.noreply.github.com>
2022-01-12 21:45:19 +0100

    Update CEF from 97.1.1+g50067f2+chromium-97.0.4692.71 to 97.1.2+gb821dc3+chromium-97.0.4692.71
    It's a small non-chromium update, but contains a crash fix that may be 1 of the biggest CEF crashes in MTA

--- utils/buildactions/install_cef.lua
+++ utils/buildactions/install_cef.lua
@@ -9,8 +9,8 @@ local CEF_URL_PREFIX = "https://cef-builds.spotifycdn.com/cef_binary_"
 local CEF_URL_SUFFIX = "_windows32_minimal.tar.bz2"
 
 -- Change here to update CEF version
-local CEF_VERSION = "97.1.1+g50067f2+chromium-97.0.4692.71"
-local CEF_HASH = "0963e38547512c32974500245530cf2615ccc14cc77dd2605cf76eae851229e2"
+local CEF_VERSION = "97.1.2+gb821dc3+chromium-97.0.4692.71"
+local CEF_HASH = "c563e4c3f150e70e20a80b3665768fa250ed9b64a3cda13955a1c00a58d2da71"
 
 function make_cef_download_url()
 	return CEF_URL_PREFIX..http.escapeUrlParam(CEF_VERSION)..CEF_URL_SUFFIX
