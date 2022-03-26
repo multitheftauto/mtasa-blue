b26cb9f6fc4cca42e455b2a553a1364a40ff4d72
Dutchman101 <12105539+Dutchman101@users.noreply.github.com>
2022-03-26 03:42:11 +0100

    Update CEF to 99.2.13+gd9af866+chromium-99.0.4844.83
    If server scripts relied on getting a players' screen content (input fields, states) there will be bugs on 99.2.12+g2977b3a+chromium-99.0.4844.74. Potentially even worse ones if the user is on macOS

--- utils/buildactions/install_cef.lua
+++ utils/buildactions/install_cef.lua
@@ -9,8 +9,8 @@ local CEF_URL_PREFIX = "https://cef-builds.spotifycdn.com/cef_binary_"
 local CEF_URL_SUFFIX = "_windows32_minimal.tar.bz2"
 
 -- Change here to update CEF version
-local CEF_VERSION = "99.2.12+g2977b3a+chromium-99.0.4844.74"
-local CEF_HASH = "d097b0f6660ea7d6be5c460b1c483714e99b0994171b75a3d0aa31f8973736db"
+local CEF_VERSION = "99.2.13+gd9af866+chromium-99.0.4844.83"
+local CEF_HASH = "f6b4ed7b6b1fa15e34f8dc12cd25a330933d3140f1e9264452fd73b67411d005"
 
 function make_cef_download_url()
 	return CEF_URL_PREFIX..http.escapeUrlParam(CEF_VERSION)..CEF_URL_SUFFIX
