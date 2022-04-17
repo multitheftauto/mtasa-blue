c316e9ea833ebe5e8f4f572822880b29ea83b782
patrikjuvonen <22572159+patrikjuvonen@users.noreply.github.com>
2022-04-17 18:55:39 +0300

    Update mysql-connector-c from 6.0.2 to 6.1.11

--- vendor/mysql/include/my_config.h
+++ vendor/mysql/include/my_config.h
@@ -1,295 +1,454 @@
+/* Copyright (c) 2009, 2016, Oracle and/or its affiliates. All rights reserved.
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; version 2 of the License.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
 
-/* Headers we may want to use. */
+#ifndef MY_CONFIG_H
+#define MY_CONFIG_H
+
+/*
+ * From configure.cmake, in order of appearance 
+ */
+/* #undef HAVE_LLVM_LIBCPP */
+#define _LARGEFILE_SOURCE 1
+
+/* Libraries */
+/* #undef HAVE_LIBM */
+/* #undef HAVE_LIBNSL */
+/* #undef HAVE_LIBCRYPT */
+/* #undef HAVE_LIBSOCKET */
+/* #undef HAVE_LIBDL */
+/* #undef HAVE_LIBRT */
+/* #undef HAVE_LIBWRAP */
+
+/* Header files */
 /* #undef HAVE_ALLOCA_H */
 /* #undef HAVE_ARPA_INET_H */
-/* #undef HAVE_CRYPT_H */
 /* #undef HAVE_DIRENT_H */
+/* #undef HAVE_DLFCN_H */
 /* #undef HAVE_EXECINFO_H */
-#define HAVE_FCNTL_H 1
-/* #undef HAVE_FENV_H */
-#define HAVE_FLOAT_H 1
 /* #undef HAVE_FPU_CONTROL_H */
 /* #undef HAVE_GRP_H */
 /* #undef HAVE_IEEEFP_H */
-#define HAVE_LIMITS_H 1
+/* #undef HAVE_LANGINFO_H */
 #define HAVE_MALLOC_H 1
-#define HAVE_MEMORY_H 1
 /* #undef HAVE_NETINET_IN_H */
-/* #undef HAVE_PATHS_H */
+/* #undef HAVE_POLL_H */
 /* #undef HAVE_PWD_H */
-/* #undef HAVE_SCHED_H */
-/* #undef HAVE_SELECT_H */
-#define HAVE_STDDEF_H 1
-#define HAVE_STDLIB_H 1
 /* #undef HAVE_STRINGS_H */
-#define HAVE_STRING_H 1
-/* #undef HAVE_SYNCH_H */
-/* #undef HAVE_SYSENT_H */
-/* #undef HAVE_SYS_FPU_H */
+/* #undef HAVE_SYS_CDEFS_H */
 /* #undef HAVE_SYS_IOCTL_H */
-/* #undef HAVE_SYS_IPC_H */
 /* #undef HAVE_SYS_MMAN_H */
-/* #undef HAVE_SYS_PRCTL_H */
+/* #undef HAVE_SYS_RESOURCE_H */
 /* #undef HAVE_SYS_SELECT_H */
-/* #undef HAVE_SYS_SHM_H */
 /* #undef HAVE_SYS_SOCKET_H */
-#define HAVE_SYS_STAT_H 1
-/* #undef HAVE_SYS_STREAM_H */
-#define HAVE_SYS_TIMEB_H 1
-#define HAVE_SYS_TYPES_H 1
-/* #undef HAVE_SYS_UN_H */
+/* #undef HAVE_TERM_H */
 /* #undef HAVE_TERMIOS_H */
 /* #undef HAVE_TERMIO_H */
 /* #undef HAVE_UNISTD_H */
-/* #undef HAVE_UTIME_H */
+/* #undef HAVE_SYS_WAIT_H */
+/* #undef HAVE_SYS_PARAM_H */
+/* #undef HAVE_FNMATCH_H */
+/* #undef HAVE_SYS_UN_H */
+/* #undef HAVE_VIS_H */
+/* #undef HAVE_SASL_SASL_H */
 
-/* Functions we may want to use. */
-#define HAVE_ACCESS 1
-/* #undef HAVE_AIOWAIT */
-/* #undef HAVE_ALARM */
-/* #undef HAVE_ALLOCA */
-/* #undef HAVE_BCMP */
-/* #undef HAVE_BFILL */
-/* #undef HAVE_BMOVE */
-/* #undef HAVE_BZERO */
+/* Libevent */
+/* #undef HAVE_DEVPOLL */
+/* #undef HAVE_SYS_DEVPOLL_H */
+/* #undef HAVE_SYS_EPOLL_H */
+/* #undef HAVE_TAILQFOREACH */
+
+/* Functions */
+#define HAVE_ALIGNED_MALLOC 1
+/* #undef HAVE_BACKTRACE */
+/* #undef HAVE_PRINTSTACK */
+/* #undef HAVE_INDEX */
 /* #undef HAVE_CLOCK_GETTIME */
-/* #undef HAVE_COMPRESS */
+/* #undef HAVE_CUSERID */
+/* #undef HAVE_DIRECTIO */
+/* #undef HAVE_FTRUNCATE */
+#define HAVE_COMPRESS 1
 /* #undef HAVE_CRYPT */
-/* #undef HAVE_DLERROR */
 /* #undef HAVE_DLOPEN */
 /* #undef HAVE_FCHMOD */
 /* #undef HAVE_FCNTL */
-/* #undef HAVE_FCONVERT */
 /* #undef HAVE_FDATASYNC */
-/* #undef HAVE_FESETROUND */
-/* #undef HAVE_FINITE */
-/* #undef HAVE_FP_EXCEPT */
+/* #undef HAVE_DECL_FDATASYNC */
+/* #undef HAVE_FEDISABLEEXCEPT */
 /* #undef HAVE_FSEEKO */
 /* #undef HAVE_FSYNC */
-/* #undef HAVE_GETADDRINFO */
-#define HAVE_GETCWD 1
 /* #undef HAVE_GETHOSTBYADDR_R */
-/* #undef HAVE_GETHOSTBYNAME_R */
 /* #undef HAVE_GETHRTIME */
 /* #undef HAVE_GETNAMEINFO */
-/* #undef HAVE_GETPAGESIZE */
 /* #undef HAVE_GETPASS */
 /* #undef HAVE_GETPASSPHRASE */
 /* #undef HAVE_GETPWNAM */
 /* #undef HAVE_GETPWUID */
 /* #undef HAVE_GETRLIMIT */
 /* #undef HAVE_GETRUSAGE */
-/* #undef HAVE_GETWD */
-/* #undef HAVE_GMTIME_R */
 /* #undef HAVE_INITGROUPS */
-/* #undef HAVE_ISNAN */
-#define HAVE_LDIV 1
-/* #undef HAVE_LOCALTIME_R */
-/* #undef HAVE_LOG2 */
-#define HAVE_LONGJMP 1
+/* #undef HAVE_ISSETUGID */
+/* #undef HAVE_GETUID */
+/* #undef HAVE_GETEUID */
+/* #undef HAVE_GETGID */
+/* #undef HAVE_GETEGID */
 /* #undef HAVE_LSTAT */
 /* #undef HAVE_MADVISE */
-/* #undef HAVE_DECL_MADVISE */
-/* #undef HAVE_MALLINFO */
-#define HAVE_MEMCPY 1
-#define HAVE_MEMCPY 1
-#define HAVE_MEMMOVE 1
-#define HAVE_MEMMOVE 1
-/* #undef HAVE_MKSTEMP */
+/* #undef HAVE_MALLOC_INFO */
+/* #undef HAVE_MEMRCHR */
 /* #undef HAVE_MLOCK */
 /* #undef HAVE_MLOCKALL */
-/* #undef HAVE_MMAP */
 /* #undef HAVE_MMAP64 */
-#define HAVE_PERROR 1
 /* #undef HAVE_POLL */
+/* #undef HAVE_POSIX_FALLOCATE */
+/* #undef HAVE_POSIX_MEMALIGN */
 /* #undef HAVE_PREAD */
-/* #undef HAVE_PTHREAD_ATTR_CREATE */
-/* #undef HAVE_PTHREAD_ATTR_GETSTACKSIZE */
-/* #undef HAVE_PTHREAD_ATTR_SETPRIO */
-/* #undef HAVE_PTHREAD_ATTR_SETSCHEDPARAM */
-/* #undef HAVE_PTHREAD_ATTR_SETSCOPE */
-/* #undef HAVE_PTHREAD_ATTR_SETSTACKSIZE */
-/* #undef HAVE_PTHREAD_CONDATTR_CREATE */
-/* #undef HAVE_PTHREAD_INIT */
-/* #undef HAVE_PTHREAD_KEY_DELETE */
-/* #undef HAVE_PTHREAD_KEY_DELETE */
-/* #undef HAVE_PTHREAD_KILL */
-/* #undef HAVE_PTHREAD_RWLOCK_RDLOCK */
-/* #undef HAVE_PTHREAD_SETPRIO_NP */
-/* #undef HAVE_PTHREAD_SETSCHEDPARAM */
+/* #undef HAVE_PTHREAD_CONDATTR_SETCLOCK */
 /* #undef HAVE_PTHREAD_SIGMASK */
-/* #undef HAVE_PTHREAD_THREADMASK */
-/* #undef HAVE_PTHREAD_YIELD_NP */
 /* #undef HAVE_READDIR_R */
 /* #undef HAVE_READLINK */
 /* #undef HAVE_REALPATH */
-#define HAVE_RENAME 1
-/* #undef HAVE_RINT */
-/* #undef HAVE_SCHED_YIELD */
-/* #undef HAVE_SELECT */
 /* #undef HAVE_SETFD */
-/* #undef HAVE_SETFILEPOINTER */
 /* #undef HAVE_SIGACTION */
-/* #undef HAVE_SIGTHREADMASK */
-/* #undef HAVE_SIGWAIT */
 /* #undef HAVE_SLEEP */
-/* #undef HAVE_SNPRINTF */
 /* #undef HAVE_STPCPY */
-#define HAVE_STRERROR 1
+/* #undef HAVE_STPNCPY */
 /* #undef HAVE_STRLCPY */
 #define HAVE_STRNLEN 1
-#define HAVE_STRPBRK 1
+/* #undef HAVE_STRLCAT */
+/* #undef HAVE_STRSIGNAL */
+/* #undef HAVE_FGETLN */
 /* #undef HAVE_STRSEP */
-#define HAVE_STRSTR 1
-/* #undef HAVE_STRTOK_R */
-/* #undef HAVE_STRTOK_R */
-#define HAVE_STRTOL 1
-/* #undef HAVE_STRTOLL */
-#define HAVE_STRTOUL 1
-/* #undef HAVE_STRTOULL */
 #define HAVE_TELL 1
-/* #undef HAVE_THR_SETCONCURRENCY */
-/* #undef HAVE_THR_YIELD */
 /* #undef HAVE_VASPRINTF */
-#define HAVE_VSNPRINTF 1
+/* #undef HAVE_MEMALIGN */
+/* #undef HAVE_NL_LANGINFO */
+/* #undef HAVE_HTONLL */
+/* #undef DNS_USE_CPU_CLOCK_FOR_ID */
+/* #undef HAVE_EPOLL */
+/* #undef HAVE_EVENT_PORTS */
+/* #undef HAVE_INET_NTOP */
+/* #undef HAVE_WORKING_KQUEUE */
+/* #undef HAVE_TIMERADD */
+/* #undef HAVE_TIMERCLEAR */
+/* #undef HAVE_TIMERCMP */
+/* #undef HAVE_TIMERISSET */
 
-/* Symbols we may use */
-/* #undef HAVE_SYS_ERRLIST */
-/* used by stacktrace functions */
-/* #undef HAVE_BSS_START */
+/* WL2373 */
+/* #undef HAVE_SYS_TIME_H */
+/* #undef HAVE_SYS_TIMES_H */
+/* #undef HAVE_TIMES */
+/* #undef HAVE_GETTIMEOFDAY */
 
-/* Does "struct timespec" have a "sec" and "nsec" field? */
-/* #undef HAVE_TIMESPEC_TS_SEC */
+/* Symbols */
+/* #undef HAVE_LRAND48 */
+/* #undef GWINSZ_IN_SYS_IOCTL */
+/* #undef FIONREAD_IN_SYS_IOCTL */
+/* #undef FIONREAD_IN_SYS_FILIO */
+/* #undef HAVE_SIGEV_THREAD_ID */
+/* #undef HAVE_SIGEV_PORT */
+#define HAVE_LOG2 1
 
-/* Types we may use */
-#define SIZEOF_CHAR 1
-#if SIZEOF_CHAR
-# define HAVE_CHAR 1
-#endif
+#define HAVE_ISINF 1
 
-#define SIZEOF_CHARP 4
-#if SIZEOF_CHARP
-# define HAVE_CHARP 1
-#endif
+/* #undef HAVE_KQUEUE_TIMERS */
+/* #undef HAVE_POSIX_TIMERS */
 
-#define SIZEOF_SHORT 2
-#if SIZEOF_SHORT
-# define HAVE_SHORT 1
-#endif
-
-#define SIZEOF_INT 4
-#if SIZEOF_INT
-# define HAVE_INT 1
-#endif
-
-#define SIZEOF_LONG 4
-#if SIZEOF_LONG
-# define HAVE_LONG 1
-#endif
-
-#define SIZEOF_LONG_LONG 8
-#if SIZEOF_LONG_LONG
-# define HAVE_LONG_LONG 1
-#endif
-
-#define SIZEOF_OFF_T 4
-#if SIZEOF_OFF_T
-# define HAVE_OFF_T 1
-#endif
-
-/* #undef SIZEOF_SIGSET_T */
-#if SIZEOF_SIGSET_T
-# define HAVE_SIGSET_T 1
-#endif
-
-#define SIZEOF_SIZE_T 4
-#if SIZEOF_SIZE_T
-# define HAVE_SIZE_T 1
-#endif
-
-/* #undef SIZEOF_UCHAR */
-#if SIZEOF_UCHAR
-# define HAVE_UCHAR 1
-#endif
-
-/* #undef SIZEOF_UINT */
-#if SIZEOF_UINT
-# define HAVE_UINT 1
-#endif
-
-/* #undef SIZEOF_ULONG */
-#if SIZEOF_ULONG
-# define HAVE_ULONG 1
-#endif
-
-/* #undef SIZEOF_INT8 */
-#if SIZEOF_INT8
-# define HAVE_INT8 1
-#endif
-/* #undef SIZEOF_UINT8 */
-#if SIZEOF_UINT8
-# define HAVE_UINT8 1
-#endif
-
-/* #undef SIZEOF_INT16 */
-#if SIZEOF_INT16
-# define HAVE_INT16 1
-#endif
-/* #undef SIZEOF_UINT16 */
-#if SIZEOF_UINT16
-# define HAVE_UINT16 1
-#endif
-
-/* #undef SIZEOF_INT32 */
-#if SIZEOF_INT32
-# define HAVE_INT32 1
-#endif
-/* #undef SIZEOF_UINT32 */
-#if SIZEOF_UINT32
-# define HAVE_UINT32 1
-#endif
-/* #undef SIZEOF_U_INT32_T */
-#if SIZEOF_U_INT32_T
-# define HAVE_U_INT32_T 1
-#endif
-
-/* #undef SIZEOF_INT64 */
-#if SIZEOF_INT64
-# define HAVE_INT64 1
-#endif
-/* #undef SIZEOF_UINT64 */
-#if SIZEOF_UINT64
-# define HAVE_UINT64 1
-#endif
-
-/* #undef SIZEOF_SOCKLEN_T */
-#if SIZEOF_SOCKLEN_T
-# define HAVE_SOCKLEN_T 1
-#endif
-
-/* XXX mysql_client_test uses this -- rip it out, please! */
-#define MAX_INDEXES 64
-
-#define QSORT_TYPE_IS_VOID 1
-#define RETQSORTTYPE void
-
-#define SIGNAL_RETURN_TYPE_IS_VOID 1
-#define RETSIGTYPE void
-
+/* Endianess */
 /* #undef WORDS_BIGENDIAN */
 
-/* Define to `__inline__' or `__inline' if that's what the C compiler calls
-   it, or to nothing if 'inline' is not supported under any name.  */
-#ifndef __cplusplus
-# define inline 
-#endif
+/* Type sizes */
+#define SIZEOF_VOIDP     4
+#define SIZEOF_CHARP     4
+#define SIZEOF_LONG      4
+#define SIZEOF_SHORT     2
+#define SIZEOF_INT       4
+#define SIZEOF_LONG_LONG 8
+#define SIZEOF_OFF_T     4
+#define SIZEOF_TIME_T    8
+/* #undef HAVE_UINT */
+/* #undef HAVE_ULONG */
+/* #undef HAVE_U_INT32_T */
+#define HAVE_STRUCT_TIMESPEC
 
-#define TIME_WITH_SYS_TIME 1
+/* Support for tagging symbols with __attribute__((visibility("hidden"))) */
+/* #undef HAVE_VISIBILITY_HIDDEN */
 
+/* Code tests*/
 #define STACK_DIRECTION -1
+/* #undef TIME_WITH_SYS_TIME */
+#define NO_FCNTL_NONBLOCK 1
+/* #undef HAVE_PAUSE_INSTRUCTION */
+/* #undef HAVE_FAKE_PAUSE_INSTRUCTION */
+/* #undef HAVE_HMT_PRIORITY_INSTRUCTION */
+/* #undef HAVE_ABI_CXA_DEMANGLE */
+/* #undef HAVE_BSS_START */
+/* #undef HAVE_BUILTIN_UNREACHABLE */
+/* #undef HAVE_BUILTIN_EXPECT */
+/* #undef HAVE_BUILTIN_STPCPY */
+/* #undef HAVE_GCC_ATOMIC_BUILTINS */
+/* #undef HAVE_GCC_SYNC_BUILTINS */
+/* #undef HAVE_VALGRIND */
 
+/* IPV6 */
+/* #undef HAVE_NETINET_IN6_H */
+#define HAVE_STRUCT_SOCKADDR_IN6 1
+#define HAVE_STRUCT_IN6_ADDR 1
+#define HAVE_IPV6 1
+
+/* #undef ss_family */
+/* #undef HAVE_SOCKADDR_IN_SIN_LEN */
+/* #undef HAVE_SOCKADDR_IN6_SIN6_LEN */
+
+/*
+ * Platform specific CMake files
+ */
+#define MACHINE_TYPE "AMD64"
+/* #undef HAVE_LINUX_LARGE_PAGES */
+/* #undef HAVE_SOLARIS_LARGE_PAGES */
+/* #undef HAVE_SOLARIS_ATOMIC */
+/* #undef HAVE_SOLARIS_STYLE_GETHOST */
+#define SYSTEM_TYPE "Win32"
+/* Windows stuff, mostly functions, that have Posix analogs but named differently */
+/* #undef IPPROTO_IPV6 */
+/* #undef IPV6_V6ONLY */
+/* This should mean case insensitive file system */
+#define FN_NO_CASE_SENSE 1
+
+/*
+ * From main CMakeLists.txt
+ */
+#define MAX_INDEXES 64U
+/* #undef WITH_INNODB_MEMCACHED */
+/* #undef ENABLE_MEMCACHED_SASL */
+/* #undef ENABLE_MEMCACHED_SASL_PWDB */
+#define ENABLED_PROFILING 1
+/* #undef HAVE_ASAN */
+#define ENABLED_LOCAL_INFILE 1
+/* #undef OPTIMIZER_TRACE */
+#define DEFAULT_MYSQL_HOME "C:/Program Files/MySQL/MySQL Server 6.1"
 #define SHAREDIR "share"
-#define THREAD 1
-#define THREAD_SAFE_CLIENT 1
+#define DEFAULT_BASEDIR "C:/Program Files/MySQL/MySQL Server 6.1"
+#define MYSQL_DATADIR "C:/Program Files/MySQL/MySQL Server 6.1/data"
+#define MYSQL_KEYRINGDIR "C:/Program Files/MySQL/MySQL Server 6.1/keyring"
+#define DEFAULT_CHARSET_HOME "C:/Program Files/MySQL/MySQL Server 6.1"
+#define PLUGINDIR "C:/Program Files/MySQL/MySQL Server 6.1/lib/plugin"
+/* #undef DEFAULT_SYSCONFDIR */
+#define DEFAULT_TMPDIR ""
+#define INSTALL_SBINDIR "/bin"
+#define INSTALL_BINDIR "/bin"
+#define INSTALL_MYSQLSHAREDIR "/share"
+#define INSTALL_SHAREDIR "/share"
+#define INSTALL_PLUGINDIR "/lib/plugin"
+#define INSTALL_INCLUDEDIR "/include"
+#define INSTALL_SCRIPTDIR "/scripts"
+#define INSTALL_MYSQLDATADIR "/data"
+#define INSTALL_MYSQLKEYRINGDIR "/keyring"
+/* #undef INSTALL_PLUGINTESTDIR */
+#define INSTALL_INFODIR "/docs"
+#define INSTALL_MYSQLTESTDIR "/mysql-test"
+#define INSTALL_DOCREADMEDIR "/."
+#define INSTALL_DOCDIR "/docs"
+#define INSTALL_MANDIR "/man"
+#define INSTALL_SUPPORTFILESDIR "/support-files"
+#define INSTALL_LIBDIR "/lib"
 
-#define DEFAULT_CHARSET_HOME "C:/mysql/"
+/*
+ * Readline
+ */
+/* #undef HAVE_MBSTATE_T */
+/* #undef HAVE_LANGINFO_CODESET */
+/* #undef HAVE_WCSDUP */
+/* #undef HAVE_WCHAR_T */
+/* #undef HAVE_WINT_T */
+/* #undef HAVE_CURSES_H */
+/* #undef HAVE_NCURSES_H */
+/* #undef USE_LIBEDIT_INTERFACE */
+/* #undef HAVE_HIST_ENTRY */
+/* #undef USE_NEW_EDITLINE_INTERFACE */
+
+/*
+ * Libedit
+ */
+/* #undef HAVE_DECL_TGOTO */
+
+/*
+ * DTrace
+ */
+/* #undef HAVE_DTRACE */
+
+/*
+ * Character sets
+ */
+#define MYSQL_DEFAULT_CHARSET_NAME "latin1"
+#define MYSQL_DEFAULT_COLLATION_NAME "latin1_swedish_ci"
+#define HAVE_CHARSET_armscii8 1
+#define HAVE_CHARSET_ascii 1
+#define HAVE_CHARSET_big5 1
+#define HAVE_CHARSET_cp1250 1
+#define HAVE_CHARSET_cp1251 1
+#define HAVE_CHARSET_cp1256 1
+#define HAVE_CHARSET_cp1257 1
+#define HAVE_CHARSET_cp850 1
+#define HAVE_CHARSET_cp852 1 
+#define HAVE_CHARSET_cp866 1
+#define HAVE_CHARSET_cp932 1
+#define HAVE_CHARSET_dec8 1
+#define HAVE_CHARSET_eucjpms 1
+#define HAVE_CHARSET_euckr 1
+#define HAVE_CHARSET_gb2312 1
+#define HAVE_CHARSET_gbk 1
+#define HAVE_CHARSET_gb18030 1
+#define HAVE_CHARSET_geostd8 1
+#define HAVE_CHARSET_greek 1
+#define HAVE_CHARSET_hebrew 1
+#define HAVE_CHARSET_hp8 1
+#define HAVE_CHARSET_keybcs2 1
+#define HAVE_CHARSET_koi8r 1
+#define HAVE_CHARSET_koi8u 1
+#define HAVE_CHARSET_latin1 1
+#define HAVE_CHARSET_latin2 1
+#define HAVE_CHARSET_latin5 1
+#define HAVE_CHARSET_latin7 1
+#define HAVE_CHARSET_macce 1
+#define HAVE_CHARSET_macroman 1
+#define HAVE_CHARSET_sjis 1
+#define HAVE_CHARSET_swe7 1
+#define HAVE_CHARSET_tis620 1
+#define HAVE_CHARSET_ucs2 1
+#define HAVE_CHARSET_ujis 1
+#define HAVE_CHARSET_utf8mb4 1
+/* #undef HAVE_CHARSET_utf8mb3 */
+#define HAVE_CHARSET_utf8 1
+#define HAVE_CHARSET_utf16 1
+#define HAVE_CHARSET_utf32 1
+#define HAVE_UCA_COLLATIONS 1
+
+/*
+ * Feature set
+ */
+#define WITH_PARTITION_STORAGE_ENGINE 1
+
+/*
+ * Performance schema
+ */
+/* #undef WITH_PERFSCHEMA_STORAGE_ENGINE */
+/* #undef DISABLE_PSI_THREAD */
+/* #undef DISABLE_PSI_MUTEX */
+/* #undef DISABLE_PSI_RWLOCK */
+/* #undef DISABLE_PSI_COND */
+/* #undef DISABLE_PSI_FILE */
+/* #undef DISABLE_PSI_TABLE */
+/* #undef DISABLE_PSI_SOCKET */
+/* #undef DISABLE_PSI_STAGE */
+/* #undef DISABLE_PSI_STATEMENT */
+/* #undef DISABLE_PSI_SP */
+/* #undef DISABLE_PSI_PS */
+/* #undef DISABLE_PSI_IDLE */
+/* #undef DISABLE_PSI_STATEMENT_DIGEST */
+/* #undef DISABLE_PSI_METADATA */
+/* #undef DISABLE_PSI_MEMORY */
+/* #undef DISABLE_PSI_TRANSACTION */
+
+/*
+ * syscall
+*/
+/* #undef HAVE_SYS_THREAD_SELFID */
+/* #undef HAVE_SYS_GETTID */
+/* #undef HAVE_PTHREAD_GETTHREADID_NP */
+/* #undef HAVE_PTHREAD_SETNAME_NP */
+/* #undef HAVE_INTEGER_PTHREAD_SELF */
+
+/* Platform-specific C++ compiler behaviors we rely upon */
+
+/*
+  This macro defines whether the compiler in use needs a 'typename' keyword
+  to access the types defined inside a class template, such types are called
+  dependent types. Some compilers require it, some others forbid it, and some
+  others may work with or without it. For example, GCC requires the 'typename'
+  keyword whenever needing to access a type inside a template, but msvc
+  forbids it.
+ */
+#define HAVE_IMPLICIT_DEPENDENT_NAME_TYPING 1
+
+
+/*
+ * MySQL version
+ */
+#define DOT_FRM_VERSION 6
+#define MYSQL_VERSION_MAJOR 6
+#define MYSQL_VERSION_MINOR 1
+#define MYSQL_VERSION_PATCH 11
+#define MYSQL_VERSION_EXTRA ""
+#define PACKAGE "mysql"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_NAME "MySQL Server"
+#define PACKAGE_STRING "MySQL Server 6.1.11"
+#define PACKAGE_TARNAME "mysql"
+#define PACKAGE_VERSION "6.1.11"
+#define VERSION "6.1.11"
+#define PROTOCOL_VERSION 10
+
+/*
+ * CPU info
+ */
+#define CPU_LEVEL1_DCACHE_LINESIZE 64
+
+/*
+ * NDB
+ */
+/* #undef WITH_NDBCLUSTER_STORAGE_ENGINE */
+/* #undef HAVE_PTHREAD_SETSCHEDPARAM */
+
+/*
+ * Other
+ */
+/* #undef EXTRA_DEBUG */
+/* #undef HAVE_CHOWN */
+
+/*
+ * Hardcoded values needed by libevent/NDB/memcached
+ */
+#define HAVE_FCNTL_H 1
+#define HAVE_GETADDRINFO 1
+#define HAVE_INTTYPES_H 1
+/* libevent's select.c is not Windows compatible */
+#ifndef _WIN32
+#define HAVE_SELECT 1
+#endif
+#define HAVE_SIGNAL_H 1
+#define HAVE_STDARG_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRDUP 1
+#define HAVE_STRTOK_R 1
+#define HAVE_STRTOLL 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define SIZEOF_CHAR 1
+
+/*
+ * Needed by libevent
+ */
+/* #undef HAVE_SOCKLEN_T */
+
+/* For --secure-file-priv */
+#define DEFAULT_SECURE_FILE_PRIV_DIR "NULL"
+#define DEFAULT_SECURE_FILE_PRIV_EMBEDDED_DIR "NULL"
+/* #undef HAVE_LIBNUMA */
+
+/* For default value of --early_plugin_load */
+/* #undef DEFAULT_EARLY_PLUGIN_LOAD */
+
+#endif
