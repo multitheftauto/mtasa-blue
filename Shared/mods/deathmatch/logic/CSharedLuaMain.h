fd705081c1a27acb8ff8969cc2c25a0fcb60e54e
Sebastian Jura <sebajura1234@gmail.com>
2022-04-24 10:00:44 +0200

    remove old, unused createElement method

--- Shared/mods/deathmatch/logic/CSharedLuaMain.h
+++ Shared/mods/deathmatch/logic/CSharedLuaMain.h
@@ -11,43 +11,6 @@ public:
     CSharedLuaMain(CResource* pResource, CLuaPhysicsRigidBodyManager* pLuaPhysicsRigidBodyManager, CLuaPhysicsStaticCollisionManager* pLuaPhysicsStaticCollisionManager, CLuaPhysicsShapeManager* pLuaPhysicsShapeManager);
     ~CSharedLuaMain();
 
-#ifdef MTA_CLIENT
-    template <class T, typename... Ty>
-    T* CreateElement(Ty... args)
-    {
-        static_assert(std::is_base_of<CClientEntity, T>::value);
-        if (m_pResource)
-        {
-            T*             pElement = new T(CLuaDefs::m_pManager, INVALID_ELEMENT_ID, this, args...);
-            CElementGroup* pGroup = m_pResource->GetElementGroup();
-            if (pGroup)
-            {
-                pGroup->Add((CClientEntity*)pElement);
-            }
-            return pElement;
-        }
-        return nullptr;
-    }
-
-#else
-    template <class T, typename... Ty>
-    T* CreateElement(Ty... args)
-    {
-        static_assert(std::is_base_of<CElement, T>::value);
-        if (m_pResource)
-        {
-            T*             pElement = new T(m_pResource->GetDynamicElementRoot(), this, args...);
-            CElementGroup* pGroup = m_pResource->GetElementGroup();
-            if (pGroup)
-            {
-                pGroup->Add((CElement*)pElement);
-            }
-            return pElement;
-        }
-        return nullptr;
-    }
-#endif
-
     void UnloadScript();
 
     CLuaPhysicsRigidBodyManager*       GetPhysicsRigidBodyManager() const { return m_pLuaPhysicsRigidBodyManager; };
