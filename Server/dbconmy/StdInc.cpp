90b4587e59de7a6495e1e147f66ddc3d2f29db49
Marek Kulik <me@botder.com>
2022-05-04 19:34:35 +0200

    Clean up Server/dbconmy PCH

--- Server/dbconmy/StdInc.cpp
+++ Server/dbconmy/StdInc.cpp
@@ -1,3 +1,12 @@
+/*****************************************************************************
+ *
+ *  PROJECT:     Multi Theft Auto
+ *  LICENSE:     See LICENSE in the top level directory
+ *  FILE:        StdInc.cpp
+ *
+ *  Multi Theft Auto is available from https://multitheftauto.com/
+ *
+ *****************************************************************************/
 
 #include "StdInc.h"
 #include "SharedUtil.hpp"
