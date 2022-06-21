175e94cbefb1b5af38c5a6276295b0720080b8a9
Merlin <Merlin@users.noreply.github.com>
2022-06-21 21:13:28 +0200

    Remove inline assembly in CPathFindSA.cpp

--- Client/game_sa/CPathFindSA.cpp
+++ Client/game_sa/CPathFindSA.cpp
@@ -18,26 +18,9 @@ CNodeAddress* CPathFindSA::FindNthNodeClosestToCoors(CVector* vecCoors, int iNod
     CNodeAddress  node;
     CNodeAddress* nodeptr;
 
-    _asm
-    {
-        lea     eax, node
-        push    eax
-        push    0
-        push    iType  // Type : 0 - cars, 1 - boats
-        push    iNodeNumber // Nth
-        push    0
-        push    0
-        push    fDistance
-        push    0
-        mov     eax, vecCoors
-        push    [eax+8]
-        push    [eax+4]
-        push    [eax]
-        lea     eax, nodeptr
-        push    eax
-        mov     ecx, CLASS_CPathFind
-        call    dwFunc
-    }
+    // CPathFind::FindNthNodeClosestToCoors
+    ((CNodeAddress * (__thiscall*)(int, CNodeAddress*, CVector, unsigned char, float, bool, bool, int, bool, bool, CNodeAddress*))
+         FUNC_FindNthNodeClosestToCoors)(CLASS_CPathFind, nodeptr, *vecCoors, 0, fDistance, false, false, iNodeNumber, iType, false, &node);
 
     *pNodeAddress = node;
     return pNodeAddress;
@@ -45,32 +28,15 @@ CNodeAddress* CPathFindSA::FindNthNodeClosestToCoors(CVector* vecCoors, int iNod
 
 CPathNode* CPathFindSA::GetPathNode(CNodeAddress* node)
 {
-    DWORD dwFunc = FUNC_FindNodePointer;
     if (node->sRegion >= 0 && node->sIndex >= 0)
-    {
-        DWORD dwPathNode = 0;
-        _asm
-        {
-            push    node
-            mov     ecx, CLASS_CPathFind
-            call    dwFunc
-            mov     dwPathNode, eax
-        }
-        return (CPathNode*)dwPathNode;
-    }
-    return NULL;
+        return ((CPathNode * (__thiscall*)(int, CNodeAddress*)) FUNC_FindNodePointer)(CLASS_CPathFind, node);
+
+    return nullptr;
 }
 
 CVector* CPathFindSA::GetNodePosition(CPathNode* pNode, CVector* pPosition)
 {
-    DWORD dwFunc = FUNC_CPathNode_GetCoors;
-    _asm
-    {
-        push    pPosition
-        mov     ecx, pNode
-        call    dwFunc
-    }
-    return pPosition;
+    return ((CVector * (__thiscall*)(CPathNode*, CVector*)) FUNC_CPathNode_GetCoors)(pNode, pPosition);
 }
 
 CVector* CPathFindSA::GetNodePosition(CNodeAddress* pNode, CVector* pPosition)
@@ -89,24 +55,8 @@ void CPathFindSA::SwitchRoadsOffInArea(CVector* vecAreaCorner1, CVector* vecArea
     float fY2 = vecAreaCorner2->fY;
     float fZ2 = vecAreaCorner2->fZ;
 
-    DWORD dwEnable = !bEnable;
-
-    DWORD dwFunc = FUNC_SwitchRoadsOffInArea;
-
-    _asm
-    {
-        mov     ecx, CLASS_CPathFind
-        push    0
-        push    1
-        push    dwEnable
-        push    fZ2
-        push    fZ1
-        push    fY2
-        push    fY1
-        push    fX2
-        push    fX1
-        call    dwFunc
-    }
+    ((void(__thiscall*)(int, float, float, float, float, float, float, bool, bool, bool))FUNC_SwitchRoadsOffInArea)(CLASS_CPathFind, fX1, fX2, fY1, fY2, fZ1,
+                                                                                                                    fZ2, bEnable, true, false);
 }
 
 void CPathFindSA::SwitchPedRoadsOffInArea(CVector* vecAreaCorner1, CVector* vecAreaCorner2, bool bEnable)
@@ -119,23 +69,8 @@ void CPathFindSA::SwitchPedRoadsOffInArea(CVector* vecAreaCorner1, CVector* vecA
     float fY2 = vecAreaCorner2->fY;
     float fZ2 = vecAreaCorner2->fZ;
 
-    DWORD dwEnable = !bEnable;
-
-    DWORD dwFunc = FUNC_SwitchPedRoadsOffInArea;
-
-    _asm
-    {
-        mov     ecx, CLASS_CPathFind
-        push    0
-        push    dwEnable
-        push    fZ2
-        push    fZ1
-        push    fY2
-        push    fY1
-        push    fX2
-        push    fX1
-        call    dwFunc
-    }
+    ((void(__thiscall*)(int, float, float, float, float, float, float, bool, bool))FUNC_SwitchPedRoadsOffInArea)(CLASS_CPathFind, fX1, fX2, fY1, fY2, fZ1, fZ2,
+                                                                                                                 bEnable, false);
 }
 
 void CPathFindSA::SetPedDensity(float fPedDensity)
