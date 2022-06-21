f29bd52b18f06b13912dea1e94e1b8a5a95b9d2a
Merlin <Merlin@users.noreply.github.com>
2022-06-21 20:37:53 +0200

    Remove inline assembly in CKeyGenSA.cpp

--- Client/game_sa/CKeyGenSA.cpp
+++ Client/game_sa/CKeyGenSA.cpp
@@ -13,58 +13,24 @@
 
 unsigned int CKeyGenSA::GetKey(const char* szString, int iLength)
 {
-    unsigned int uiReturn;
-    DWORD        dwFunc = FUNC_CKeyGen_GetKey_len;
-    _asm
-    {
-        push    iLength
-        push    szString
-        call    dwFunc
-        add     esp, 0x8
-        mov     uiReturn, eax
-    }
-    return uiReturn;
+    // CKeyGen::GetKey
+    return ((unsigned int(__cdecl*)(const char*, int))FUNC_CKeyGen_GetKey_len)(szString, iLength);
 }
 
 unsigned int CKeyGenSA::GetKey(const char* szString)
 {
-    unsigned int uiReturn;
-    DWORD        dwFunc = FUNC_CKeyGen_GetKey;
-    _asm
-    {
-        push    szString
-        call    dwFunc
-        add     esp, 0x4
-        mov     uiReturn, eax
-    }
-    return uiReturn;
+    // CKeyGen::GetKey
+    return ((unsigned int(__cdecl*)(const char*))FUNC_CKeyGen_GetKey)(szString);
 }
 
 unsigned int CKeyGenSA::GetUppercaseKey(const char* szString)
 {
-    unsigned int uiReturn;
-    DWORD        dwFunc = FUNC_CKeyGen_GetUppercaseKey;
-    _asm
-    {
-        push    szString
-        call    dwFunc
-        add     esp, 0x4
-        mov     uiReturn, eax
-    }
-    return uiReturn;
+    // CKeyGen::GetUppercaseKey
+    return ((unsigned int(__cdecl*)(const char*))FUNC_CKeyGen_GetUppercaseKey)(szString);
 }
 
 unsigned int CKeyGenSA::AppendStringToKey(unsigned int uiKey, const char* szString)
 {
-    unsigned int uiReturn;
-    DWORD        dwFunc = FUNC_CKeyGen_AppendStringToKey;
-    _asm
-    {
-        push    szString
-        push    uiKey
-        call    dwFunc
-        add     esp, 0x8
-        mov     uiReturn, eax
-    }
-    return uiReturn;
+    // CKeyGen::AppendStringToKey
+    return ((unsigned int(__cdecl*)(unsigned int, const char*))FUNC_CKeyGen_AppendStringToKey)(uiKey, szString);
 }
