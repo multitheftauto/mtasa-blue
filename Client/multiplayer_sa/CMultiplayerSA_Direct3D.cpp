5207a314272fda9b54443edd30efb11e6e846dd6
Sam <samr46@users.noreply.github.com>
2022-07-10 21:59:28 +0300

    Fix #1905: Refresh rate limited at 60Hz in full screen mode (#2664)
    Set fullscreen refresh rate to display adapter's value

--- Client/multiplayer_sa/CMultiplayerSA_Direct3D.cpp
+++ Client/multiplayer_sa/CMultiplayerSA_Direct3D.cpp
@@ -47,6 +47,11 @@ void _cdecl OnPreCreateDevice(IDirect3D9* pDirect3D, UINT Adapter, D3DDEVTYPE De
     ms_BehaviorFlags = *BehaviorFlags;
     ms_pPresentationParameters = pPresentationParameters;
     ms_ppReturnedDeviceInterface = ppReturnedDeviceInterface;
+
+    // Set fullscreen refresh rate to display adapter's value
+    D3DDISPLAYMODE DisplayMode;
+    if (!FAILED(ms_pDirect3D->GetAdapterDisplayMode(Adapter, &DisplayMode)))
+        ms_pPresentationParameters->FullScreen_RefreshRateInHz = DisplayMode.RefreshRate;
 }
 
 // Hook info
