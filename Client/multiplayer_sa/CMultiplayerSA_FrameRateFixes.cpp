f597c46a45dea26742a680b5a2ab56d6dcb02368
Merlin <Merlin@users.noreply.github.com>
2022-08-02 13:03:13 +0200

    Update CTimer::m_FrameCounter only every 33+ ms (#2684)
    Instead of updating every frame we now update only every 33+ ms.
    Remove now unnecessary CHud::RenderHealthBar framerate fix.

--- Client/multiplayer_sa/CMultiplayerSA_FrameRateFixes.cpp
+++ Client/multiplayer_sa/CMultiplayerSA_FrameRateFixes.cpp
@@ -10,6 +10,9 @@
 
 #include "StdInc.h"
 
+static bool bWouldBeNewFrame = false;
+static unsigned int nLastFrameTime = 0;
+
 constexpr float kOriginalTimeStep = 50.0f / 30.0f;
 
 #define HOOKPOS_CTaskSimpleUseGun__SetMoveAnim 0x61E4F2
@@ -30,7 +33,34 @@ void _declspec(naked) HOOK_CTaskSimpleUseGun__SetMoveAnim()
     }
 }
 
+#define HOOKPOS_CTimer__Update 0x561C5D
+#define HOOKSIZE_CTimer__Update 0xE
+void _declspec(naked) HOOK_CTimer__Update()
+{
+    _asm {
+        add esp, 0x4
+
+        mov bWouldBeNewFrame, 0
+        mov eax, nLastFrameTime
+        add eax, 33                 // 33 = 1000 / 30
+        mov ecx, ds:[0xB7CB84]      // CTimer::m_snTimeInMilliseconds
+        cmp ecx, eax
+        jb skip
+
+        mov bWouldBeNewFrame, 1
+        mov nLastFrameTime, ecx
+        mov eax, ds:[0xB7CB4C]      // CTimer::m_FrameCounter
+        inc eax
+        mov ds:[0xB7CB4C], eax      // CTimer::m_FrameCounter
+
+    skip:
+        add esp, 0xC
+        ret
+    }
+}
+
 void CMultiplayerSA::InitHooks_FrameRateFixes()
 {
     EZHookInstall(CTaskSimpleUseGun__SetMoveAnim);
+    EZHookInstall(CTimer__Update);
 }
