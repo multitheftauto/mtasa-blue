e84a15e1ea4d47769e14917243ddb2eac54ae5ee
Lpsd <40902730+Lpsd@users.noreply.github.com>
2022-09-11 15:43:55 +0100

    Add isCapsLockEnabled client function (PR #2708)

--- Client/mods/deathmatch/logic/luadefs/CLuaClientDefs.cpp
+++ Client/mods/deathmatch/logic/luadefs/CLuaClientDefs.cpp
@@ -15,13 +15,16 @@
 
 void CLuaClientDefs::LoadFunctions()
 {
-    constexpr static const std::pair<const char*, lua_CFunction> functions[]{{"setTransferBoxVisible", ArgumentParser<SetTransferBoxVisible>},
-                                                                             {"isTransferBoxVisible", ArgumentParser<IsTransferBoxVisible>},
-                                                                             {"isTransferBoxAlwaysVisible", ArgumentParser<IsTransferBoxAlwaysVisible>},
-                                                                             {"showChat", ArgumentParserWarn<false, ShowChat>},
-                                                                             {"isChatVisible", ArgumentParserWarn<false, IsChatVisible>},
-                                                                             {"isChatInputBlocked", ArgumentParser<IsChatInputBlocked>},
-                                                                             {"clearDebugBox", ArgumentParser<ClearDebug>}};
+    constexpr static const std::pair<const char*, lua_CFunction> functions[]{
+        {"setTransferBoxVisible", ArgumentParser<SetTransferBoxVisible>},
+        {"isTransferBoxVisible", ArgumentParser<IsTransferBoxVisible>},
+        {"isTransferBoxAlwaysVisible", ArgumentParser<IsTransferBoxAlwaysVisible>},
+        {"showChat", ArgumentParserWarn<false, ShowChat>},
+        {"isChatVisible", ArgumentParserWarn<false, IsChatVisible>},
+        {"isChatInputBlocked", ArgumentParser<IsChatInputBlocked>},
+        {"clearDebugBox", ArgumentParser<ClearDebug>},
+        {"isCapsLockEnabled", ArgumentParser<IsCapsLockEnabled>}
+    };
 
     for (const auto& [name, func] : functions)
         CLuaCFunctions::AddFunction(name, func);
@@ -67,4 +70,9 @@ bool CLuaClientDefs::ClearDebug()
 {
     g_pCore->DebugClear();
     return true;
+}
+
+bool CLuaClientDefs::IsCapsLockEnabled()
+{
+    return ((::GetKeyState(VK_CAPITAL) & 0x0001) != 0);
 }
