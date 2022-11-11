3845bc36602a95f46d89d9a8c5ac22cdd3dd50e7
Sebastian <sebastian.salata783@gmail.com>
2022-11-11 21:42:57 +0000

    Update default _beta_qc_rightclick_command to empty string

--- Client/core/CClientVariables.cpp
+++ Client/core/CClientVariables.cpp
@@ -353,7 +353,7 @@ void CClientVariables::LoadDefaults()
     DEFAULT("browser_remote_javascript", true);                                       // Execute javascript on remote websites?
     DEFAULT("filter_duplicate_log_lines", true);                                      // Filter duplicate log lines for debug view and clientscript.log
     DEFAULT("always_show_transferbox", false);            // Should the transfer box always be visible for downloads? (and ignore scripted control)
-    DEFAULT("_beta_qc_rightclick_command", _S("reconnect"));            // Command to run when right clicking quick connect (beta - can be removed at any time)
+    DEFAULT("_beta_qc_rightclick_command", _S(""));            // Command to run when right clicking quick connect (beta - can be removed at any time)
 
     if (!Exists("locale"))
     {
