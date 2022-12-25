a527e3fba68a9ae493282dfd6e93b28145e8bea0
patrikjuvonen <22572159+patrikjuvonen@users.noreply.github.com>
2022-12-25 14:10:39 +0200

    Add Detours to software credits
    [ci skip]

--- Client/core/CCredits.cpp
+++ Client/core/CCredits.cpp
@@ -266,6 +266,7 @@ CCredits::CCredits()
         "CRC-32 (https://bochs.sourceforge.io/)\n"
         "cryptopp (https://cryptopp.com/)\n"
         "cURL (https://curl.se/)\n"
+        "Detours (https://github.com/microsoft/Detours)\n"
         "Embedded HTTP Server (https://ehs.fritz-elfert.de/)\n"
         "inspect.lua by kikito (https://github.com/kikito/inspect.lua)\n"
         "libpcre (https://www.pcre.org/)\n"
