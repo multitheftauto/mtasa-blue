57e91d92199798ebadc7e10741f037db9e1a21aa
patrikjuvonen <22572159+patrikjuvonen@users.noreply.github.com>
2022-12-25 16:12:08 +0200

    Add nvapi to software credits
    [ci skip]

--- Client/core/CCredits.cpp
+++ Client/core/CCredits.cpp
@@ -279,6 +279,7 @@ CCredits::CCredits()
         "lunasvg (https://github.com/sammycage/lunasvg)\n"
         "mbedtls (https://github.com/Mbed-TLS/mbedtls)\n"
         "mysql-connector-c (https://downloads.mysql.com/archives/c-c/)\n"
+        "nvapi (https://developer.nvidia.com/rtx/path-tracing/nvapi/get-started)\n"
         "PortAudio (http://www.portaudio.com/)\n"
         "RakNet (http://www.jenkinssoftware.com/)\n"
         "SA Limit adjuster source code (Credits to Sacky)\n"
