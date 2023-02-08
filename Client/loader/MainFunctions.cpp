9aec6d663258b1b8e92b40c2dbb8ccd18b52b364
Marek Kulik <me@botder.com>
2023-02-08 21:28:31 +0100

    Remove superfluous files from checks in loader

--- Client/loader/MainFunctions.cpp
+++ Client/loader/MainFunctions.cpp
@@ -798,10 +798,6 @@ void CheckDataFiles()
                                     "MTA\\bassopus.dll",
                                     "MTA\\basswma.dll",
                                     "MTA\\tags.dll",
-                                    "MTA\\vea.dll",
-                                    "MTA\\vog.dll",
-                                    "MTA\\vvo.dll",
-                                    "MTA\\vvof.dll",
                                     "MTA\\sa.dat",
                                     "MTA\\xinput1_3_mta.dll",
                                     "MTA\\XInput9_1_0_mta.dll"};
@@ -871,12 +867,10 @@ void CheckDataFiles()
                               {"65F79B61AD377DE06D88FE40B1D70538", "bassflac.dll"},        {"9AAF837944A9763CD914AC7D31ABC8C7", "bassmidi.dll"},
                               {"D31DA7583083C1370F3C6B9C15F363CC", "bassmix.dll"},         {"75FCD499EE86AC9B234FF837D2080CDA", "bassopus.dll"},
                               {"07852D9E8DB268D0187EDDBDD25A29E9", "basswebm.dll"},        {"1507C60C02E159B5FB247FEC6B209B09", "basswma.dll"},
-                              {"6E2C5DCF4EE973E69ECA39288D20C436", "tags.dll"},            {"309D860FC8137E5FE9E7056C33B4B8BE", "vea.dll"},
-                              {"0602F672BA595716E64EC4040E6DE376", "vog.dll"},             {"B37D7DF4A1430DB65AD3EA84801F9EC3", "vvo.dll"},
+                              {"6E2C5DCF4EE973E69ECA39288D20C436", "tags.dll"},            {"D439E8EDD8C93D7ADE9C04BCFE9197C6", "sa.dat"},
                               {"B33B21DB610116262D906305CE65C354", "D3DCompiler_42.dll"},  {"1C9B45E87528B8BB8CFA884EA0099A85", "d3dcompiler_43.dll"},
                               {"C6A44FC3CF2F5801561804272217B14D", "D3DX9_42.dll"},        {"E1677EC0E21E27405E65E31419980348", "d3dcompiler_47.dll"},
-                              {"47FF3EE45DE53528F1AFD9F5982DF8C7", "vvof.dll"},            {"F137D5BE2D8E76597B3F269B73DBB6A6", "XInput9_1_0_mta.dll"},
-                              {"D439E8EDD8C93D7ADE9C04BCFE9197C6", "sa.dat"}};
+                              {"F137D5BE2D8E76597B3F269B73DBB6A6", "XInput9_1_0_mta.dll"}};
 
     for (const auto& item : integrityCheckList)
     {
