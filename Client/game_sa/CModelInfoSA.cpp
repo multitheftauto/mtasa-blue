multitheftauto/mtasa-blue

commit ec314df0362829ed52a52aa3ac0b1302c2097c3a

Add interface for model flags (#1714)
Co-authored-by: Nikita Obrekht <obrekht@gmail.com>
Co-authored-by: Vladislav Nikolaevich <v.nikolaevich@redbrixwall.com>

Client/game_sa/CModelInfoSA.cpp
@@ -28,11 +28,48 @@ CBaseModelInfoSAInterface** ppModelInfo = (CBaseModelInfoSAInterface**)ARRAY_Mod
 
 std::map<unsigned short, int>                                         CModelInfoSA::ms_RestreamTxdIDMap;
 std::map<DWORD, float>                                                CModelInfoSA::ms_ModelDefaultLodDistanceMap;
+std::map<DWORD, unsigned short>                                       CModelInfoSA::ms_ModelDefaultFlagsMap;
 std::map<DWORD, BYTE>                                                 CModelInfoSA::ms_ModelDefaultAlphaTransparencyMap;
 std::unordered_map<std::uint32_t, std::map<eVehicleDummies, CVector>> CModelInfoSA::ms_ModelDefaultDummiesPosition;
 std::map<CTimeInfoSAInterface*, CTimeInfoSAInterface*>                CModelInfoSA::ms_ModelDefaultModelTimeInfo;
 std::unordered_map<DWORD, unsigned short>                             CModelInfoSA::ms_OriginalObjectPropertiesGroups;
 std::unordered_map<DWORD, std::pair<float, float>>                    CModelInfoSA::ms_VehicleModelDefaultWheelSizes;
+
+union tIdeFlags
+{
+    struct
+    {
+        char bIsRoad : 1;
+        char bFlag2 : 1;
+        char bDrawLast : 1;
+        char bAdditive : 1;
+        char bFlag5 : 1;
+        char bFlag6 : 1;
+        char bNoZBufferWrite : 1;
+        char bDontReceiveShadows : 1;
+
+        char bFlag9 : 1;
+        char bIsGlassType1 : 1;
+        char bIsGlassType2 : 1;
+        char bIsGarageDoor : 1;
+        char bIsDamagable : 1;
+        char bIsTree : 1;
+        char bIsPalm : 1;
+        char bDontCollideWithFlyer : 1;
+
+        char bFlag17 : 1;
+        char bFlag18 : 1;
+        char bFlag19 : 1;
+        char bFlag20 : 1;
+        char bIsTag : 1;
+        char bDisableBackfaceCulling : 1;
+        char bIsBreakableStatue : 1;
+        char bFlag24 : 1;
+
+        char cPad : 8;
+    };
+    unsigned int uiFlags;
+};
 
 static constexpr uintptr_t vftable_CVehicleModelInfo = 0x85C5C8u;
 static constexpr size_t    RESOURCE_ID_COL = 25000;
@@ -437,19 +474,245 @@ bool CModelInfoSA::DoIsLoaded()
     return bLoaded;
 }
 
-BYTE CModelInfoSA::GetFlags()
+unsigned short CModelInfoSA::GetFlags()
 {
-    DWORD dwFunc = FUNC_GetModelFlags;
-    DWORD ModelID = m_dwModelID;
-    BYTE  bFlags = 0;
-    _asm
+    return ppModelInfo[m_dwModelID]->usFlags;
+}
+
+unsigned short CModelInfoSA::GetOriginalFlags()
+{
+    if (MapContains(ms_ModelDefaultFlagsMap, m_dwModelID))
+        return MapGet(ms_ModelDefaultFlagsMap, m_dwModelID);
+
+    return ppModelInfo[m_dwModelID]->usFlags;
+}
+
+void CModelInfoSA::SetFlags(unsigned short usFlags)
+{
+    m_pInterface = ppModelInfo[m_dwModelID];
+    if (!m_pInterface)
+        return;
+
+    // Save default value if not done yet
+    if (!MapContains(ms_ModelDefaultFlagsMap, m_dwModelID))
+        MapSet(ms_ModelDefaultFlagsMap, m_dwModelID, m_pInterface->usFlags);
+
+    // Don't change bIsColLoaded flag
+    usFlags &= 0xFF7F;                                  // Disable flag in input
+    usFlags |= m_pInterface->usFlags & 0x80;            // Apply current bIsColLoaded flag
+
+    m_pInterface->usFlags = usFlags;
+}
+
+void CModelInfoSA::SetIdeFlags(unsigned int uiFlags)
+{
+    m_pInterface = ppModelInfo[m_dwModelID];
+    if (!m_pInterface)
+        return;
+
+    // Save default value if not done yet
+    if (!MapContains(ms_ModelDefaultFlagsMap, m_dwModelID))
+        MapSet(ms_ModelDefaultFlagsMap, m_dwModelID, m_pInterface->usFlags);
+
+    tIdeFlags ideFlags;
+    ideFlags.uiFlags = uiFlags;
+
+    // Default value is 0xC0 (bIsColLoaded + bIsBackfaceCulled)
+    // But bIsColLoaded should not be changed
+    m_pInterface->usFlags &= 0x80;            // Reset all flags except bIsColLoaded
+    m_pInterface->bIsBackfaceCulled = true;
+
+    // setBaseModelInfoFlags
+    if (ideFlags.bDrawLast)
     {
-        push    ModelID
-        call    dwFunc
-        add     esp, 4
-        mov     bFlags, al
+        m_pInterface->bAlphaTransparency = true;
+        m_pInterface->bAdditiveRender = true;
     }
-    return bFlags;
+
+    m_pInterface->bAdditiveRender = ideFlags.bAdditive;
+    m_pInterface->bDontWriteZBuffer = ideFlags.bNoZBufferWrite;
+    m_pInterface->bDontCastShadowsOn = ideFlags.bDontReceiveShadows;
+    m_pInterface->bIsBackfaceCulled = !ideFlags.bDisableBackfaceCulling;
+
+    switch (GetModelType())
+    {
+        case eModelInfoType::ATOMIC:
+        case eModelInfoType::TIME:
+        {
+            // SetAtomicModelInfoFlags
+            m_pInterface->bIsRoad = ideFlags.bIsRoad;
+
+            m_pInterface->bDontCollideWithFlyer = ideFlags.bDontCollideWithFlyer;
+
+            if (ideFlags.bFlag20)
+                m_pInterface->eSpecialModelType = eModelSpecialType::CRANE;
+            else if (ideFlags.bFlag24)
+                m_pInterface->eSpecialModelType = eModelSpecialType::UNKNOW_1;
+            else if (ideFlags.bIsBreakableStatue)
+                m_pInterface->eSpecialModelType = eModelSpecialType::BREAKABLE_STATUE;
+            else if (ideFlags.bIsTag)
+                m_pInterface->eSpecialModelType = eModelSpecialType::TAG;
+            else if (ideFlags.bIsPalm)
+                m_pInterface->eSpecialModelType = eModelSpecialType::PALM;
+            else if (ideFlags.bIsTree)
+                m_pInterface->eSpecialModelType = eModelSpecialType::TREE;
+            else if (ideFlags.bIsGarageDoor)
+                m_pInterface->eSpecialModelType = eModelSpecialType::GARAGE_DOOR;
+            else if (ideFlags.bIsGlassType2)
+                m_pInterface->eSpecialModelType = eModelSpecialType::GLASS_2;
+            else if (ideFlags.bIsGlassType1)
+                m_pInterface->eSpecialModelType = eModelSpecialType::GLASS_1;
+            break;
+        }
+        case eModelInfoType::CLUMP:
+        {
+            m_pInterface->bAdditiveRender = ideFlags.bFlag6;
+            break;
+        }
+        default:
+            break;
+    }
+
+}
+
+void CModelInfoSA::SetIdeFlag(eModelIdeFlag eIdeFlag, bool bState)
+{
+    m_pInterface = ppModelInfo[m_dwModelID];
+    if (!m_pInterface)
+        return;
+
+    switch (eIdeFlag)
+    {
+        case eModelIdeFlag::IS_ROAD:
+            m_pInterface->bIsRoad = bState;
+            break;
+        case eModelIdeFlag::DRAW_LAST:
+            m_pInterface->bAlphaTransparency = bState;
+            if (bState)
+                m_pInterface->bAdditiveRender = true;
+            break;
+        case eModelIdeFlag::ADDITIVE:
+            m_pInterface->bAdditiveRender = bState;
+            break;
+        case eModelIdeFlag::IGNORE_LIGHTING:
+            m_pInterface->bAdditiveRender = bState;
+            break;
+        case eModelIdeFlag::NO_ZBUFFER_WRITE:
+            m_pInterface->bDontWriteZBuffer = bState;
+            break;
+        case eModelIdeFlag::DONT_RECEIVE_SHADOWS:
+            m_pInterface->bDontCastShadowsOn = bState;
+            break;
+        case eModelIdeFlag::IS_GLASS_TYPE_1:
+            SetModelSpecialType(eModelSpecialType::GLASS_1, bState);
+            break;
+        case eModelIdeFlag::IS_GLASS_TYPE_2:
+            SetModelSpecialType(eModelSpecialType::GLASS_2, bState);
+            break;
+        case eModelIdeFlag::IS_GARAGE_DOOR:
+            SetModelSpecialType(eModelSpecialType::GARAGE_DOOR, bState);
+            break;
+        case eModelIdeFlag::IS_TREE:
+            SetModelSpecialType(eModelSpecialType::TREE, bState);
+            break;
+        case eModelIdeFlag::IS_PALM:
+            SetModelSpecialType(eModelSpecialType::PALM, bState);
+            break;
+        case eModelIdeFlag::IS_TAG:
+            SetModelSpecialType(eModelSpecialType::TAG, bState);
+            break;
+        case eModelIdeFlag::IS_BREAKABLE_STATUE:
+            SetModelSpecialType(eModelSpecialType::BREAKABLE_STATUE, bState);
+            break;
+        case eModelIdeFlag::IS_CRANE:
+            SetModelSpecialType(eModelSpecialType::CRANE, bState);
+            break;
+        case eModelIdeFlag::IS_DAMAGABLE:
+            // Can't set
+            break;
+        case eModelIdeFlag::DOES_NOT_COLLIDE_WITH_FLYER:
+            m_pInterface->bDontCollideWithFlyer = bState;
+            break;
+        case eModelIdeFlag::DISABLE_BACKFACE_CULLING:
+            m_pInterface->bIsBackfaceCulled = !bState;
+            break;
+        default:
+            break;
+    }
+}
+
+bool CModelInfoSA::GetIdeFlag(eModelIdeFlag eIdeFlag)
+{
+    m_pInterface = ppModelInfo[m_dwModelID];
+    if (!m_pInterface)
+        return false;
+
+    switch (eIdeFlag)
+    {
+        case eModelIdeFlag::IS_ROAD:
+            return m_pInterface->bIsRoad;
+        case eModelIdeFlag::DRAW_LAST:
+           return m_pInterface->bAlphaTransparency;
+        case eModelIdeFlag::ADDITIVE:
+            return m_pInterface->bAdditiveRender;
+        case eModelIdeFlag::IGNORE_LIGHTING:
+            return m_pInterface->bAdditiveRender;
+        case eModelIdeFlag::NO_ZBUFFER_WRITE:
+            return m_pInterface->bDontWriteZBuffer;
+        case eModelIdeFlag::DONT_RECEIVE_SHADOWS:
+            return m_pInterface->bDontCastShadowsOn;
+        case eModelIdeFlag::IS_GLASS_TYPE_1:
+            return m_pInterface->eSpecialModelType == eModelSpecialType::GLASS_1;
+        case eModelIdeFlag::IS_GLASS_TYPE_2:
+            return m_pInterface->eSpecialModelType == eModelSpecialType::GLASS_2;
+        case eModelIdeFlag::IS_GARAGE_DOOR:
+            return m_pInterface->eSpecialModelType == eModelSpecialType::GARAGE_DOOR;
+        case eModelIdeFlag::IS_TREE:
+            return m_pInterface->eSpecialModelType == eModelSpecialType::TREE;
+        case eModelIdeFlag::IS_PALM:
+            return m_pInterface->eSpecialModelType == eModelSpecialType::PALM;
+        case eModelIdeFlag::IS_TAG:
+            return m_pInterface->eSpecialModelType == eModelSpecialType::TAG;
+        case eModelIdeFlag::IS_BREAKABLE_STATUE:
+            return m_pInterface->eSpecialModelType == eModelSpecialType::BREAKABLE_STATUE;
+        case eModelIdeFlag::IS_CRANE:
+            return m_pInterface->eSpecialModelType == eModelSpecialType::CRANE;
+        case eModelIdeFlag::IS_DAMAGABLE:
+            return ((bool (*)())m_pInterface->VFTBL->AsDamageAtomicModelInfoPtr)();
+        case eModelIdeFlag::DOES_NOT_COLLIDE_WITH_FLYER:
+            return m_pInterface->bDontCollideWithFlyer;
+        case eModelIdeFlag::DISABLE_BACKFACE_CULLING:
+            return !m_pInterface->bIsBackfaceCulled;
+        default:
+            return false;
+    }
+}
+
+void CModelInfoSA::SetModelSpecialType(eModelSpecialType eType, bool bState)
+{
+    if (bState)
+        m_pInterface->eSpecialModelType = eType;
+    else if (m_pInterface->eSpecialModelType == eType)
+        m_pInterface->eSpecialModelType = eModelSpecialType::NONE;
+}
+
+void CModelInfoSA::StaticResetFlags()
+{
+    // Restore default values
+    for (std::map<DWORD, unsigned short>::const_iterator iter = ms_ModelDefaultFlagsMap.begin(); iter != ms_ModelDefaultFlagsMap.end(); ++iter)
+    {
+        CBaseModelInfoSAInterface* pInterface = ppModelInfo[iter->first];
+        if (pInterface)
+        {
+            // Don't change bIsColLoaded flag
+            ushort usFlags = iter->second;
+            usFlags &= 0xFF7F;
+            usFlags |= pInterface->usFlags & 0x80; 
+            pInterface->usFlags = usFlags;
+        }
+    }
+
+    ms_ModelDefaultFlagsMap.clear();
 }
 
 CBoundingBox* CModelInfoSA::GetBoundingBox()
@@ -1238,7 +1501,7 @@ void CModelInfoSA::SetColModel(CColModel* pColModel)
         if (!m_pOriginalColModelInterface)
         {
             m_pOriginalColModelInterface = m_pInterface->pColModel;
-            m_originalFlags = m_pInterface->flags;
+            m_originalFlags = GetOriginalFlags();
         }
 
         // Apply some low-level hacks
@@ -1249,7 +1512,6 @@ void CModelInfoSA::SetColModel(CColModel* pColModel)
 
         // SetColModel sets bDoWeOwnTheColModel if the last parameter is truthy
         m_pInterface->bDoWeOwnTheColModel = false;
-        m_pInterface->bCollisionWasStreamedWithModel = false;
 
         // Fix random foliage on custom collisions by calling CPlantMgr::SetPlantFriendlyFlagInAtomicMI
         (reinterpret_cast<void(__cdecl*)(CBaseModelInfoSAInterface*)>(0x5DB650))(m_pInterface);
@@ -1283,7 +1545,7 @@ void CModelInfoSA::RestoreColModel()
         CBaseModelInfo_SetColModel(m_pInterface, m_pOriginalColModelInterface, true);
         CColAccel_addCacheCol(m_dwModelID, m_pInterface->pColModel);
 
-        m_pInterface->flags = m_originalFlags;
+        m_pInterface->usFlags = m_originalFlags;
 
         // Force the game to load the original collision model data, if we applied a custom collision model before
         // there was any object/building, which would've provoked CColStore to request it.

Client/game_sa/CGameSA.cpp
@@ -896,6 +896,11 @@ void CGameSA::ResetModelLodDistances()
     CModelInfoSA::StaticResetLodDistances();
 }
 
+void CGameSA::ResetModelFlags()
+{
+    CModelInfoSA::StaticResetFlags();
+}
+
 void CGameSA::ResetModelTimes()
 {
     CModelInfoSA::StaticResetModelTimes();

Client/game_sa/CGameSA.h
@@ -248,6 +248,7 @@ public:
     CWeaponStat* CreateWeaponStat(eWeaponType weaponType, eWeaponSkill weaponSkill);
     void         FlushPendingRestreamIPL();
     void         ResetModelLodDistances();
+    void         ResetModelFlags();
     void         ResetAlphaTransparencies();
     void         DisableVSync();
     void         ResetModelTimes();

Client/game_sa/CModelInfoSA.h
@@ -40,7 +40,6 @@ static void* ARRAY_ModelLoaded = (char*)CStreaming__ms_aInfoForModel + 0x10;
 static void* ARRAY_ModelInfo = *(void**)(0x403DA4 + 3);
 #define     CLASS_CText                     0xC1B340
 #define     FUNC_CText_Get                  0x6A0050
-#define     FUNC_GetModelFlags              0x4044E0
 #define     FUNC_GetBoundingBox             0x4082F0
 
 #define     FUNC_RemoveRef                  0x4C4BB0
@@ -134,6 +133,20 @@ class CVehicleModelInfo_SA_VTBL : public CClumpModelInfo_SA_VTBL
 {
 };
 
+enum class eModelSpecialType : unsigned char
+{
+    NONE = 0,
+    TREE = 1,
+    PALM = 2,
+    GLASS_1 = 4,
+    GLASS_2 = 5,
+    TAG = 6,
+    GARAGE_DOOR = 7,
+    CRANE = 9,                // Not present in IDE but used in gta
+    UNKNOW_1 = 10,            // Read only
+    BREAKABLE_STATUE = 11,
+};
+
 class CBaseModelInfoSAInterface
 {
 public:
@@ -152,28 +165,24 @@ public:
     // Flags used by CBaseModelInfo
     union
     {
-        std::uint16_t flags;
-
         struct
         {
-            unsigned char bHasBeenPreRendered : 1;            // +18
-            unsigned char bAlphaTransparency : 1;
-            unsigned char bIsLod : 1;
-            unsigned char bDontWriteZBuffer : 1;
-            unsigned char bDontCastShadowsOn : 1;
-            unsigned char bDrawAdditive : 1;
-            unsigned char bDrawLast : 1;
-            unsigned char bDoWeOwnTheColModel : 1;
-
-            unsigned char dwUnknownFlag25 : 1;            // +19
-            unsigned char dwUnknownFlag26 : 1;
-            unsigned char dwUnknownFlag27 : 1;
-            unsigned char bSwaysInWind : 1;
-            unsigned char bCollisionWasStreamedWithModel : 1;            // CClumpModelInfo::SetCollisionWasStreamedWithModel(unsigned int)
-            unsigned char bDontCollideWithFlyer : 1;                     // CAtomicModelInfo::SetDontCollideWithFlyer(unsigned int)
-            unsigned char bHasComplexHierarchy : 1;                      // CClumpModelInfo::SetHasComplexHierarchy(unsigned int)
-            unsigned char bWetRoadReflection : 1;                        // CAtomicModelInfo::SetWetRoadReflection(unsigned int)
+            unsigned char      bHasBeenPreRendered : 1;            // we use this because we need to apply changes only once
+            unsigned char      bAlphaTransparency : 1;             // bDrawLast
+            unsigned char      bAdditiveRender : 1;
+            unsigned char      bDontWriteZBuffer : 1;
+            unsigned char      bDontCastShadowsOn : 1;
+            unsigned char      bDoWeOwnTheColModel : 1;
+            unsigned char      bIsBackfaceCulled : 1;
+            unsigned char      bIsColLoaded : 1;
+            unsigned char      bIsRoad : 1;
+            unsigned char      bHasComplexHierarchy : 1;
+            unsigned char      bDontCollideWithFlyer : 1;
+            eModelSpecialType  eSpecialModelType : 4;
+            unsigned char      bWetRoadReflection : 1;            // Used for tags 
         };
+
+        unsigned short usFlags;
     };
 
     CColModelSAInterface* pColModel;            // +20      CColModel: public CBoundingBox
@@ -223,6 +232,7 @@ public:
     // +762 = Array of WORD containing something relative to paintjobs
     // +772 = Anim file index
 };
+static_assert(sizeof(CBaseModelInfoSAInterface) == 0x20, "Invalid size for CBaseModelInfoSAInterface");
 
 struct CTimeInfoSAInterface
 {
@@ -302,6 +312,7 @@ protected:
     RpClump*                                                                     m_pCustomClump;
     static std::map<unsigned short, int>                                         ms_RestreamTxdIDMap;
     static std::map<DWORD, float>                                                ms_ModelDefaultLodDistanceMap;
+    static std::map<DWORD, unsigned short>                                       ms_ModelDefaultFlagsMap;
     static std::map<DWORD, BYTE>                                                 ms_ModelDefaultAlphaTransparencyMap;
     static std::unordered_map<std::uint32_t, std::map<eVehicleDummies, CVector>> ms_ModelDefaultDummiesPosition;
     static std::map<CTimeInfoSAInterface*, CTimeInfoSAInterface*>                ms_ModelDefaultModelTimeInfo;
@@ -342,7 +353,13 @@ public:
     void           Remove();
     bool           IsLoaded();
     bool           DoIsLoaded();
-    BYTE           GetFlags();
+    unsigned short GetFlags();
+    unsigned short GetOriginalFlags();
+    void           SetIdeFlags(unsigned int uiFlags);
+    void           SetIdeFlag(eModelIdeFlag eIdeFlag, bool bState);
+    bool           GetIdeFlag(eModelIdeFlag eIdeFlag);
+    void           SetFlags(unsigned short usFlags);
+    static void    StaticResetFlags();
     CBoundingBox*  GetBoundingBox();
     bool           IsValid();
     bool           IsAllocatedInArchive();
@@ -437,4 +454,5 @@ public:
 private:
     void CopyStreamingInfoFromModel(ushort usCopyFromModelID);
     void RwSetSupportedUpgrades(RwFrame* parent, DWORD dwModel);
+    void SetModelSpecialType(eModelSpecialType eType, bool bState);
 };

Client/mods/deathmatch/logic/CClientGame.cpp
@@ -3288,6 +3288,7 @@ void CClientGame::Event_OnIngame()
     g_pGame->GetWorld()->SetOcclusionsEnabled(true);
 
     g_pGame->ResetModelLodDistances();
+    g_pGame->ResetModelFlags();
     g_pGame->ResetAlphaTransparencies();
     g_pGame->ResetModelTimes();
 

Client/mods/deathmatch/logic/lua/CLuaFunctionParseHelpers.cpp
@@ -758,6 +758,26 @@ ADD_ENUM(eSoundEffectParams::Reverb::REVERB_TIME, "reverbTime")
 ADD_ENUM(eSoundEffectParams::Reverb::HIGH_FREQ_RT_RATIO, "highFreqRTRatio")
 IMPLEMENT_ENUM_CLASS_END("soundeffect-params-reverb")
 
+IMPLEMENT_ENUM_CLASS_BEGIN(eModelIdeFlag)
+ADD_ENUM(eModelIdeFlag::IS_ROAD, "is_road")
+ADD_ENUM(eModelIdeFlag::DRAW_LAST, "draw_last")
+ADD_ENUM(eModelIdeFlag::ADDITIVE, "additive")
+ADD_ENUM(eModelIdeFlag::IGNORE_LIGHTING, "ignore_lighting")
+ADD_ENUM(eModelIdeFlag::NO_ZBUFFER_WRITE, "no_zbuffer_write")
+ADD_ENUM(eModelIdeFlag::DONT_RECEIVE_SHADOWS, "dont_receive_shadows")
+ADD_ENUM(eModelIdeFlag::IS_GLASS_TYPE_1, "is_glass_type_1")
+ADD_ENUM(eModelIdeFlag::IS_GLASS_TYPE_2, "is_glass_type_2")
+ADD_ENUM(eModelIdeFlag::IS_GARAGE_DOOR, "is_garage_door")
+ADD_ENUM(eModelIdeFlag::IS_DAMAGABLE, "is_damagable")
+ADD_ENUM(eModelIdeFlag::IS_TREE, "is_tree")
+ADD_ENUM(eModelIdeFlag::IS_PALM, "is_palm")
+ADD_ENUM(eModelIdeFlag::DOES_NOT_COLLIDE_WITH_FLYER, "does_not_collide_with_flyer")
+ADD_ENUM(eModelIdeFlag::IS_TAG, "is_tag")
+ADD_ENUM(eModelIdeFlag::DISABLE_BACKFACE_CULLING, "disable_backface_culling")
+ADD_ENUM(eModelIdeFlag::IS_BREAKABLE_STATUE, "is_breakable_statue")
+ADD_ENUM(eModelIdeFlag::IS_CRANE, "is_crane")
+IMPLEMENT_ENUM_CLASS_END("model-ide-flag")
+
 //
 // CResource from userdata
 //

Client/mods/deathmatch/logic/lua/CLuaFunctionParseHelpers.h
@@ -78,6 +78,7 @@ DECLARE_ENUM_CLASS(eSoundEffectParams::Gargle);
 DECLARE_ENUM_CLASS(eSoundEffectParams::I3DL2Reverb);
 DECLARE_ENUM_CLASS(eSoundEffectParams::ParamEq);
 DECLARE_ENUM_CLASS(eSoundEffectParams::Reverb);
+DECLARE_ENUM_CLASS(eModelIdeFlag);
 
 class CRemoteCall;
 

Client/mods/deathmatch/logic/luadefs/CLuaEngineDefs.cpp
@@ -63,6 +63,11 @@ void CLuaEngineDefs::LoadFunctions()
         {"engineSetObjectGroupPhysicalProperty", EngineSetObjectGroupPhysicalProperty},
         {"engineGetObjectGroupPhysicalProperty", EngineGetObjectGroupPhysicalProperty},
         {"engineRestoreObjectGroupPhysicalProperties", EngineRestoreObjectGroupPhysicalProperties},
+        {"engineGetModelFlags", ArgumentParser<EngineGetModelFlags>},
+        {"engineSetModelFlags", ArgumentParser<EngineSetModelFlags>},
+        {"engineGetModelFlag", ArgumentParser<EngineGetModelFlag>},
+        {"engineSetModelFlag", ArgumentParser<EngineSetModelFlag>},
+        {"engineResetModelFlags", ArgumentParser<EngineResetModelFlags>},
         {"engineRestreamWorld", ArgumentParser<EngineRestreamWorld>},
         {"engineLoadIMG", ArgumentParser<EngineLoadIMG>},
         {"engineImageLinkDFF", ArgumentParser<EngineImageLinkDFF>},
@@ -117,6 +122,10 @@ void CLuaEngineDefs::AddClass(lua_State* luaVM)
     lua_classfunction(luaVM, "setObjectGroupPhysicalProperty", "engineSetObjectGroupPhysicalProperty");
     lua_classfunction(luaVM, "getObjectGroupPhysicalProperty", "engineGetObjectGroupPhysicalProperty");
     lua_classfunction(luaVM, "restoreObjectGroupPhysicalProperties", "engineRestoreObjectGroupPhysicalProperties");
+
+    lua_classfunction(luaVM, "setModelFlags", "engineSetModelFlags");
+    lua_classfunction(luaVM, "resetModelFlags", "engineResetModelFlags");
+    lua_classfunction(luaVM, "getModelFlags", "engineGetModelFlags");
 
     lua_registerstaticclass(luaVM, "Engine");
 
@@ -2234,7 +2243,75 @@ int CLuaEngineDefs::EngineRestoreObjectGroupPhysicalProperties(lua_State* luaVM)
     return 1;
 }
 
-bool CLuaEngineDefs::EngineRestreamWorld(lua_State* const luaVM)
+uint CLuaEngineDefs::EngineGetModelFlags(uint uiModelId)
+{
+    CModelInfo* pModelInfo = g_pGame->GetModelInfo(uiModelId);
+
+    if (uiModelId >= 20000 || !pModelInfo)
+        throw std::invalid_argument("Expected a valid model ID in range [0-19999] at argument 1");
+
+    return pModelInfo->GetFlags();
+}
+
+bool CLuaEngineDefs::EngineGetModelFlag(uint uiModelId, eModelIdeFlag eFlag)
+{
+    CModelInfo* pModelInfo = g_pGame->GetModelInfo(uiModelId);
+
+    if (uiModelId >= 20000 || !pModelInfo)
+        throw std::invalid_argument("Expected a valid model ID in range [0-19999] at argument 1");
+
+    return pModelInfo->GetIdeFlag(eFlag);
+}
+
+bool CLuaEngineDefs::EngineSetModelFlags(uint uiModelID, uint uiFlags, std::optional<bool> bIdeFlags)
+{
+    // bool engineSetModelFlags ( int modelID, int flags [, bool isIde ] )
+
+    CModelInfo* pModelInfo = g_pGame->GetModelInfo(uiModelID);
+
+    if (uiModelID >= 20000 || !pModelInfo)
+        throw std::invalid_argument("Expected a valid model ID in range [0-19999] at argument 1");
+
+    if (bIdeFlags.value_or(false))
+        pModelInfo->SetIdeFlags(uiFlags);
+    else
+        pModelInfo->SetFlags(uiFlags);
+
+    return true; 
+}
+
+bool CLuaEngineDefs::EngineSetModelFlag(uint uiModelID, eModelIdeFlag eFlag, bool bState)
+{
+    // bool engineSetModelFlags ( int modelID, int flags [, bool isIde ] )
+
+    CModelInfo* pModelInfo = g_pGame->GetModelInfo(uiModelID);
+
+    if (uiModelID >= 20000 || !pModelInfo)
+        throw std::invalid_argument("Expected a valid model ID in range [0-19999] at argument 1");
+
+    pModelInfo->SetIdeFlag(eFlag, bState);
+
+    return true;
+}
+
+bool CLuaEngineDefs::EngineResetModelFlags(uint uiModelID)
+{
+    CModelInfo* pModelInfo = g_pGame->GetModelInfo(uiModelID);
+    if (!pModelInfo)
+        return false;
+
+    ushort usCurrentFlags = pModelInfo->GetFlags();
+    ushort usOriginalFlags = pModelInfo->GetOriginalFlags();
+    if (usOriginalFlags != usCurrentFlags)
+    {
+        pModelInfo->SetFlags(usOriginalFlags);
+        return true;
+    }
+
+    return false;
+}
+
+bool CLuaEngineDefs::EngineRestreamWorld()
 {
     g_pClientGame->RestreamWorld();
     return true;

commit bbffc97947523c111f03544da8f32663e3255646

Fix collision streaming regression from #1714 (#2950)

Client/game_sa/CModelInfoSA.cpp
@@ -1511,6 +1511,7 @@ void CModelInfoSA::SetColModel(CColModel* pColModel)
 
         // SetColModel sets bDoWeOwnTheColModel if the last parameter is truthy
         m_pInterface->bDoWeOwnTheColModel = false;
+        m_pInterface->bIsColLoaded = false;
 
         // Fix random foliage on custom collisions by calling CPlantMgr::SetPlantFriendlyFlagInAtomicMI
         (reinterpret_cast<void(__cdecl*)(CBaseModelInfoSAInterface*)>(0x5DB650))(m_pInterface);