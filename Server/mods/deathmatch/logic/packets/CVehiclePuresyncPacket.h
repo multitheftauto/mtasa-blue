309f5b1067fcd61371346d01200d387d591ef95f
patrikjuvonen <22572159+patrikjuvonen@users.noreply.github.com>
2023-04-08 16:26:11 +0300

    Fix header comments
    [ci skip]

--- Server/mods/deathmatch/logic/packets/CVehiclePuresyncPacket.h
+++ Server/mods/deathmatch/logic/packets/CVehiclePuresyncPacket.h
@@ -1,11 +1,11 @@
 /*****************************************************************************
  *
- *  PROJECT:     Multi Theft Auto
+ *  PROJECT:     Multi Theft Auto v1.0
  *  LICENSE:     See LICENSE in the top level directory
  *  FILE:        mods/deathmatch/logic/packets/CVehiclePuresyncPacket.h
  *  PURPOSE:     Vehicle pure synchronization packet class
  *
- *  Multi Theft Auto is available from https://multitheftauto.com/
+ *  Multi Theft Auto is available from http://www.multitheftauto.com/
  *
  *****************************************************************************/
 
