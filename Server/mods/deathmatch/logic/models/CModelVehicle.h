2b60f7957ba9fd9976a7ce8947e1d736ad194da1
patrikjuvonen <22572159+patrikjuvonen@users.noreply.github.com>
2023-04-08 15:45:26 +0300

    Update CModelVehicle.h
    [ci skip]

--- Server/mods/deathmatch/logic/models/CModelVehicle.h
+++ Server/mods/deathmatch/logic/models/CModelVehicle.h
@@ -1,11 +1,11 @@
 /*****************************************************************************
  *
- *  PROJECT:     Multi Theft Auto v1.0
+ *  PROJECT:     Multi Theft Auto
  *  LICENSE:     See LICENSE in the top level directory
- *  FILE:        mods/deathmatch/logic/CModelAtomic.h
- *  PURPOSE:     Atomic model class
+ *  FILE:        mods/deathmatch/logic/CModelVehicle.h
+ *  PURPOSE:     Vehicle model class
  *
- *  Multi Theft Auto is available from http://www.multitheftauto.com/
+ *  Multi Theft Auto is available from https://www.multitheftauto.com/
  *
  *****************************************************************************/
 
