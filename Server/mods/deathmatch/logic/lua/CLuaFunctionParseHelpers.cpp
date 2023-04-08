e07956c61f6284021692009e5f12185897836420
patrikjuvonen <22572159+patrikjuvonen@users.noreply.github.com>
2023-04-08 16:17:17 +0300

    Fix header comments
    [ci skip]

--- Server/mods/deathmatch/logic/lua/CLuaFunctionParseHelpers.cpp
+++ Server/mods/deathmatch/logic/lua/CLuaFunctionParseHelpers.cpp
@@ -4,6 +4,8 @@
  *  LICENSE:     See LICENSE in the top level directory
  *  FILE:        mods/deathmatch/logic/lua/CLuaFunctionParseHelpers.cpp
  *
+ *  Multi Theft Auto is available from https://multitheftauto.com/
+ *
  *****************************************************************************/
 
 #include "StdInc.h"
