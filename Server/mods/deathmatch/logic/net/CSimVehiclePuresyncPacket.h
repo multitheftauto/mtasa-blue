04f6f98e177a5280f862834bcf108a8763c43887
patrikjuvonen <22572159+patrikjuvonen@users.noreply.github.com>
2023-04-08 16:28:51 +0300

    Fix header comments
    [ci skip]

--- Server/mods/deathmatch/logic/net/CSimVehiclePuresyncPacket.h
+++ Server/mods/deathmatch/logic/net/CSimVehiclePuresyncPacket.h
@@ -1,9 +1,9 @@
 /*****************************************************************************
  *
- *  PROJECT:     Multi Theft Auto
+ *  PROJECT:     Multi Theft Auto v1.0
  *  LICENSE:     See LICENSE in the top level directory
  *
- *  Multi Theft Auto is available from https://multitheftauto.com/
+ *  Multi Theft Auto is available from http://www.multitheftauto.com/
  *
  *****************************************************************************/
 
