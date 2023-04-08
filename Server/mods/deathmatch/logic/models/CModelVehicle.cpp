11ec287977217591114e9df93f126a6d4862f0f0
patrikjuvonen <22572159+patrikjuvonen@users.noreply.github.com>
2023-04-08 15:46:43 +0300

    Update CModelVehicle.cpp
    [ci skip]

--- Server/mods/deathmatch/logic/models/CModelVehicle.cpp
+++ Server/mods/deathmatch/logic/models/CModelVehicle.cpp
@@ -1,11 +1,11 @@
 /*****************************************************************************
  *
- *  PROJECT:     Multi Theft Auto v1.0
+ *  PROJECT:     Multi Theft Auto
  *  LICENSE:     See LICENSE in the top level directory
  *  FILE:        mods/deathmatch/logic/models/CModelVehicle.cpp
  *  PURPOSE:     Vehicle model info class
  *
- *  Multi Theft Auto is available from http://www.multitheftauto.com/
+ *  Multi Theft Auto is available from https://www.multitheftauto.com/
  *
  *****************************************************************************/
 
