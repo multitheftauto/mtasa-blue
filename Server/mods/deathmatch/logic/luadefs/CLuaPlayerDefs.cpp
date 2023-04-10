c56add86d36034b1f32c5ef010b28156c022246e
Tete <teteomar2010@hotmail.com>
2023-04-10 06:06:41 -0600

    Fix #2774: Prevent kick during redirect (#2955)

--- Server/mods/deathmatch/logic/luadefs/CLuaPlayerDefs.cpp
+++ Server/mods/deathmatch/logic/luadefs/CLuaPlayerDefs.cpp
@@ -1929,6 +1929,12 @@ int CLuaPlayerDefs::KickPlayer(lua_State* luaVM)
     CScriptArgReader argStream(luaVM);
     argStream.ReadUserData(pPlayer);
 
+    if (pPlayer->IsRedirecting())
+    {
+        lua_pushboolean(luaVM, false);
+        return 1;
+    }
+
     if (argStream.NextIsUserData())
     {
         CClient* pResponsible;
