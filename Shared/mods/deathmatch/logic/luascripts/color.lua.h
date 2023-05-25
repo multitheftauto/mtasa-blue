373290354b453b61ec0d85a28cf79381b884b3ac
TracerDS <43095317+TracerDS@users.noreply.github.com>
2023-05-25 10:34:42 +0200

    Added range checks

--- Shared/mods/deathmatch/logic/luascripts/color.lua.h
+++ Shared/mods/deathmatch/logic/luascripts/color.lua.h
@@ -16,6 +16,10 @@ tocolor = function(r,g,b,a)
     assert(type(b) == 'number', _errMsg:format(3, type(b)))
 
     if type(a) ~= 'number' then a = 255 end
+    r = r > 255 and 255 or r < 0 and 0 or r
+    g = g > 255 and 255 or g < 0 and 0 or g
+    b = b > 255 and 255 or b < 0 and 0 or b
+    a = a > 255 and 255 or a < 0 and 0 or a
 
     return b + g * 256 + r * 256 * 256 + a * 256 * 256 * 256
 end
