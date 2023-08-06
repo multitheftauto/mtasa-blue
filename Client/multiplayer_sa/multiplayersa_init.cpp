e85b1733ef5266ea61ff7edcaca6b4758a43a2f2
Sebastian Jura <sebajura1234@gmail.com>
2023-08-06 18:45:04 +0200

    update

--- Client/multiplayer_sa/multiplayersa_init.cpp
+++ Client/multiplayer_sa/multiplayersa_init.cpp
@@ -11,6 +11,7 @@
 
 #include "StdInc.h"
 #define DECLARE_PROFILER_SECTION_multiplayersa_init
+#include "profiler/SharedUtil.Profiler.h"
 
 CGame*          pGameInterface = 0;
 CMultiplayerSA* pMultiplayer = 0;
