bef5ac956d44bd7ba222c67095370a87dbf9fddd
znajder <l0nger.programmer@gmail.com>
2023-09-16 21:57:18 +0200

    Remove unused client event

--- Client/mods/deathmatch/logic/CClientGame.cpp
+++ Client/mods/deathmatch/logic/CClientGame.cpp
@@ -2496,7 +2496,6 @@ void CClientGame::AddBuiltInEvents()
     m_Events.AddEvent("onClientElementModelChange", "oldModel, newModel", nullptr, false);
     m_Events.AddEvent("onClientElementDimensionChange", "oldDimension, newDimension", nullptr, false);
     m_Events.AddEvent("onClientElementInteriorChange", "oldInterior, newInterior", nullptr, false);
-    m_Events.AddEvent("onClientElementHealthChange", "oldHealth, newHealth", nullptr, false);
 
     // Player events
     m_Events.AddEvent("onClientPlayerJoin", "", NULL, false);
