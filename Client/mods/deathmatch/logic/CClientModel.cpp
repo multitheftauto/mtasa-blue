multitheftauto/mtasa-blue

commit fdaa114b177a06e45c39748ea075f784ffb82fe7

Allow allocating new TXD's (#1802)
Co-authored-by: Vladislav Nikolaevich <v.nikolaevich@redbrixwall.com>
Co-authored-by: Pirulax <patrikjankovics7@gmail.com>

Client/mods/deathmatch/logic/CClientModel.cpp
@@ -9,6 +9,7 @@
  *****************************************************************************/
 
 #include "StdInc.h"
+#include "game/CStreaming.h"
 
 CClientModel::CClientModel(CClientManager* pManager, int iModelID, eClientModelType eModelType)
 {
@@ -77,19 +78,30 @@ bool CClientModel::Allocate(ushort usParentID)
     return false;
 }
 
-bool CClientModel::Deallocate(void)
+bool CClientModel::Deallocate()
 {
     if (!m_bAllocatedByUs)
         return false;
     CModelInfo* pModelInfo = g_pGame->GetModelInfo(m_iModelID, true);
     if (!pModelInfo || !pModelInfo->IsValid())
         return false;
-    pModelInfo->DeallocateModel();
+
     SetParentResource(nullptr);
-    return true;
+
+    switch (m_eModelType)
+    {
+        case eClientModelType::PED:
+        case eClientModelType::OBJECT:
+        case eClientModelType::VEHICLE:
+            return DeallocateDFF(pModelInfo);
+        case eClientModelType::TXD:
+            return DeallocateTXD(pModelInfo);
+        default:
+            return false;
+    }
 }
 
-void CClientModel::RestoreEntitiesUsingThisModel()
+bool CClientModel::DeallocateDFF(CModelInfo* pModelInfo)
 {
     auto unloadModelsAndCallEvents = [&](auto iterBegin, auto iterEnd, unsigned short usParentID, auto setElementModelLambda) {
         for (auto iter = iterBegin; iter != iterEnd; iter++)
@@ -152,4 +164,35 @@ void CClientModel::RestoreEntitiesUsingThisModel()
 
     // Restore DFF/TXD
     g_pClientGame->GetManager()->GetDFFManager()->RestoreModel(m_iModelID);
+
+    return true;
+}
+
+bool CClientModel::AllocateTXD(std::string &strTxdName)
+{
+    uint uiSlotID = g_pGame->GetPools()->AllocateTextureDictonarySlot(m_iModelID - MAX_MODEL_DFF_ID, strTxdName);
+    if (uiSlotID != -1)
+    {
+        m_bAllocatedByUs = true;
+        return true;
+    }
+    return false;
+}
+
+bool CClientModel::DeallocateTXD(CModelInfo* pModelInfo)
+{
+    uint uiTextureDictonarySlotID = pModelInfo->GetModel() - MAX_MODEL_DFF_ID;
+
+    for (uint uiModelID = 0; uiModelID < MAX_MODEL_DFF_ID; uiModelID++)
+    {
+        CModelInfo* pModelInfo = g_pGame->GetModelInfo(uiModelID, true);
+
+        if (pModelInfo->GetTextureDictionaryID() == uiTextureDictonarySlotID)
+            pModelInfo->SetTextureDictionaryID(0);
+    }
+
+    g_pGame->GetPools()->RemoveTextureDictonarySlot(uiTextureDictonarySlotID);
+    g_pGame->GetStreaming()->SetStreamingInfo(pModelInfo->GetModel(), 0, 0, 0, -1);
+
+    return true;
 }

Client/mods/deathmatch/logic/luadefs/CLuaEngineDefs.cpp
@@ -132,6 +132,9 @@ void CLuaEngineDefs::LoadFunctions()
         {"engineStreamingGetBufferSize", ArgumentParser<EngineStreamingGetBufferSize>},
         {"engineStreamingSetModelCacheLimits", ArgumentParser<EngineStreamingSetModelCacheLimits>},
         
+        {"engineRequestTXD", ArgumentParser<EngineRequestTXD>},
+        {"engineFreeTXD", ArgumentParser<EngineFreeTXD>},
+        
         // CLuaCFunctions::AddFunction ( "engineReplaceMatchingAtomics", EngineReplaceMatchingAtomics );
         // CLuaCFunctions::AddFunction ( "engineReplaceWheelAtomics", EngineReplaceWheelAtomics );
         // CLuaCFunctions::AddFunction ( "enginePositionAtomic", EnginePositionAtomic );
@@ -849,12 +852,9 @@ int CLuaEngineDefs::EngineRequestModel(lua_State* luaVM)
             if (!argStream.HasErrors())
             {
                 int iModelID = m_pManager->GetModelManager()->GetFirstFreeModelID();
-                if (iModelID != INVALID_MODEL_ID)
-                {
-                    std::shared_ptr<CClientModel> pModel = m_pManager->GetModelManager()->FindModelByID(iModelID);
-                    if (pModel == nullptr)
-                        pModel = std::make_shared<CClientModel>(m_pManager, iModelID, eModelType);
-                    m_pManager->GetModelManager()->Add(pModel);
+                if (iModelID != INVALID_MODEL_ID) {
+                    std::shared_ptr<CClientModel> pModel = m_pManager->GetModelManager()->Request(m_pManager, iModelID, eModelType);
+
                     ushort usParentID = -1;
 
                     if (argStream.NextIsNumber())
@@ -2424,4 +2424,28 @@ bool CLuaEngineDefs::EngineRestreamWorld(lua_State* const luaVM)
 
     g_pClientGame->RestreamWorld(restreamLODs);
     return true;
+}
+
+uint CLuaEngineDefs::EngineRequestTXD(lua_State* const luaVM, std::string strTxdName)
+{
+    if (strTxdName.size() > 24)
+        throw std::invalid_argument("TXD name length shoudn't be more than 24 characters");
+
+    int iModelID = m_pManager->GetModelManager()->GetFreeTxdModelID();
+    if (iModelID == INVALID_MODEL_ID)
+        return false;
+
+    std::shared_ptr<CClientModel> pModel = m_pManager->GetModelManager()->Request(m_pManager, iModelID, eClientModelType::TXD);
+
+    pModel->AllocateTXD(strTxdName);
+    pModel->SetParentResource(m_pLuaManager->GetVirtualMachine(luaVM)->GetResource());
+    m_pManager->GetModelManager()->Add(pModel);
+
+    return iModelID - MAX_MODEL_DFF_ID;
+}
+
+bool CLuaEngineDefs::EngineFreeTXD(uint txdID)
+{
+    std::shared_ptr<CClientModel> pModel = m_pManager->GetModelManager()->FindModelByID(MAX_MODEL_DFF_ID + txdID);
+    return pModel && pModel->Deallocate();
 }

Client/game_sa/CGameSA.h
@@ -36,6 +36,10 @@ extern unsigned int OBJECTDYNAMICINFO_MAX;            // default: 160
 #define NUM_WeaponInfosOtherSkill       11
 #define NUM_WeaponInfosTotal            (NUM_WeaponInfosStdSkill + (3*NUM_WeaponInfosOtherSkill)) // std, (poor, pro, special)
 
+#define MODELINFO_DFF_MAX               20000
+#define MODELINFO_TXD_MAX               25000
+#define MODELINFO_MAX                   26000       // Actual max is 25755
+
 #define VAR_FlyingCarsEnabled           0x969160
 #define VAR_ExtraBunnyhopEnabled        0x969161
 #define VAR_HoveringCarsEnabled         0x969152

Client/game_sa/CModelInfoSA.cpp
@@ -732,6 +732,9 @@ CBoundingBox* CModelInfoSA::GetBoundingBox()
 
 bool CModelInfoSA::IsValid()
 {
+    if (m_dwModelID >= MODELINFO_DFF_MAX && m_dwModelID < MODELINFO_TXD_MAX)
+        return !pGame->GetPools()->IsFreeTextureDictonarySlot(m_dwModelID - MODELINFO_DFF_MAX);
+        
     if (m_dwModelID >= pGame->GetBaseIDforTXD() && m_dwModelID < pGame->GetCountOfAllFileIDs())
         return true;
 

Client/game_sa/CPoolsSA.cpp
@@ -23,6 +23,7 @@
 #include "CTrailerSA.h"
 #include "CTrainSA.h"
 #include "CWorldSA.h"
+#include "CKeyGenSA.h"
 
 extern CGameSA* pGame;
 
@@ -31,6 +32,7 @@ CPoolsSA::CPoolsSA()
     m_ppPedPoolInterface = (CPoolSAInterface<CPedSAInterface>**)0xB74490;
     m_ppObjectPoolInterface = (CPoolSAInterface<CObjectSAInterface>**)0xB7449C;
     m_ppVehiclePoolInterface = (CPoolSAInterface<CVehicleSAInterface>**)0xB74494;
+    m_ppTxdPoolInterface = (CPoolSAInterface<CTextureDictonarySAInterface>**)0xC8800C;
 
     m_bGetVehicleEnabled = true;
 }
@@ -1052,4 +1054,41 @@ int CPoolsSA::GetNumberOfUsedSpaces(ePools pool)
 void CPoolsSA::InvalidateLocalPlayerClientEntity()
 {
     m_pedPool.arrayOfClientEntities[0] = {m_pedPool.arrayOfClientEntities[0].pEntity, nullptr};
+}
+
+unsigned int CPoolsSA::AllocateTextureDictonarySlot(uint uiSlotId, std::string& strTxdName)
+{
+    CTextureDictonarySAInterface* pTxd = (*m_ppTxdPoolInterface)->AllocateAt(uiSlotId);
+    if (!pTxd)
+        return -1;
+
+    strTxdName.resize(24);
+
+    pTxd->usUsagesCount = 0;
+    pTxd->hash = pGame->GetKeyGen()->GetUppercaseKey(strTxdName.c_str());
+    pTxd->rwTexDictonary = nullptr;
+    pTxd->usParentIndex = -1;
+
+    return (*m_ppTxdPoolInterface)->GetObjectIndex(pTxd);
+}
+
+void CPoolsSA::RemoveTextureDictonarySlot(uint uiTxdId)
+{
+    if (!(*m_ppTxdPoolInterface)->IsContains(uiTxdId))
+        return;
+
+    typedef uint(__cdecl * Function_TxdReleaseSlot)(uint uiTxdId);
+    ((Function_TxdReleaseSlot)(0x731E90))(uiTxdId);
+
+    (*m_ppTxdPoolInterface)->Release(uiTxdId);
+}
+
+bool CPoolsSA::IsFreeTextureDictonarySlot(uint uiTxdId)
+{
+    return (*m_ppTxdPoolInterface)->IsEmpty(uiTxdId);
+}
+
+ushort CPoolsSA::GetFreeTextureDictonarySlot()
+{
+    return (*m_ppTxdPoolInterface)->GetFreeSlot();
 }

Client/game_sa/CPoolsSA.h
@@ -14,6 +14,7 @@
 #include "CPedSA.h"
 #include "CVehicleSA.h"
 #include "CObjectSA.h"
+#include "CTextureDictonarySA.h"
 
 #define INVALID_POOL_ARRAY_ID 0xFFFFFFFF
 
@@ -54,8 +55,84 @@ public:
         m_bOwnsAllocations = false;
     }
 
+    uint GetFreeSlot()
+    {
+        bool bLooped = false;
+        uint index = m_nFirstFree + 1;
+
+        while (true)
+        {
+            if (index >= m_nSize)
+            {
+                if (bLooped)
+                    return -1;
+
+                index = 0;
+                bLooped = true;
+            }
+
+            if (m_byteMap[index].bEmpty)
+            {
+                m_nFirstFree = index;
+                return index;
+            }
+            index++;
+        }
+
+        return -1;
+    };
+
+    B* Allocate()
+    {
+        m_nFirstFree++;          // Continue after the last allocated slot
+        const auto sz = m_nSize; // Storing size to avoid reloads from memory - should help out the optimizer
+        for (auto i = 0u; i < sz; i++) {
+            const auto slot = (m_nFirstFree + i) % sz;
+            const auto e    = &m_byteMap[slot];
+            if (!e->bEmpty) {
+                continue;
+            }
+            m_nFirstFree = slot;
+            e->bEmpty = false;
+            e->nId++;
+            return &m_pObjects[slot];
+        }
+        return nullptr;
+    }
+
+    B* AllocateAt(uint uiSlot)
+    {
+        m_pObjects[uiSlot] = B();
+        m_byteMap[uiSlot].bEmpty = false;
+        m_byteMap[uiSlot].nId ^= uiSlot ^ (uiSlot + 1);
+
+        return &m_pObjects[uiSlot];
+    }
+
+    void Release(uint index)
+    {
+        m_byteMap[index].bEmpty = true;
+        m_byteMap[index].nId = 0;
+        if (index == m_nFirstFree)
+            --m_nFirstFree;
+    }
+
+    void Delete(uint index)
+    {
+        Release(index);
+    }
+
     bool IsEmpty(std::int32_t objectIndex) { return m_byteMap[objectIndex].bEmpty; }
+    bool IsContains(uint index)
+    {
+        if (m_nSize <= index)
+            return false;
+        return !IsEmpty(index);
+    }
+
     B*   GetObject(std::int32_t objectIndex) { return &m_pObjects[objectIndex]; }
+
+    uint GetObjectIndex(B* pObject) { return ((DWORD)pObject - (DWORD)m_pObjects) / sizeof(B); }
 };
 
 class CPoolsSA : public CPools
@@ -127,6 +204,12 @@ public:
     void ResetPedPoolCount() { m_pedPool.ulCount = 0; }
     void InvalidateLocalPlayerClientEntity();
 
+    uint AllocateTextureDictonarySlot(uint uiSlotID, std::string& strTxdName);
+    void RemoveTextureDictonarySlot(uint uiTxdId);
+    bool IsFreeTextureDictonarySlot(uint uiTxdId);
+
+    ushort GetFreeTextureDictonarySlot();
+
 private:
     // Generic container for pools
     template <class T, class I, unsigned long MAX>
@@ -157,6 +240,7 @@ private:
     CPoolSAInterface<CPedSAInterface>**                              m_ppPedPoolInterface;
     CPoolSAInterface<CObjectSAInterface>**                           m_ppObjectPoolInterface;
     CPoolSAInterface<CVehicleSAInterface>**                          m_ppVehiclePoolInterface;
+    CPoolSAInterface<CTextureDictonarySAInterface>**                 m_ppTxdPoolInterface;
 
     bool m_bGetVehicleEnabled;
 };

Client/game_sa/CTextureDictonarySA.h
@@ -1,0 +1,23 @@
+/*****************************************************************************
+ *
+ *  PROJECT:     Multi Theft Auto v1.0
+ *  LICENSE:     See LICENSE in the top level directory
+ *  FILE:        game_sa/CTextureDictonarySA.h
+ *  PURPOSE:     Header file for game texture dictonary class
+ *
+ *  Multi Theft Auto is available from http://www.multitheftauto.com/
+ *
+ *****************************************************************************/
+
+#pragma once
+
+#include <game/RenderWare.h>
+
+class CTextureDictonarySAInterface
+{
+public:
+    RwTexDictionary* rwTexDictonary;
+    unsigned short   usUsagesCount;
+    unsigned short   usParentIndex;
+    unsigned int     hash;
+};

Client/mods/deathmatch/logic/CClientModel.h
@@ -21,6 +21,7 @@ enum class eClientModelType
     VEHICLE,
     TIMED_OBJECT,
     CLUMP,
+    TXD,
 };
 
 class CResource;
@@ -37,10 +38,14 @@ public:
     int              GetModelID(void) const { return m_iModelID; };
     eClientModelType GetModelType(void) const { return m_eModelType; };
     bool             Allocate(ushort usParentID);
+    bool             AllocateTXD(std::string& strTxdName);
     bool             Deallocate(void);
-    void             RestoreEntitiesUsingThisModel();
     void             SetParentResource(CResource* pResource) { m_pParentResource = pResource; }
     CResource*       GetParentResource(void) const { return m_pParentResource; }
+
+private:
+    bool DeallocateDFF(CModelInfo* pModelInfo);
+    bool DeallocateTXD(CModelInfo* pModelInfo);
 
 protected:
     CClientManager* m_pManager;

commit 2db0598d0339769d6a2a7ef9f835bc00a6e4f709

Fix engineFreeModel regression caused by #1802

Client/mods/deathmatch/logic/CClientModel.cpp
@@ -165,6 +165,9 @@ bool CClientModel::DeallocateDFF(CModelInfo* pModelInfo)
     // Restore DFF/TXD
     g_pClientGame->GetManager()->GetDFFManager()->RestoreModel(m_iModelID);
 
+    // Remove model info
+    pModelInfo->DeallocateModel();
+
     return true;
 }
 

Client/mods/deathmatch/logic/luadefs/CLuaEngineDefs.cpp
@@ -854,6 +854,7 @@ int CLuaEngineDefs::EngineRequestModel(lua_State* luaVM)
                 int iModelID = m_pManager->GetModelManager()->GetFirstFreeModelID();
                 if (iModelID != INVALID_MODEL_ID) {
                     std::shared_ptr<CClientModel> pModel = m_pManager->GetModelManager()->Request(m_pManager, iModelID, eModelType);
+                    m_pManager->GetModelManager()->Add(pModel);
 
                     ushort usParentID = -1;
 