df357d8516079010506bc9da2da332ba0f0ddc9b
Nurupo <191977649@qq.com>
2023-12-20 20:32:44 +1100

    Update CPlaneSA.h

--- Client/game_sa/CPlaneSA.h
+++ Client/game_sa/CPlaneSA.h
@@ -14,36 +14,6 @@
 #include <game/CPlane.h>
 #include "CAutomobileSA.h"
 
-enum class ePlaneNodes
-{
-    PLANE_NODE_NONE = 0,
-    PLANE_CHASSIS = 1,
-    PLANE_WHEEL_RF = 2,
-    PLANE_WHEEL_RM = 3,
-    PLANE_WHEEL_RB = 4,
-    PLANE_WHEEL_LF = 5,
-    PLANE_WHEEL_LM = 6,
-    PLANE_WHEEL_LB = 7,
-    PLANE_DOOR_RF = 8,
-    PLANE_DOOR_RR = 9,
-    PLANE_DOOR_LF = 10,
-    PLANE_DOOR_LR = 11,
-    PLANE_STATIC_PROP = 12,
-    PLANE_MOVING_PROP = 13,
-    PLANE_STATIC_PROP2 = 14,
-    PLANE_MOVING_PROP2 = 15,
-    PLANE_RUDDER = 16,
-    PLANE_ELEVATOR_L = 17,
-    PLANE_ELEVATOR_R = 18,
-    PLANE_AILERON_L = 19,
-    PLANE_AILERON_R = 20,
-    PLANE_GEAR_L = 21,
-    PLANE_GEAR_R = 22,
-    PLANE_MISC_A = 23,
-    PLANE_MISC_B = 24,
-    PLANE_NUM_NODES
-};
-
 class CPlaneSAInterface : public CAutomobileSAInterface
 {
     // + 2508 = undercarrige possition (float - 1.0 = up, 0.0 = down)
