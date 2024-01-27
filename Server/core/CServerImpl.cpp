multitheftauto/mtasa-blue

commit 4d37b6b87925b0ccce94be38df3f598fb628a4c8

Run ingame server in a separate process

Server/core/CServerImpl.cpp
@@ -47,6 +47,9 @@ bool    IsCursesActive()
 {
     return m_wndInput != NULL;
 }
+#else
+bool g_isChildProcess = false;
+HANDLE g_readyEvent = nullptr;
 #endif
 
 #ifdef WIN32
@@ -57,7 +60,6 @@ CServerImpl::CServerImpl()
 {
     #ifdef WIN32
     m_pThreadCommandQueue = pThreadCommandQueue;
-    m_fClientFeedback = NULL;
     m_hConsole = NULL;
     #else
     m_wndMenu = NULL;
@@ -122,17 +124,6 @@ void CServerImpl::Printf(const char* szFormat, ...)
 #endif
     }
 
-    // Eventually feed stuff back to our client if we run inside GTA
-    #ifdef WIN32
-    if (m_fClientFeedback)
-    {
-        char szOutput[512];
-        szOutput[511] = 0;
-        VSNPRINTF(szOutput, 511, szFormat, ap);
-        m_fClientFeedback(szOutput);
-    }
-    #endif
-
     va_end(ap);
 }
 
@@ -177,9 +168,7 @@ int CServerImpl::Run(int iArgumentCount, char* szArguments[])
     if (!ParseArguments(iArgumentCount, szArguments))
         return 1;
 
-#ifdef WIN32
-    if (!m_fClientFeedback)
-#else
+#ifndef WIN32
     if (!g_bNoCrashHandler)
 #endif
     {
@@ -206,9 +195,7 @@ int CServerImpl::Run(int iArgumentCount, char* szArguments[])
         m_hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
         m_hConsoleInput = GetStdHandle(STD_INPUT_HANDLE);
 
-        // If stdout is piped GetConsoleScreenBufferInfo will fail
-        // ==> check if stdin is piped
-        if (HasConsole())
+        if (!g_isChildProcess && HasConsole())
         {
             // Disable QuickEdit mode to prevent text selection causing server freeze
             DWORD dwConInMode;
@@ -241,7 +228,18 @@ int CServerImpl::Run(int iArgumentCount, char* szArguments[])
         {
             // Enable non-blocking read mode
             DWORD pipeState = PIPE_NOWAIT;
-            SetNamedPipeHandleState(GetStdHandle(STD_INPUT_HANDLE), &pipeState, nullptr, nullptr);
+            SetNamedPipeHandleState(m_hConsoleInput, &pipeState, nullptr, nullptr);
+        }
+
+        if (g_isChildProcess)
+        {
+            DWORD bytesRead{};
+
+            if (!ReadFile(m_hConsoleInput, &g_readyEvent, sizeof(HANDLE), &bytesRead, nullptr) || bytesRead != sizeof(HANDLE))
+            {
+                Print("ERROR: Failed to read ready-event handle from input (%08x)\n", GetLastError());
+                return ERROR_OTHER;
+            }
         }
 #else
         // support user locales
@@ -481,6 +479,13 @@ void CServerImpl::MainLoop()
 
         if (m_pModManager->IsFinished())
             m_bRequestedQuit = true;
+
+        if (g_readyEvent != nullptr && m_pModManager->IsReadyToAcceptConnections())
+        {
+            SetEvent(g_readyEvent);
+            CloseHandle(g_readyEvent);
+            g_readyEvent = nullptr;
+        }
 
         HandlePulseSleep();
     }
@@ -1071,16 +1076,6 @@ bool CServerImpl::ParseArguments(int iArgumentCount, char* szArguments[])
                 break;
             }
 
-            // Client feedback pointer?
-            #ifdef WIN32
-            case 'c':
-            {
-                m_fClientFeedback = reinterpret_cast<FClientFeedback*>(szArguments[i]);
-                ucNext = 0;
-                break;
-            }
-            #endif
-
             // Nothing we know, proceed
             default:
             {
@@ -1123,13 +1118,16 @@ bool CServerImpl::ParseArguments(int iArgumentCount, char* szArguments[])
                 {
                     g_bNoCrashHandler = true;
                 }
-
-                #ifdef WIN32
-                else if (strcmp(szArguments[i], "--clientfeedback") == 0)
+#ifdef WIN32
+                else if (!strcmp(szArguments[i], "--child-process"))
                 {
-                    ucNext = 'c';
+                    g_isChildProcess = true;
+                    g_bNoTopBar = true;
+                    g_bNoCurses = true;
+                    std::setbuf(stdout, nullptr);
+                    std::setbuf(stderr, nullptr);
                 }
-                #endif
+#endif
             }
         }
     }

Client/mods/deathmatch/logic/CClientGame.cpp
@@ -752,7 +752,7 @@ bool CClientGame::StartLocalGame(eServerType Type, const char* szPassword)
         {
             m_bWaitingForLocalConnect = true;
             m_bErrorStartingLocal = true;
-            g_pCore->ShowMessageBox(_("Error") + _E("CD04"), _("The server is not installed"), MB_ICON_ERROR | MB_BUTTON_OK);
+            g_pCore->ShowMessageBox(_("Error") + _E("CD60"), _("Could not start the local server. See console for details."), MB_BUTTON_OK | MB_ICON_ERROR);
             g_pCore->GetModManager()->RequestUnload();
             return false;
         }
@@ -1122,8 +1122,8 @@ void CClientGame::DoPulses()
     // Call debug code if debug mode
     m_Foo.DoPulse();
 
-    // Output stuff from our internal server eventually
-    m_Server.DoPulse();
+    // Output stuff from our server eventually
+    m_Server.Pulse();
 
     if (m_pManager->IsGameLoaded() && m_Status == CClientGame::STATUS_JOINED && GetTickCount64_() - m_llLastTransgressionTime > 60000)
     {

Client/mods/deathmatch/logic/CDynamicLibrary.cpp
@@ -10,6 +10,7 @@
  *****************************************************************************/
 
 #include <StdInc.h>
+#include "CDynamicLibrary.h"
 
 CDynamicLibrary::CDynamicLibrary()
 {

Client/mods/deathmatch/logic/CServer.cpp
@@ -1,330 +1,287 @@
 /*****************************************************************************
  *
- *  PROJECT:     Multi Theft Auto v1.0
+ *  PROJECT:     Multi Theft Auto
  *  LICENSE:     See LICENSE in the top level directory
  *  FILE:        mods/deathmatch/logic/CServer.cpp
  *  PURPOSE:     Local server instancing class
  *
- *  Multi Theft Auto is available from http://www.multitheftauto.com/
+ *  Multi Theft Auto is available from https://multitheftauto.com/
  *
  *****************************************************************************/
 
 #include <StdInc.h>
-
-static volatile bool           g_bIsStarted = false;
-extern CCoreInterface*         g_pCore;
-extern CLocalizationInterface* g_pLocalization;
-CCriticalSection               CServer::m_OutputCC;
-std::list<std::string>         CServer::m_OutputQueue;
+#include <array>
+#include <string_view>
 
 #ifdef MTA_DEBUG
-    #define SERVER_DLL_PATH "core_d.dll"
+    #define SERVER_EXE_PATH "MTA Server_d.exe"
 #else
-    #define SERVER_DLL_PATH "core.dll"
+    #define SERVER_EXE_PATH "MTA Server.exe"
 #endif
 
-#define ERROR_NO_ERROR 0
-#define ERROR_NO_NETWORK_LIBRARY 1
-#define ERROR_NETWORK_LIBRARY_FAILED 2
-#define ERROR_LOADING_MOD 3
+constexpr UINT PROCESS_FORCEFULLY_TERMINATED = 0x90804050u;
 
 static const SFixedArray<const char*, 4> szServerErrors = {"Server stopped", "Could not load network library", "Loading network library failed",
                                                            "Error loading mod"};
 
-CServer::CServer()
+static bool IsProcessRunning(HANDLE process)
 {
-    assert(!g_bIsStarted);
-
-    // Initialize
-    m_bIsReady = false;
-    m_pLibrary = NULL;
-    m_hThread = INVALID_HANDLE_VALUE;
-    m_iLastError = ERROR_NO_ERROR;
-
-    m_strServerRoot = CalcMTASAPath("server");
-    m_strDLLFile = PathJoin(m_strServerRoot, SERVER_DLL_PATH);
+    return WaitForSingleObject(process, 0) == WAIT_TIMEOUT;
 }
 
-CServer::~CServer()
+struct PipeHandlePair
 {
-    // Make sure the server is stopped
-    Stop();
+    bool   AutoClose{};
+    HANDLE Read{};
+    HANDLE Write{};
 
-    // Make sure the thread handle is closed
-    if (m_hThread != INVALID_HANDLE_VALUE)
+    ~PipeHandlePair()
     {
-        CloseHandle(m_hThread);
+        if (AutoClose)
+        {
+            CloseHandle(Read);
+            CloseHandle(Write);
+        }
     }
-}
+};
 
-void CServer::DoPulse()
+bool CServer::Start(const char* configFileName)
 {
-    if (IsRunning())
+    if (m_isRunning)
+        return false;
+
+    // Create and use an optional job object to kill the server process automatically when we close the job object.
+    // This is pretty handy in case the game process unexpectedly crashes and we miss terminating the server process.
+    HANDLE job = CreateJobObjectW(nullptr, nullptr);
+
+    if (job != nullptr)
     {
-        // Make sure the server doesn't happen to be adding anything right now
-        m_OutputCC.Lock();
+        JOBOBJECT_EXTENDED_LIMIT_INFORMATION limits{};
+        limits.BasicLimitInformation.LimitFlags = JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE;
 
-        // Anything to output to console?
-        if (m_OutputQueue.size() > 0)
+        if (!SetInformationJobObject(job, JobObjectExtendedLimitInformation, &limits, sizeof(limits)))
         {
-            // Loop through our output queue and echo it to console
-            std::list<std::string>::const_iterator iter = m_OutputQueue.begin();
-            for (; iter != m_OutputQueue.end(); ++iter)
-            {
-                // Echo it
-                const char* szString = iter->c_str();
-                g_pCore->GetConsole()->Echo(szString);
+            g_pCore->GetConsole()->Printf("Server process warning [error: %08x]: failed to setup job object\n", GetLastError());
 
-                // Does the message end with "Server started and is ready to accept connections!\n"?
-                size_t sizeString = iter->length();
-                if (sizeString >= 51 && stricmp(szString + sizeString - 51, "Server started and is ready to accept connections!\n") == 0)
-                {
-                    m_bIsReady = true;
-                }
-            }
-
-            // Clear the list
-            m_OutputQueue.clear();
-        }
-        // Unlock again
-        m_OutputCC.Unlock();
-    }
-    else
-    {
-        // not running, errored?
-        if (GetLastError() != ERROR_NO_ERROR)
-        {
-            Stop();
+            CloseHandle(job);
+            job = nullptr;
         }
     }
-}
 
-bool CServer::Start(const char* szConfig)
-{
-    // Not already started?
-    if (!g_bIsStarted)
+    SECURITY_ATTRIBUTES securityAttributes{};
+    securityAttributes.nLength = sizeof(securityAttributes);
+    securityAttributes.bInheritHandle = TRUE;
+
+    // Create the input pipe for the server process.
+    PipeHandlePair in;
+
+    if (!CreatePipe(&in.Read, &in.Write, &securityAttributes, 0))
     {
-        m_strConfig = szConfig;
-
-        // Check that the DLL exists
-        if (!FileExists(m_strDLLFile))
-        {
-            g_pCore->GetConsole()->Printf("Unable to find: '%s'", m_strDLLFile.c_str());
-            return false;
-        }
-
-        // We're now started, but not ready
-        g_bIsStarted = true;
-        m_bIsReady = false;
-
-        // Close the previous thread?
-        if (m_hThread != INVALID_HANDLE_VALUE)
-        {
-            CloseHandle(m_hThread);
-        }
-
-        // Create a thread to run the server
-        DWORD dwTemp;
-        m_hThread = CreateThread(NULL, 0, Thread_EntryPoint, this, 0, &dwTemp);
-        return m_hThread != NULL;
+        g_pCore->GetConsole()->Printf("Server process failed to start [error: %08x]: failed to create input pipe\n", GetLastError());
+        return false;
     }
 
-    return false;
-}
+    in.AutoClose = true;
 
-bool CServer::IsStarted()
-{
-    return g_bIsStarted;
-}
-
-bool CServer::Stop()
-{
-    // Started?
-    if (g_bIsStarted)
+    // Only inherit the read handle to the input pipe.
+    if (!SetHandleInformation(in.Write, HANDLE_FLAG_INHERIT, 0))
     {
-        // Wait for the library to come true or is started to go false
-        // This is so a call to Start then fast call to Stop will work. Otherwize it might not
-        // get time to start the server thread before we terminate it and we will end up
-        // starting it after this call to Stop.
-        while (g_bIsStarted && !m_pLibrary)
-        {
-            Sleep(1);
-        }
-
-        // Lock
-        m_CriticalSection.Lock();
-
-        // Is the server running?
-        if (m_pLibrary)
-        {
-            // Send the exit message
-            Send("exit");
-        }
-
-        // Unlock it so we won't deadlock
-        m_CriticalSection.Unlock();
+        g_pCore->GetConsole()->Printf("Server process failed to start [error: %08x]: failed to modify input pipe\n", GetLastError());
+        return false;
     }
 
-    // If we have a thread, wait for it to finish
-    if (m_hThread != INVALID_HANDLE_VALUE)
+    // Create the output pipe for the server process.
+    PipeHandlePair out;
+
+    if (!CreatePipe(&out.Read, &out.Write, &securityAttributes, 0))
     {
-        // Let the thread finish
-        WaitForSingleObject(m_hThread, INFINITE);
-
-        // If we can get an exit code, see if it's non-zero
-        DWORD dwExitCode = 0;
-        if (GetExitCodeThread(m_hThread, &dwExitCode))
-        {
-            // Handle non-zero exit codes
-            if (dwExitCode != ERROR_NO_ERROR)
-            {
-                g_pCore->ShowMessageBox(_("Error") + _E("CD60"), _("Could not start the local server. See console for details."), MB_BUTTON_OK | MB_ICON_ERROR);
-                g_pCore->GetConsole()->Printf(_("Error: Could not start local server. [%s]"), szServerErrors[GetLastError()]);
-            }
-        }
-
-        // Close it
-        CloseHandle(m_hThread);
-        m_hThread = INVALID_HANDLE_VALUE;
+        g_pCore->GetConsole()->Printf("Server process failed to start [error: %08x]: failed to create output pipe\n", GetLastError());
+        return false;
     }
 
-    m_iLastError = ERROR_NO_ERROR;
+    out.AutoClose = true;
 
+    // Only inherit the write handle to the output pipe.
+    if (!SetHandleInformation(out.Read, HANDLE_FLAG_INHERIT, 0))
+    {
+        g_pCore->GetConsole()->Printf("Server process failed to start [error: %08x]: failed to modify output pipe\n", GetLastError());
+        return false;
+    }
+
+    // Create an anonymous event to signal the readyness of the server to accept connections.
+    HANDLE readyEvent = CreateEventA(&securityAttributes, FALSE, FALSE, nullptr);
+
+    if (readyEvent == nullptr)
+    {
+        g_pCore->GetConsole()->Printf("Server process failed to start [error: %08x]: failed to create ready event\n", GetLastError());
+        return false;
+    }
+
+    // We write the event handle to standard input, which we then extract in CServerImpl::Run (Server Core).
+    DWORD bytesWritten{};
+
+    if (!WriteFile(in.Write, &readyEvent, sizeof(readyEvent), &bytesWritten, nullptr) || bytesWritten != sizeof(readyEvent))
+    {
+        g_pCore->GetConsole()->Printf("Server process failed to start [error: %08x]: failed to write ready event\n", GetLastError());
+        CloseHandle(readyEvent);
+        return false;
+    }
+
+    // Create the server process now.
+    const SString serverRoot = CalcMTASAPath("server");
+    const SString serverExePath = PathJoin(serverRoot, SERVER_EXE_PATH);
+    const SString commandLine("\"%s\" --child-process --config \"%s\"", SERVER_EXE_PATH, configFileName);
+
+    STARTUPINFOW startupInfo{};
+    startupInfo.cb = sizeof(STARTUPINFOW);
+    startupInfo.hStdError = out.Write;
+    startupInfo.hStdOutput = out.Write;
+    startupInfo.hStdInput = in.Read;
+    startupInfo.dwFlags = STARTF_USESTDHANDLES;
+
+    PROCESS_INFORMATION processInfo{};
+
+    if (!CreateProcessW(*FromUTF8(serverExePath), const_cast<wchar_t*>(*FromUTF8(commandLine)), nullptr, nullptr, TRUE,
+                        CREATE_NO_WINDOW | CREATE_UNICODE_ENVIRONMENT, nullptr, nullptr, &startupInfo, &processInfo))
+    {
+        g_pCore->GetConsole()->Printf("Server process failed to start [error: %08x]: failed to create process\n", GetLastError());
+        CloseHandle(readyEvent);
+        return false;
+    }
+
+    // Try to assign the project to our job object, if we have one.
+    if (job != nullptr && !AssignProcessToJobObject(job, processInfo.hProcess))
+    {
+        CloseHandle(job);
+        job = nullptr;
+    }
+
+    // Close the handles managed by the server process.
+    CloseHandle(in.Read);
+    CloseHandle(out.Write);
+
+    // Close the thread handle, because we don't need it.
+    CloseHandle(processInfo.hThread);
+
+    in.AutoClose = false;
+    out.AutoClose = false;
+
+    m_stdin = in.Write;
+    m_stdout = out.Read;
+    m_isAcceptingConnections = false;
+    m_isRunning = true;
+    m_job = job;
+    m_readyEvent = readyEvent;
+    m_process = processInfo.hProcess;
+    m_processId = processInfo.dwProcessId;
+
+    g_pCore->GetConsole()->Printf("Server process has started [pid: %lu]\n", m_processId);
     return true;
 }
 
-bool CServer::Send(const char* szString)
+void CServer::Stop(bool graceful)
 {
-    // Server running?
-    bool bReturn = false;
-    if (g_bIsStarted)
+    if (!m_isRunning)
+        return;
+
+    bool isRunning = IsProcessRunning(m_process);
+
+    if (graceful && isRunning)
     {
-        // Wait for the library to come true or is started to go false
-        while (g_bIsStarted && !m_pLibrary)
+        DWORD bytesWritten{};
+
+        // Try to write a normal "exit" command to the server process.
+        if (WriteFile(m_stdin, "exit\n", 5, &bytesWritten, nullptr) && bytesWritten == 5)
         {
-            Sleep(1);
+            g_pCore->GetConsole()->Printf("Sent 'exit' command to the server to shut it down\n");
+            WaitForSingleObject(m_process, 8000);
+        }
+        // Try to send a CTRl+C event to the process console.
+        else if (AttachConsole(m_processId))
+        {
+            SetConsoleCtrlHandler(nullptr, true);
+            GenerateConsoleCtrlEvent(CTRL_C_EVENT, 0);
+            FreeConsole();
+            g_pCore->GetConsole()->Printf("Sent 'CTRL+C' event to the server to shut it down\n");
+            WaitForSingleObject(m_process, 8000);
+            SetConsoleCtrlHandler(nullptr, false);
         }
 
-        // Lock
-        m_CriticalSection.Lock();
-
-        // Are we running the server
-        if (m_pLibrary)
-        {
-            // Grab the SendServerCommand function pointer
-            typedef bool(SendServerCommand_t)(const char*);
-            SendServerCommand_t* pfnSendServerCommand = reinterpret_cast<SendServerCommand_t*>(m_pLibrary->GetProcedureAddress("SendServerCommand"));
-            if (pfnSendServerCommand)
-            {
-                // Call it with the command
-                bReturn = pfnSendServerCommand(szString);
-            }
-        }
-
-        // Unlock
-        m_CriticalSection.Unlock();
+        isRunning = IsProcessRunning(m_process);
     }
 
-    // Return
-    return bReturn;
-}
-
-DWORD WINAPI CServer::Thread_EntryPoint(LPVOID pThis)
-{
-    return reinterpret_cast<CServer*>(pThis)->Thread_Run();
-}
-
-unsigned long CServer::Thread_Run()
-{
-    // Enter critical section
-    m_CriticalSection.Lock();
-
-    // Already loaded? Just return or we get a memory leak.
-    if (m_pLibrary)
+    // We either forcefully terminating the server, or the graceful shutdown failed.
+    if (isRunning)
     {
-        m_CriticalSection.Unlock();
-        return 0;
-    }
+        g_pCore->GetConsole()->Printf("Terminating the server forcefully now\n");
 
-    // Load the DLL
-    m_pLibrary = new CDynamicLibrary;
-    if (m_pLibrary->Load(m_strDLLFile))
-    {
-        // Grab the entrypoint
-        typedef int(Main_t)(int, char*[]);
-        Main_t* pfnEntryPoint = reinterpret_cast<Main_t*>(m_pLibrary->GetProcedureAddress("Run"));
-        if (pfnEntryPoint)
+        TerminateProcess(m_process, PROCESS_FORCEFULLY_TERMINATED);
+        WaitForSingleObject(m_process, 3000);
+
+        if (m_job != nullptr)
         {
-            // Populate the arguments array
-            char szArgument1[8];
-            strcpy(szArgument1, "-D");
-
-            char szArgument2[256];
-            strncpy(szArgument2, m_strServerRoot, 256);
-            szArgument2[255] = 0;
-
-            char szArgument3[16];
-            strcpy(szArgument3, "--config");
-
-            char szArgument4[64];
-            strcpy(szArgument4, m_strConfig);
-
-            char szArgument5[8];
-            strcpy(szArgument5, "-s");
-
-            char* szArguments[7];
-            szArguments[0] = szArgument1;
-            szArguments[1] = szArgument2;
-            szArguments[2] = szArgument3;
-            szArguments[3] = szArgument4;
-            szArguments[4] = szArgument5;
-
-            // Give the server our function to output stuff to the console
-            char szArgument6[32];
-            strcpy(szArgument6, "--clientfeedback");
-            szArguments[5] = szArgument6;
-            szArguments[6] = reinterpret_cast<char*>(CServer::AddServerOutput);
-
-            // We're now running the server
-            m_CriticalSection.Unlock();
-
-            // Call it and grab what it returned
-            int iReturn = pfnEntryPoint(7, szArguments);
-
-            m_iLastError = iReturn;
-
-            // Lock again
-            m_CriticalSection.Lock();
-
-            // Delete the library
-            delete m_pLibrary;
-            m_pLibrary = NULL;
-
-            // Return what the server returned
-            m_CriticalSection.Unlock();
-            g_bIsStarted = false;
-            return iReturn;
+            CloseHandle(m_job);
+            m_job = nullptr;
         }
     }
 
-    // Delete the library again
-    delete m_pLibrary;
-    m_pLibrary = NULL;
+    if (DWORD exitCode{}; GetExitCodeProcess(m_process, &exitCode))
+    {
+        const char* errorText = "Unknown exit code";
 
-    // Unlock the critialsection and return failed
-    m_CriticalSection.Unlock();
-    g_bIsStarted = false;
-    return 1;
+        if (exitCode == PROCESS_FORCEFULLY_TERMINATED)
+            errorText = "Process was forcefully terminated";
+        else if (exitCode < (sizeof(szServerErrors) / sizeof(const char*)))
+            errorText = szServerErrors[exitCode];
+
+        g_pCore->GetConsole()->Printf("Server process has been terminated [%08X]: %s\n", exitCode, errorText);
+    }
+
+    CloseHandle(m_readyEvent);
+    CloseHandle(m_stdin);
+    CloseHandle(m_stdout);
+    CloseHandle(m_process);
+
+    if (m_job != nullptr)
+    {
+        CloseHandle(m_job);
+        m_job = nullptr;
+    }
+
+    m_isRunning = false;
 }
 
-void CServer::AddServerOutput(const char* szOutput)
+void CServer::Pulse()
 {
-    // Make sure the client doesn't process the queue right now
-    m_OutputCC.Lock();
+    if (!m_isRunning)
+        return;
 
-    // Add the string to the queue
-    m_OutputQueue.push_back(szOutput);
+    // Try to read the process standard output and then write it to the console window.
+    if (DWORD numBytes{}; PeekNamedPipe(m_stdout, nullptr, 0, nullptr, &numBytes, nullptr) && numBytes > 0)
+    {
+        std::array<char, 4096> buffer{};
 
-    // Unlock again
-    m_OutputCC.Unlock();
+        if (ReadFile(m_stdout, buffer.data(), std::min<DWORD>(buffer.size(), numBytes), &numBytes, nullptr) && numBytes > 0)
+        {
+            g_pCore->GetConsole()->Printf("%.*s", numBytes, buffer.data());
+        }
+    }
+
+    // Check if the server process was terminated externally.
+    if (!IsProcessRunning(m_process))
+    {
+        Stop();
+        return;
+    }
+
+    // Check if the server process signalised readyness to accept connections.
+    if (m_readyEvent != nullptr)
+    {
+        if (WaitForSingleObject(m_readyEvent, 0) != WAIT_TIMEOUT)
+        {
+            CloseHandle(m_readyEvent);
+            m_readyEvent = nullptr;
+            m_isAcceptingConnections = true;
+        }
+    }
 }

Client/mods/deathmatch/logic/CServer.h
@@ -1,59 +1,43 @@
 /*****************************************************************************
  *
- *  PROJECT:     Multi Theft Auto v1.0
+ *  PROJECT:     Multi Theft Auto
  *  LICENSE:     See LICENSE in the top level directory
  *  FILE:        mods/deathmatch/logic/CServer.h
  *  PURPOSE:     Header for server class
  *
- *  Multi Theft Auto is available from http://www.multitheftauto.com/
+ *  Multi Theft Auto is available from https://multitheftauto.com/
  *
  *****************************************************************************/
 
 #pragma once
 
-#include "CDynamicLibrary.h"
-#include <list>
-#include <string>
-
-class CServer
+class CServer final
 {
 public:
-    CServer();
-    ~CServer();
+    ~CServer() { Stop(); }
 
-    void DoPulse();
+    bool Start(const char* configFileName);
 
-    bool Start(const char* szConfig);
-    bool Stop();
-    bool IsStarted();
-    bool IsRunning() { return m_pLibrary != NULL; };
-    bool IsReady() { return m_bIsReady; };
+    void Stop(bool graceful = true);
 
-    int GetLastError() { return m_iLastError; };
+    void Pulse();
 
-    bool Send(const char* szString);
+    bool IsRunning() const noexcept { return m_isRunning; }
 
-    const std::string& GetPassword() { return m_strPassword; };
-    void               SetPassword(const char* szPassword) { m_strPassword = szPassword; };
+    bool IsReady() const noexcept { return m_isRunning && m_isAcceptingConnections; }
+
+    void SetPassword(const char* password) { m_password = password; }
+
+    const std::string& GetPassword() const noexcept { return m_password; }
 
 private:
-    static DWORD WINAPI Thread_EntryPoint(LPVOID pThis);
-    unsigned long       Thread_Run();
-
-    bool   m_bIsReady;
-    HANDLE m_hThread;
-    CDynamicLibrary* volatile m_pLibrary;
-    CCriticalSection m_CriticalSection;
-    SString          m_strServerRoot;
-    SString          m_strDLLFile;
-    SString          m_strConfig;
-
-    int m_iLastError;
-
-    std::string m_strPassword;
-
-    static CCriticalSection       m_OutputCC;
-    static std::list<std::string> m_OutputQueue;
-
-    static void AddServerOutput(const char* szOutput);
+    bool        m_isRunning{};
+    bool        m_isAcceptingConnections{};
+    HANDLE      m_job{};
+    HANDLE      m_readyEvent{};
+    HANDLE      m_process{};
+    DWORD       m_processId{};
+    HANDLE      m_stdout{};
+    HANDLE      m_stdin{};
+    std::string m_password;
 };

Server/core/CModManagerImpl.cpp
@@ -148,6 +148,11 @@ void CModManagerImpl::DoPulse()
     }
 }
 
+bool CModManagerImpl::IsReadyToAcceptConnections() const noexcept
+{
+    return (m_pBase != nullptr) && m_pBase->IsReadyToAcceptConnections();
+}
+
 bool CModManagerImpl::IsFinished()
 {
     if (m_pBase)

Server/core/CModManagerImpl.h
@@ -46,6 +46,8 @@ public:
 
     void DoPulse();
 
+    bool IsReadyToAcceptConnections() const noexcept;
+
     bool IsFinished();
 
     bool             PendingWorkToDo();

Server/core/CServerImpl.h
@@ -30,7 +30,6 @@ typedef CXML* (*InitXMLInterface)(const char* szSaveFlagDirectory);
 typedef CNetServer* (*InitNetServerInterface)();
 
 #ifdef WIN32
-typedef void(FClientFeedback)(const char* szText);
 constexpr SHORT SCREEN_BUFFER_SIZE = 256;
 #endif
 
@@ -88,10 +87,6 @@ private:
     CNetServer*      m_pNetwork;
     CModManagerImpl* m_pModManager;
     CXML*            m_pXML;
-
-#ifdef WIN32
-    FClientFeedback* m_fClientFeedback;
-#endif
 
     SString m_strServerPath;
     SString m_strServerModPath;

commit a3c6f0f6b47d8b756f2fa1e7f036c2d21ef32967

Fix server compile error introduced in 4d37b6b

Server/core/CServerImpl.cpp
@@ -480,12 +480,14 @@ void CServerImpl::MainLoop()
         if (m_pModManager->IsFinished())
             m_bRequestedQuit = true;
 
+#ifdef WIN32
         if (g_readyEvent != nullptr && m_pModManager->IsReadyToAcceptConnections())
         {
             SetEvent(g_readyEvent);
             CloseHandle(g_readyEvent);
             g_readyEvent = nullptr;
         }
+#endif
 
         HandlePulseSleep();
     }