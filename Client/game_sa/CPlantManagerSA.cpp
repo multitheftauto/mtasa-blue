4a7a5fa2930141dde64bc4e1c8a101e83bea4e55
Uladzislau Nikalayevich <thenormalnij@gmail.com>
2024-03-26 21:11:46 +0300

    Fix plant crash

--- Client/game_sa/CPlantManagerSA.cpp
+++ Client/game_sa/CPlantManagerSA.cpp
@@ -1,15 +1,36 @@
 
 #include "StdInc.h"
 #include "CPlantManagerSA.h"
+#include "CPtrNodeSingleListSA.h"
+
+class CPlantLocTri;
+
+class CPlantColEntEntry
+{
+public:
+    CEntitySAInterface* m_Entity;
+    CPlantLocTri**     m_Objects;
+    uint16             m_numTriangles;
+    CPlantColEntEntry* m_NextEntry;
+    CPlantColEntEntry* m_PrevEntry;
+
+public:
+    void               ReleaseEntry() {
+        using CPlantColEntEntry_ReleaseEntry = void* ( __thiscall *)(CPlantColEntEntry*);
+        ((CPlantColEntEntry_ReleaseEntry)0x5DB8A0)(this);
+    };
+};
 
 void CPlantManagerSA::RemoveAllPlants()
 {
-    int    removedCount = 0;
-    void* list = *(void**)0xC0399C;
-    while (list != nullptr)
+    while (true)
     {
-        RemovePlant((CEntitySAInterface*)list);
-        removedCount++;
+        auto list = reinterpret_cast<CPlantColEntEntry**>(0xC0399C);
+        if (*list == nullptr)
+        {
+            break;
+        }
+
+        (*list)->ReleaseEntry();
     }
-    int a = removedCount;
 }
