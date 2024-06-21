ba32b26c24cec431e131cd4e4b64d5e355930ad6
srslyyyy <51768772+srslyyyy@users.noreply.github.com>
2024-06-22 01:35:07 +0200

    Fix enum errors.

--- Server/mods/deathmatch/logic/CScriptDebugging.cpp
+++ Server/mods/deathmatch/logic/CScriptDebugging.cpp
@@ -15,8 +15,7 @@
 
 extern CGame* g_pGame;
 
-enum CScriptDebugging::DebugMessageLevels
-{
+enum CScriptDebugging::DebugMessageLevels : std::uint32_t {
     MESSAGE_TYPE_DEBUG = 0,
     MESSAGE_TYPE_ERROR = 1,
     MESSAGE_TYPE_WARNING = 2,
@@ -24,8 +23,7 @@ enum CScriptDebugging::DebugMessageLevels
     MESSAGE_TYPE_CUSTOM = 4,
 };
 
-enum CScriptDebugging::DebugScriptLevels
-{
+enum CScriptDebugging::DebugScriptLevels : std::uint32_t {
     ERRORS_ONLY = 1,
     ERRORS_AND_WARNINGS = 2,
     ALL = 3,
