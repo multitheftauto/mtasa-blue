f6cb724a96bbe3cdbf2dfddc8aea189d7689e1c8
Proxy-99 <77501848+Proxy-99@users.noreply.github.com>
2024-07-16 22:20:15 +0300

    moved to clock class

--- Client/game_sa/CWeatherSA.h
+++ Client/game_sa/CWeatherSA.h
@@ -60,6 +60,4 @@ private:
     static unsigned char* VAR_CWeather__OldWeatherType;
     static unsigned char* VAR_CWeather__NewWeatherType;
     static float*         VAR_CWeather__Rain;
-    bool                  m_bTimeCycleFrozen;
-
 };
