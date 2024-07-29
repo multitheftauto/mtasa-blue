e5f51fe46c495223c17209c9156aade45233cd8c
Tracer <43095317+TracerDS@users.noreply.github.com>
2024-07-29 19:24:36 +0200

    Update CCameraSA.cpp

--- Client/game_sa/CCameraSA.cpp
+++ Client/game_sa/CCameraSA.cpp
@@ -446,8 +446,13 @@ float CCameraSA::GetShakeForce()
 
 void CCameraSA::ShakeCamera(float radius, float x, float y, float z) noexcept
 {
-    DWORD               dwFunc = FUNC_ShakeCam;
-    CCameraSAInterface* cameraInterface = GetInterface();
+    static CCameraSAInterface* cameraInterface = GetInterface();
+    if (radius == 0.0f) {
+        cameraInterface->m_fCamShakeForce = 0;
+        return;
+    }
+
+    static const DWORD dwFunc = FUNC_ShakeCam;
     _asm
     {
         mov  ecx, cameraInterface
