b85ff829603f94090689282849fa50afe33cfe97
Tracer <43095317+TracerDS@users.noreply.github.com>
2024-08-07 15:05:08 +0200

    Update CHudSA.cpp

--- Client/game_sa/CHudSA.cpp
+++ Client/game_sa/CHudSA.cpp
@@ -43,6 +43,8 @@ void CHudSA::Disable(bool bDisabled)
     else
         MemPut<BYTE>(FUNC_Draw, 0x80);
 
+    SetComponentVisible(HUD_ALL, !bDisabled);    
+
     // Also disable the radar as the above code will not hide it before the local player has spawned
     if (bDisabled)
         MemPut<BYTE>(FUNC_DrawRadarPlanB, 0xC3);
