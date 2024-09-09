a9d305d678454c030ac5686a03cf4e3470c6cf29
Marek Kulik <me@botder.com>
2024-09-10 00:25:08 +0200

    Revert commit f51218256661e4f4a9434c36594e4e75f68c3f8d

--- utils/buildactions/install_cef.lua
+++ utils/buildactions/install_cef.lua
@@ -6,7 +6,7 @@ premake.modules.install_cef = {}
 local CEF_PATH = "vendor/cef3/cef/"
 local CEF_TEMP_PATH = "vendor/cef3/"
 local CEF_URL_PREFIX = "https://cef-builds.spotifycdn.com/cef_binary_"
-local CEF_URL_SUFFIX = "_windows32_beta_minimal.tar.bz2"
+local CEF_URL_SUFFIX = "_windows32_minimal.tar.bz2"
 
 -- Change here to update CEF version
 local CEF_VERSION = "109.1.18+gf1c41e4+chromium-109.0.5414.120"
