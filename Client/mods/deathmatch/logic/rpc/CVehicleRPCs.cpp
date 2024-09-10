ec6a4ad79fb01b022c2ac002e464f8a65576dacb
Proxy-99 <77501848+Proxy-99@users.noreply.github.com>
2024-09-10 18:41:45 +0300

    correct type

--- Client/mods/deathmatch/logic/rpc/CVehicleRPCs.cpp
+++ Client/mods/deathmatch/logic/rpc/CVehicleRPCs.cpp
@@ -668,7 +668,7 @@ void CVehicleRPCs::SetVehicleNitroActivated(CClientEntity* pSourceEntity, NetBit
         return;
 
      // If nitro level < 0, nitro is activated. (until nitro level reaches -1, at that point it will become 0 and increase instead of decrease)
-    if ((vehicle->GetNitroLevel() < 0) == state)
+    if ((vehicle->GetNitroLevel() < 0.0f) == state)
         return;
 
     // Apply nitro level change
