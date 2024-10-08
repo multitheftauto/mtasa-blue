ba6b8a4e518a747fa5b6784a68681571126df402
lopsi <40902730+Lpsd@users.noreply.github.com>
2024-10-08 18:34:06 +0100

    Update Client/cefweb/CWebApp.cpp
    Co-authored-by: Uladzislau Nikalayevich <thenormalnij@gmail.com>

--- Client/cefweb/CWebApp.cpp
+++ Client/cefweb/CWebApp.cpp
@@ -28,10 +28,6 @@ void CWebApp::OnRegisterCustomSchemes(CefRawPtr<CefSchemeRegistrar> registrar)
 void CWebApp::OnBeforeCommandLineProcessing(const CefString& process_type, CefRefPtr<CefCommandLine> command_line)
 {
     command_line->AppendSwitch("disable-gpu-compositing");
-    
-    // "disable-gpu" disables hardware acceleration and then WebGL is not working
-    // command_line->AppendSwitch("disable-gpu");
-     
     // command_line->AppendSwitch("disable-d3d11");
     command_line->AppendSwitch("enable-begin-frame-scheduling");
 
