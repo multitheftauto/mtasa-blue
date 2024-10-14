22f98f7de02460ceb73f0e3a8154daded2fc0aec
G_Moris <129879037+Black-Sun-Team@users.noreply.github.com>
2024-10-14 14:48:44 +0300

    adjustments

--- Client/game_sa/premake5.lua
+++ Client/game_sa/premake5.lua
@@ -4,8 +4,6 @@ project "Game SA"
 	targetname "game_sa"
 	targetdir(buildpath("mta"))
 
-	-- HACK(Jusonex): Temp fix for ebp not being set in naked functions
-	-- More information on this in multiplayer_sa's premake5.lua
 	cppdialect "C++20"
 
 	pchheader "StdInc.h"
