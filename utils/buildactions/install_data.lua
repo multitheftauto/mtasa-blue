d3d57403403661388bfd7acc5669840c3f625db0
Fernando-A-Rocha <frocha2001@gmail.com>
2024-11-22 11:07:15 +0000

    mtaserver.conf.template add header comment

--- utils/buildactions/install_data.lua
+++ utils/buildactions/install_data.lua
@@ -49,7 +49,7 @@ newaction {
 			return
 		end
 
-		if not os.copyfile(BIN_DIR.."/server/mods/deathmatch/mtaserver.conf", BIN_DIR.."/server/mods/deathmatch/mtaserver.conf.template") then
+		if not os.makeconfigtemplate(BIN_DIR.."/server/mods/deathmatch/mtaserver.conf", BIN_DIR.."/server/mods/deathmatch/mtaserver.conf.template") then
 			errormsg("ERROR: Could not copy mtaserver.conf to mtaserver.conf.template")
 			os.exit(1)
 			return
@@ -75,7 +75,7 @@ newaction {
 			success = success and http.download_print_errors(NET_PATH_X64_WIN, BIN_DIR.."/server/x64/net.dll")
 			success = success and http.download_print_errors(NET_PATH_ARM64_WIN, BIN_DIR.."/server/arm64/net.dll")
 			success = success and http.download_print_errors(NETC_PATH_WIN, BIN_DIR.."/MTA/netc.dll")
-			
+
 			-- A download failed
 			if not success then
 				os.exit(1)
