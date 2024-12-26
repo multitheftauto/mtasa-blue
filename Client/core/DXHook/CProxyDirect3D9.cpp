a2543bdb72d88d5e400be75ec4046ce6b5323633
G_Moris <129879037+Black-Sun-Team@users.noreply.github.com>
2024-12-26 14:51:46 +0300

    Clean

--- Client/core/DXHook/CProxyDirect3D9.cpp
+++ Client/core/DXHook/CProxyDirect3D9.cpp
@@ -12,6 +12,54 @@
 #include "StdInc.h"
 #include <dwmapi.h>
 #include <resource.h>
+#include <tlhelp32.h>
+
+static HWND FindWindowByProcessName(const char* processName)
+{
+    HWND  hwnd = NULL;
+    DWORD pid = 0;
+
+    // Найти процесс по имени
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hSnapshot != INVALID_HANDLE_VALUE)
+    {
+        PROCESSENTRY32 pe;
+        pe.dwSize = sizeof(pe);
+
+        if (Process32First(hSnapshot, &pe))
+        {
+            do
+            {
+                if (strcmp(pe.szExeFile, processName) == 0)
+                {
+                    pid = pe.th32ProcessID;
+                    break;
+                }
+            } while (Process32Next(hSnapshot, &pe));
+        }
+        CloseHandle(hSnapshot);
+    }
+
+    if (pid == 0)
+        return NULL;
+
+    // Найти окно по PID
+    EnumWindows(
+        [](HWND hwnd, LPARAM lParam) -> BOOL
+        {
+            DWORD windowPid;
+            GetWindowThreadProcessId(hwnd, &windowPid);
+            if (windowPid == (DWORD)lParam)
+            {
+                *((HWND*)lParam) = hwnd;
+                return FALSE;
+            }
+            return TRUE;
+        },
+        (LPARAM)&pid);
+
+    return hwnd;
+}
 
 extern HINSTANCE g_hModule;
 
@@ -176,13 +224,16 @@ HRESULT CProxyDirect3D9::CreateDevice(UINT Adapter, D3DDEVTYPE DeviceType, HWND 
     DwmSetWindowAttribute(hFocusWindow, DWMWA_USE_IMMERSIVE_DARK_MODE, &darkTitleBar, sizeof(darkTitleBar));
 
     // Update icon
-    if (const HICON icon = LoadIconA(g_hModule, MAKEINTRESOURCE(IDI_ICON1)))
+    if (HICON icon = LoadIconA(g_hModule, MAKEINTRESOURCE(IDI_ICON1)))
     {
         const auto paramIcon = reinterpret_cast<LPARAM>(icon);
         for (const WPARAM size : {ICON_SMALL, ICON_BIG})
         {
             SendMessage(hFocusWindow, WM_SETICON, size, paramIcon);
         }
+
+        // Clean
+        DestroyIcon(icon);
     }
 
     // Redraw, we avoid possible problems with the fact that it won't replace the icon somewhere
