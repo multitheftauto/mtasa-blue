b32eafc70816ece8ad995d98d380d8f6e9950475
FileEX <alkom@t.pl>
2025-01-03 23:19:10 +0100

    Synchronize ped animations for new players (PR #3520, Fixes #467)

--- Client/mods/deathmatch/logic/CClientPed.h
+++ Client/mods/deathmatch/logic/CClientPed.h
@@ -552,6 +552,9 @@ public:
 
     std::unique_ptr<CAnimBlendAssociation> GetAnimAssociation(CAnimBlendHierarchySAInterface* pHierarchyInterface);
 
+    void SetHasSyncedAnim(bool synced) noexcept { m_hasSyncedAnim = synced; }
+    bool HasSyncedAnim() const noexcept { return m_hasSyncedAnim; }
+
 protected:
     // This constructor is for peds managed by a player. These are unknown to the ped manager.
     CClientPed(CClientManager* pManager, unsigned long ulModelID, ElementID ID, bool bIsLocalPlayer);
@@ -789,4 +792,7 @@ public:
     CClientPed*   m_pGettingJackedBy;                    // The ped that is jacking us
 
     std::shared_ptr<CClientModel> m_clientModel;
+
+    bool m_hasSyncedAnim{};
+    bool m_animationOverridedByClient{};
 };
