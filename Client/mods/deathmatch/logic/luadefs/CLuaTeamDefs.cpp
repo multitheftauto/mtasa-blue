45f09df09c012d0a8bf1e5a4db2e3341103b299c
Nico <122193236+Nico8340@users.noreply.github.com>
2025-01-10 22:50:53 +0100

    Make changes

--- Client/mods/deathmatch/logic/luadefs/CLuaTeamDefs.cpp
+++ Client/mods/deathmatch/logic/luadefs/CLuaTeamDefs.cpp
@@ -45,10 +45,9 @@ void CLuaTeamDefs::AddClass(lua_State* luaVM)
     lua_registerclass(luaVM, "Team", "Element");
 }
 
-std::variant<CClientTeam*, bool> CLuaTeamDefs::GetTeamFromName(const std::string_view name) noexcept
+std::variant<CClientTeam*, bool> CLuaTeamDefs::GetTeamFromName(const std::string name) noexcept
 {
-    std::string  string(name);
-    CClientTeam* team = m_pTeamManager->GetTeam(string.c_str());
+    CClientTeam* team = m_pTeamManager->GetTeam(name.c_str());
 
     if (!team)
         return false;
