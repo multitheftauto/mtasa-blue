e283332072d629a6c4b68296549601845a7335ee
Felipe A. <73603452+imfelipedev@users.noreply.github.com>
2025-01-09 23:58:12 -0300

    Update Server/mods/deathmatch/logic/CGame.cpp
    Co-authored-by: Nico <122193236+Nico8340@users.noreply.github.com>

--- Server/mods/deathmatch/logic/CGame.cpp
+++ Server/mods/deathmatch/logic/CGame.cpp
@@ -1638,7 +1638,7 @@ void CGame::AddBuiltInEvents()
     m_Events.AddEvent("onPlayerTriggerInvalidEvent", "eventName, isAdded, isRemote", nullptr, false);
     m_Events.AddEvent("onPlayerChangesProtectedData", "element, key, value", nullptr, false);
     m_Events.AddEvent("onPlayerChangesWorldSpecialProperty", "property, enabled", nullptr, false);
-    m_Events.AddEvent("onPlayerTeleport", "", NULL, false);
+    m_Events.AddEvent("onPlayerTeleport", "", nullptr, false);
 
     // Ped events
     m_Events.AddEvent("onPedVehicleEnter", "vehicle, seat, jacked", NULL, false);
