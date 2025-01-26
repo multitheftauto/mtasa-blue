ee0a4f932d01b537fb89d2e16c47cdf8a83ee579
Uladzislau Nikalayevich <thenormalnij@gmail.com>
2025-01-26 19:51:50 +0300

    Add m_pBuilding check

--- Client/mods/deathmatch/logic/CClientBuilding.cpp
+++ Client/mods/deathmatch/logic/CClientBuilding.cpp
@@ -132,6 +132,10 @@ void CClientBuilding::Create()
     ConvertZXYEulersToQuaternion(m_vRot, vRot4D);
 
     m_pBuilding = g_pGame->GetPools()->GetBuildingsPool().AddBuilding(this, m_usModelId, &m_vPos, &vRot4D, m_interior);
+
+    if (!m_pBuilding)
+        return;
+
     m_pBuilding->SetBackfaceCulled(!m_bDoubleSided);
 
     if (!m_usesCollision)
