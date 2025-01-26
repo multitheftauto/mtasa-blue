3b6b736043f4040457f1a6c034d6cff107239996
Walace <fefeduartepers@gmail.com>
2025-01-26 10:38:44 -0300

    Create CDirectorySA Class

---
+++ Client/game_sa/CDirectorySA.cpp
@@ -0,0 +1,40 @@
+#include "StdInc.h"
+#include "CDirectorySA.h"
+
+DirectoryInfo* CDirectorySA::GetModelEntry(ushort modelId)
+{
+    if (m_nNumEntries <= 0)
+        return nullptr;
+
+    DirectoryInfo* entry = m_pEntries + modelId;
+
+    if (!entry)
+        return nullptr;
+
+    return entry;
+}
+
+bool CDirectorySA::SetModelStreamingSize(ushort modelId, uint16 size)
+{
+    DirectoryInfo* entry = GetModelEntry(modelId);
+
+    if (!entry)
+        return false;
+
+    if (entry->m_nStreamingSize == size)
+        return false;
+
+    entry->m_nStreamingSize = size;
+    return true;
+}
+
+
+uint16 CDirectorySA::GetModelStreamingSize(ushort modelId)
+{
+    DirectoryInfo* entry = GetModelEntry(modelId);
+
+    if (!entry)
+        return false;
+
+    return entry->m_nStreamingSize;
+}
---
+++ Client/game_sa/CDirectorySA.h
@@ -0,0 +1,23 @@
+#include <SharedUtil.IntTypes.h>
+
+struct DirectoryInfo
+{
+    uint32 m_nOffset;
+    uint16 m_nStreamingSize;
+    uint16 m_nSizeInArchive;
+    char   m_szName[24];
+};
+
+class CDirectorySA
+{
+    public:
+        DirectoryInfo*  GetModelEntry(ushort modelId);
+        bool            SetModelStreamingSize(ushort modelId, uint16 size);
+        uint16          GetModelStreamingSize(ushort modelId);
+
+    private:
+        DirectoryInfo* m_pEntries{};
+        uint32         m_nCapacity{};
+        uint32         m_nNumEntries{};
+        bool           m_bOwnsEntries{};
+};
