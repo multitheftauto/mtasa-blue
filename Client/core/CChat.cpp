multitheftauto/mtasa-blue

commit 4639aea8a5544bfa4460bfcc8bba1d5b032e931a

Hide login cmd from chat history (#3725)
Censor /login command in chatbox history (fixes#2518). This will also stop logging anything said using `/bind <key> chatbox login`

Client/core/CChat.cpp
@@ -650,7 +650,28 @@ bool CChat::CharacterKeyHandler(CGUIKeyEventArgs KeyboardArgs)
 
             // If the input isn't empty and isn't identical to the previous entry in history, add it to the history
             if (!m_strInputText.empty() && (m_pInputHistory->Empty() || m_pInputHistory->GetLast() != m_strInputText))
-                m_pInputHistory->Add(m_strInputText);
+            {
+                if (m_strCommand.empty() && m_strInputText[0] != '/')
+                {
+                    // If the input is not a command, store it
+                    m_pInputHistory->Add(m_strInputText);
+                }
+                else if (m_strCommand.compare("login") != 0)
+                {
+                    // If the input is a command, check that it isn't the 'login' command, if it is censor it
+                    char szInput[256];
+                    strncpy(szInput, m_strInputText.c_str() + 1, 256);
+
+                    const char* szCommand;
+                    if (szInput[0])
+                        szCommand = strtok(szInput, " ");
+
+                    if ((strcmp(szCommand, "login") != 0))
+                        m_pInputHistory->Add(m_strInputText);
+                    else if ((m_pInputHistory->Empty() || m_pInputHistory->GetLast() != std::string("/login")))
+                        m_pInputHistory->Add("/login");
+                }
+            }
 
             SetInputVisible(false);
 

commit d279e3c5aa6991968b18db6b3c750b06e3528487

Fix crash caused by #3725 in `CChat::CharacterKeyHandler` (#4060)

Client/core/CChat.cpp
@@ -660,13 +660,14 @@ bool CChat::CharacterKeyHandler(CGUIKeyEventArgs KeyboardArgs)
                 {
                     // If the input is a command, check that it isn't the 'login' command, if it is censor it
                     char szInput[256];
-                    strncpy(szInput, m_strInputText.c_str() + 1, 256);
+                    unsigned int uiLength = sizeof(szInput) - 1;
 
-                    const char* szCommand;
-                    if (szInput[0])
-                        szCommand = strtok(szInput, " ");
+                    strncpy(szInput, m_strInputText.c_str() + 1, uiLength);
+                    szInput[uiLength] = '\0';
 
-                    if ((strcmp(szCommand, "login") != 0))
+                    const char* szCommand = strtok(szInput, " ");
+                    
+                    if (szCommand && (strcmp(szCommand, "login") != 0))
                         m_pInputHistory->Add(m_strInputText);
                     else if ((m_pInputHistory->Empty() || m_pInputHistory->GetLast() != std::string("/login")))
                         m_pInputHistory->Add("/login");