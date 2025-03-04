multitheftauto/mtasa-blue

commit 924c9204d70e1e6c46ea2b87112802ce103d3168

Add digital signature to DLL's updated today:
bass_aac.dll
bass_ac3.dll
bass_fx.dll
tags.dll
XInput9_1_0_mta.dll

Client/loader/MainFunctions.cpp
@@ -863,15 +863,15 @@ void CheckDataFiles()
     {
         const char* expected;
         const char* fileName;
-    } integrityCheckList[] = {{"24F61A108D285381A67CE643ABBF589C", "bass.dll"},           {"FC6ECF2463FE550A2C849D1B4855400A", "bass_aac.dll"},
-                              {"07C11F7D8058F350ADF6FC9AB81B38AC", "bass_ac3.dll"},       {"C9B7CA8AB10E128FAEFC78F4F48D6220", "bass_fx.dll"},
+    } integrityCheckList[] = {{"24F61A108D285381A67CE643ABBF589C", "bass.dll"},           {"5F9352A9D36E12E1E167E28040EADA83", "bass_aac.dll"},
+                              {"F0D770E77C5A5816D9E2323CC683BA32", "bass_ac3.dll"},       {"D6669FA6DFD548E60CC20503ABCF2FF8", "bass_fx.dll"},
                               {"F47DCE69DAFAA06A55A4BC1F07F80C8A", "bassflac.dll"},       {"CD9B45DFE7D8B4D547F8B0193C6CF30B", "bassmidi.dll"},
                               {"F1F587B6515530634AD489CE507BC957", "bassmix.dll"},        {"9CFA31A873FF89C2CC491B9974FC5C65", "bassopus.dll"},
                               {"0F1B2FC6C0C703A43A24DC05352E7ADA", "basswebm.dll"},       {"1507C60C02E159B5FB247FEC6B209B09", "basswma.dll"},
                               {"C6A44FC3CF2F5801561804272217B14D", "D3DX9_42.dll"},       {"D439E8EDD8C93D7ADE9C04BCFE9197C6", "sa.dat"},
-                              {"B33B21DB610116262D906305CE65C354", "D3DCompiler_42.dll"}, {"C9523E51FF62D5122939FD4BDF47ED9D", "tags.dll"},
+                              {"B33B21DB610116262D906305CE65C354", "D3DCompiler_42.dll"}, {"4B3932359373F11CBC542CC96D9A9285", "tags.dll"},
                               {"0B3DD892007FB366D1F52F2247C046F5", "d3dcompiler_43.dll"}, {"D5D8C8561C6DDA7EF0D7D6ABB0D772F4", "xinput1_3_mta.dll"},
-                              {"2C0C596EE071B93CE15130BD5EE9CD31", "d3dcompiler_47.dll"}, {"CF99634BBF5E557B95FC6C0691418CB3", "XInput9_1_0_mta.dll"}};
+                              {"2C0C596EE071B93CE15130BD5EE9CD31", "d3dcompiler_47.dll"}, {"F1CA5A1E77965777AC26A81EAF345A7A", "XInput9_1_0_mta.dll"}};
 
     for (const auto& item : integrityCheckList)
     {

commit 270c2986e56cafcaf0ae3f454f2ada9efae00e89

Fix integrity checklist
It broke after commit 924c9204d70e1e6c46ea2b87112802ce103d3168

Client/loader/MainFunctions.cpp
@@ -863,15 +863,15 @@ void CheckDataFiles()
     {
         const char* expected;
         const char* fileName;
-    } integrityCheckList[] = {{"24F61A108D285381A67CE643ABBF589C", "bass.dll"},           {"FC6ECF2463FE550A2C849D1B4855400A", "bass_aac.dll"},
-                              {"07C11F7D8058F350ADF6FC9AB81B38AC", "bass_ac3.dll"},       {"C9B7CA8AB10E128FAEFC78F4F48D6220", "bass_fx.dll"},
+    } integrityCheckList[] = {{"24F61A108D285381A67CE643ABBF589C", "bass.dll"},           {"5F9352A9D36E12E1E167E28040EADA83", "bass_aac.dll"},
+                              {"F0D770E77C5A5816D9E2323CC683BA32", "bass_ac3.dll"},       {"D6669FA6DFD548E60CC20503ABCF2FF8", "bass_fx.dll"},
                               {"F47DCE69DAFAA06A55A4BC1F07F80C8A", "bassflac.dll"},       {"CD9B45DFE7D8B4D547F8B0193C6CF30B", "bassmidi.dll"},
                               {"F1F587B6515530634AD489CE507BC957", "bassmix.dll"},        {"9CFA31A873FF89C2CC491B9974FC5C65", "bassopus.dll"},
                               {"0F1B2FC6C0C703A43A24DC05352E7ADA", "basswebm.dll"},       {"1507C60C02E159B5FB247FEC6B209B09", "basswma.dll"},
                               {"C6A44FC3CF2F5801561804272217B14D", "D3DX9_42.dll"},       {"D439E8EDD8C93D7ADE9C04BCFE9197C6", "sa.dat"},
-                              {"B33B21DB610116262D906305CE65C354", "D3DCompiler_42.dll"}, {"C9523E51FF62D5122939FD4BDF47ED9D", "tags.dll"},
+                              {"B33B21DB610116262D906305CE65C354", "D3DCompiler_42.dll"}, {"4B3932359373F11CBC542CC96D9A9285", "tags.dll"},
                               {"0B3DD892007FB366D1F52F2247C046F5", "d3dcompiler_43.dll"}, {"D5D8C8561C6DDA7EF0D7D6ABB0D772F4", "xinput1_3_mta.dll"},
-                              {"E1677EC0E21E27405E65E31419980348", "d3dcompiler_47.dll"}, {"87F689D5636B6F31DBB4B9CBF56E17E3", "XInput9_1_0_mta.dll"}};
+                              {"E1677EC0E21E27405E65E31419980348", "d3dcompiler_47.dll"}, {"F1CA5A1E77965777AC26A81EAF345A7A", "XInput9_1_0_mta.dll"}};
 
     for (const auto& item : integrityCheckList)
     {