f4958998ea932526837e307f41efb576ec29bbfd
Dutchman101 <12105539+Dutchman101@users.noreply.github.com>
2025-03-11 21:02:59 +0000

    Update CEF to 134.3.0+g2830d3a+chromium-134.0.6998.44

--- utils/buildactions/install_cef.lua
+++ utils/buildactions/install_cef.lua
@@ -9,8 +9,8 @@ local CEF_URL_PREFIX = "https://cef-builds.spotifycdn.com/cef_binary_"
 local CEF_URL_SUFFIX = "_windows32_minimal.tar.bz2"
 
 -- Change here to update CEF version
-local CEF_VERSION = "133.4.5+gdb28106+chromium-133.0.6943.142"
-local CEF_HASH = "043212ba3af5285529ee1f3c167f5b1c93a05493a095b6408cbbfde0569f6c5d"
+local CEF_VERSION = "134.3.0+g2830d3a+chromium-134.0.6998.44"
+local CEF_HASH = "fda0e6f5a4183983db0add6c46ec7c7b10309c8891e5cb78beccd4aee1e1f8d8"
 
 function make_cef_download_url()
 	return CEF_URL_PREFIX..CEF_VERSION..CEF_URL_SUFFIX
