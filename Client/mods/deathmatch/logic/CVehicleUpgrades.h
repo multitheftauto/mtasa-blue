f497b5f6b821532aa25d776d10f2150f45b9b5de
Nico <122193236+Nico8340@users.noreply.github.com>
2025-04-07 23:57:27 +0200

    Fix vehicle upgrades for assigned vehicles (#4133)

--- Client/mods/deathmatch/logic/CVehicleUpgrades.h
+++ Client/mods/deathmatch/logic/CVehicleUpgrades.h
@@ -30,7 +30,7 @@ public:
     CVehicleUpgrades(CClientVehicle* pVehicle);
 
     static bool IsUpgrade(unsigned short usModel);
-    bool        IsUpgradeCompatible(unsigned short usUpgrade);
+    bool        IsUpgradeCompatible(std::uint16_t upgrade);
     static bool GetSlotFromUpgrade(unsigned short usUpgrade, unsigned char& ucSlot);
 
     bool               AddUpgrade(unsigned short usUpgrade, bool bAddedLocally);
