multitheftauto/mtasa-blue

commit 466c162fbd1af45b290849cc0771fab66f55ba34

Eliminate the use of http:// across whole project * Needs some testing. If something turns out to not support https and breaks, please file an issue

Server/mods/deathmatch/logic/CRemoteCalls.cpp
@@ -5,7 +5,7 @@
  *  FILE:        mods/deathmatch/logic/CRemoteCalls.cpp
  *  PURPOSE:     Remote HTTP call (callRemote) class
  *
- *  Multi Theft Auto is available from http://www.multitheftauto.com/
+ *  Multi Theft Auto is available from https://www.multitheftauto.com/
  *
  *****************************************************************************/
 
@@ -162,7 +162,7 @@ CRemoteCall::CRemoteCall(const char* szServerHost, const char* szResourceName, c
     m_options.requestHeaders["Content-Type"] = "application/json";
     m_bIsFetch = false;
 
-    m_strURL = SString("http://%s/%s/call/%s", szServerHost, szResourceName, szFunctionName);
+    m_strURL = SString("https://%s/%s/call/%s", szServerHost, szResourceName, szFunctionName);
     m_strQueueName = strQueueName;
     m_options.uiConnectionAttempts = uiConnectionAttempts;
     m_options.uiConnectTimeoutMs = uiConnectTimeoutMs;

Client/mods/deathmatch/logic/CRemoteCalls.cpp
@@ -5,7 +5,7 @@
  *  FILE:        mods/deathmatch/logic/CRemoteCalls.cpp
  *  PURPOSE:     Remote HTTP call (callRemote) class
  *
- *  Multi Theft Auto is available from http://www.multitheftauto.com/
+ *  Multi Theft Auto is available from https://www.multitheftauto.com/
  *
  *****************************************************************************/
 
@@ -149,7 +149,7 @@ void CRemoteCall::MakeCall()
     // GetDomainFromURL requires protocol://, but curl does not (defaults to http)
     SString strDomain = g_pCore->GetWebCore()->GetDomainFromURL(m_strURL);
     if (strDomain.empty())
-        strDomain = g_pCore->GetWebCore()->GetDomainFromURL("http://" + m_strURL);
+        strDomain = g_pCore->GetWebCore()->GetDomainFromURL("https://" + m_strURL);
     // Bypass net module IP check if we are allowed to access the URL
     bool bAnyHost = (g_pCore->GetWebCore()->GetDomainState(strDomain) == eURLState::WEBPAGE_ALLOWED);
     m_downloadMode = g_pClientGame->GetRemoteCalls()->GetDownloadModeForQueueName(m_strQueueName, bAnyHost);

commit 4f119961d3975d88b98b6821d842c39ffc4dc123

Fix callRemote (regression from 466c162)

Server/mods/deathmatch/logic/CRemoteCalls.cpp
@@ -162,7 +162,7 @@ CRemoteCall::CRemoteCall(const char* szServerHost, const char* szResourceName, c
     m_options.requestHeaders["Content-Type"] = "application/json";
     m_bIsFetch = false;
 
-    m_strURL = SString("https://%s/%s/call/%s", szServerHost, szResourceName, szFunctionName);
+    m_strURL = SString("http://%s/%s/call/%s", szServerHost, szResourceName, szFunctionName);
     m_strQueueName = strQueueName;
     m_options.uiConnectionAttempts = uiConnectionAttempts;
     m_options.uiConnectTimeoutMs = uiConnectTimeoutMs;