012c25f36d3a15b9805e4ce73a313103cddf3e0a
FileEX <kongali@interia.pl>
2025-05-17 01:23:56 +0200

    Update CMultiplayerSA_FrameRateFixes.cpp

--- Client/multiplayer_sa/CMultiplayerSA_FrameRateFixes.cpp
+++ Client/multiplayer_sa/CMultiplayerSA_FrameRateFixes.cpp
@@ -586,6 +586,10 @@ static void __declspec(naked) HOOK_CWeapon_Update()
 {
     _asm
     {
+        // Temp fix for camera
+        cmp [esi], 0x2B // CWeapon::m_eWeaponType
+        je skip
+
         // timeStep / kOriginalTimeStep
         fld ds:[0xB7CB5C] // CTimer::ms_fTimeStep
         fdiv kOriginalTimeStep
@@ -606,6 +610,10 @@ static void __declspec(naked) HOOK_CWeapon_Update()
         mov eax, ebx
 
         xor ebx, ebx
+        jmp RETURN_CWeapon_Update
+
+        skip:
+        mov eax, ds:[0xB7CB84]
         jmp RETURN_CWeapon_Update
     }
 }
