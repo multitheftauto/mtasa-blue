6230161f8d0c83b60aec3f4afa5be88dd213b88b
Sam <samr46@users.noreply.github.com>
2025-05-17 23:34:31 +0300

    Add zlib compression support (PR #4080)

--- Shared/mods/deathmatch/logic/Enums.h
+++ Shared/mods/deathmatch/logic/Enums.h
@@ -72,6 +72,8 @@ DECLARE_ENUM_CLASS(PasswordHashFunction);
 DECLARE_ENUM_CLASS(StringEncodeFunction);
 DECLARE_ENUM_CLASS(KeyPairAlgorithm);
 DECLARE_ENUM_CLASS(HmacAlgorithm);
+DECLARE_ENUM_CLASS(ZLibFormat);
+DECLARE_ENUM_CLASS(ZLibStrategy);
 
 enum class WorldSpecialProperty
 {
