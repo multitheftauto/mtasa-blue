dd3014b60b0e3c263b36e0293d9a2cb5225684ee
ColombuxMaximus <momemtumcorpfzco@gmail.com>
2025-07-16 20:12:47 +0200

    Update CClientGame.cpp

--- Client/mods/deathmatch/logic/CClientGame.cpp
+++ Client/mods/deathmatch/logic/CClientGame.cpp
@@ -2722,6 +2722,7 @@ void CClientGame::AddBuiltInEvents()
     // Console events
     m_Events.AddEvent("onClientConsole", "text", NULL, false);
     m_Events.AddEvent("onClientCoreCommand", "command", NULL, false);
+    m_Events.AddEvent("onClientCommand", "command", NULL, false);
 
     // Chat events
     m_Events.AddEvent("onClientChatMessage", "text, r, g, b, messageType", NULL, false);
