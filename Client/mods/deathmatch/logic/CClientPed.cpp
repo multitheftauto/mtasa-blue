a2e87991929a6beb1912db589a7f6177e3b04b21
FileEX <kongali@interia.pl>
2025-08-21 23:53:03 +0200

    Better tolerance

--- Client/mods/deathmatch/logic/CClientPed.cpp
+++ Client/mods/deathmatch/logic/CClientPed.cpp
@@ -4682,7 +4682,7 @@ bool CClientPed::IsOnGround(bool checkVehicles)
     if (DefinitelyLessThan(vecPosition.fZ, fGroundLevel))
         return false;
 
-    bool isOnGround = DefinitelyLessThan((vecPosition.fZ - fGroundLevel), 1.0f) || EssentiallyEqual((vecPosition.fZ - fGroundLevel), 1.0f);
+    bool isOnGround = DefinitelyLessThan((vecPosition.fZ - fGroundLevel), 1.0f, 1e-4f) || EssentiallyEqual((vecPosition.fZ - fGroundLevel), 1.0f, 1e-4f);
     if (!isOnGround && checkVehicles && m_pPlayerPed)
         return m_pPlayerPed->IsStandingOnEntity();
 
