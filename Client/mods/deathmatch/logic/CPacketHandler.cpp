85e37b9cee6291c30b657ad8078bcc9b1c443e17
Proxy-99 <77501848+Proxy-99@users.noreply.github.com>
2025-09-01 19:21:53 +0300

    put it at correct packet

--- Client/mods/deathmatch/logic/CPacketHandler.cpp
+++ Client/mods/deathmatch/logic/CPacketHandler.cpp
@@ -2651,25 +2651,6 @@ void CPacketHandler::Packet_MapInfo(NetBitStreamInterface& bitStream)
     bitStream.ReadBit(bOcclusionsEnabled);
 
     g_pGame->GetWorld()->SetOcclusionsEnabled(bOcclusionsEnabled);
-
-    unsigned int count = 0;
-    bitStream.ReadCompressed(count);
-    for (unsigned int i = 0; i < count; ++i)
-    {
-        ElementID id1, id2;
-        bool      canCollide;
-        bitStream.Read(id1);
-        bitStream.Read(id2);
-        bitStream.ReadBit(canCollide);
-        // Use id1, id2, canCollide
-
-        CClientEntity* pEntity1 = CElementIDs::GetElement(id1);
-        CClientEntity* pEntity2 = CElementIDs::GetElement(id2);
-
-      pEntity1->SetCollidableWith(pEntity2, canCollide);
-    }
-     SString strCount = SString("Collision pairs count: %u", count);
-    CStaticFunctionDefinitions::OutputConsole(strCount);
 }
 
 void CPacketHandler::Packet_PartialPacketInfo(NetBitStreamInterface& bitStream)
@@ -4203,7 +4184,7 @@ retry:
                 {
                     assert(0);
                     break;
-                }
+                }       
             }
 
             if (pEntity)
@@ -4286,6 +4267,27 @@ retry:
     }
     newEntitiesStuff.clear();
     g_pCore->UpdateDummyProgress(0);
+
+
+        unsigned int count = 0;
+    bitStream.ReadCompressed(count);
+    for (unsigned int i = 0; i < count; ++i)
+    {
+        ElementID id1, id2;
+        bool      canCollide;
+        bitStream.Read(id1);
+        bitStream.Read(id2);
+        bitStream.ReadBit(canCollide);
+        // Use id1, id2, canCollide
+
+        CClientEntity* pEntity1 = CElementIDs::GetElement(id1);
+        CClientEntity* pEntity2 = CElementIDs::GetElement(id2);
+
+        pEntity1->SetCollidableWith(pEntity2, canCollide);
+    }
+    SString strCount = SString("Collision pairs count: %u", count);
+    CStaticFunctionDefinitions::OutputConsole(strCount);
+
 }
 
 void CPacketHandler::Packet_EntityRemove(NetBitStreamInterface& bitStream)
