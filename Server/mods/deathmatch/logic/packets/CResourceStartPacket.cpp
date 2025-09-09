d1d77f6a055f0106bb7db8b9115661c265c6de26
Mohab <133429578+MohabCodeX@users.noreply.github.com>
2025-09-09 13:04:26 +0300

    Fix localization features in CResourceStartPacket

--- Server/mods/deathmatch/logic/packets/CResourceStartPacket.cpp
+++ Server/mods/deathmatch/logic/packets/CResourceStartPacket.cpp
@@ -27,130 +27,132 @@ CResourceStartPacket::CResourceStartPacket(const std::string& resourceName, CRes
 
 bool CResourceStartPacket::Write(NetBitStreamInterface& BitStream) const
 {
-    if (m_strResourceName.empty())
-        return false;
-
-    // Write the resource name
-    unsigned char sizeResourceName = static_cast<unsigned char>(m_strResourceName.size());
-    BitStream.Write(sizeResourceName);
-    if (sizeResourceName > 0)
+    if (!m_strResourceName.empty())
     {
-        BitStream.Write(m_strResourceName.c_str(), sizeResourceName);
-    }
+        // Write the resource name
+        unsigned char sizeResourceName = static_cast<unsigned char>(m_strResourceName.size());
+        BitStream.Write(sizeResourceName);
+        if (sizeResourceName > 0)
+        {
+            BitStream.Write(m_strResourceName.c_str(), sizeResourceName);
+        }
 
-    // Write the start counter
-    BitStream.Write(m_startCounter);
+        // Write the start counter
+        BitStream.Write(m_startCounter);
 
-    // Write the resource id
-    BitStream.Write(m_pResource->GetNetID());
+        // Write the resource id
+        BitStream.Write(m_pResource->GetNetID());
 
-    // Write the resource element id
-    BitStream.Write(m_pResource->GetResourceRootElement()->GetID());
+        // Write the resource element id
+        BitStream.Write(m_pResource->GetResourceRootElement()->GetID());
 
-    // Write the resource dynamic element id
-    BitStream.Write(m_pResource->GetDynamicElementRoot()->GetID());
+        // Write the resource dynamic element id
+        BitStream.Write(m_pResource->GetDynamicElementRoot()->GetID());
 
-    // Count the amount of 'no client cache' scripts
-    unsigned short usNoClientCacheScriptCount = 0;
-    if (m_pResource->IsClientScriptsOn())
-    {
+        // Count the amount of 'no client cache' scripts
+        unsigned short usNoClientCacheScriptCount = 0;
+        if (m_pResource->IsClientScriptsOn())
+        {
+            for (CResourceFile* resourceFile : m_pResource->GetFiles())
+            {
+                if (resourceFile->GetType() == CResourceScriptItem::RESOURCE_FILE_TYPE_CLIENT_SCRIPT &&
+                    static_cast<CResourceClientScriptItem*>(resourceFile)->IsNoClientCache())
+                {
+                    ++usNoClientCacheScriptCount;
+                }
+            }
+        }
+        BitStream.Write(usNoClientCacheScriptCount);
+
+        // Write the declared min client version for this resource
+        BitStream.WriteString(m_pResource->GetMinServerRequirement());
+        BitStream.WriteString(m_pResource->GetMinClientRequirement());
+        BitStream.WriteBit(m_pResource->IsOOPEnabledInMetaXml());
+        BitStream.Write(m_pResource->GetDownloadPriorityGroup());
+
+        // Send the resource files info
         for (CResourceFile* resourceFile : m_pResource->GetFiles())
         {
-            if (resourceFile->GetType() == CResourceScriptItem::RESOURCE_FILE_TYPE_CLIENT_SCRIPT &&
-                static_cast<CResourceClientScriptItem*>(resourceFile)->IsNoClientCache())
+            if ((resourceFile->GetType() == CResourceScriptItem::RESOURCE_FILE_TYPE_CLIENT_CONFIG && m_pResource->IsClientConfigsOn()) ||
+                (resourceFile->GetType() == CResourceScriptItem::RESOURCE_FILE_TYPE_CLIENT_SCRIPT && m_pResource->IsClientScriptsOn() &&
+                 static_cast<CResourceClientScriptItem*>(resourceFile)->IsNoClientCache() == false) ||
+                (resourceFile->GetType() == CResourceScriptItem::RESOURCE_FILE_TYPE_CLIENT_FILE && m_pResource->IsClientFilesOn()) ||
+                (resourceFile->GetType() == CResourceScriptItem::RESOURCE_FILE_TYPE_TRANSLATION) ||
+                (resourceFile->GetType() == CResourceScriptItem::RESOURCE_FILE_TYPE_GLOBAL_TRANSLATION))
             {
-                ++usNoClientCacheScriptCount;
+                // Write the Type of chunk to read (F - File, E - Exported Function)
+                BitStream.Write(static_cast<unsigned char>('F'));
+
+                // Write the map name
+                const char* szFileName = resourceFile->GetWindowsName();
+                size_t      sizeFileName = strlen(szFileName);
+
+                // Make sure we don't have any backslashes in the name
+                char* szCleanedFilename = new char[sizeFileName + 1];
+                strcpy(szCleanedFilename, szFileName);
+                for (unsigned int i = 0; i < sizeFileName; i++)
+                {
+                    if (szCleanedFilename[i] == '\\')
+                        szCleanedFilename[i] = '/';
+                }
+
+                BitStream.Write(static_cast<unsigned char>(sizeFileName));
+                if (sizeFileName > 0)
+                {
+                    BitStream.Write(szCleanedFilename, sizeFileName);
+                }
+
+                // ChrML: Don't forget this...
+                delete[] szCleanedFilename;
+
+                BitStream.Write(static_cast<unsigned char>(resourceFile->GetType()));
+                CChecksum checksum = resourceFile->GetLastChecksum();
+                BitStream.Write(checksum.ulCRC);
+                BitStream.Write((const char*)checksum.md5.data, sizeof(checksum.md5.data));
+                BitStream.Write((double)resourceFile->GetSizeHint());            // Has to be double for bitstream format compatibility
+                if (resourceFile->GetType() == CResourceScriptItem::RESOURCE_FILE_TYPE_CLIENT_FILE)
+                {
+                    CResourceClientFileItem* pRCFItem = reinterpret_cast<CResourceClientFileItem*>(resourceFile);
+                    // write bool whether to download or not
+                    BitStream.WriteBit(pRCFItem->IsAutoDownload());
+                }
+                else if (resourceFile->GetType() == CResourceScriptItem::RESOURCE_FILE_TYPE_TRANSLATION)
+                {
+                    CResourceTranslationItem* translationItem = dynamic_cast<CResourceTranslationItem*>(resourceFile);
+                    bool isPrimary = translationItem ? translationItem->IsPrimary() : false;
+                    BitStream.WriteBit(isPrimary);
+                }
             }
         }
-    }
-    BitStream.Write(usNoClientCacheScriptCount);
 
-    // Write the declared min client version for this resource
-    BitStream.WriteString(m_pResource->GetMinServerRequirement());
-    BitStream.WriteString(m_pResource->GetMinClientRequirement());
-    BitStream.WriteBit(m_pResource->IsOOPEnabledInMetaXml());
-    BitStream.Write(m_pResource->GetDownloadPriorityGroup());
-
-    // Send the resource files info
-    for (CResourceFile* resourceFile : m_pResource->GetFiles())
-    {
-        if ((resourceFile->GetType() == CResourceScriptItem::RESOURCE_FILE_TYPE_CLIENT_CONFIG && m_pResource->IsClientConfigsOn()) ||
-            (resourceFile->GetType() == CResourceScriptItem::RESOURCE_FILE_TYPE_CLIENT_SCRIPT && m_pResource->IsClientScriptsOn() &&
-             static_cast<CResourceClientScriptItem*>(resourceFile)->IsNoClientCache() == false) ||
-            (resourceFile->GetType() == CResourceScriptItem::RESOURCE_FILE_TYPE_CLIENT_FILE && m_pResource->IsClientFilesOn()) ||
-            (resourceFile->GetType() == CResourceScriptItem::RESOURCE_FILE_TYPE_TRANSLATION) ||
-            (resourceFile->GetType() == CResourceScriptItem::RESOURCE_FILE_TYPE_GLOBAL_TRANSLATION))
+        // Loop through the exported functions
+        std::list<CExportedFunction>::iterator iterExportedFunction = m_pResource->IterBeginExportedFunctions();
+        for (; iterExportedFunction != m_pResource->IterEndExportedFunctions(); iterExportedFunction++)
         {
-            // Write the Type of chunk to read (F - File, E - Exported Function)
-            BitStream.Write(static_cast<unsigned char>('F'));
-
-            // Write the map name
-            const char* szFileName = resourceFile->GetWindowsName();
-            size_t      sizeFileName = strlen(szFileName);
-
-            // Make sure we don't have any backslashes in the name
-            char* szCleanedFilename = new char[sizeFileName + 1];
-            strcpy(szCleanedFilename, szFileName);
-            for (unsigned int i = 0; i < sizeFileName; i++)
+            // Check to see if the exported function is 'client'
+            if (iterExportedFunction->GetType() == CExportedFunction::EXPORTED_FUNCTION_TYPE_CLIENT)
             {
-                if (szCleanedFilename[i] == '\\')
-                    szCleanedFilename[i] = '/';
-            }
+                // Write the Type of chunk to read (F - File, E - Exported Function)
+                BitStream.Write(static_cast<unsigned char>('E'));
 
-            BitStream.Write(static_cast<unsigned char>(sizeFileName));
-            if (sizeFileName > 0)
-            {
-                BitStream.Write(szCleanedFilename, sizeFileName);
-            }
+                // Write the exported function
+                std::string strFunctionName = iterExportedFunction->GetFunctionName();
+                size_t      sizeFunctionName = strFunctionName.length();
 
-            // ChrML: Don't forget this...
-            delete[] szCleanedFilename;
-
-            BitStream.Write(static_cast<unsigned char>(resourceFile->GetType()));
-            CChecksum checksum = resourceFile->GetLastChecksum();
-            BitStream.Write(checksum.ulCRC);
-            BitStream.Write((const char*)checksum.md5.data, sizeof(checksum.md5.data));
-            BitStream.Write((double)resourceFile->GetSizeHint());            // Has to be double for bitstream format compatibility
-            if (resourceFile->GetType() == CResourceScriptItem::RESOURCE_FILE_TYPE_CLIENT_FILE)
-            {
-                CResourceClientFileItem* pRCFItem = reinterpret_cast<CResourceClientFileItem*>(resourceFile);
-                // write bool whether to download or not
-                BitStream.WriteBit(pRCFItem->IsAutoDownload());
-            }
-            else if (resourceFile->GetType() == CResourceScriptItem::RESOURCE_FILE_TYPE_TRANSLATION)
-            {
-                CResourceTranslationItem* translationItem = dynamic_cast<CResourceTranslationItem*>(resourceFile);
-                bool isPrimary = translationItem ? translationItem->IsPrimary() : false;
-                BitStream.WriteBit(isPrimary);
+                BitStream.Write(static_cast<unsigned char>(sizeFunctionName));
+                if (sizeFunctionName > 0)
+                {
+                    BitStream.Write(strFunctionName.c_str(), sizeFunctionName);
+                }
             }
         }
+
+        // Write global translation provider flag
+        bool isGlobalProvider = m_pResource->GetTranslationManager() && m_pResource->GetTranslationManager()->IsGlobalProvider();
+        BitStream.WriteBit(isGlobalProvider);
+
+        return true;
     }
 
-    // Loop through the exported functions
-    std::list<CExportedFunction>::iterator iterExportedFunction = m_pResource->IterBeginExportedFunctions();
-    for (; iterExportedFunction != m_pResource->IterEndExportedFunctions(); iterExportedFunction++)
-    {
-        // Check to see if the exported function is 'client'
-        if (iterExportedFunction->GetType() == CExportedFunction::EXPORTED_FUNCTION_TYPE_CLIENT)
-        {
-            // Write the Type of chunk to read (F - File, E - Exported Function)
-            BitStream.Write(static_cast<unsigned char>('E'));
-
-            // Write the exported function
-            std::string strFunctionName = iterExportedFunction->GetFunctionName();
-            size_t      sizeFunctionName = strFunctionName.length();
-
-            BitStream.Write(static_cast<unsigned char>(sizeFunctionName));
-            if (sizeFunctionName > 0)
-            {
-                BitStream.Write(strFunctionName.c_str(), sizeFunctionName);
-            }
-        }
-    }
-
-    // Write global translation provider flag
-    bool isGlobalProvider = m_pResource->GetTranslationManager() && m_pResource->GetTranslationManager()->IsGlobalProvider();
-    BitStream.WriteBit(isGlobalProvider);
-
-    return true;
+    return false;
 }
