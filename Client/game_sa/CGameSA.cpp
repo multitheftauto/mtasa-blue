multitheftauto/mtasa-blue

commit 46f3580fbd8ea5cf48c14cf8fee0bd6eb6691854

Add new special world property ignorefirestate (#3845)

Client/game_sa/CGameSA.cpp
@@ -848,6 +848,34 @@ void CGameSA::SetRoadSignsTextEnabled(bool isEnabled)
     m_isRoadSignsTextEnabled = isEnabled;
 }
 
+void CGameSA::SetIgnoreFireStateEnabled(bool isEnabled)
+{
+    if (isEnabled == m_isIgnoreFireStateEnabled)
+        return;
+
+    if (isEnabled)
+    {
+        MemSet((void*)0x6511B9, 0x90, 10);            // CCarEnterExit::IsVehicleStealable
+        MemSet((void*)0x643A95, 0x90, 14);            // CTaskComplexEnterCar::CreateFirstSubTask
+        MemSet((void*)0x6900B5, 0x90, 14);            // CTaskComplexCopInCar::ControlSubTask
+        MemSet((void*)0x64F3DB, 0x90, 14);            // CCarEnterExit::IsPlayerToQuitCarEnter
+
+        MemSet((void*)0x685A7F, 0x90, 14);            // CTaskSimplePlayerOnFoot::ProcessPlayerWeapon
+    }
+    else
+    {
+        // Restore original bytes
+        MemCpy((void*)0x6511B9, "\x88\x86\x90\x04\x00\x00\x85\xC0\x75\x3E", 10);
+        MemCpy((void*)0x643A95, "\x8B\x88\x90\x04\x00\x00\x85\xC9\x0F\x85\x99\x01\x00\x00", 14);
+        MemCpy((void*)0x6900B5, "\x8B\x81\x90\x04\x00\x00\x85\xC0\x0F\x85\x1A\x01\x00\x00", 14);
+        MemCpy((void*)0x64F3DB, "\x8B\x85\x90\x04\x00\x00\x85\xC0\x0F\x85\x1B\x01\x00\x00", 14);
+
+        MemCpy((void*)0x685A7F, "\x8B\x86\x30\x07\x00\x00\x85\xC0\x0F\x85\x1D\x01\x00\x00", 14);
+    }
+
+    m_isIgnoreFireStateEnabled = isEnabled;
+}
+
 bool CGameSA::PerformChecks()
 {
     std::map<std::string, SCheatSA*>::iterator it;

Client/game_sa/CGameSA.h
@@ -249,6 +249,8 @@ public:
     bool IsTunnelWeatherBlendEnabled() const noexcept override { return m_isTunnelWeatherBlendEnabled; }
     void SetTunnelWeatherBlendEnabled(bool isEnabled) override;
 
+    bool IsIgnoreFireStateEnabled() const noexcept override { return m_isIgnoreFireStateEnabled; }
+    void SetIgnoreFireStateEnabled(bool isEnabled) override;
 
     unsigned long GetMinuteDuration();
     void          SetMinuteDuration(unsigned long ulTime);
@@ -378,6 +380,7 @@ private:
     bool         m_isRoadSignsTextEnabled{true};
     bool         m_isBuildingsRemoved{false};
     bool         m_isExtendedWaterCannonsEnabled{false};
+    bool         m_isIgnoreFireStateEnabled{false};
 
     static unsigned int&  ClumpOffset;
 

Client/mods/deathmatch/logic/CClientGame.cpp
@@ -135,6 +135,7 @@ CClientGame::CClientGame(bool bLocalPlay) : m_ServerInfo(new CServerInfo())
     m_Glitches[GLITCH_BADDRIVEBYHITBOX] = false;
     m_Glitches[GLITCH_QUICKSTAND] = false;
     m_Glitches[GLITCH_KICKOUTOFVEHICLE_ONMODELREPLACE] = false;
+
     g_pMultiplayer->DisableBadDrivebyHitboxes(true);
 
     // Remove Night & Thermal vision view (if enabled).
@@ -6025,6 +6026,9 @@ bool CClientGame::SetWorldSpecialProperty(WorldSpecialProperty property, bool is
         case WorldSpecialProperty::TUNNELWEATHERBLEND:
             g_pGame->SetTunnelWeatherBlendEnabled(isEnabled);
             return true;
+        case WorldSpecialProperty::IGNOREFIRESTATE:
+            g_pGame->SetIgnoreFireStateEnabled(isEnabled);
+            return true;
     }
     return false;
 }
@@ -6062,6 +6066,8 @@ bool CClientGame::IsWorldSpecialProperty(WorldSpecialProperty property)
             return g_pGame->IsRoadSignsTextEnabled();
         case WorldSpecialProperty::TUNNELWEATHERBLEND:
             return g_pGame->IsTunnelWeatherBlendEnabled();
+        case WorldSpecialProperty::IGNOREFIRESTATE:
+            return g_pGame->IsIgnoreFireStateEnabled();
     }
     return false;
 }
@@ -6475,7 +6481,7 @@ void CClientGame::OutputServerInfo()
     {
         SString     strEnabledGlitches;
         const char* szGlitchNames[] = {"Quick reload",         "Fast fire",  "Fast move", "Crouch bug", "Close damage", "Hit anim", "Fast sprint",
-                                       "Bad driveby hitboxes", "Quick stand"};
+                                       "Bad driveby hitboxes", "Quick stand", "Kickout of vehicle on model replace"};
         for (uint i = 0; i < NUM_GLITCHES; i++)
         {
             if (IsGlitchEnabled(i))

Client/mods/deathmatch/logic/CPacketHandler.cpp
@@ -2397,6 +2397,7 @@ void CPacketHandler::Packet_MapInfo(NetBitStreamInterface& bitStream)
     g_pClientGame->SetWorldSpecialProperty(WorldSpecialProperty::ROADSIGNSTEXT, wsProps.data3.roadsignstext);
     g_pClientGame->SetWorldSpecialProperty(WorldSpecialProperty::EXTENDEDWATERCANNONS, wsProps.data4.extendedwatercannons);
     g_pClientGame->SetWorldSpecialProperty(WorldSpecialProperty::TUNNELWEATHERBLEND, wsProps.data5.tunnelweatherblend);
+    g_pClientGame->SetWorldSpecialProperty(WorldSpecialProperty::IGNOREFIRESTATE, wsProps.data6.ignoreFireState);
 
     float fJetpackMaxHeight = 100;
     if (!bitStream.Read(fJetpackMaxHeight))

Client/multiplayer_sa/CMultiplayerSA.cpp
@@ -1862,6 +1862,7 @@ void CMultiplayerSA::DisableCloseRangeDamage(bool bDisabled)
         MemPut<BYTE>(0x73BA00, 0x86);
     }
 }
+
 bool CMultiplayerSA::GetInteriorSoundsEnabled()
 {
     return bInteriorSoundsEnabled;

Client/sdk/game/CGame.h
@@ -230,6 +230,9 @@ public:
     virtual bool IsTunnelWeatherBlendEnabled() const noexcept = 0;
     virtual void SetTunnelWeatherBlendEnabled(bool isEnabled) = 0;
 
+    virtual bool IsIgnoreFireStateEnabled() const noexcept = 0;
+    virtual void SetIgnoreFireStateEnabled(bool isEnabled) = 0;
+
     virtual CWeapon*     CreateWeapon() = 0;
     virtual CWeaponStat* CreateWeaponStat(eWeaponType weaponType, eWeaponSkill weaponSkill) = 0;
 

Server/mods/deathmatch/logic/CGame.cpp
@@ -258,6 +258,7 @@ CGame::CGame() : m_FloodProtect(4, 30000, 30000)            // Max of 4 connecti
     m_WorldSpecialProps[WorldSpecialProperty::EXTENDEDWATERCANNONS] = true;
     m_WorldSpecialProps[WorldSpecialProperty::ROADSIGNSTEXT] = true;
     m_WorldSpecialProps[WorldSpecialProperty::TUNNELWEATHERBLEND] = true;
+    m_WorldSpecialProps[WorldSpecialProperty::IGNOREFIRESTATE] = false;
 
     m_JetpackWeapons[WEAPONTYPE_MICRO_UZI] = true;
     m_JetpackWeapons[WEAPONTYPE_TEC9] = true;

Server/mods/deathmatch/logic/packets/CMapInfoPacket.cpp
@@ -192,6 +192,7 @@ bool CMapInfoPacket::Write(NetBitStreamInterface& BitStream) const
         wsProps.data3.roadsignstext = g_pGame->IsWorldSpecialPropertyEnabled(WorldSpecialProperty::ROADSIGNSTEXT);
         wsProps.data4.extendedwatercannons = g_pGame->IsWorldSpecialPropertyEnabled(WorldSpecialProperty::EXTENDEDWATERCANNONS);
         wsProps.data5.tunnelweatherblend = g_pGame->IsWorldSpecialPropertyEnabled(WorldSpecialProperty::TUNNELWEATHERBLEND);
+        wsProps.data6.ignoreFireState = g_pGame->IsWorldSpecialPropertyEnabled(WorldSpecialProperty::IGNOREFIRESTATE);
         BitStream.Write(&wsProps);
     }
 

commit 3c268657d6477334b308be26b31282833e63da82

Fix crashes introduced by PR #3845 Like a common one at gta_sa 0x0116112B

Client/game_sa/CGameSA.cpp
@@ -898,12 +898,13 @@ void CGameSA::SetIgnoreFireStateEnabled(bool isEnabled)
 
     if (isEnabled)
     {
-        MemSet((void*)0x6511B9, 0x90, 10);            // CCarEnterExit::IsVehicleStealable
-        MemSet((void*)0x643A95, 0x90, 14);            // CTaskComplexEnterCar::CreateFirstSubTask
-        MemSet((void*)0x6900B5, 0x90, 14);            // CTaskComplexCopInCar::ControlSubTask
-        MemSet((void*)0x64F3DB, 0x90, 14);            // CCarEnterExit::IsPlayerToQuitCarEnter
-
-        MemSet((void*)0x685A7F, 0x90, 14);            // CTaskSimplePlayerOnFoot::ProcessPlayerWeapon
+        // All these patches disable fire state checks (m_pFire != nullptr)
+        // Related crash protection is handled by checks in TaskSA.cpp and CTaskManagementSystemSA.cpp
+        MemSet((void*)0x6511B9, 0x90, 10);            // CCarEnterExit::IsVehicleStealable - fire check
+        MemSet((void*)0x643A95, 0x90, 14);            // CTaskComplexEnterCar::CreateFirstSubTask - fire check
+        MemSet((void*)0x6900B5, 0x90, 14);            // CTaskComplexCopInCar::ControlSubTask - fire check
+        MemSet((void*)0x64F3DB, 0x90, 14);            // CCarEnterExit::IsPlayerToQuitCarEnter - fire check  
+        MemSet((void*)0x685A7F, 0x90, 14);            // CTaskSimplePlayerOnFoot::ProcessPlayerWeapon - fire check
 
         MemSet((void*)0x53A899, 0x90, 5);             // CFire::ProcessFire
         MemSet((void*)0x53A990, 0x90, 5);             // CFire::ProcessFire

Client/game_sa/CTaskManagementSystemSA.cpp
@@ -102,8 +102,21 @@ void CTaskManagementSystemSA::RemoveTask(CTaskSAInterface* pTaskInterface)
 CTaskSA* CTaskManagementSystemSA::GetTask(CTaskSAInterface* pTaskInterface)
 {
     // Return NULL if we got passed NULL
-    if (pTaskInterface == 0)
-        return NULL;
+    if (!pTaskInterface)
+        return nullptr;
+
+    // Check vtable pointer to prevent crash from corrupted task objects
+    TaskVTBL* pVTBL = pTaskInterface->VTBL;
+    if (!pVTBL)
+        return nullptr;
+
+    // Vtable should be in executable memory range (.text/.rdata sections)
+    // GTA SA base is around 0x400000-0x900000 range
+    constexpr DWORD GTA_BASE_MIN = 0x400000;
+    constexpr DWORD GTA_BASE_MAX = 0x900000;
+    DWORD dwVTableAddr = reinterpret_cast<DWORD>(pVTBL);
+    if (dwVTableAddr < GTA_BASE_MIN || dwVTableAddr > GTA_BASE_MAX)
+        return nullptr;
 
     // Find it in our list
     STaskListItem*                       pListItem;
@@ -122,7 +135,7 @@ CTaskSA* CTaskManagementSystemSA::GetTask(CTaskSAInterface* pTaskInterface)
     // its not existed before, lets create the task
     // First, we create a temp task
     int   iTaskType = 9999;
-    DWORD dwFunc = pTaskInterface->VTBL->GetTaskType;
+    DWORD dwFunc = pVTBL->GetTaskType;
     if (dwFunc && dwFunc != 0x82263A)
     {
         __asm

Client/game_sa/TaskSA.cpp
@@ -84,12 +84,18 @@ CTask* CTaskSA::GetSubTask()
 {
     static CTaskManagementSystemSA* s_pTaskManagementSystem = pGame->GetTaskManagementSystem();
 
-    DWORD dwThisInterface = (DWORD)GetInterface();
-    DWORD dwFunc = GetInterface()->VTBL->GetSubTask;
+    CTaskSAInterface* pTaskInterface = GetInterface();
+    if (!pTaskInterface)
+        return nullptr;
+
+    DWORD dwFunc = pTaskInterface->VTBL->GetSubTask;
+    if (!dwFunc || dwFunc == 0x82263A)
+        return nullptr;
+
     DWORD dwReturn = 0;
     __asm
     {
-        mov     ecx, dwThisInterface
+        mov     ecx, pTaskInterface
         call    dwFunc
         mov     dwReturn, eax
     }
@@ -326,21 +332,24 @@ CTask* CTaskComplexSA::CreateNextSubTask(CPed* pPed)
 {
     CPedSA* pPedSA = dynamic_cast<CPedSA*>(pPed);
     if (!pPedSA)
-        return NULL;
+        return nullptr;
+
+    CTaskSAInterface* pTaskInterface = GetInterface();
+    if (!pTaskInterface)
+        return nullptr;
+
+    DWORD dwFunc = ((TaskComplexVTBL*)pTaskInterface->VTBL)->CreateNextSubTask;
+    if (!dwFunc || dwFunc == 0x82263A)
+        return nullptr;
 
     DWORD dwPedInterface = (DWORD)pPedSA->GetInterface();
-    DWORD dwThisInterface = (DWORD)GetInterface();
-    DWORD dwFunc = ((TaskComplexVTBL*)GetInterface()->VTBL)->CreateNextSubTask;
     DWORD dwReturn = 0;
-    if (dwFunc != 0x82263A && dwFunc)
+    __asm
     {
-        __asm
-        {
-            mov     ecx, dwThisInterface
-            push    dwPedInterface
-            call    dwFunc
-            mov     dwReturn, eax
-        }
+        mov     ecx, pTaskInterface
+        push    dwPedInterface
+        call    dwFunc
+        mov     dwReturn, eax
     }
     return pGame->GetTaskManagementSystem()->GetTask((CTaskSAInterface*)dwReturn);
 }
@@ -349,21 +358,24 @@ CTask* CTaskComplexSA::CreateFirstSubTask(CPed* pPed)
 {
     CPedSA* pPedSA = dynamic_cast<CPedSA*>(pPed);
     if (!pPedSA)
-        return NULL;
+        return nullptr;
+
+    CTaskSAInterface* pTaskInterface = GetInterface();
+    if (!pTaskInterface)
+        return nullptr;
+
+    DWORD dwFunc = ((TaskComplexVTBL*)pTaskInterface->VTBL)->CreateFirstSubTask;
+    if (!dwFunc || dwFunc == 0x82263A)
+        return nullptr;
 
     DWORD dwPedInterface = (DWORD)pPedSA->GetInterface();
-    DWORD dwThisInterface = (DWORD)GetInterface();
-    DWORD dwFunc = ((TaskComplexVTBL*)GetInterface()->VTBL)->CreateFirstSubTask;
     DWORD dwReturn = 0;
-    if (dwFunc != 0x82263A && dwFunc)
+    __asm
     {
-        __asm
-        {
-            mov     ecx, dwThisInterface
-            push    dwPedInterface
-            call    dwFunc
-            mov     dwReturn, eax
-        }
+        mov     ecx, pTaskInterface
+        push    dwPedInterface
+        call    dwFunc
+        mov     dwReturn, eax
     }
     return pGame->GetTaskManagementSystem()->GetTask((CTaskSAInterface*)dwReturn);
 }
@@ -372,21 +384,24 @@ CTask* CTaskComplexSA::ControlSubTask(CPed* pPed)
 {
     CPedSA* pPedSA = dynamic_cast<CPedSA*>(pPed);
     if (!pPedSA)
-        return NULL;
+        return nullptr;
+
+    CTaskSAInterface* pTaskInterface = GetInterface();
+    if (!pTaskInterface)
+        return nullptr;
+
+    DWORD dwFunc = ((TaskComplexVTBL*)pTaskInterface->VTBL)->ControlSubTask;
+    if (!dwFunc || dwFunc == 0x82263A)
+        return nullptr;
 
     DWORD dwPedInterface = (DWORD)pPedSA->GetInterface();
-    DWORD dwThisInterface = (DWORD)GetInterface();
-    DWORD dwFunc = ((TaskComplexVTBL*)GetInterface()->VTBL)->ControlSubTask;
     DWORD dwReturn = 0;
-    if (dwFunc != 0x82263A && dwFunc)
+    __asm
     {
-        __asm
-        {
-            mov     ecx, dwThisInterface
-            push    dwPedInterface
-            call    dwFunc
-            mov     dwReturn, eax
-        }
+        mov     ecx, pTaskInterface
+        push    dwPedInterface
+        call    dwFunc
+        mov     dwReturn, eax
     }
     return pGame->GetTaskManagementSystem()->GetTask((CTaskSAInterface*)dwReturn);
 }