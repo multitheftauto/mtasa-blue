multitheftauto/mtasa-blue

commit 3308359b37cc74291a5487f93604745650ab97c8

Fix memory leak in engineImageLinkDFF (#4544)

Client/game_sa/CStreamingSA.cpp
@@ -15,6 +15,7 @@
 #include "CModelInfoSA.h"
 #include "Fileapi.h"
 #include "processthreadsapi.h"
+#include "CGameSA.h"
 
 extern CCoreInterface* g_pCore;
 
@@ -333,6 +334,14 @@ void CStreamingSA::ReinitStreaming()
 void CStreamingSA::SetStreamingInfo(uint modelid, unsigned char usStreamID, uint uiOffset, ushort usSize, uint uiNextInImg)
 {
     CStreamingInfo* pItemInfo = GetStreamingInfo(modelid);
+
+    // We remove the existing RwObject because, after switching the archive, the streamer will load a new one.
+    // ReInit doesn't delete all RwObjects unless certain conditions are met.
+    // In this case, we must force-remove the RwObject from memory, because it is no longer used,
+    // and due to the archive change the streamer no longer detects it and therefore won't delete it.
+    // As a result, a memory leak occurs after every call to engineImageLinkDFF.
+    if (CModelInfo* modelInfo = g_pCore->GetGame()->GetModelInfo(modelid); modelInfo->GetRwObject())
+        RemoveModel(modelid);
 
     // Change nextInImg field for prev model
     for (CStreamingInfo& info : ms_aInfoForModel)

Client/mods/deathmatch/logic/CClientIMG.cpp
@@ -211,6 +211,11 @@ bool CClientIMG::LinkModel(unsigned int uiModelID, size_t uiFileID)
 
     m_restoreInfo.emplace_back(uiModelID, pCurrInfo->offsetInBlocks, pCurrInfo->sizeInBlocks, pCurrInfo->archiveId);
 
+    // Internally stream out the vehicle before calling CStreamingSA::RemoveModel
+    // otherwise a crash will occur if the player is inside a vehicle that gets unloaded by the streamer
+    if (CClientVehicleManager::IsValidModel(uiModelID))
+        g_pClientGame->GetVehicleManager()->RestreamVehicles(static_cast<unsigned short>(uiModelID));
+
     g_pGame->GetStreaming()->SetStreamingInfo(uiModelID, m_ucArchiveID, pFileInfo->uiOffset, pFileInfo->usSize);
 
     return true;

commit 6fec5d56c55b8a649b2b4921d4d469b889007079

Fix crash introduced by PR #4544

Client/game_sa/CStreamingSA.cpp
@@ -340,9 +340,16 @@ void CStreamingSA::SetStreamingInfo(uint modelid, unsigned char usStreamID, uint
     // In this case, we must force-remove the RwObject from memory, because it is no longer used,
     // and due to the archive change the streamer no longer detects it and therefore won't delete it.
     // As a result, a memory leak occurs after every call to engineImageLinkDFF.
-    CModelInfo* modelInfo = g_pCore->GetGame()->GetModelInfo(modelid);
-    if (modelInfo->GetRwObject())
-        RemoveModel(modelid);
+    const auto baseTxdId = g_pCore->GetGame()->GetBaseIDforTXD();
+    if (modelid < static_cast<uint>(baseTxdId))
+    {
+        if (CModelInfo* modelInfo = g_pCore->GetGame()->GetModelInfo(modelid, true))
+        {
+            // Only DFF models got RwObjects, TXDs don't, so we only flush those here (or crash)
+            if (modelInfo->GetRwObject())
+                RemoveModel(modelid);
+        }
+    }
 
     // Change nextInImg field for prev model
     for (CStreamingInfo& info : ms_aInfoForModel)