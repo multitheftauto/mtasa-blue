0745deaa960502fcd3668c0cbd60395cf0047440
Dutchman101 <12105539+Dutchman101@users.noreply.github.com>
2025-11-20 12:07:01 +0000

    Addendum to last

--- utils/buildactions/install_cef.lua
+++ utils/buildactions/install_cef.lua
@@ -10,7 +10,7 @@ local CEF_URL_SUFFIX = "_windows32_minimal.tar.bz2"
 
 -- Change here to update CEF version
 local CEF_VERSION = "142.0.15+g6dfdb28+chromium-142.0.7444.176"
-local CEF_HASH = "03b2c7a316d0a70ef172b4a1d3f83cc32385326cf13496b14b7c4fd92ac09167"
+local CEF_HASH = "26def0b5b40d02a5a33d52f8568a92748329ad0771b6c1150ea7a3be160cf164"
 
 function make_cef_download_url()
 	return CEF_URL_PREFIX..CEF_VERSION..CEF_URL_SUFFIX
