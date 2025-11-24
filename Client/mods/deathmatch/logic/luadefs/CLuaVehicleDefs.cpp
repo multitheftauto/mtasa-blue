60986f5cad359149f8907d94575c7e66b642b4f4
Xenius97 <freejanos@gmail.com>
2025-11-24 18:07:53 +0100

    fix

--- Client/mods/deathmatch/logic/luadefs/CLuaVehicleDefs.cpp
+++ Client/mods/deathmatch/logic/luadefs/CLuaVehicleDefs.cpp
@@ -333,6 +333,7 @@ void CLuaVehicleDefs::AddClass(lua_State* luaVM)
     lua_classvariable(luaVM, "blown", NULL, "isVehicleBlown");
     lua_classvariable(luaVM, "vehicleType", NULL, "getVehicleType");
     lua_classvariable(luaVM, "gear", NULL, "getVehicleCurrentGear");
+    lua_classvariable(luaVM, "onGround", NULL, "isVehicleOnGround");
     lua_classvariable(luaVM, "damageProof", NULL, "isVehicleDamageProof");
     lua_classvariable(luaVM, "helicopterRotorSpeed", "setHelicopterRotorSpeed", "getHelicopterRotorSpeed");
     lua_classvariable(luaVM, "heliBladeCollisionsEnabled", "setHeliBladeCollisionsEnabled", "getHeliBladeCollisionsEnabled");
