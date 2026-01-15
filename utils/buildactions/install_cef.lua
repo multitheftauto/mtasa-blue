0f43464425ca0fff9e456d44355fa2dd61fcb1b4
Dutchman101 <mcak105@outlook.com>
2026-01-15 10:14:06 +0800

    Undo accidental commit inclusion (For proper tracking)

--- utils/buildactions/install_cef.lua
+++ utils/buildactions/install_cef.lua
@@ -9,8 +9,8 @@ local CEF_URL_PREFIX = "https://cef-builds.spotifycdn.com/cef_binary_"
 local CEF_URL_SUFFIX = "_windows32_minimal.tar.bz2"
 
 -- Change here to update CEF version
-local CEF_VERSION = "143.0.14+gdd46a37+chromium-143.0.7499.193"
-local CEF_HASH = "b1a4e9649dc8aefedbf1aa5797376b1fe12ef29d14c7f722a3a58fa95150a496"
+local CEF_VERSION = "143.0.13+g30cb3bd+chromium-143.0.7499.170"
+local CEF_HASH = "eba58e6abd6dba6abf2ec70d4bba6c084b4ae3a21b3ddaf758c1eb82b33b4106"
 
 function make_cef_download_url()
 	return CEF_URL_PREFIX..CEF_VERSION..CEF_URL_SUFFIX
