95be41351b3e591ba205b6289f8b0c673d0e6df6
Dutchman101 <mcak105@outlook.com>
2026-01-23 16:37:01 +0800

    Addendum to last (clang-format)

--- Shared/sdk/SharedUtil.File.hpp
+++ Shared/sdk/SharedUtil.File.hpp
@@ -678,7 +678,7 @@ SString SharedUtil::GetLaunchPathFilename()
         {
             try
             {
-                std::wstring wstrModulePath(512, L'\0');
+                std::wstring     wstrModulePath(512, L'\0');
                 constexpr size_t MAX_PATH_LENGTH = 32768;
 
                 for (;;)
@@ -726,7 +726,7 @@ SString SharedUtil::GetLaunchPathFilename()
                     if (dwNeeded > 0)
                     {
                         std::wstring wstrFullPath(dwNeeded, L'\0');
-                        DWORD dwWritten = GetFullPathNameW(wstrModulePath.c_str(), dwNeeded, &wstrFullPath[0], nullptr);
+                        DWORD        dwWritten = GetFullPathNameW(wstrModulePath.c_str(), dwNeeded, &wstrFullPath[0], nullptr);
 
                         if (dwWritten > 0)
                         {
