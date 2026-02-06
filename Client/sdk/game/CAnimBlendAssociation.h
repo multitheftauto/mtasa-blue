f4c56c402f3014dc1044d31d25553bfa30b692a7
FileEX <kongali@interia.pl>
2026-02-07 00:38:58 +0100

    Reverse anim flags

--- Client/sdk/game/CAnimBlendAssociation.h
+++ Client/sdk/game/CAnimBlendAssociation.h
@@ -26,27 +26,23 @@ struct Rpclump;
 
 enum eAnimationFlags
 {
-    ANIMATION_DEFAULT = 0,                      // 0x0,
-    ANIMATION_IS_PLAYING = 1 << 0,              // 0x1,
-    ANIMATION_IS_LOOPED = 1 << 1,               // 0x2,
-    ANIMATION_IS_BLEND_AUTO_REMOVE = 1 << 2,    //!< (0x4) Automatically `delete this` once faded out (`m_BlendAmount <= 0 && m_BlendDelta <= 0`)
-    ANIMATION_IS_FINISH_AUTO_REMOVE = 1 << 3,   // 0x8,  // Animation will be stuck on last frame, if not set
-    ANIMATION_IS_PARTIAL = 1 << 4,              // 0x10,
-    ANIMATION_IS_SYNCRONISED = 1 << 5,          // 0x20,
-    ANIMATION_CAN_EXTRACT_VELOCITY = 1 << 6,    // 0x40,
-    ANIMATION_CAN_EXTRACT_X_VELOCITY = 1 << 7,  // 0x80,
-
-    // ** User defined flags **
-    ANIMATION_WALK = 1 << 8,                        // 0x100,
-    ANIMATION_200 = 1 << 9,                         // 0x200,
-    ANIMATION_DONT_ADD_TO_PARTIAL_BLEND = 1 << 10,  // 0x400,
-    ANIMATION_IS_FRONT = 1 << 11,                   // 0x800,
-    ANIMATION_SECONDARY_TASK_ANIM = 1 << 12,        // 0x1000,
-    // **
-
-    ANIMATION_IGNORE_ROOT_TRANSLATION = 1 << 13,  // 0x2000,
-    ANIMATION_REFERENCE_BLOCK = 1 << 14,          // 0x4000,
-    ANIMATION_FACIAL = 1 << 15,                   // 0x8000 // The animation is never destroyed if this flag is set, NO MATTER WHAT
+    ANIMATION_DEFAULT = 0,
+    ANIMATION_IS_PLAYING = 0x1,  // (0x5DED30) CPed::StopNonPartialAnims()
+    ANIMATION_IS_LOOPED = 0x2,   // (0x5B042F) CCutsceneMgr::SetCutsceneAnimToLoop
+    ANIMATION_IS_BLEND_AUTO_REMOVE =
+        0x4,  // (0x5E4B12) CPed::SetMoveAnim() - Automatically `delete this` once faded out (`m_BlendAmount <= 0 && m_BlendDelta <= 0`)
+    ANIMATION_IS_FINISH_AUTO_REMOVE = 0x8,   // (0x5E4B00) CPed::SetMoveAnim() - Animation will be stuck on last frame, if not set
+    ANIMATION_IS_PARTIAL = 0x10,             // (0x5DED28) CPed::StopNonPartialAnims()
+    ANIMATION_IS_MOVING = 0x20,              // (0x4D4651) CAnimManager::BlendAnimation()
+    ANIMATION_TRANSLATE_Y = 0x40,            // (0x4D1773)
+    ANIMATION_TRANSLATE_X = 0x80,            // (0x4D17A2)
+    ANIMATION_PED_WALK = 0x100,              // PEDFLAG (0x5E58A5) CPed::PlayFootSteps()
+    ANIMATION_PED_UPPERBODYONLY = 0x400,     // PEDFLAG (0x5E58BB) CPed::PlayFootSteps()
+    ANIMATION_FORWARD_ANIM = 0x800,          // PEDFLAG (0x678579) CTaskSimpleFall::ProcessFall()
+    ANIMATION_SECONDARY_TASK_ANIM = 0x1000,  // (0x64F688) CCarEnterExit::RemoveCarSitAnim()
+    ANIMATION_FREEZE_TRANSLATION = 0x2000,   // (0x4D21EB)
+    ANIMATION_BLOCK_REFERENCED = 0x4000,     // (0x4CEA59) CAnimBlendAssociation::ReferenceAnimBlock()
+    ANIMATION_IS_INDESTRUCTIBLE = 0x8000,    // (0x4D4687) CAnimManager::BlendAnimation() - The animation is never destroyed if this flag is set
 };
 
 class CAnimBlendAssociation
