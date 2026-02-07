8d34f5f69bd711bcad6eeff1ddc17418038ec4df
Dutchman101 <mcak105@outlook.com>
2026-02-07 02:38:09 +0100

    clang fix

--- Client/sdk/core/D3DProxyDeviceGuids.h
+++ Client/sdk/core/D3DProxyDeviceGuids.h
@@ -1,11 +1,11 @@
 /*****************************************************************************
-*
-*  PROJECT:     Multi Theft Auto
-*  LICENSE:     See LICENSE in the top level directory
-*  FILE:        sdk/core/D3DProxyDeviceGuids.h
-*  Multi Theft Auto is available from https://www.multitheftauto.com/
-*
-*****************************************************************************/
+ *
+ *  PROJECT:     Multi Theft Auto
+ *  LICENSE:     See LICENSE in the top level directory
+ *  FILE:        sdk/core/D3DProxyDeviceGuids.h
+ *  Multi Theft Auto is available from https://www.multitheftauto.com/
+ *
+ *****************************************************************************/
 
 #pragma once
 
@@ -13,5 +13,4 @@
 
 // GUID for identifying CProxyDirect3DDevice9 instances via QueryInterface
 // {B4C5C640-B3F2-4888-9D30-D41A59CC72F0}
-DEFINE_GUID(CProxyDirect3DDevice9_GUID,
-    0xb4c5c640, 0xb3f2, 0x4888, 0x9d, 0x30, 0xd4, 0x1a, 0x59, 0xcc, 0x72, 0xf0);
+DEFINE_GUID(CProxyDirect3DDevice9_GUID, 0xb4c5c640, 0xb3f2, 0x4888, 0x9d, 0x30, 0xd4, 0x1a, 0x59, 0xcc, 0x72, 0xf0);
