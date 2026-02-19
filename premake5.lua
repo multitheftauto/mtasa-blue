c18d6d8e0b483bc1663755880086ce64995edaf8
Marek Kulik <noreply@bug32.com>
2026-02-19 15:58:59 +0100

    Revert MSVC toolset to v143 (VS2022)

--- premake5.lua
+++ premake5.lua
@@ -122,7 +122,7 @@ workspace "MTASA"
 		symbolspath "$(SolutionDir)Symbols\\$(Configuration)_$(Platform)\\$(ProjectName).pdb"
 
 	filter "system:windows"
-		toolset "v145"
+		toolset "v143"
 		preferredtoolarchitecture "x86_64"
 		staticruntime "On"
 		defines { "WIN32", "_WIN32", "_WIN32_WINNT=0x601", "_MSC_PLATFORM_TOOLSET=$(PlatformToolsetVersion)" }
