2cec030037fb16fd3bc31b574e6dbacdbe1a36ff
omar-o22 <newomar02@gmail.com>
2026-05-10 15:16:18 +0300

    fix clang

--- Client/core/CVersionUpdater.cpp
+++ Client/core/CVersionUpdater.cpp
@@ -1917,8 +1917,7 @@ void CVersionUpdater::_DialogServerSaysUpdateQuestion()
     // Display message
     GetQuestionBox().Reset();
     GetQuestionBox().SetTitle(_("MANDATORY UPDATE"));
-    GetQuestionBox().SetMessage(
-        SString(_("To join this server, you must update MTA to version %s.\n Do you want to update now?"), *m_strServerSaysData));
+    GetQuestionBox().SetMessage(SString(_("To join this server, you must update MTA to version %s.\n Do you want to update now?"), *m_strServerSaysData));
     GetQuestionBox().SetButton(0, _("No"));
     GetQuestionBox().SetButton(1, _("Yes"));
     GetQuestionBox().Show();
