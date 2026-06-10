e8d5a4741fddd5c7d8faba74749fb238db50429d
lopsi <40902730+Lpsd@users.noreply.github.com>
2026-06-10 18:02:08 +0100

    Update "Client Deathmatch" premake5.lua

--- Client/mods/deathmatch/premake5.lua
+++ Client/mods/deathmatch/premake5.lua
@@ -10,7 +10,7 @@ project "Client Deathmatch"
 
 	defines { "LUNASVG_BUILD", "LUA_USE_APICHECK", "SDK_WITH_BCRYPT" }
 	links {
-		"Lua_Client", "pcre", "json-c", "ws2_32", "portaudio", "zlib", "cryptopp", "libspeex", "blowfish_bcrypt", "lunasvg",
+		"Lua_Client", "pcre2", "json-c", "ws2_32", "portaudio", "zlib", "cryptopp", "libspeex", "blowfish_bcrypt", "lunasvg",
 		"../../../vendor/bass/lib/bass",
 		"../../../vendor/bass/lib/bass_fx",
 		"../../../vendor/bass/lib/bassmix",
@@ -39,7 +39,7 @@ project "Client Deathmatch"
 			"../../../vendor/bass",
 			"../../../vendor/libspeex",
 			"../../../vendor/zlib",
-			"../../../vendor/pcre",
+			"../../../vendor/pcre2",
 			"../../../vendor/json-c",
 			"../../../vendor/lua/src",
 			"../../../Shared/mods/deathmatch/logic",
