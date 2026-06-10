df7f0c5fb474163a7cc9e45e338273b841ee764c
lopsi <40902730+Lpsd@users.noreply.github.com>
2026-06-10 20:30:44 +0100

    Add cpp-httplib vendor

---
+++ vendor/cpp-httplib/premake5.lua
@@ -0,0 +1,34 @@
+-- cpp-httplib: A C++11 single-header HTTP/HTTPS library
+-- Header-only, no source files needed
+
+project "httplib"
+	language "C++"
+	kind "StaticLib"
+	targetname "httplib"
+	warnings "Off"
+
+	-- httplib can optionally use OpenSSL; we don't enable it here
+	-- as SSL is handled separately via EHS's existing OpenSSL setup
+	defines {
+		"CPPHTTPLIB_USE_POLL",
+	}
+
+	includedirs {
+		".",
+	}
+
+	vpaths {
+		["Headers/*"] = "*.h",
+		["*"] = "premake5.lua"
+	}
+
+	files {
+		"premake5.lua",
+		"*.h",
+	}
+
+	filter "system:windows"
+		links { "ws2_32", "crypt32" }
+
+	filter "system:linux"
+		links { "pthread" }
