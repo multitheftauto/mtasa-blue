multitheftauto/mtasa-blue

commit e0d8ccdf9509f03a15af86ad97aaf7da71975fde

Remove 32-bit server support & enforce W10+ (#4977)
#### Summary
This PR removes 32-bit server support from the build system in
preparation for the EHS to cpp-httplib replacement (#4940).

Changes include dropping Linux x86 server platforms, removing Windows
32-bit server project builds, bumping the server minimum Windows version
to 10, and cleaning up dead code for pre-Win10 OS compatibility paths in
the client.

#### Motivation
cpp-httplib does not officially support 32-bit platforms.
Rather than maintain unsupported configurations we are dropping 32-bit
server builds entirely, see additional reasoning:
https://github.com/multitheftauto/mtasa-blue/pull/4940#issuecomment-4762032798
The client remains 32-bit (obviously) and the installer gate for
pre-Win10 is already in place for 1.7 release.
This cleanup ensures no 32-bit server binaries are built going forward.

#### Test plan
Verified that premake5 generates correct project files for both Windows
(x86 x64 arm64) and Linux (x64 arm arm64) without the x86 Linux
platform.
Confirmed that Windows x86 builds still compile the client while server
projects are excluded via ExcludeFromBuild.
The server launcher now checks for Windows 10 or later at startup and
the installer no longer offers the 32-bit server executable or Win7 SHA2
KB patches.

#### Checklist

* [x] Your code should follow the coding guidelines.
* [x] Smaller pull requests are easier to review. If your pull request
is beefy, your pull request should be reviewable commit-by-commit.

Shared/sdk/SharedUtil.Misc.h
@@ -419,6 +419,7 @@ namespace SharedUtil
     bool IsWindowsVistaOrGreater();
     bool IsWindows7OrGreater();
     bool IsWindows8OrGreater();
+    bool IsWindows10OrGreater();
 
     bool QueryThreadEntryPointAddress(void* thread, DWORD* entryPointAddress);
 

Shared/sdk/SharedUtil.Misc.hpp
@@ -29,7 +29,7 @@
     #define SHAREDUTIL_PLATFORM_WINDOWS 1
 #endif
 
-#if defined(__linux__) || defined(LINUX_x86) || defined(LINUX_x64) || defined(LINUX_arm) || defined(LINUX_arm64)
+#if defined(__linux__) || defined(LINUX_x64) || defined(LINUX_arm) || defined(LINUX_arm64)
     #include <fstream>
     #include <string>
     #include <sstream>
@@ -636,7 +636,7 @@ bool SharedUtil::IsReadablePointer(const void* ptr, size_t size)
     }
 
     return true;
-    #elif defined(LINUX_x86) || defined(LINUX_x64) || defined(LINUX_arm) || defined(LINUX_arm64)
+    #elif defined(LINUX_x64) || defined(LINUX_arm) || defined(LINUX_arm64)
     static_assert(sizeof(uintptr_t) <= sizeof(unsigned long long), "Unexpected uintptr_t size");
 
     std::ifstream maps("/proc/self/maps");
@@ -2021,7 +2021,8 @@ bool SharedUtil::ShellExecuteNonBlocking(const SString& strAction, const SString
 #endif  // MTA_CLIENT
 
 #ifdef SHAREDUTIL_PLATFORM_WINDOWS
-    #define _WIN32_WINNT_WIN8 0x0602
+    #define _WIN32_WINNT_WIN8  0x0602
+    #define _WIN32_WINNT_WIN10 0x0A00
 ///////////////////////////////////////////////////////////////////////////
 //
 // SharedUtil::IsWindowsVersionOrGreater
@@ -2062,6 +2063,11 @@ bool SharedUtil::IsWindows7OrGreater()
 bool SharedUtil::IsWindows8OrGreater()
 {
     return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WIN8), LOBYTE(_WIN32_WINNT_WIN8), 0);
+}
+
+bool SharedUtil::IsWindows10OrGreater()
+{
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WIN10), LOBYTE(_WIN32_WINNT_WIN10), 0);
 }
 #endif  // SHAREDUTIL_PLATFORM_WINDOWS
 

Client/core/CCore.cpp
@@ -1914,7 +1914,6 @@ void CCore::OnPostColorFilterRender()
 
 void CCore::ApplyCoreInitSettings()
 {
-#if (_WIN32_WINNT >= _WIN32_WINNT_LONGHORN)
     bool aware = CVARS_GET_VALUE<bool>("process_dpi_aware");
 
     // The minimum supported client for the function below is Windows Vista (Longhorn).
@@ -1922,7 +1921,6 @@ void CCore::ApplyCoreInitSettings()
     // https://learn.microsoft.com/en-us/windows/win32/hidpi/high-dpi-desktop-application-development-on-windows
     if (aware)
         SetProcessDPIAware();
-#endif
 
     int revision = GetApplicationSettingInt("reset-settings-revision");
 

Client/core/CMainMenu.cpp
@@ -344,24 +344,6 @@ CMainMenu::CMainMenu(CGUI* pManager)
     m_pFeatureBranchAlertLabel->SetHorizontalAlign(CGUI_ALIGN_HORIZONTALCENTER);
     m_pFeatureBranchAlertLabel->SetVerticalAlign(CGUI_ALIGN_VERTICALCENTER);
 #endif
-
-#if _WIN32_WINNT <= _WIN32_WINNT_WINXP
-    // Add annonying alert
-    m_pAlertTexture.reset(reinterpret_cast<CGUITexture*>(m_pManager->CreateTexture()));
-    std::int32_t buffer = 0xFFFF0000;
-    m_pAlertTexture->LoadFromMemory(&buffer, 1, 1);  // HACK: Load red dot
-
-    m_pAlertImage.reset(reinterpret_cast<CGUIStaticImage*>(m_pManager->CreateStaticImage(m_pBackground)));
-    m_pAlertImage->LoadFromTexture(m_pAlertTexture.get());
-    m_pAlertImage->SetPosition({0.0f, 0.0f}, false);
-    m_pAlertImage->SetSize({ScreenSize.fX, 20.0f});
-
-    #define XP_VISTA_WARNING _("MTA will not receive updates on XP/Vista after July 2019.\n\nUpgrade Windows to play on the latest servers.")
-    m_pAlertLabel.reset(reinterpret_cast<CGUILabel*>(m_pManager->CreateLabel(m_pAlertImage.get(), XP_VISTA_WARNING)));
-    m_pAlertLabel->SetPosition({0.0f, 2.0f}, false);
-    m_pAlertLabel->SetSize({ScreenSize.fX, 20.0f});
-    m_pAlertLabel->SetHorizontalAlign(CGUI_ALIGN_HORIZONTALCENTER);
-#endif
 }
 
 CMainMenu::~CMainMenu()

Client/core/CMainMenu.h
@@ -172,12 +172,6 @@ private:
     std::unique_ptr<CGUILabel>       m_pFeatureBranchAlertLabel;
 #endif
 
-#if _WIN32_WINNT <= _WIN32_WINNT_WINXP
-    std::unique_ptr<CGUITexture>     m_pAlertTexture;
-    std::unique_ptr<CGUIStaticImage> m_pAlertImage;
-    std::unique_ptr<CGUILabel>       m_pAlertLabel;
-#endif
-
     // Fade states
     enum eFadeStates
     {

Client/core/CSettings.cpp
@@ -296,9 +296,6 @@ void CSettings::ResetGuiPointers()
     m_pProgressAnimationCombo = NULL;
     m_pDebugSettingLabel = NULL;
     m_pDebugSettingCombo = NULL;
-    m_pWin8Label = NULL;
-    m_pWin8ColorCheckBox = NULL;
-    m_pWin8MouseCheckBox = NULL;
     m_pPhotoSavingCheckbox = NULL;
     m_pCheckBoxAskBeforeDisconnect = NULL;
     m_pProcessAffinityCheckbox = NULL;
@@ -1864,33 +1861,6 @@ void CSettings::CreateGUI()
     vecTemp.fX = 22.f;
     vecTemp.fY += fLineHeight;
 
-    // Windows 8 compatibility
-    m_pWin8Label = reinterpret_cast<CGUILabel*>(pManager->CreateLabel(pTabAdvanced, _("Windows 8 compatibility:")));
-    m_pWin8Label->SetPosition(CVector2D(vecTemp.fX, vecTemp.fY));
-    m_pWin8Label->AutoSize();
-
-    m_pWin8ColorCheckBox = reinterpret_cast<CGUICheckBox*>(pManager->CreateCheckBox(pTabAdvanced, _("16-bit color")));
-    m_pWin8ColorCheckBox->SetPosition(CVector2D(vecTemp.fX + fIndentX, vecTemp.fY));
-    m_pWin8ColorCheckBox->AutoSize(NULL, 20.0f);
-    vecTemp.fX += 140;
-
-    m_pWin8MouseCheckBox = reinterpret_cast<CGUICheckBox*>(pManager->CreateCheckBox(pTabAdvanced, _("Mouse fix")));
-    m_pWin8MouseCheckBox->SetPosition(CVector2D(vecTemp.fX + fIndentX, vecTemp.fY));
-    m_pWin8MouseCheckBox->AutoSize(NULL, 20.0f);
-    vecTemp.fY += fLineHeight;
-    vecTemp.fX -= 140;
-
-    // Hide if not Win8
-    if (atoi(GetApplicationSetting("real-os-version")) != 8)
-    {
-#ifndef MTA_DEBUG  // Don't hide when debugging
-        m_pWin8Label->SetVisible(false);
-        m_pWin8ColorCheckBox->SetVisible(false);
-        m_pWin8MouseCheckBox->SetVisible(false);
-        vecTemp.fY -= fLineHeight;
-#endif
-    }
-
     // Cache path info
     m_pCachePathLabel = reinterpret_cast<CGUILabel*>(pManager->CreateLabel(pTabAdvanced, _("Client resource files:")));
     m_pCachePathLabel->SetPosition(CVector2D(vecTemp.fX, vecTemp.fY));
@@ -2086,12 +2056,6 @@ void CSettings::CreateGUI()
 
     m_pUpdateBuildTypeCombo->SetMouseEnterHandler(GUI_CALLBACK(&CSettings::OnShowAdvancedSettingDescription, this));
     m_pUpdateBuildTypeCombo->SetMouseLeaveHandler(GUI_CALLBACK(&CSettings::OnHideAdvancedSettingDescription, this));
-
-    m_pWin8ColorCheckBox->SetMouseEnterHandler(GUI_CALLBACK(&CSettings::OnShowAdvancedSettingDescription, this));
-    m_pWin8ColorCheckBox->SetMouseLeaveHandler(GUI_CALLBACK(&CSettings::OnHideAdvancedSettingDescription, this));
-
-    m_pWin8MouseCheckBox->SetMouseEnterHandler(GUI_CALLBACK(&CSettings::OnShowAdvancedSettingDescription, this));
-    m_pWin8MouseCheckBox->SetMouseLeaveHandler(GUI_CALLBACK(&CSettings::OnHideAdvancedSettingDescription, this));
 
     m_pUpdateAutoInstallLabel->SetMouseEnterHandler(GUI_CALLBACK(&CSettings::OnShowAdvancedSettingDescription, this));
     m_pUpdateAutoInstallLabel->SetMouseLeaveHandler(GUI_CALLBACK(&CSettings::OnHideAdvancedSettingDescription, this));
@@ -4144,14 +4108,6 @@ void CSettings::LoadData()
     else if (iVar == 1)
         m_pProgressAnimationCombo->SetText(_("Default"));
 
-    // Windows 8 16-bit color
-    iVar = GetApplicationSettingInt("Win8Color16");
-    m_pWin8ColorCheckBox->SetSelected(iVar != 0);
-
-    // Windows 8 mouse fix
-    iVar = GetApplicationSettingInt("Win8MouseFix");
-    m_pWin8MouseCheckBox->SetSelected(iVar != 0);
-
     // Save camera photos inside user documents folder
     CVARS_GET("photosaving", bVar);
     m_pPhotoSavingCheckbox->SetSelected(bVar);
@@ -4593,12 +4549,6 @@ void CSettings::SaveData()
         int iSelected = (int)pSelected->GetData();
         CVARS_SET("progress_animation", iSelected);
     }
-
-    // Windows 8 16-bit color
-    SetApplicationSettingInt("Win8Color16", m_pWin8ColorCheckBox->GetSelected());
-
-    // Windows 8 mouse fix
-    SetApplicationSettingInt("Win8MouseFix", m_pWin8MouseCheckBox->GetSelected());
 
     // Save photos in documents folder
     bool photoSaving = m_pPhotoSavingCheckbox->GetSelected();
@@ -6119,10 +6069,6 @@ bool CSettings::OnShowAdvancedSettingDescription(CGUIElement* pElement)
         strText = std::string(_("Auto updater:")) + " " + std::string(_("Select default unless you like filling out bug reports."));
     else if (pLabel && pLabel == m_pUpdateAutoInstallLabel || pComboBox && pComboBox == m_pUpdateAutoInstallCombo)
         strText = std::string(_("Auto updater:")) + " " + std::string(_("Select default to automatically install important updates."));
-    else if (pCheckBox && pCheckBox == m_pWin8ColorCheckBox)
-        strText = std::string(_("16-bit color:")) + " " + std::string(_("Enable 16 bit color modes - Requires MTA restart"));
-    else if (pCheckBox && pCheckBox == m_pWin8MouseCheckBox)
-        strText = std::string(_("Mouse fix:")) + " " + std::string(_("Mouse movement fix - May need PC restart"));
     else if (pCheckBox && pCheckBox == m_pProcessAffinityCheckbox)
         strText = std::string(_("CPU affinity:")) + " " + std::string(_("Only change if you're having stability issues."));
 

Client/core/CSettings.h
@@ -237,9 +237,6 @@ protected:
     CGUIComboBox*  m_pProgressAnimationCombo;
     CGUILabel*     m_pDebugSettingLabel;
     CGUIComboBox*  m_pDebugSettingCombo;
-    CGUILabel*     m_pWin8Label;
-    CGUICheckBox*  m_pWin8ColorCheckBox;
-    CGUICheckBox*  m_pWin8MouseCheckBox;
     CGUICheckBox*  m_pPhotoSavingCheckbox;
     CGUICheckBox*  m_pCheckBoxAskBeforeDisconnect;
     CGUICheckBox*  m_pProcessAffinityCheckbox;

Client/mods/deathmatch/logic/lua/CLuaFunctionDefs.Util.cpp
@@ -227,7 +227,6 @@ int CLuaFunctionDefs::GetKeyboardLayout(lua_State* luaVM)
 {
     const char* readingLayout = "ltr";
 
-#if _WIN32_WINNT >= _WIN32_WINNT_WIN7
     DWORD readingLayoutValue = 0;
 
     if (::GetLocaleInfoEx(LOCALE_NAME_USER_DEFAULT, LOCALE_IREADINGLAYOUT | LOCALE_RETURN_NUMBER, reinterpret_cast<LPWSTR>(&readingLayoutValue),
@@ -251,20 +250,6 @@ int CLuaFunctionDefs::GetKeyboardLayout(lua_State* luaVM)
                 break;
         }
     }
-
-#else
-    HKL             keyboardLayout = ::GetKeyboardLayout(0 /* current thread*/);
-    LCID            locale = MAKELCID(LOWORD(keyboardLayout), SORT_DEFAULT);
-    LOCALESIGNATURE localeSignature = {};
-
-    if (GetLocaleInfoW(locale, LOCALE_FONTSIGNATURE, reinterpret_cast<LPWSTR>(&localeSignature), sizeof(localeSignature) / sizeof(WCHAR)) != 0)
-    {
-        if ((localeSignature.lsUsb[3] & 0x08000000) != 0)
-        {
-            readingLayout = "rtl";
-        }
-    }
-#endif
 
     lua_createtable(luaVM, 0, 1);
     lua_pushstring(luaVM, "readingLayout");

commit 21564b8f354d30a1c245589d9fdf7f78facc608f

Use RtlGetVersion() instead of VerifyVersionInfo() to bypass the manifest compatibility shim (SharedUtil::IsWindows10OrGreater) (#4998)
Fixes W10 check issue on server introduced by e0d8ccd

Shared/sdk/SharedUtil.Misc.h
@@ -413,12 +413,7 @@ namespace SharedUtil
     void    SetClipboardText(const SString& strText);
     SString GetClipboardText();
 
-    // Version checks
-    bool IsWindowsVersionOrGreater(WORD wMajorVersion, WORD wMinorVersion, WORD wServicePackMajor);
-    bool IsWindowsXPSP3OrGreater();
-    bool IsWindowsVistaOrGreater();
-    bool IsWindows7OrGreater();
-    bool IsWindows8OrGreater();
+    // Version check
     bool IsWindows10OrGreater();
 
     bool QueryThreadEntryPointAddress(void* thread, DWORD* entryPointAddress);

Shared/sdk/SharedUtil.Misc.hpp
@@ -2023,51 +2023,23 @@ bool SharedUtil::ShellExecuteNonBlocking(const SString& strAction, const SString
 #ifdef SHAREDUTIL_PLATFORM_WINDOWS
     #define _WIN32_WINNT_WIN8  0x0602
     #define _WIN32_WINNT_WIN10 0x0A00
-///////////////////////////////////////////////////////////////////////////
-//
-// SharedUtil::IsWindowsVersionOrGreater
-//
-// From Windows Kits\8.1 VersionHelpers.h
-// (Ignores compatibility mode)
-//
-///////////////////////////////////////////////////////////////////////////
-bool SharedUtil::IsWindowsVersionOrGreater(WORD wMajorVersion, WORD wMinorVersion, WORD wServicePackMajor)
-{
-    OSVERSIONINFOEXW osvi = {sizeof(osvi), 0, 0, 0, 0, {0}, 0, 0};
-    DWORDLONG const  dwlConditionMask =
-        VerSetConditionMask(VerSetConditionMask(VerSetConditionMask(0, VER_MAJORVERSION, VER_GREATER_EQUAL), VER_MINORVERSION, VER_GREATER_EQUAL),
-                            VER_SERVICEPACKMAJOR, VER_GREATER_EQUAL);
-
-    osvi.dwMajorVersion = wMajorVersion;
-    osvi.dwMinorVersion = wMinorVersion;
-    osvi.wServicePackMajor = wServicePackMajor;
-
-    return VerifyVersionInfoW(&osvi, VER_MAJORVERSION | VER_MINORVERSION | VER_SERVICEPACKMAJOR, dwlConditionMask) != FALSE;
-}
-
-bool SharedUtil::IsWindowsXPSP3OrGreater()
-{
-    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WINXP), LOBYTE(_WIN32_WINNT_WINXP), 3);
-}
-
-bool SharedUtil::IsWindowsVistaOrGreater()
-{
-    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_VISTA), LOBYTE(_WIN32_WINNT_VISTA), 0);
-}
-
-bool SharedUtil::IsWindows7OrGreater()
-{
-    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WIN7), LOBYTE(_WIN32_WINNT_WIN7), 0);
-}
-
-bool SharedUtil::IsWindows8OrGreater()
-{
-    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WIN8), LOBYTE(_WIN32_WINNT_WIN8), 0);
-}
 
 bool SharedUtil::IsWindows10OrGreater()
 {
-    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WIN10), LOBYTE(_WIN32_WINNT_WIN10), 0);
+    using RtlGetVersionFn = LONG(WINAPI*)(OSVERSIONINFOEXW*);
+    if (HMODULE ntdll = GetModuleHandleW(L"ntdll.dll"))
+    {
+        RtlGetVersionFn pfnRtlGetVersion = reinterpret_cast<RtlGetVersionFn>(GetProcAddress(ntdll, "RtlGetVersion"));
+        if (pfnRtlGetVersion)
+        {
+            OSVERSIONINFOEXW info{};
+            info.dwOSVersionInfoSize = sizeof(info);
+            if (pfnRtlGetVersion(&info) == 0 /* STATUS_SUCCESS */)
+                return info.dwMajorVersion >= 10;
+        }
+    }
+
+    return false;
 }
 #endif  // SHAREDUTIL_PLATFORM_WINDOWS
 