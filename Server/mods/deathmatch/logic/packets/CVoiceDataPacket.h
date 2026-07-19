4626ad331965ac952c438bca232062c8b5b59d5c
MoriskaSosiska <dzwdzaf.dwadawdw@bk.ru>
2026-07-19 21:40:32 +0300

    Update CVoiceDataPacket.h

--- Server/mods/deathmatch/logic/packets/CVoiceDataPacket.h
+++ Server/mods/deathmatch/logic/packets/CVoiceDataPacket.h
@@ -14,19 +14,17 @@
 #include "CPacket.h"
 #include <vector>
 
+constexpr auto MAX_VOICE_BUFFER = 2048;
+
 class CVoiceDataPacket final : public CPacket
 {
 public:
-    CVoiceDataPacket();
-
     ePacketID               GetPacketID() const { return PACKET_ID_VOICE_DATA; }
     unsigned long           GetFlags() const { return PACKET_LOW_PRIORITY | PACKET_SEQUENCED; };
     virtual ePacketOrdering GetPacketOrdering() const { return PACKET_ORDERING_VOICE; }
 
     bool Read(NetBitStreamInterface& BitStream);
     bool Write(NetBitStreamInterface& BitStream) const;
-
-    void SetVoiceData(const unsigned char* voiceBuffer, unsigned short voiceBufferLength);
 
     bool IsEmpty() const noexcept { return m_voiceBuffer.empty(); }
 
