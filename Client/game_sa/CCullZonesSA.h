fa419c0797981e4c3807d4f8fd81299294181211
Dryxio <215281400+Dryxio@users.noreply.github.com>
2026-07-31 01:17:33 +0100

    Fix CULL zone file headers

--- Client/game_sa/CCullZonesSA.h
+++ Client/game_sa/CCullZonesSA.h
@@ -1,8 +1,11 @@
 /*****************************************************************************
  *
- *  PROJECT:     Multi Theft Auto v1.0
+ *  PROJECT:     Multi Theft Auto
  *  LICENSE:     See LICENSE in the top level directory
+ *  FILE:        game_sa/CCullZonesSA.h
  *  PURPOSE:     GTA SA CULL zone relocation and editable catalog
+ *
+ *  Multi Theft Auto is available from https://www.multitheftauto.com/
  *
  *****************************************************************************/
 
