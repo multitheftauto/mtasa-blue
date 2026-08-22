f70e68105cb8654cc83fb5ea0362c688ebe870e2
Federico Romero <fedehromero@outlook.com>
2026-08-22 20:57:32 -0300

    Cap GetFirstFreeModelID at MAX_MODEL_DFF_ID

--- Client/mods/deathmatch/logic/CClientModelManager.cpp
+++ Client/mods/deathmatch/logic/CClientModelManager.cpp
@@ -66,7 +66,7 @@ bool CClientModelManager::Remove(const std::shared_ptr<CClientModel>& pModel)
 
 int CClientModelManager::GetFirstFreeModelID(void)
 {
-    const unsigned int uiMaxModelID = g_pGame->GetBaseIDforCOL();
+    const unsigned int uiMaxModelID = MAX_MODEL_DFF_ID;
     for (unsigned int i = 0; i < uiMaxModelID; i++)
     {
         CModelInfo* pModelInfo = g_pGame->GetModelInfo(i, true);
