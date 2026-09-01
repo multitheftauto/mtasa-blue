bf6533fdbd81d1f562ae224c22e93b187d8fa0db
FileEX <kongali@interia.pl>
2026-09-01 03:48:35 +0200

    HTTPDownloadType enum

---
+++ Shared/sdk/enums/HTTPDownloadType.h
@@ -0,0 +1,22 @@
+/*****************************************************************************
+ *
+ *  PROJECT:     Multi Theft Auto
+ *  LICENSE:     See LICENSE in the top level directory
+ *  FILE:        sdk/HTTPDownloadType.h
+ *  PURPOSE:     Header for common definitions
+ *
+ *  Multi Theft Auto is available from https://www.multitheftauto.com/
+ *
+ *****************************************************************************/
+
+#pragma once
+
+namespace HTTPDownloadType
+{
+    enum Enum
+    {
+        HTTP_DOWNLOAD_DISABLED = 0,
+        HTTP_DOWNLOAD_ENABLED_PORT,
+        HTTP_DOWNLOAD_ENABLED_URL
+    };
+}
