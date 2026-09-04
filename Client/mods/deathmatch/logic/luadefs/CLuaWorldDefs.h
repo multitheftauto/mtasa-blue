132cddfbfc6145e1ab067b8e9c2398f6fe2b2bfb
Youssef Maged <57957016+iManGaaX@users.noreply.github.com>
2026-09-04 19:34:17 +0300

    Restore GetStreamedWorldModels declaration

--- Client/mods/deathmatch/logic/luadefs/CLuaWorldDefs.h
+++ Client/mods/deathmatch/logic/luadefs/CLuaWorldDefs.h
@@ -17,8 +17,8 @@ public:
 
     LUA_DECLARE(GetTime);
     LUA_DECLARE(GetGroundPosition);
-    LUA_DECLARE(GetStreamedWorldModels);
     LUA_DECLARE(GetRoofPosition);
+    LUA_DECLARE(GetStreamedWorldModels);
     static std::variant<bool, CLuaMultiReturn<bool, float, float, const char*, const char*, float, float, float>> ProcessLineAgainstMesh(CClientEntity* e,
                                                                                                                                          CVector        start,
                                                                                                                                          CVector        end);
