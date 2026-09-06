bda3f3f724f03d1b09f64d886e768a9beee6d9ca
Youssef Maged <57957016+iManGaaX@users.noreply.github.com>
2026-09-06 21:10:19 +0300

    Add CClientWorldSoundManager header file

---
+++ Client/mods/deathmatch/logic/CClientWorldSoundManager.h
@@ -0,0 +1,69 @@
+/*****************************************************************************
+ *
+ *  PROJECT:     Multi Theft Auto v1.0
+ *  LICENSE:     See LICENSE in the top level directory
+ *  FILE:        mods/deathmatch/logic/CClientWorldSoundManager.h
+ *  PURPOSE:     Client-side world sound replacement (engineReplaceWorldSound)
+ *
+ *  Multi Theft Auto is available from https://www.multitheftauto.com/
+ *
+ *****************************************************************************/
+
+#pragma once
+
+#include <list>
+#include <unordered_map>
+#include <game/CAudioEngine.h>
+
+class CClientManager;
+class CClientSound;
+
+class CClientWorldSoundManager
+{
+public:
+    CClientWorldSoundManager(CClientManager* pManager);
+    ~CClientWorldSoundManager();
+
+    bool ReplaceSound(uint uiGroup, uint uiIndex, const SString& strSound, bool bIsRawData, float fMinDistance = 0.0f, float fMaxDistance = 0.0f,
+                      SString* pOutError = nullptr);
+    bool RestoreSound(uint uiGroup, uint uiIndex);
+    void RestoreAll();
+
+    bool IsSoundReplaced(uint uiGroup, uint uiIndex) const;
+
+    bool HandleWorldSound(const SWorldSoundEvent& event, bool bAllowPlay);
+
+    void DoPulse();
+
+private:
+    struct SReplacement
+    {
+        bool    bRawData;
+        SString strSound;
+        float   fMinDistance;
+        float   fMaxDistance;
+    };
+
+    struct SLastPlayed
+    {
+        uint    uiTick;
+        CVector vecPosition;
+    };
+
+    struct SFollowSound
+    {
+        CClientSound*       pSound;
+        CEntitySAInterface* pGameEntity;
+        uint                uiStartTick;
+    };
+
+    static uint MakeKey(uint uiGroup, uint uiIndex) { return (uiGroup << 16) | (uiIndex & 0xFFFF); }
+
+    bool FindReplacement(uint uiGroup, uint uiIndex, const SReplacement** ppOutReplacement) const;
+
+    CClientManager*                        m_pManager;
+    std::unordered_map<uint, SReplacement> m_Replacements;
+    std::unordered_map<uint, SLastPlayed>  m_LastPlayed;
+    std::list<SFollowSound>                m_FollowSounds;
+    uint                                   m_uiLastPruneTick;
+};
