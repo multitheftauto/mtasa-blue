00f53f2cf53417dca46e46f15b0662c18b68fc08
Youssef Maged <57957016+iManGaaX@users.noreply.github.com>
2026-09-07 06:36:19 +0300

    Remove unused world sound Lua engine declarations

--- Client/mods/deathmatch/logic/luadefs/CLuaEngineDefs.h
+++ Client/mods/deathmatch/logic/luadefs/CLuaEngineDefs.h
@@ -28,10 +28,6 @@ public:
     LUA_DECLARE(EngineRestoreCOL);
     LUA_DECLARE(EngineReplaceModel);
     LUA_DECLARE(EngineRestoreModel);
-    LUA_DECLARE(EngineReplaceWorldSound);
-    LUA_DECLARE(EngineRestoreWorldSound);
-    LUA_DECLARE(EngineRestoreAllWorldSounds);
-    LUA_DECLARE(EngineIsWorldSoundReplaced);
     LUA_DECLARE(EngineRequestModel);
     LUA_DECLARE(EngineFreeModel);
     LUA_DECLARE(EngineReplaceAnimation);
